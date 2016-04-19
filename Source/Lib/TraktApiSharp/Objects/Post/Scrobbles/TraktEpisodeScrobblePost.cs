7aba4f3f2bf047259442155e6adc06f152f537d9
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-19 20:07:33 +0200

    Add scrobble post request objects.

---
+++ Source/Lib/TraktApiSharp/Objects/Post/Scrobbles/TraktEpisodeScrobblePost.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Objects.Post.Scrobbles
+{
+    using Get.Shows.Episodes;
+    using Newtonsoft.Json;
+
+    public class TraktEpisodeScrobblePost : TraktScrobblePost
+    {
+        [JsonProperty(PropertyName = "episode")]
+        public TraktEpisode Episode { get; set; }
+    }
+}
