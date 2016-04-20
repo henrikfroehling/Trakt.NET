1481e6608cfd0587fadf72876d1c2965aac06f48
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-20 22:00:37 +0200

    Add sync playback progress objects implementation.

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Syncs/TraktSyncPlaybackProgressItem.cs
@@ -0,0 +1,34 @@
+﻿namespace TraktApiSharp.Objects.Get.Syncs
+{
+    using Enums;
+    using Movies;
+    using Newtonsoft.Json;
+    using Shows;
+    using Shows.Episodes;
+    using System;
+
+    public class TraktSyncPlaybackProgressItem
+    {
+        [JsonProperty(PropertyName = "progress")]
+        public float Progress { get; set; }
+
+        [JsonProperty(PropertyName = "paused_at")]
+        public DateTime PausedAt { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        [JsonConverter(typeof(TraktSyncTypeConverter))]
+        public TraktSyncType Type { get; set; }
+
+        [JsonProperty(PropertyName = "movie")]
+        public TraktMovie Movie { get; set; }
+
+        [JsonProperty(PropertyName = "episode")]
+        public TraktEpisode Episode { get; set; }
+
+        [JsonProperty(PropertyName = "show")]
+        public TraktShow Show { get; set; }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Syncs/TraktSyncPlaybackProgressItemTests.cs
@@ -0,0 +1,93 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Syncs
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
+    using System;
+    using TraktApiSharp.Enums;
+    using TraktApiSharp.Objects.Get.Syncs;
+    using Utils;
+
+    [TestClass]
+    public class TraktSyncPlaybackProgressItemTests
+    {
+        [TestMethod]
+        public void TestTraktSyncPlaybackProgressItemDefaultConstructor()
+        {
+            var playbackProgress = new TraktSyncPlaybackProgressItem();
+
+            playbackProgress.Progress.Should().Be(0.0f);
+            playbackProgress.PausedAt.Should().Be(DateTime.MinValue);
+            playbackProgress.Id.Should().Be(0);
+            playbackProgress.Type.Should().Be(TraktSyncType.Unspecified);
+            playbackProgress.Movie.Should().BeNull();
+            playbackProgress.Episode.Should().BeNull();
+            playbackProgress.Show.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void TestTraktSyncPlaybackProgressItemMovieReadFromJson()
+        {
+            var jsonFile = TestUtility.ReadFileContents(@"Objects\Get\Syncs\SyncPlaybackProgressItemMovie.json");
+
+            jsonFile.Should().NotBeNullOrEmpty();
+
+            var playbackProgress = JsonConvert.DeserializeObject<TraktSyncPlaybackProgressItem>(jsonFile);
+
+            playbackProgress.Should().NotBeNull();
+
+            playbackProgress.Progress.Should().Be(10.0f);
+            playbackProgress.PausedAt.Should().Be(DateTime.Parse("2015-01-25T22:01:32.000Z").ToUniversalTime());
+            playbackProgress.Id.Should().Be(13);
+            playbackProgress.Type.Should().Be(TraktSyncType.Movie);
+            playbackProgress.Movie.Should().NotBeNull();
+            playbackProgress.Movie.Title.Should().Be("Batman Begins");
+            playbackProgress.Movie.Year.Should().Be(2005);
+            playbackProgress.Movie.Ids.Should().NotBeNull();
+            playbackProgress.Movie.Ids.Trakt.Should().Be(1);
+            playbackProgress.Movie.Ids.Slug.Should().Be("batman-begins-2005");
+            playbackProgress.Movie.Ids.Imdb.Should().Be("tt0372784");
+            playbackProgress.Movie.Ids.Tmdb.Should().Be(272);
+            playbackProgress.Episode.Should().BeNull();
+            playbackProgress.Show.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void TestTraktSyncPlaybackProgressItemEpisodeReadFromJson()
+        {
+            var jsonFile = TestUtility.ReadFileContents(@"Objects\Get\Syncs\SyncPlaybackProgressItemEpisode.json");
+
+            jsonFile.Should().NotBeNullOrEmpty();
+
+            var playbackProgress = JsonConvert.DeserializeObject<TraktSyncPlaybackProgressItem>(jsonFile);
+
+            playbackProgress.Should().NotBeNull();
+
+            playbackProgress.Progress.Should().Be(65.5f);
+            playbackProgress.PausedAt.Should().Be(DateTime.Parse("2015-01-25T22:01:32.000Z").ToUniversalTime());
+            playbackProgress.Id.Should().Be(37);
+            playbackProgress.Type.Should().Be(TraktSyncType.Episode);
+            playbackProgress.Movie.Should().BeNull();
+            playbackProgress.Episode.Should().NotBeNull();
+            playbackProgress.Episode.SeasonNumber.Should().Be(0);
+            playbackProgress.Episode.Number.Should().Be(1);
+            playbackProgress.Episode.Title.Should().Be("Good Cop Bad Cop");
+            playbackProgress.Episode.Ids.Should().NotBeNull();
+            playbackProgress.Episode.Ids.Trakt.Should().Be(1);
+            playbackProgress.Episode.Ids.Tvdb.Should().Be(3859781);
+            playbackProgress.Episode.Ids.Imdb.Should().Be("");
+            playbackProgress.Episode.Ids.Tmdb.Should().Be(62131);
+            playbackProgress.Episode.Ids.TvRage.Should().NotHaveValue();
+            playbackProgress.Show.Should().NotBeNull();
+            playbackProgress.Show.Title.Should().Be("Breaking Bad");
+            playbackProgress.Show.Year.Should().Be(2008);
+            playbackProgress.Show.Ids.Should().NotBeNull();
+            playbackProgress.Show.Ids.Trakt.Should().Be(1);
+            playbackProgress.Show.Ids.Slug.Should().Be("breaking-bad");
+            playbackProgress.Show.Ids.Tvdb.Should().Be(81189);
+            playbackProgress.Show.Ids.Imdb.Should().Be("tt0903747");
+            playbackProgress.Show.Ids.Tmdb.Should().Be(1396);
+            playbackProgress.Show.Ids.TvRage.Should().Be(18164);
+        }
+    }
+}
