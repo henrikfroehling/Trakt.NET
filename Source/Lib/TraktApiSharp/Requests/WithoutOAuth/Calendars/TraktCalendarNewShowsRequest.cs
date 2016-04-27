076c6588d49fadae3de55082159c120612895e25
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-27 18:30:26 +0200

    Add request implementation for calendar new shows.

---
+++ Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Calendars/TraktCalendarNewShowsRequest.cs
@@ -0,0 +1,31 @@
+﻿namespace TraktApiSharp.Requests.WithoutOAuth.Calendars
+{
+    using Base.Get;
+    using Objects.Basic;
+    using Objects.Get.Calendars;
+    using System;
+    using System.Collections.Generic;
+
+    internal class TraktCalendarNewShowsRequest : TraktGetRequest<TraktListResult<TraktCalendarShowItem>, TraktCalendarShowItem>
+    {
+        public TraktCalendarNewShowsRequest(TraktClient client) : base(client) { }
+
+        internal DateTime? StartDate { get; set; }
+
+        internal int? Days { get; set; }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetPathParameters()
+        {
+            return new Dictionary<string, string> { { "start_date", StartDate.HasValue ? StartDate.Value.ToString("yyyy-MM-dd") : string.Empty },
+                                                    { "days", Days.HasValue ? Days.Value.ToString() : string.Empty } };
+        }
+
+        protected override TraktAuthenticationRequirement AuthenticationRequirement => TraktAuthenticationRequirement.NotRequired;
+
+        protected override bool IsListResult => true;
+
+        protected override TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Shows;
+
+        protected override string UriTemplate => "calendars/all/shows/new/{start_date}/{days}";
+    }
+}
