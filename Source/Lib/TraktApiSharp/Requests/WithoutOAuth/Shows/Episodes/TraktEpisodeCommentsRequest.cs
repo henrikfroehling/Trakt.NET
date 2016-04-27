fe38dc750d29c57f1c8a652165ea01042433dbe1
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-27 16:44:49 +0200

    Add support for sorting comments for an episode.

--- Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Shows/Episodes/TraktEpisodeCommentsRequest.cs
+++ Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Shows/Episodes/TraktEpisodeCommentsRequest.cs
@@ -1,13 +1,22 @@
 ﻿namespace TraktApiSharp.Requests.WithoutOAuth.Shows.Episodes
 {
+    using Enums;
     using Objects.Basic;
     using Objects.Get.Shows.Episodes;
+    using System.Collections.Generic;
 
     internal class TraktEpisodeCommentsRequest : TraktGetByIdEpisodeRequest<TraktPaginationListResult<TraktEpisodeComment>, TraktEpisodeComment>
     {
         internal TraktEpisodeCommentsRequest(TraktClient client) : base(client) { }
 
-        protected override string UriTemplate => "shows/{id}/seasons/{season}/episodes/{episode}/comments";
+        internal TraktCommentSortOrder Sorting { get; set; }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetPathParameters()
+        {
+            return new Dictionary<string, string> { { "sorting", Sorting.AsString() } };
+        }
+
+        protected override string UriTemplate => "shows/{id}/seasons/{season}/episodes/{episode}/comments/{sorting}";
 
         protected override bool SupportsPagination => true;
 
