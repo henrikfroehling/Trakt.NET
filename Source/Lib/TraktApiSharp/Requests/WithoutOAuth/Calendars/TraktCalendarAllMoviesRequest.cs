6e8dbfe3b42f04b4828e31e889040acc38951b31
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-27 18:36:53 +0200

    Add request implementation for calendar movies.

---
+++ Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Calendars/TraktCalendarAllMoviesRequest.cs
@@ -0,0 +1,31 @@
+﻿namespace TraktApiSharp.Requests.WithoutOAuth.Calendars
+{
+    using Base.Get;
+    using Objects.Basic;
+    using Objects.Get.Calendars;
+    using System;
+    using System.Collections.Generic;
+
+    internal class TraktCalendarAllMoviesRequest : TraktGetRequest<TraktListResult<TraktCalendarMovieItem>, TraktCalendarMovieItem>
+    {
+        public TraktCalendarAllMoviesRequest(TraktClient client) : base(client) { }
+
+        internal DateTime? StartDate { get; set; }
+
+        internal int? Days { get; set; }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetPathParameters()
+        {
+            return new Dictionary<string, string> { { "start_date", StartDate.HasValue ? StartDate.Value.ToString("yyyy-MM-dd") : string.Empty },
+                                                    { "days", Days.HasValue ? Days.Value.ToString() : string.Empty } };
+        }
+
+        protected override TraktAuthenticationRequirement AuthenticationRequirement => TraktAuthenticationRequirement.NotRequired;
+
+        protected override bool IsListResult => true;
+
+        protected override TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Movies;
+
+        protected override string UriTemplate => "calendars/all/movies/{start_date}/{days}";
+    }
+}
