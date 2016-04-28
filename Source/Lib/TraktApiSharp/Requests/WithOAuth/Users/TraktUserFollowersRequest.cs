87248ec1bfacdc02eb844aea93f8f272f0ab4064
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-28 21:42:22 +0200

    Add request implementation for user followers.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Users/TraktUserFollowersRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Users
+{
+    using Base.Get;
+    using Objects.Basic;
+    using Objects.Get.Users;
+    using System.Collections.Generic;
+
+    internal class TraktUserFollowersRequest : TraktGetRequest<TraktListResult<TraktUserFollower>, TraktUserFollower>
+    {
+        internal TraktUserFollowersRequest(TraktClient client) : base(client) { }
+
+        protected override TraktAuthenticationRequirement AuthenticationRequirement => TraktAuthenticationRequirement.Optional;
+
+        internal string Username { get; set; }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetPathParameters()
+        {
+            return new Dictionary<string, string> { { "username", Username } };
+        }
+
+        protected override string UriTemplate => "users/{username}/followers";
+
+        protected override bool IsListResult => true;
+    }
+}
