d15dba94ffb1a8ef7df31efa715e77598056fb46
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-28 15:00:25 +0200

    Add request implementation for user show recommendations.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Recommendations/TraktUserShowRecommendationsRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Recommendations
+{
+    using Base.Get;
+    using Objects.Basic;
+    using Objects.Get.Recommendations;
+
+    internal class TraktUserShowRecommendationsRequest : TraktGetRequest<TraktListResult<TraktShowRecommendation>, TraktShowRecommendation>
+    {
+        internal TraktUserShowRecommendationsRequest(TraktClient client) : base(client) { }
+
+        protected override TraktAuthenticationRequirement AuthenticationRequirement => TraktAuthenticationRequirement.Required;
+
+        protected override string UriTemplate => "recommendations/shows";
+    }
+}
