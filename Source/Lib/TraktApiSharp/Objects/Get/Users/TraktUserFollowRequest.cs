b6a39937063e08e373f57e65b55b81659d802ade
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-28 19:22:27 +0200

    Add request implementation for user follow requests.

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/TraktUserFollowRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Objects.Get.Users
+{
+    using Newtonsoft.Json;
+    using System;
+
+    public class TraktUserFollowRequest
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "requested_at")]
+        public DateTime RequestedAt { get; set; }
+
+        [JsonProperty(PropertyName = "user")]
+        public TraktUser User { get; set; }
+    }
+}
