5cd181810572da6b6f11c2a2e6d7d6f683b764ac
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-28 22:12:05 +0200

    Add request implementation for user watched statistics.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Users/TraktUserStatisticsRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Users
+{
+    using Base.Get;
+    using Objects.Get.Users.Statistics;
+    using System.Collections.Generic;
+
+    internal class TraktUserStatisticsRequest : TraktGetRequest<TraktUserStatistics, TraktUserStatistics>
+    {
+        internal TraktUserStatisticsRequest(TraktClient client) : base(client) { }
+
+        protected override TraktAuthenticationRequirement AuthenticationRequirement => TraktAuthenticationRequirement.Optional;
+
+        internal string Username { get; set; }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetPathParameters()
+        {
+            return new Dictionary<string, string> { { "username", Username } };
+        }
+
+        protected override string UriTemplate => "users/{username}/stats";
+    }
+}
