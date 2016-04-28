1b8505b99fd428965d293a797d01e67c84b06589
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-28 22:22:40 +0200

    Add request implementation for user settings.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Users/TraktUserSettingsRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Users
+{
+    using Base.Get;
+    using Objects.Get.Users;
+
+    internal class TraktUserSettingsRequest : TraktGetRequest<TraktUserSettings, TraktUserSettings>
+    {
+        internal TraktUserSettingsRequest(TraktClient client) : base(client) { }
+
+        protected override TraktAuthenticationRequirement AuthenticationRequirement => TraktAuthenticationRequirement.Required;
+
+        protected override string UriTemplate => "users/settings";
+    }
+}
