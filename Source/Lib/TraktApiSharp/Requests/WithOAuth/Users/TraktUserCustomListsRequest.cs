7e1aeeef6f4806ef0973cb60b91ce60ef6ac447d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-28 21:00:23 +0200

    Add request implementation for user custom lists.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Users/TraktUserCustomListsRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Users
+{
+    using Base.Get;
+    using Objects.Basic;
+    using Objects.Get.Users.Lists;
+    using System.Collections.Generic;
+
+    internal class TraktUserCustomListsRequest : TraktGetRequest<TraktListResult<TraktList>, TraktList>
+    {
+        internal TraktUserCustomListsRequest(TraktClient client) : base(client) { }
+
+        protected override TraktAuthenticationRequirement AuthenticationRequirement => TraktAuthenticationRequirement.Optional;
+
+        internal string Username { get; set; }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetPathParameters()
+        {
+            return new Dictionary<string, string> { { "username", Username } };
+        }
+
+        protected override string UriTemplate => "users/{username}/lists";
+
+        protected override bool IsListResult => true;
+    }
+}
