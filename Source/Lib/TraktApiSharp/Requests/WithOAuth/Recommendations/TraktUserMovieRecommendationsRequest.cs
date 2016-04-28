f123f63f9da2e1e4946ffc280d29eddf29a8c56c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-28 14:58:47 +0200

    Add request implementation for user movie recommendations.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Recommendations/TraktUserMovieRecommendationsRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Recommendations
+{
+    using Base.Get;
+    using Objects.Basic;
+    using Objects.Get.Recommendations;
+
+    internal class TraktUserMovieRecommendationsRequest : TraktGetRequest<TraktListResult<TraktMovieRecommendation>, TraktMovieRecommendation>
+    {
+        internal TraktUserMovieRecommendationsRequest(TraktClient client) : base(client) { }
+
+        protected override TraktAuthenticationRequirement AuthenticationRequirement => TraktAuthenticationRequirement.Required;
+
+        protected override string UriTemplate => "recommendations/movies";
+    }
+}
