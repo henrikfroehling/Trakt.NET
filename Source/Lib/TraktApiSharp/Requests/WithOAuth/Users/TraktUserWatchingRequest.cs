6543cbc836c95bf7b18c35a3a549905ad99627c4
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-28 22:03:24 +0200

    Add request implementation for user watching.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Users/TraktUserWatchingRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Users
+{
+    using Base.Get;
+    using Objects.Basic;
+    using Objects.Get.Users;
+    using System.Collections.Generic;
+
+    internal class TraktUserWatchingRequest : TraktGetRequest<TraktListResult<TraktUserWatchingItem>, TraktUserWatchingItem>
+    {
+        internal TraktUserWatchingRequest(TraktClient client) : base(client) { }
+
+        protected override TraktAuthenticationRequirement AuthenticationRequirement => TraktAuthenticationRequirement.Optional;
+
+        internal string Username { get; set; }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetPathParameters()
+        {
+            return new Dictionary<string, string> { { "username", Username } };
+        }
+
+        protected override string UriTemplate => "users/{username}/watching";
+
+        protected override bool IsListResult => true;
+    }
+}
