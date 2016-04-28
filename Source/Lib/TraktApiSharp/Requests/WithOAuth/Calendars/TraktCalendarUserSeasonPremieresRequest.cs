d9d6f7995c027dcc4a1eb62b2c8ea1cf5486f78d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-28 14:40:57 +0200

    Add request implementation for calendar user season premieres.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Calendars/TraktCalendarUserSeasonPremieresRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Calendars
+{
+    using Objects.Get.Calendars;
+
+    internal class TraktCalendarUserSeasonPremieresRequest : TraktCalendarUserRequest<TraktCalendarShowItem>
+    {
+        internal TraktCalendarUserSeasonPremieresRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "calendars/my/shows/premieres/{start_date}/{days}";
+    }
+}
