0a18a68bd6070de7625f3832778ea5f5efb1967a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-04-28 19:04:32 +0200

    Add request implementation for user list comments.

---
+++ Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Users/TraktUserListCommentsRequest.cs
@@ -0,0 +1,32 @@
+﻿namespace TraktApiSharp.Requests.WithoutOAuth.Users
+{
+    using Base.Get;
+    using Enums;
+    using Objects.Basic;
+    using System.Collections.Generic;
+
+    internal class TraktUserListCommentsRequest : TraktGetByIdRequest<TraktPaginationListResult<TraktComment>, TraktComment>
+    {
+        internal TraktUserListCommentsRequest(TraktClient client) : base(client) { }
+
+        protected override TraktAuthenticationRequirement AuthenticationRequirement => TraktAuthenticationRequirement.NotRequired;
+
+        internal string Username { get; set; }
+
+        internal TraktCommentSortOrder? Sorting { get; set; }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetPathParameters()
+        {
+            return new Dictionary<string, string> { { "username", Username },
+                                                    { "sorting", Sorting.HasValue ? Sorting.Value.AsString() : string.Empty } };
+        }
+
+        protected override string UriTemplate => "users/{username}/lists/{id}/comments/{sorting}";
+
+        protected override bool SupportsPagination => true;
+
+        protected override TraktRequestObjectType? RequestObjectType => TraktRequestObjectType.Lists;
+
+        protected override bool IsListResult => true;
+    }
+}
