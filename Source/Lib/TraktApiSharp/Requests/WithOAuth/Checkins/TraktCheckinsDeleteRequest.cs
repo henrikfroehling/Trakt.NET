7728e0ff3c2733a9e385967efe8aac2b0ac22da6
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 01:29:13 +0200

    Add delete request implementation for checkins.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Checkins/TraktCheckinsDeleteRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Checkins
+{
+    using Base.Delete;
+
+    internal class TraktCheckinsDeleteRequest : TraktDeleteRequest
+    {
+        internal TraktCheckinsDeleteRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "checkin";
+    }
+}
