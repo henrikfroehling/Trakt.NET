06fcda3abd60ff3b01067f34c3728cbcd975fa1c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 06:01:33 +0200

    Add post request implementation for movie scrobble stop.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Scrobbles/TraktScrobbleMovieStopRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Scrobbles
+{
+    using Base.Post;
+    using Objects.Post.Scrobbles;
+    using Objects.Post.Scrobbles.Responses;
+
+    internal class TraktScrobbleMovieStopRequest : TraktPostRequest<TraktMovieScrobblePostResponse, TraktMovieScrobblePostResponse, TraktMovieScrobblePost>
+    {
+        internal TraktScrobbleMovieStopRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "scrobble/stop";
+
+        protected override void Validate()
+        {
+            base.Validate();
+            RequestBody.Validate();
+        }
+    }
+}
