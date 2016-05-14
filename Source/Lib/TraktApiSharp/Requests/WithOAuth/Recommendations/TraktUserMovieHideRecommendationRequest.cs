aa0c1ce53e9bf460cc8f1635499eab72d6a52fb5
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 02:11:31 +0200

    Add delete request implementation for movie recommendation.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Recommendations/TraktUserMovieHideRecommendationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Recommendations
+{
+    using Base.Delete;
+
+    internal class TraktUserMovieHideRecommendationRequest : TraktDeleteByIdRequest
+    {
+        internal TraktUserMovieHideRecommendationRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "recommendations/movies/{id}";
+    }
+}
