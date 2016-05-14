91fca2baf8bd6a984d75a196ee51c6114ce1785e
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 16:47:28 +0200

    Add post request implementation for sync watchlis items.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Syncs/TraktSyncWatchlistAddRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Syncs
+{
+    using Base.Post;
+    using Objects.Post.Syncs.Watchlist;
+    using Objects.Post.Syncs.Watchlist.Responses;
+
+    internal class TraktSyncWatchlistAddRequest : TraktPostRequest<TraktSyncWatchlistPostResponse, TraktSyncWatchlistPostResponse, TraktSyncWatchlistPost>
+    {
+        internal TraktSyncWatchlistAddRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "sync/watchlist";
+
+        protected override void Validate()
+        {
+            base.Validate();
+            RequestBody.Validate();
+        }
+    }
+}
