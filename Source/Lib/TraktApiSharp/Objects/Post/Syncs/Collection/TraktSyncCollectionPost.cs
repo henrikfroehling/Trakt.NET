4231dfb2069a29550fbb3fedb5e8850adf195ac1
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 16:38:29 +0200

    Add post request implementation for sync collection items.

--- Source/Lib/TraktApiSharp/Objects/Post/Syncs/Collection/TraktSyncCollectionPost.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Syncs/Collection/TraktSyncCollectionPost.cs
@@ -1,9 +1,11 @@
 ﻿namespace TraktApiSharp.Objects.Post.Syncs.Collection
 {
     using Newtonsoft.Json;
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
 
-    public class TraktSyncCollectionPost
+    public class TraktSyncCollectionPost : IValidatable
     {
         [JsonProperty(PropertyName = "movies")]
         public IEnumerable<TraktSyncCollectionPostMovieItem> Movies { get; set; }
@@ -13,5 +15,15 @@
 
         [JsonProperty(PropertyName = "episodes")]
         public IEnumerable<TraktSyncCollectionPostEpisodeItem> Episodes { get; set; }
+
+        public void Validate()
+        {
+            var bHasNoMovies = Movies == null || !Movies.Any();
+            var bHasNoShows = Shows == null || !Shows.Any();
+            var bHasNoEpisodes = Episodes == null || !Episodes.Any();
+
+            if (bHasNoMovies && bHasNoShows && bHasNoEpisodes)
+                throw new ArgumentException("no items to add");
+        }
     }
 }
