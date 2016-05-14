c7cf69f60553c8c9ba4394564a0d3876e2aec283
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 18:29:33 +0200

    Add delete request implementation for user lists.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Users/TraktUserListDeleteRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Users
+{
+    using Base.Delete;
+    using System.Collections.Generic;
+
+    internal class TraktUserListDeleteRequest : TraktDeleteByIdRequest
+    {
+        internal TraktUserListDeleteRequest(TraktClient client) : base(client) { }
+
+        internal string Username { get; set; }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetPathParameters()
+        {
+            return new Dictionary<string, string> { { "id", Id },
+                                                    { "username", Username } };
+        }
+
+        protected override string UriTemplate => "users/{username}/lists/{id}";
+    }
+}
