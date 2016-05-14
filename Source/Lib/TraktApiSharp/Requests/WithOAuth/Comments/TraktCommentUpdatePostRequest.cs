22fd18ff80bdba7c28ce4902d44790d6b8298f04
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 15:18:54 +0200

    Add post request implementation for comment update posts.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Comments/TraktCommentUpdatePostRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Comments
+{
+    using Base.Post;
+    using Objects.Post.Comments;
+    using Objects.Post.Comments.Responses;
+
+    internal class TraktCommentUpdatePostRequest : TraktPostByIdRequest<TraktCommentPostResponse, TraktCommentPostResponse, TraktCommentUpdatePost>
+    {
+        internal TraktCommentUpdatePostRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "comments/{id}";
+
+        protected override void Validate()
+        {
+            base.Validate();
+            RequestBody.Validate();
+        }
+    }
+}
