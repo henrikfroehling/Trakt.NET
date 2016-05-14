b4bd108149b3b0f61dd63da51de942f12b572ecc
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 15:12:24 +0200

    Add post request implementation for comment posts.

--- Source/Lib/TraktApiSharp/Objects/Post/Comments/TraktEpisodeCommentPost.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Comments/TraktEpisodeCommentPost.cs
@@ -2,10 +2,20 @@
 {
     using Get.Shows.Episodes;
     using Newtonsoft.Json;
+    using System;
 
-    public class TraktEpisodeCommentPost : TraktCommentPost
+    public class TraktEpisodeCommentPost : TraktCommentPost, IValidatable
     {
         [JsonProperty(PropertyName = "episode")]
         public TraktEpisode Episode { get; set; }
+
+        public void Validate()
+        {
+            if (Episode == null)
+                throw new ArgumentException("episode not set");
+
+            if (!Episode.Ids.HasAnyId)
+                throw new ArgumentException("episode ids not set");
+        }
     }
 }
---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Comments/TraktCommentPostRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Comments
+{
+    using Base.Post;
+    using Objects.Post;
+
+    internal class TraktCommentPostRequest<TResponse, TRequest> : TraktPostRequest<TResponse, TResponse, TRequest> where TRequest : IValidatable
+    {
+        internal TraktCommentPostRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "comments";
+
+        protected override void Validate()
+        {
+            base.Validate();
+            RequestBody.Validate();
+        }
+    }
+}
