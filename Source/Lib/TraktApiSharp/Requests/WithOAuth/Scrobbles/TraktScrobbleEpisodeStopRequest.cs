510dddce9934de25d7157e28906ded8cbe69bd02
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 06:04:30 +0200

    Add post request implementation for episode scrobble stop.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Scrobbles/TraktScrobbleEpisodeStopRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Scrobbles
+{
+    using Base.Post;
+    using Objects.Post.Scrobbles;
+    using Objects.Post.Scrobbles.Responses;
+
+    internal class TraktScrobbleEpisodeStopRequest : TraktPostRequest<TraktEpisodeScrobblePostResponse, TraktEpisodeScrobblePostResponse, TraktEpisodeScrobblePost>
+    {
+        internal TraktScrobbleEpisodeStopRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "scrobble/stop";
+
+        protected override void Validate()
+        {
+            base.Validate();
+            RequestBody.Validate();
+        }
+    }
+}
