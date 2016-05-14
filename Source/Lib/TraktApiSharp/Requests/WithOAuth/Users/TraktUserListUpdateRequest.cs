a6bf8bbec1c5077a8ddb0e45985784f8c62c875c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 18:26:20 +0200

    Add post request implementation for user list updates.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Users/TraktUserListUpdateRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Users
+{
+    using Base.Put;
+    using Objects.Post.Users;
+    using Objects.Post.Users.Responses;
+    using System.Collections.Generic;
+
+    internal class TraktUserListUpdateRequest : TraktPutByIdRequest<TraktUserListUpdatePostResponse, TraktUserListUpdatePostResponse, TraktUserListUpdatePost>
+    {
+        internal TraktUserListUpdateRequest(TraktClient client) : base(client) { }
+
+        internal string Username { get; set; }
+
+        protected override IEnumerable<KeyValuePair<string, string>> GetPathParameters()
+        {
+            return new Dictionary<string, string> { { "id", Id },
+                                                    { "username", Username } };
+        }
+
+        protected override string UriTemplate => "users/{username}/lists/{id}";
+
+        protected override void Validate()
+        {
+            base.Validate();
+            RequestBody.Validate();
+        }
+    }
+}
