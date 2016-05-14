c3437ff9a293d4ae89ecca0022e6570712b157ad
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 02:15:23 +0200

    Add delete request implementation for show recommendation.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Recommendations/TraktUserShowHideRecommendationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Recommendations
+{
+    using Base.Delete;
+
+    internal class TraktUserShowHideRecommendationRequest : TraktDeleteByIdRequest
+    {
+        internal TraktUserShowHideRecommendationRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "recommendations/shows/{id}";
+    }
+}
