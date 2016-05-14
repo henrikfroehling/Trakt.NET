6234584563e1c4c709d85d8d13f48e9e6d8aa5dd
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-14 18:11:59 +0200

    Add post request implementation for user follower approval.

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Users/TraktUserApproveFollowerRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Users
+{
+    using Base.Post;
+    using Objects.Get.Users;
+
+    internal class TraktUserApproveFollowerRequest : TraktBodylessPostByIdRequest<TraktUserFollower, TraktUserFollower>
+    {
+        internal TraktUserApproveFollowerRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "users/requests/{id}";
+    }
+}
