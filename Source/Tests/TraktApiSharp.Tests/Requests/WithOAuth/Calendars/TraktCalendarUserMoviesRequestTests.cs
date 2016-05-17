5eb1684bff44467a130348771b0c10e7c511d35f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-17 16:37:41 +0200

    Add unit tests for calendar requests with oauth requirement.

---
+++ Source/Tests/TraktApiSharp.Tests/Requests/WithOAuth/Calendars/TraktCalendarUserMoviesRequestTests.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Tests.Requests.WithOAuth.Calendars
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Objects.Get.Calendars;
+    using TraktApiSharp.Requests.WithOAuth.Calendars;
+
+    [TestClass]
+    public class TraktCalendarUserMoviesRequestTests
+    {
+        [TestMethod]
+        public void TestTraktCalendarUserMoviesRequestIsTraktCalendarAllRequest()
+        {
+            typeof(TraktCalendarUserRequest<TraktCalendarMovie>).IsAssignableFrom(typeof(TraktCalendarUserMoviesRequest)).Should().BeTrue();
+        }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Requests/WithOAuth/Calendars/TraktCalendarUserRequestTests.cs
@@ -0,0 +1,24 @@
+﻿namespace TraktApiSharp.Tests.Requests.WithOAuth.Calendars
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Objects.Basic;
+    using TraktApiSharp.Requests.Base.Get;
+    using TraktApiSharp.Requests.WithOAuth.Calendars;
+
+    [TestClass]
+    public class TraktCalendarUserRequestTests
+    {
+        [TestMethod]
+        public void TestTraktCalendarUserRequestIsAbstract()
+        {
+            typeof(TraktCalendarUserRequest<object>).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestTraktCalendarUserRequestIsTraktGetRequest()
+        {
+            typeof(TraktGetRequest<TraktListResult<object>, object>).IsAssignableFrom(typeof(TraktCalendarUserRequest<object>)).Should().BeTrue();
+        }
+    }
+}
