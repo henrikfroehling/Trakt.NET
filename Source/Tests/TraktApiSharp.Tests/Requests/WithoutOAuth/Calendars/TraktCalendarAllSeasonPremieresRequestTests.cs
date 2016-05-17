a40c1e8fbf44036b6b6115ce5d0354a09534d411
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-17 16:29:54 +0200

    Add unit tests for calendar requests without oauth requirement.

---
+++ Source/Tests/TraktApiSharp.Tests/Requests/WithoutOAuth/Calendars/TraktCalendarAllSeasonPremieresRequestTests.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Tests.Requests.WithoutOAuth.Calendars
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Objects.Get.Calendars;
+    using TraktApiSharp.Requests.WithoutOAuth.Calendars;
+
+    [TestClass]
+    public class TraktCalendarAllSeasonPremieresRequestTests
+    {
+        [TestMethod]
+        public void TestTraktCalendarAllSeasonPremieresRequestIsTraktCalendarAllRequest()
+        {
+            typeof(TraktCalendarAllRequest<TraktCalendarShow>).IsAssignableFrom(typeof(TraktCalendarAllSeasonPremieresRequest)).Should().BeTrue();
+        }
+    }
+}
