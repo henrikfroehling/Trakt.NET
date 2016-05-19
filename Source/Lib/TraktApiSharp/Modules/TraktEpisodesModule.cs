d3cbf4f87ea40bf7dc6eed352d98ac8e42da6017
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-19 19:44:03 +0200

    Add unit tests for episodes module episode requests.

--- Source/Lib/TraktApiSharp/Modules/TraktEpisodesModule.cs
+++ Source/Lib/TraktApiSharp/Modules/TraktEpisodesModule.cs
@@ -5,6 +5,7 @@
     using Objects.Get.Shows.Episodes;
     using Requests;
     using Requests.WithoutOAuth.Shows.Episodes;
+    using System;
     using System.Threading.Tasks;
 
     public class TraktEpisodesModule : TraktBaseModule
@@ -14,6 +15,8 @@
         public async Task<TraktEpisode> GetEpisodeAsync(string showId, int season, int episode,
                                                         TraktExtendedOption extended = null)
         {
+            Validate(showId, season, episode);
+
             return await QueryAsync(new TraktEpisodeSummaryRequest(Client)
             {
                 Id = showId,
@@ -27,6 +30,8 @@
                                                                                                   TraktCommentSortOrder? sorting = null,
                                                                                                   int? page = null, int? limit = null)
         {
+            Validate(showId, season, episode);
+
             return await QueryAsync(new TraktEpisodeCommentsRequest(Client)
             {
                 Id = showId,
@@ -39,6 +44,8 @@
 
         public async Task<TraktEpisodeRating> GetEpisodeRatingsAsync(string showId, int season, int episode)
         {
+            Validate(showId, season, episode);
+
             return await QueryAsync(new TraktEpisodeRatingsRequest(Client)
             {
                 Id = showId,
@@ -49,6 +56,8 @@
 
         public async Task<TraktEpisodeStatistics> GetEpisodeStatisticsAsync(string showId, int season, int episode)
         {
+            Validate(showId, season, episode);
+
             return await QueryAsync(new TraktEpisodeStatisticsRequest(Client)
             {
                 Id = showId,
@@ -59,12 +68,26 @@
 
         public async Task<TraktListResult<TraktEpisodeWatchingUser>> GetEpisodeWatchingUsersAsync(string showId, int season, int episode)
         {
+            Validate(showId, season, episode);
+
             return await QueryAsync(new TraktEpisodeWatchingUsersRequest(Client)
             {
                 Id = showId,
                 Season = season,
                 Episode = episode
             });
+        }
+
+        private void Validate(string showId, int season, int episode)
+        {
+            if (string.IsNullOrEmpty(showId))
+                throw new ArgumentException("show id not valid", "showId");
+
+            if (season < 0)
+                throw new ArgumentException("season nr not valid", "season");
+
+            if (episode < 0)
+                throw new ArgumentException("episode nr not valid", "episode");
         }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Modules/TraktEpisodesModuleTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Modules/TraktEpisodesModuleTests.cs
@@ -2,7 +2,13 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+    using TraktApiSharp.Exceptions;
     using TraktApiSharp.Modules;
+    using TraktApiSharp.Objects.Get.Shows.Episodes;
+    using TraktApiSharp.Requests;
     using Utils;
 
     [TestClass]
@@ -40,25 +46,180 @@
         [TestMethod]
         public void TestTraktEpisodesModuleGetEpisode()
         {
-            Assert.Fail();
+            var episode = TestUtility.ReadFileContents(@"Objects\Get\Shows\Episodes\EpisodeSummaryFullAndImages.json");
+            episode.Should().NotBeNullOrEmpty();
+
+            var showId = "1390";
+            var seasonNr = 1;
+            var episodeNr = 1;
+
+            TestUtility.SetupMockResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", episode);
+
+            var response = TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr).Result;
+
+            response.Should().NotBeNull();
+            response.Title.Should().Be("Winter Is Coming");
+            response.SeasonNumber.Should().Be(1);
+            response.Number.Should().Be(1);
+            response.NumberAbsolute.Should().Be(50);
+            response.Overview.Should().Be("Ned Stark, Lord of Winterfell learns that his mentor, Jon Arryn, has died and that King Robert is on his way north to offer Ned Arryn’s position as the King’s Hand. Across the Narrow Sea in Pentos, Viserys Targaryen plans to wed his sister Daenerys to the nomadic Dothraki warrior leader, Khal Drogo to forge an alliance to take the throne.");
+            response.FirstAired.Should().Be(DateTime.Parse("2011-04-18T01:00:00.000Z").ToUniversalTime());
+            response.UpdatedAt.Should().Be(DateTime.Parse("2014-08-29T23:16:39.000Z").ToUniversalTime());
+            response.Rating.Should().Be(9.0f);
+            response.Votes.Should().Be(111);
+            response.Ids.Should().NotBeNull();
+            response.Ids.Trakt.Should().Be(73640);
+            response.Ids.Tvdb.Should().Be(3254641);
+            response.Ids.Imdb.Should().Be("tt1480055");
+            response.Ids.Tmdb.Should().Be(63056);
+            response.Ids.TvRage.Should().Be(1065008299);
+            response.Images.Should().NotBeNull();
+            response.Images.Screenshot.Should().NotBeNull();
+            response.Images.Screenshot.Full.Should().Be("https://walter.trakt.us/images/episodes/000/073/640/screenshots/original/dd3fc55725.jpg");
+            response.Images.Screenshot.Medium.Should().Be("https://walter.trakt.us/images/episodes/000/073/640/screenshots/medium/dd3fc55725.jpg");
+            response.Images.Screenshot.Thumb.Should().Be("https://walter.trakt.us/images/episodes/000/073/640/screenshots/thumb/dd3fc55725.jpg");
+            response.AvailableTranslationLanguageCodes.Should().NotBeNull().And.HaveCount(1);
         }
 
         [TestMethod]
         public void TestTraktEpisodesModuleGetEpisodeWithExtendedOption()
         {
-            Assert.Fail();
+            var episode = TestUtility.ReadFileContents(@"Objects\Get\Shows\Episodes\EpisodeSummaryFullAndImages.json");
+            episode.Should().NotBeNullOrEmpty();
+
+            var showId = "1390";
+            var seasonNr = 1;
+            var episodeNr = 1;
+
+            var extendedOption = new TraktExtendedOption
+            {
+                Full = true,
+                Images = true
+            };
+
+            TestUtility.SetupMockResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}?extended=images,full", episode);
+
+            var response = TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr, extendedOption).Result;
+
+            response.Should().NotBeNull();
+            response.Title.Should().Be("Winter Is Coming");
+            response.SeasonNumber.Should().Be(1);
+            response.Number.Should().Be(1);
+            response.NumberAbsolute.Should().Be(50);
+            response.Overview.Should().Be("Ned Stark, Lord of Winterfell learns that his mentor, Jon Arryn, has died and that King Robert is on his way north to offer Ned Arryn’s position as the King’s Hand. Across the Narrow Sea in Pentos, Viserys Targaryen plans to wed his sister Daenerys to the nomadic Dothraki warrior leader, Khal Drogo to forge an alliance to take the throne.");
+            response.FirstAired.Should().Be(DateTime.Parse("2011-04-18T01:00:00.000Z").ToUniversalTime());
+            response.UpdatedAt.Should().Be(DateTime.Parse("2014-08-29T23:16:39.000Z").ToUniversalTime());
+            response.Rating.Should().Be(9.0f);
+            response.Votes.Should().Be(111);
+            response.Ids.Should().NotBeNull();
+            response.Ids.Trakt.Should().Be(73640);
+            response.Ids.Tvdb.Should().Be(3254641);
+            response.Ids.Imdb.Should().Be("tt1480055");
+            response.Ids.Tmdb.Should().Be(63056);
+            response.Ids.TvRage.Should().Be(1065008299);
+            response.Images.Should().NotBeNull();
+            response.Images.Screenshot.Should().NotBeNull();
+            response.Images.Screenshot.Full.Should().Be("https://walter.trakt.us/images/episodes/000/073/640/screenshots/original/dd3fc55725.jpg");
+            response.Images.Screenshot.Medium.Should().Be("https://walter.trakt.us/images/episodes/000/073/640/screenshots/medium/dd3fc55725.jpg");
+            response.Images.Screenshot.Thumb.Should().Be("https://walter.trakt.us/images/episodes/000/073/640/screenshots/thumb/dd3fc55725.jpg");
+            response.AvailableTranslationLanguageCodes.Should().NotBeNull().And.HaveCount(1);
         }
 
         [TestMethod]
         public void TestTraktEpisodesModuleGetEpisodeExceptions()
         {
-            Assert.Fail();
+            var showId = "1390";
+            var seasonNr = 1;
+            var episodeNr = 1;
+
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", HttpStatusCode.NotFound);
+
+            Func<Task<TraktEpisode>> act =
+                async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktEpisodeNotFoundException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", HttpStatusCode.BadRequest);
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktBadRequestException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", HttpStatusCode.Forbidden);
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktForbiddenException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", (HttpStatusCode)412);
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktPreconditionFailedException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", (HttpStatusCode)429);
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktRateLimitException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", HttpStatusCode.InternalServerError);
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktServerException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", (HttpStatusCode)503);
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", (HttpStatusCode)504);
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", (HttpStatusCode)520);
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", (HttpStatusCode)521);
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", (HttpStatusCode)522);
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, episodeNr);
+            act.ShouldThrow<TraktServerUnavailableException>();
         }
 
         [TestMethod]
         public void TestTraktEpisodesModuleGetEpisodeArgumentExceptions()
         {
-            Assert.Fail();
+            var episode = TestUtility.ReadFileContents(@"Objects\Get\Shows\Episodes\EpisodeSummaryFullAndImages.json");
+            episode.Should().NotBeNullOrEmpty();
+
+            var showId = "1390";
+            var seasonNr = 1;
+            var episodeNr = 1;
+
+            TestUtility.SetupMockResponseWithOAuth($"shows/{showId}/seasons/{seasonNr}/episodes/{episodeNr}", episode);
+
+            Func<Task<TraktEpisode>> act =
+                async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(null, seasonNr, episodeNr);
+            act.ShouldThrow<ArgumentException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, -1, episodeNr);
+            act.ShouldThrow<ArgumentException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Episodes.GetEpisodeAsync(showId, seasonNr, -1);
+            act.ShouldThrow<ArgumentException>();
         }
 
         #endregion
