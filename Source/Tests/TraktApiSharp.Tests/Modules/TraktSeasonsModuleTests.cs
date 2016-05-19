4653d15fcda09d25daa2328225c38de6ee25ce0a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-19 23:00:44 +0200

    Setup unit tests for seasons module.

---
+++ Source/Tests/TraktApiSharp.Tests/Modules/TraktSeasonsModuleTests.cs
@@ -0,0 +1,245 @@
+﻿namespace TraktApiSharp.Tests.Modules
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Modules;
+    using Utils;
+
+    [TestClass]
+    public class TraktSeasonsModuleTests
+    {
+        [TestMethod]
+        public void TestTraktSeasonsModuleIsModule()
+        {
+            typeof(TraktBaseModule).IsAssignableFrom(typeof(TraktSeasonsModule)).Should().BeTrue();
+        }
+
+        [ClassInitialize]
+        public static void InitializeTests(TestContext context)
+        {
+            TestUtility.SetupMockHttpClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanupTests()
+        {
+            TestUtility.ResetMockHttpClient();
+        }
+
+        [TestCleanup]
+        public void CleanupSingleTest()
+        {
+            TestUtility.ClearMockHttpClient();
+        }
+
+        // -----------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------
+
+        #region SeasonsAll
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetAllSeasons()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetAllSeasonsWithExtendedOption()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetAllSeasonsExceptions()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetAllSeasonsArgumentExceptions()
+        {
+            Assert.Fail();
+        }
+
+        #endregion
+
+        // -----------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------
+
+        #region Season
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeason()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonWithExtendedOption()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonExceptions()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonArgumentExceptions()
+        {
+            Assert.Fail();
+        }
+
+        #endregion
+
+        // -----------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------
+
+        #region SeasonComments
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonCommments()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonCommmentsWithSortOrder()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonCommmentsWithPage()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonCommmentsWithSortOrderAndPage()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonCommmentsWithLimit()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonCommmentsWithSortOrderAndLimit()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonCommmentsWithPageAndLimit()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonCommmentsComplete()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonCommmentsExceptions()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonCommmentsArgumentExceptions()
+        {
+            Assert.Fail();
+        }
+
+        #endregion
+
+        // -----------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------
+
+        #region SeasonRatings
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonRatings()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonRatingsExceptions()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonRatingsArgumentExceptions()
+        {
+            Assert.Fail();
+        }
+
+        #endregion
+
+        // -----------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------
+
+        #region SeasonStatistics
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonStatistics()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonStatisticsExceptions()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonStatisticsArgumentExceptions()
+        {
+            Assert.Fail();
+        }
+
+        #endregion
+
+        // -----------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------
+
+        #region SeasonWatchingUsers
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonWatchingUsers()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonWatchingUsersWithExtendedOption()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonWatchingUsersExceptions()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestTraktSeasonsModuleGetSeasonWatchingUsersArgumentExceptions()
+        {
+            Assert.Fail();
+        }
+
+        #endregion
+    }
+}
