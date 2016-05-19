274ad3a4d930fc2807761207dd723023412f657c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-19 18:39:42 +0200

    Add argument validation for checkins module methods.

--- Source/Lib/TraktApiSharp/Modules/TraktCheckinsModule.cs
+++ Source/Lib/TraktApiSharp/Modules/TraktCheckinsModule.cs
@@ -18,6 +18,8 @@
                                                                            string message = null, TraktSharing sharing = null,
                                                                            string foursquareVenueID = null, string foursquareVenueName = null)
         {
+            Validate(movie, appVersion);
+
             return await QueryAsync(new TraktCheckinRequest<TraktMovieCheckinPostResponse, TraktMovieCheckinPost>(Client)
             {
                 RequestBody = new TraktMovieCheckinPost
@@ -42,6 +44,8 @@
                                                                                string message = null, TraktSharing sharing = null,
                                                                                string foursquareVenueID = null, string foursquareVenueName = null)
         {
+            Validate(episode, appVersion);
+
             return await QueryAsync(new TraktCheckinRequest<TraktEpisodeCheckinPostResponse, TraktEpisodeCheckinPost>(Client)
             {
                 RequestBody = new TraktEpisodeCheckinPost
@@ -68,6 +72,8 @@
                                                                                string message = null, TraktSharing sharing = null,
                                                                                string foursquareVenueID = null, string foursquareVenueName = null)
         {
+            Validate(episode, show, appVersion);
+
             return await QueryAsync(new TraktCheckinRequest<TraktEpisodeCheckinPostResponse, TraktEpisodeCheckinPost>(Client)
             {
                 RequestBody = new TraktEpisodeCheckinPost
@@ -92,6 +98,59 @@
         public async Task DeleteActiveCheckinsAsync()
         {
             await QueryAsync(new TraktCheckinsDeleteRequest(Client));
+        }
+
+        private void Validate(TraktMovie movie, string appVersion)
+        {
+            if (movie == null)
+                throw new ArgumentNullException("movie", "movie instance must not be null");
+
+            if (string.IsNullOrEmpty(movie.Title))
+                throw new ArgumentException("movie title not valid", "movie");
+
+            if (movie.Year <= 0)
+                throw new ArgumentException("movie year not valid", "movie");
+
+            if (movie.Ids == null)
+                throw new ArgumentNullException("movie.Ids", "movie.Ids must not be null");
+
+            if (!movie.Ids.HasAnyId)
+                throw new ArgumentException("movie.Ids have no valid id", "movie");
+
+            if (string.IsNullOrEmpty(appVersion))
+                throw new ArgumentException("app version not valid", "appVersion");
+        }
+
+        private void Validate(TraktEpisode episode, string appVersion)
+        {
+            if (episode == null)
+                throw new ArgumentNullException("episode", "episode instance must not be null");
+
+            if (episode.Ids == null)
+                throw new ArgumentNullException("episode.Ids", "episode.Ids must not be null");
+
+            if (!episode.Ids.HasAnyId)
+                throw new ArgumentException("episode.Ids have no valid id", "episode");
+
+            if (episode.SeasonNumber < 0)
+                throw new ArgumentException("episode season number not valid", "episode");
+
+            if (episode.Number < 0)
+                throw new ArgumentException("episode number not valid", "episode");
+
+            if (string.IsNullOrEmpty(appVersion))
+                throw new ArgumentException("app version not valid", "appVersion");
+        }
+
+        private void Validate(TraktEpisode episode, TraktShow show, string appVersion)
+        {
+            Validate(episode, appVersion);
+
+            if (show == null)
+                throw new ArgumentNullException("show", "show instance must not be null");
+
+            if (string.IsNullOrEmpty(show.Title))
+                throw new ArgumentException("show title not valid", "show");
         }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Modules/TraktCheckinsModuleTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Modules/TraktCheckinsModuleTests.cs
@@ -1510,5 +1510,214 @@
         }
 
         #endregion
+
+        [TestMethod]
+        public void TestTraktCheckinsModuleCheckinMovieArgumentExceptions()
+        {
+            var checkinMovieResponse = TestUtility.ReadFileContents(@"Objects\Post\Checkins\Responses\MovieCheckinPostResponse.json");
+            checkinMovieResponse.Should().NotBeNullOrEmpty();
+
+            var movie = new TraktMovie
+            {
+                Title = "Guardians of the Galaxy",
+                Year = 2014,
+                Ids = new TraktMovieIds
+                {
+                    Trakt = 28,
+                    Slug = "guardians-of-the-galaxy-2014",
+                    Imdb = "tt2015381",
+                    Tmdb = 118340
+                }
+            };
+
+            var appVersion = "app_version";
+            var appBuildDate = DateTime.UtcNow;
+
+            var movieCheckinPost = new TraktMovieCheckinPost
+            {
+                Movie = movie,
+                AppVersion = appVersion,
+                AppDate = appBuildDate.ToString("yyyy-MM-dd")
+            };
+
+            var postJson = TestUtility.SerializeObject(movieCheckinPost);
+            postJson.Should().NotBeNullOrEmpty();
+
+            TestUtility.SetupMockResponseWithOAuth("checkin", postJson, checkinMovieResponse);
+
+            Func<Task<TraktMovieCheckinPostResponse>> act =
+                async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinMovieAsync(null, appVersion, appBuildDate);
+
+            act.ShouldThrow<ArgumentNullException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinMovieAsync(movie, null, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinMovieAsync(movie, string.Empty, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            movie.Year = 0;
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinMovieAsync(movie, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            movie.Year = 2014;
+            movie.Ids = null;
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinMovieAsync(movie, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentNullException>();
+
+            movie.Ids = new TraktMovieIds();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinMovieAsync(movie, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void TestTraktCheckinsModuleCheckinEpisodeArgumentExceptions()
+        {
+            var checkinEpisodeResponse = TestUtility.ReadFileContents(@"Objects\Post\Checkins\Responses\EpisodeCheckinPostResponse.json");
+            checkinEpisodeResponse.Should().NotBeNullOrEmpty();
+
+            var episode = new TraktEpisode
+            {
+                Number = 1,
+                SeasonNumber = 1,
+                Ids = new TraktEpisodeIds
+                {
+                    Trakt = 16,
+                    Tvdb = 349232,
+                    Imdb = "tt0959621",
+                    Tmdb = 62085,
+                    TvRage = 637041
+                }
+            };
+
+            var appVersion = "app_version";
+            var appBuildDate = DateTime.UtcNow;
+
+            var episodeCheckinPost = new TraktEpisodeCheckinPost
+            {
+                Episode = episode,
+                AppVersion = appVersion,
+                AppDate = appBuildDate.ToString("yyyy-MM-dd")
+            };
+
+            var postJson = TestUtility.SerializeObject(episodeCheckinPost);
+            postJson.Should().NotBeNullOrEmpty();
+
+            TestUtility.SetupMockResponseWithOAuth("checkin", postJson, checkinEpisodeResponse);
+
+            Func<Task<TraktEpisodeCheckinPostResponse>> act =
+                async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(null, appVersion, appBuildDate);
+
+            act.ShouldThrow<ArgumentNullException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, null, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, string.Empty, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            episode.Number = -1;
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            episode.Number = 1;
+            episode.SeasonNumber = -1;
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            episode.SeasonNumber = 1;
+            episode.Ids = new TraktEpisodeIds();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void TestTraktCheckinsModuleCheckinEpisodeShowArgumentExceptions()
+        {
+            var checkinEpisodeResponse = TestUtility.ReadFileContents(@"Objects\Post\Checkins\Responses\EpisodeCheckinPostResponse.json");
+            checkinEpisodeResponse.Should().NotBeNullOrEmpty();
+
+            var episode = new TraktEpisode
+            {
+                Number = 1,
+                SeasonNumber = 1,
+                Ids = new TraktEpisodeIds
+                {
+                    Trakt = 16,
+                    Tvdb = 349232,
+                    Imdb = "tt0959621",
+                    Tmdb = 62085,
+                    TvRage = 637041
+                }
+            };
+
+            var show = new TraktShow { Title = "Breaking Bad" };
+
+            var appVersion = "app_version";
+            var appBuildDate = DateTime.UtcNow;
+
+            var episodeCheckinPost = new TraktEpisodeCheckinPost
+            {
+                Episode = episode,
+                AppVersion = appVersion,
+                AppDate = appBuildDate.ToString("yyyy-MM-dd")
+            };
+
+            var postJson = TestUtility.SerializeObject(episodeCheckinPost);
+            postJson.Should().NotBeNullOrEmpty();
+
+            TestUtility.SetupMockResponseWithOAuth("checkin", postJson, checkinEpisodeResponse);
+
+            Func<Task<TraktEpisodeCheckinPostResponse>> act =
+                async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(null, show, appVersion, appBuildDate);
+
+            act.ShouldThrow<ArgumentNullException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, null, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentNullException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, show, null, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, show, string.Empty, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            episode.Number = -1;
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, show, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            episode.Number = 1;
+            episode.SeasonNumber = -1;
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, show, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            episode.SeasonNumber = 1;
+            episode.Ids = new TraktEpisodeIds();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, show, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+
+            episode.Ids = new TraktEpisodeIds
+            {
+                Trakt = 16,
+                Tvdb = 349232,
+                Imdb = "tt0959621",
+                Tmdb = 62085,
+                TvRage = 637041
+            };
+
+            show.Title = string.Empty;
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.Checkins.CheckinEpisodeAsync(episode, show, appVersion, appBuildDate);
+            act.ShouldThrow<ArgumentException>();
+        }
     }
 }
