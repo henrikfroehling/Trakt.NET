ed4b7b1870a77971cc1388b5ceb06317d9868f05
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-26 16:10:06 +0200

    Integrate uritemplates library implementation.

---
+++ Source/Lib/TraktApiSharp/Requests/Base/UriTemplates/VarSpec.cs
@@ -0,0 +1,34 @@
+namespace UriTemplates
+{
+    using System.Text;
+
+    /// <summary>
+    /// <see cref="https://github.com/tavis-software/Tavis.UriTemplates/blob/master/src/UriTemplates/VarSpec.cs" />
+    /// </summary>
+    internal class VarSpec
+    {
+        private readonly OperatorInfo _operatorInfo;
+        internal StringBuilder VarName = new StringBuilder();
+        internal bool Explode = false;
+        internal int PrefixLength = 0;
+        internal bool First = true;
+
+        internal VarSpec(OperatorInfo operatorInfo)
+        {
+            _operatorInfo = operatorInfo;
+        }
+
+        internal OperatorInfo OperatorInfo
+        {
+            get { return _operatorInfo; }
+        }
+
+        public override string ToString()
+        {
+            return (First ? _operatorInfo.First : "") +
+                   VarName.ToString()
+                   + (Explode ? "*" : "")
+                   + (PrefixLength > 0 ? ":" + PrefixLength : "");
+        }
+    }
+}
