bdcdec549eab1975c17c9c24e2a8f5dfc4913d5c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-27 04:31:06 +0200

    Add unit tests for people module person movie credits request.

--- Source/Tests/TraktApiSharp.Tests/Modules/TraktPeopleModuleTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Modules/TraktPeopleModuleTests.cs
@@ -3,12 +3,14 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System;
+    using System.Linq;
     using System.Net;
     using System.Threading.Tasks;
     using TraktApiSharp.Exceptions;
     using TraktApiSharp.Modules;
     using TraktApiSharp.Objects.Basic;
     using TraktApiSharp.Objects.Get.People;
+    using TraktApiSharp.Objects.Get.People.Credits;
     using TraktApiSharp.Requests;
     using Utils;
 
@@ -214,25 +216,259 @@
         [TestMethod]
         public void TestTraktPeopleModuleGetPersonMovieCredits()
         {
-            Assert.Fail();
+            var personMovieCredits = TestUtility.ReadFileContents(@"Objects\Get\People\Credits\PersonMovieCredits.json");
+            personMovieCredits.Should().NotBeNullOrEmpty();
+
+            var personId = "297737";
+
+            TestUtility.SetupMockResponseWithoutOAuth($"people/{personId}/movies", personMovieCredits);
+
+            var response = TestUtility.MOCK_TEST_CLIENT.People.GetPersonMovieCreditsAsync(personId).Result;
+
+            response.Should().NotBeNull();
+
+            response.Cast.Should().NotBeNull().And.HaveCount(2);
+
+            var cast = response.Cast.ToArray();
+
+            cast[0].Character.Should().Be("Li (voice)");
+            cast[0].Movie.Should().NotBeNull();
+            cast[0].Movie.Title.Should().Be("Kung Fu Panda 3");
+            cast[0].Movie.Year.Should().Be(2016);
+            cast[0].Movie.Ids.Should().NotBeNull();
+            cast[0].Movie.Ids.Trakt.Should().Be(93870);
+            cast[0].Movie.Ids.Slug.Should().Be("kung-fu-panda-3-2016");
+            cast[0].Movie.Ids.Imdb.Should().Be("tt2267968");
+            cast[0].Movie.Ids.Tmdb.Should().Be(140300);
+
+            cast[1].Character.Should().Be("Joe Brody");
+            cast[1].Movie.Should().NotBeNull();
+            cast[1].Movie.Title.Should().Be("Godzilla");
+            cast[1].Movie.Year.Should().Be(2014);
+            cast[1].Movie.Ids.Should().NotBeNull();
+            cast[1].Movie.Ids.Trakt.Should().Be(24);
+            cast[1].Movie.Ids.Slug.Should().Be("godzilla-2014");
+            cast[1].Movie.Ids.Imdb.Should().Be("tt0831387");
+            cast[1].Movie.Ids.Tmdb.Should().Be(124905);
+
+            response.Crew.Should().NotBeNull();
+            response.Crew.Art.Should().BeNull();
+            response.Crew.Camera.Should().BeNull();
+            response.Crew.CostumeAndMakeup.Should().BeNull();
+            response.Crew.Crew.Should().BeNull();
+            response.Crew.Directing.Should().NotBeNull().And.HaveCount(1);
+
+            var directing = response.Crew.Directing.ToArray();
+
+            directing[0].Job.Should().Be("Director");
+            directing[0].Movie.Should().NotBeNull();
+            directing[0].Movie.Title.Should().Be("Godzilla");
+            directing[0].Movie.Year.Should().Be(2014);
+            directing[0].Movie.Ids.Should().NotBeNull();
+            directing[0].Movie.Ids.Trakt.Should().Be(24);
+            directing[0].Movie.Ids.Slug.Should().Be("godzilla-2014");
+            directing[0].Movie.Ids.Imdb.Should().Be("tt0831387");
+            directing[0].Movie.Ids.Tmdb.Should().Be(124905);
+
+            response.Crew.Editing.Should().BeNull();
+            response.Crew.Lighting.Should().BeNull();
+            response.Crew.Production.Should().NotBeNull().And.HaveCount(1);
+
+            var production = response.Crew.Production.ToArray();
+
+            production[0].Job.Should().Be("Producer");
+            production[0].Movie.Should().NotBeNull();
+            production[0].Movie.Title.Should().Be("Godzilla");
+            production[0].Movie.Year.Should().Be(2014);
+            production[0].Movie.Ids.Should().NotBeNull();
+            production[0].Movie.Ids.Trakt.Should().Be(24);
+            production[0].Movie.Ids.Slug.Should().Be("godzilla-2014");
+            production[0].Movie.Ids.Imdb.Should().Be("tt0831387");
+            production[0].Movie.Ids.Tmdb.Should().Be(124905);
+
+            response.Crew.Sound.Should().BeNull();
+            response.Crew.VisualEffects.Should().BeNull();
+            response.Crew.Writing.Should().NotBeNull().And.HaveCount(1);
+
+            var writing = response.Crew.Writing.ToArray();
+
+            writing[0].Job.Should().Be("Screenplay");
+            writing[0].Movie.Should().NotBeNull();
+            writing[0].Movie.Title.Should().Be("Godzilla");
+            writing[0].Movie.Year.Should().Be(2014);
+            writing[0].Movie.Ids.Should().NotBeNull();
+            writing[0].Movie.Ids.Trakt.Should().Be(24);
+            writing[0].Movie.Ids.Slug.Should().Be("godzilla-2014");
+            writing[0].Movie.Ids.Imdb.Should().Be("tt0831387");
+            writing[0].Movie.Ids.Tmdb.Should().Be(124905);
         }
 
         [TestMethod]
         public void TestTraktPeopleModuleGetPersonMovieCreditsWithExtendedOption()
         {
-            Assert.Fail();
+            var personMovieCredits = TestUtility.ReadFileContents(@"Objects\Get\People\Credits\PersonMovieCredits.json");
+            personMovieCredits.Should().NotBeNullOrEmpty();
+
+            var personId = "297737";
+
+            var extendedOption = new TraktExtendedOption
+            {
+                Full = true,
+                Images = true
+            };
+
+            TestUtility.SetupMockResponseWithoutOAuth($"people/{personId}/movies?extended={extendedOption.ToString()}",
+                                                      personMovieCredits);
+
+            var response = TestUtility.MOCK_TEST_CLIENT.People.GetPersonMovieCreditsAsync(personId, extendedOption).Result;
+
+            response.Should().NotBeNull();
+
+            response.Cast.Should().NotBeNull().And.HaveCount(2);
+
+            var cast = response.Cast.ToArray();
+
+            cast[0].Character.Should().Be("Li (voice)");
+            cast[0].Movie.Should().NotBeNull();
+            cast[0].Movie.Title.Should().Be("Kung Fu Panda 3");
+            cast[0].Movie.Year.Should().Be(2016);
+            cast[0].Movie.Ids.Should().NotBeNull();
+            cast[0].Movie.Ids.Trakt.Should().Be(93870);
+            cast[0].Movie.Ids.Slug.Should().Be("kung-fu-panda-3-2016");
+            cast[0].Movie.Ids.Imdb.Should().Be("tt2267968");
+            cast[0].Movie.Ids.Tmdb.Should().Be(140300);
+
+            cast[1].Character.Should().Be("Joe Brody");
+            cast[1].Movie.Should().NotBeNull();
+            cast[1].Movie.Title.Should().Be("Godzilla");
+            cast[1].Movie.Year.Should().Be(2014);
+            cast[1].Movie.Ids.Should().NotBeNull();
+            cast[1].Movie.Ids.Trakt.Should().Be(24);
+            cast[1].Movie.Ids.Slug.Should().Be("godzilla-2014");
+            cast[1].Movie.Ids.Imdb.Should().Be("tt0831387");
+            cast[1].Movie.Ids.Tmdb.Should().Be(124905);
+
+            response.Crew.Should().NotBeNull();
+            response.Crew.Art.Should().BeNull();
+            response.Crew.Camera.Should().BeNull();
+            response.Crew.CostumeAndMakeup.Should().BeNull();
+            response.Crew.Crew.Should().BeNull();
+            response.Crew.Directing.Should().NotBeNull().And.HaveCount(1);
+
+            var directing = response.Crew.Directing.ToArray();
+
+            directing[0].Job.Should().Be("Director");
+            directing[0].Movie.Should().NotBeNull();
+            directing[0].Movie.Title.Should().Be("Godzilla");
+            directing[0].Movie.Year.Should().Be(2014);
+            directing[0].Movie.Ids.Should().NotBeNull();
+            directing[0].Movie.Ids.Trakt.Should().Be(24);
+            directing[0].Movie.Ids.Slug.Should().Be("godzilla-2014");
+            directing[0].Movie.Ids.Imdb.Should().Be("tt0831387");
+            directing[0].Movie.Ids.Tmdb.Should().Be(124905);
+
+            response.Crew.Editing.Should().BeNull();
+            response.Crew.Lighting.Should().BeNull();
+            response.Crew.Production.Should().NotBeNull().And.HaveCount(1);
+
+            var production = response.Crew.Production.ToArray();
+
+            production[0].Job.Should().Be("Producer");
+            production[0].Movie.Should().NotBeNull();
+            production[0].Movie.Title.Should().Be("Godzilla");
+            production[0].Movie.Year.Should().Be(2014);
+            production[0].Movie.Ids.Should().NotBeNull();
+            production[0].Movie.Ids.Trakt.Should().Be(24);
+            production[0].Movie.Ids.Slug.Should().Be("godzilla-2014");
+            production[0].Movie.Ids.Imdb.Should().Be("tt0831387");
+            production[0].Movie.Ids.Tmdb.Should().Be(124905);
+
+            response.Crew.Sound.Should().BeNull();
+            response.Crew.VisualEffects.Should().BeNull();
+            response.Crew.Writing.Should().NotBeNull().And.HaveCount(1);
+
+            var writing = response.Crew.Writing.ToArray();
+
+            writing[0].Job.Should().Be("Screenplay");
+            writing[0].Movie.Should().NotBeNull();
+            writing[0].Movie.Title.Should().Be("Godzilla");
+            writing[0].Movie.Year.Should().Be(2014);
+            writing[0].Movie.Ids.Should().NotBeNull();
+            writing[0].Movie.Ids.Trakt.Should().Be(24);
+            writing[0].Movie.Ids.Slug.Should().Be("godzilla-2014");
+            writing[0].Movie.Ids.Imdb.Should().Be("tt0831387");
+            writing[0].Movie.Ids.Tmdb.Should().Be(124905);
         }
 
         [TestMethod]
         public void TestTraktPeopleModuleGetPersonMovieCreditsExceptions()
         {
-            Assert.Fail();
+            var personId = "297737";
+            var uri = $"people/{personId}/movies";
+
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, HttpStatusCode.NotFound);
+
+            Func<Task<TraktPersonMovieCredits>> act =
+                async () => await TestUtility.MOCK_TEST_CLIENT.People.GetPersonMovieCreditsAsync(personId);
+            act.ShouldThrow<TraktPersonNotFoundException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, HttpStatusCode.BadRequest);
+            act.ShouldThrow<TraktBadRequestException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, HttpStatusCode.Forbidden);
+            act.ShouldThrow<TraktForbiddenException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)412);
+            act.ShouldThrow<TraktPreconditionFailedException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)429);
+            act.ShouldThrow<TraktRateLimitException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, HttpStatusCode.InternalServerError);
+            act.ShouldThrow<TraktServerException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)503);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)504);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)520);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)521);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)522);
+            act.ShouldThrow<TraktServerUnavailableException>();
         }
 
         [TestMethod]
         public void TestTraktPeopleModuleGetPersonMovieCreditsArgumentExceptions()
         {
-            Assert.Fail();
+            var personMovieCredits = TestUtility.ReadFileContents(@"Objects\Get\People\Credits\PersonMovieCredits.json");
+            personMovieCredits.Should().NotBeNullOrEmpty();
+
+            var personId = "297737";
+
+            TestUtility.SetupMockResponseWithoutOAuth($"people/{personId}/movies", personMovieCredits);
+
+            Func<Task<TraktPersonMovieCredits>> act =
+                async () => await TestUtility.MOCK_TEST_CLIENT.People.GetPersonMovieCreditsAsync(null);
+            act.ShouldThrow<ArgumentException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.People.GetPersonMovieCreditsAsync(string.Empty);
+            act.ShouldThrow<ArgumentException>();
         }
 
         #endregion
