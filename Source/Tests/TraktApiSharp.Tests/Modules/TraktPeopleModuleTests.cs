806bae4ef270d6a1e6ebbc16b457b213ced170b3
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-27 04:08:29 +0200

    Add unit tests for people module single person request.

--- Source/Tests/TraktApiSharp.Tests/Modules/TraktPeopleModuleTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Modules/TraktPeopleModuleTests.cs
@@ -2,7 +2,13 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+    using TraktApiSharp.Exceptions;
     using TraktApiSharp.Modules;
+    using TraktApiSharp.Objects.Get.People;
+    using TraktApiSharp.Requests;
     using Utils;
 
     [TestClass]
@@ -40,25 +46,137 @@
         [TestMethod]
         public void TestTraktPeopleModuleGetPerson()
         {
-            Assert.Fail();
+            var person = TestUtility.ReadFileContents(@"Objects\Get\People\PersonFullAndImages.json");
+            person.Should().NotBeNullOrEmpty();
+
+            var personId = "297737";
+
+            TestUtility.SetupMockResponseWithoutOAuth($"people/{personId}", person);
+
+            var response = TestUtility.MOCK_TEST_CLIENT.People.GetPersonAsync(personId).Result;
+
+            response.Should().NotBeNull();
+            response.Name.Should().Be("Bryan Cranston");
+            response.Ids.Should().NotBeNull();
+            response.Ids.Trakt.Should().Be(297737);
+            response.Ids.Slug.Should().Be("bryan-cranston");
+            response.Ids.Imdb.Should().Be("nm0186505");
+            response.Ids.Tmdb.Should().Be(17419);
+            response.Ids.TvRage.Should().Be(1797);
         }
 
         [TestMethod]
         public void TestTraktPeopleModuleGetPersonWithExtendedOption()
         {
-            Assert.Fail();
+            var person = TestUtility.ReadFileContents(@"Objects\Get\People\PersonFullAndImages.json");
+            person.Should().NotBeNullOrEmpty();
+
+            var personId = "297737";
+
+            var extendedOption = new TraktExtendedOption
+            {
+                Full = true,
+                Images = true
+            };
+
+            TestUtility.SetupMockResponseWithoutOAuth($"people/{personId}?extended={extendedOption.ToString()}", person);
+
+            var response = TestUtility.MOCK_TEST_CLIENT.People.GetPersonAsync(personId, extendedOption).Result;
+
+            response.Should().NotBeNull();
+            response.Name.Should().Be("Bryan Cranston");
+            response.Ids.Should().NotBeNull();
+            response.Ids.Trakt.Should().Be(297737);
+            response.Ids.Slug.Should().Be("bryan-cranston");
+            response.Ids.Imdb.Should().Be("nm0186505");
+            response.Ids.Tmdb.Should().Be(17419);
+            response.Ids.TvRage.Should().Be(1797);
+            response.Images.Should().NotBeNull();
+            response.Images.Headshot.Should().NotBeNull();
+            response.Images.Headshot.Full.Should().Be("https://walter.trakt.us/images/people/000/297/737/headshots/original/47aebaace9.jpg");
+            response.Images.Headshot.Medium.Should().Be("https://walter.trakt.us/images/people/000/297/737/headshots/medium/47aebaace9.jpg");
+            response.Images.Headshot.Thumb.Should().Be("https://walter.trakt.us/images/people/000/297/737/headshots/thumb/47aebaace9.jpg");
+            response.Images.FanArt.Should().NotBeNull();
+            response.Images.FanArt.Full.Should().Be("https://walter.trakt.us/images/people/000/297/737/fanarts/original/0e436db5dd.jpg");
+            response.Images.FanArt.Medium.Should().Be("https://walter.trakt.us/images/people/000/297/737/fanarts/medium/0e436db5dd.jpg");
+            response.Images.FanArt.Thumb.Should().Be("https://walter.trakt.us/images/people/000/297/737/fanarts/thumb/0e436db5dd.jpg");
+            response.Biography.Should().Be("Bryan Lee Cranston (born March 7, 1956) is an American actor, voice actor, writer and director.He is perhaps best known for his roles as Hal, the father in the Fox situation comedy \"Malcolm in the Middle\", and as Walter White in the AMC drama series Breaking Bad, for which he has won three consecutive Outstanding Lead Actor in a Drama Series Emmy Awards. Other notable roles include Dr. Tim Whatley on Seinfeld, Doug Heffernan's neighbor in The King of Queens, Astronaut Buzz Aldrin in From the Earth to the Moon, and Ted Mosby's boss on How I Met Your Mother. Description above from the Wikipedia article Bryan Cranston, licensed under CC-BY-SA, full list of contributors on Wikipedia.");
+            response.Birthday.Should().Be(DateTime.Parse("1956-03-07T00:00:00Z").ToUniversalTime());
+            response.Death.Should().Be(DateTime.Parse("2016-04-06T00:00:00Z").ToUniversalTime());
+            response.Age.Should().Be(60);
+            response.Birthplace.Should().Be("San Fernando Valley, California, USA");
+            response.Homepage.Should().Be("http://www.bryancranston.com/");
         }
 
         [TestMethod]
         public void TestTraktPeopleModuleGetPersonExceptions()
         {
-            Assert.Fail();
+            var personId = "297737";
+            var uri = $"people/{personId}";
+
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, HttpStatusCode.NotFound);
+
+            Func<Task<TraktPerson>> act =
+                async () => await TestUtility.MOCK_TEST_CLIENT.People.GetPersonAsync(personId);
+            act.ShouldThrow<TraktPersonNotFoundException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, HttpStatusCode.BadRequest);
+            act.ShouldThrow<TraktBadRequestException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, HttpStatusCode.Forbidden);
+            act.ShouldThrow<TraktForbiddenException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)412);
+            act.ShouldThrow<TraktPreconditionFailedException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)429);
+            act.ShouldThrow<TraktRateLimitException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, HttpStatusCode.InternalServerError);
+            act.ShouldThrow<TraktServerException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)503);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)504);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)520);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)521);
+            act.ShouldThrow<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockErrorResponseWithoutOAuth(uri, (HttpStatusCode)522);
+            act.ShouldThrow<TraktServerUnavailableException>();
         }
 
         [TestMethod]
         public void TestTraktPeopleModuleGetPersonArgumentExceptions()
         {
-            Assert.Fail();
+            var person = TestUtility.ReadFileContents(@"Objects\Get\People\PersonFullAndImages.json");
+            person.Should().NotBeNullOrEmpty();
+
+            var personId = "297737";
+
+            TestUtility.SetupMockResponseWithoutOAuth($"people/{personId}", person);
+
+            Func<Task<TraktPerson>> act =
+                async () => await TestUtility.MOCK_TEST_CLIENT.People.GetPersonAsync(null);
+            act.ShouldThrow<ArgumentException>();
+
+            act = async () => await TestUtility.MOCK_TEST_CLIENT.People.GetPersonAsync(string.Empty);
+            act.ShouldThrow<ArgumentException>();
         }
 
         #endregion
