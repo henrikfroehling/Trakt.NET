35d1122d035ddb93f91c404d2cc0907a4ba48964
Henrik Fröhling <henrik.froehling@gmail.com>
2016-05-27 16:07:39 +0200

    Add validation implementation in search module.

--- Source/Lib/TraktApiSharp/Modules/TraktSearchModule.cs
+++ Source/Lib/TraktApiSharp/Modules/TraktSearchModule.cs
@@ -4,6 +4,7 @@
     using Objects.Basic;
     using Requests;
     using Requests.WithoutOAuth.Search;
+    using System;
     using System.Threading.Tasks;
 
     public class TraktSearchModule : TraktBaseModule
@@ -13,6 +14,8 @@
         public async Task<TraktPaginationListResult<TraktSearchResult>> SearchTextQueryAsync(string query, TraktSearchResultType? type = null,
                                                                                              int? year = null, int? page = null, int? limit = null)
         {
+            ValidateQuery(query);
+
             return await QueryAsync(new TraktSearchTextQueryRequest(Client)
             {
                 Query = query,
@@ -25,12 +28,26 @@
         public async Task<TraktPaginationListResult<TraktSearchIdLookupResult>> SearchIdLookupAsync(TraktSearchLookupIdType type, string lookupId,
                                                                                                     int? page = null, int? limit = null)
         {
+            ValidateIdLookup(lookupId);
+
             return await QueryAsync(new TraktSearchIdLookupRequest(Client)
             {
                 Type = type,
                 LookupId = lookupId,
                 PaginationOptions = new TraktPaginationOptions(page, limit)
             });
+        }
+
+        private void ValidateQuery(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+                throw new ArgumentException("search query not valid", "query");
+        }
+
+        private void ValidateIdLookup(string lookupId)
+        {
+            if (string.IsNullOrEmpty(lookupId))
+                throw new ArgumentException("search lookup id not valid", "lookupId");
         }
     }
 }
