68a82146a93164c6044cb3bc7d558a85f2048bde
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-23 05:20:46 +0200

    Add missing metadata properties to sync collection post implementation.

--- Source/Tests/TraktApiSharp.Tests/Objects/Post/Syncs/Collection/TraktSyncCollectionRemovePostTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Objects/Post/Syncs/Collection/TraktSyncCollectionRemovePostTests.cs
@@ -5,6 +5,8 @@
     using Newtonsoft.Json;
     using System.Collections.Generic;
     using System.Linq;
+    using TraktApiSharp.Enums;
+    using TraktApiSharp.Objects.Basic;
     using TraktApiSharp.Objects.Get.Movies;
     using TraktApiSharp.Objects.Get.Shows;
     using TraktApiSharp.Objects.Get.Shows.Episodes;
@@ -40,6 +42,14 @@
                             Slug = "batman-begins-2005",
                             Imdb = "tt0372784",
                             Tmdb = 272
+                        },
+                        Metadata = new TraktMetadata
+                        {
+                            Audio = TraktMediaAudio.DTS,
+                            AudioChannels = TraktMediaAudioChannel.Channels_5_1,
+                            MediaResolution = TraktMediaResolution.HD_1080p,
+                            MediaType = TraktMediaType.Digital,
+                            ThreeDimensional = false
                         }
                     },
                     new TraktSyncCollectionPostMovieItem
@@ -85,6 +95,14 @@
                             {
                                 Number = 3
                             }
+                        },
+                        Metadata = new TraktMetadata
+                        {
+                            Audio = TraktMediaAudio.DTS,
+                            AudioChannels = TraktMediaAudioChannel.Channels_5_1,
+                            MediaResolution = TraktMediaResolution.HD_1080p,
+                            MediaType = TraktMediaType.Digital,
+                            ThreeDimensional = false
                         }
                     },
                     new TraktSyncCollectionPostShowItem
@@ -115,6 +133,14 @@
                                     {
                                         Number = 2
                                     }
+                                },
+                                Metadata = new TraktMetadata
+                                {
+                                    Audio = TraktMediaAudio.DTS,
+                                    AudioChannels = TraktMediaAudioChannel.Channels_5_1,
+                                    MediaResolution = TraktMediaResolution.HD_1080p,
+                                    MediaType = TraktMediaType.Digital,
+                                    ThreeDimensional = false
                                 }
                             }
                         }
@@ -131,6 +157,14 @@
                             Imdb = "tt007404",
                             Tmdb = 422183,
                             TvRage = 12345
+                        },
+                        Metadata = new TraktMetadata
+                        {
+                            Audio = TraktMediaAudio.DTS,
+                            AudioChannels = TraktMediaAudioChannel.Channels_5_1,
+                            MediaResolution = TraktMediaResolution.HD_1080p,
+                            MediaType = TraktMediaType.Digital,
+                            ThreeDimensional = false
                         }
                     }
                 }
@@ -158,7 +192,12 @@
             movies[0].Ids.Slug.Should().Be("batman-begins-2005");
             movies[0].Ids.Imdb.Should().Be("tt0372784");
             movies[0].Ids.Tmdb.Should().Be(272);
-            movies[0].Metadata.Should().BeNull();
+            movies[0].Metadata.Should().NotBeNull();
+            movies[0].Metadata.Audio.Should().Be(TraktMediaAudio.DTS);
+            movies[0].Metadata.AudioChannels.Should().Be(TraktMediaAudioChannel.Channels_5_1);
+            movies[0].Metadata.MediaResolution.Should().Be(TraktMediaResolution.HD_1080p);
+            movies[0].Metadata.MediaType.Should().Be(TraktMediaType.Digital);
+            movies[0].Metadata.ThreeDimensional.Should().BeFalse();
 
             movies[1].CollectedAt.Should().NotHaveValue();
             movies[1].Title.Should().BeNullOrEmpty();
@@ -181,6 +220,7 @@
             shows[0].Ids.Imdb.Should().Be("tt0903747");
             shows[0].Ids.Tmdb.Should().Be(1396);
             shows[0].Ids.TvRage.Should().Be(18164);
+            shows[0].Metadata.Should().BeNull();
             shows[0].Seasons.Should().BeNull();
 
             shows[1].Title.Should().Be("The Walking Dead");
@@ -192,12 +232,19 @@
             shows[1].Ids.Imdb.Should().Be("tt1520211");
             shows[1].Ids.Tmdb.Should().Be(1402);
             shows[1].Ids.TvRage.Should().Be(25056);
+            shows[1].Metadata.Should().NotBeNull();
+            shows[1].Metadata.Audio.Should().Be(TraktMediaAudio.DTS);
+            shows[1].Metadata.AudioChannels.Should().Be(TraktMediaAudioChannel.Channels_5_1);
+            shows[1].Metadata.MediaResolution.Should().Be(TraktMediaResolution.HD_1080p);
+            shows[1].Metadata.MediaType.Should().Be(TraktMediaType.Digital);
+            shows[1].Metadata.ThreeDimensional.Should().BeFalse();
             shows[1].Seasons.Should().NotBeNull().And.HaveCount(1);
 
             var show2Seasons = shows[1].Seasons.ToArray();
 
             show2Seasons[0].Number.Should().Be(3);
             show2Seasons[0].Episodes.Should().BeNull();
+            show2Seasons[0].Metadata.Should().BeNull();
 
             shows[2].Title.Should().Be("Mad Men");
             shows[2].Year.Should().Be(2007);
@@ -208,11 +255,20 @@
             shows[2].Ids.Imdb.Should().Be("tt0804503");
             shows[2].Ids.Tmdb.Should().Be(1104);
             shows[2].Ids.TvRage.Should().Be(16356);
+            shows[2].Metadata.Should().BeNull();
             shows[2].Seasons.Should().NotBeNull().And.HaveCount(1);
 
             var show3Seasons = shows[2].Seasons.ToArray();
 
             show3Seasons[0].Number.Should().Be(1);
+
+            show3Seasons[0].Metadata.Should().NotBeNull();
+            show3Seasons[0].Metadata.Audio.Should().Be(TraktMediaAudio.DTS);
+            show3Seasons[0].Metadata.AudioChannels.Should().Be(TraktMediaAudioChannel.Channels_5_1);
+            show3Seasons[0].Metadata.MediaResolution.Should().Be(TraktMediaResolution.HD_1080p);
+            show3Seasons[0].Metadata.MediaType.Should().Be(TraktMediaType.Digital);
+            show3Seasons[0].Metadata.ThreeDimensional.Should().BeFalse();
+
             show3Seasons[0].Episodes.Should().NotBeNull().And.HaveCount(2);
 
             var show3Season1Episodes = show3Seasons[0].Episodes.ToArray();
@@ -228,7 +284,6 @@
             var episodes = collectionPostFromJson.Episodes.ToArray();
 
             episodes[0].CollectedAt.Should().NotHaveValue();
-            episodes[0].Metadata.Should().BeNull();
             episodes[0].Ids.Should().NotBeNull();
             episodes[0].Ids.Trakt.Should().Be(1061);
             episodes[0].Ids.Slug.Should().BeNullOrEmpty();
@@ -236,6 +291,12 @@
             episodes[0].Ids.Imdb.Should().Be("tt007404");
             episodes[0].Ids.Tmdb.Should().Be(422183);
             episodes[0].Ids.TvRage.Should().Be(12345);
+            episodes[0].Metadata.Should().NotBeNull();
+            episodes[0].Metadata.Audio.Should().Be(TraktMediaAudio.DTS);
+            episodes[0].Metadata.AudioChannels.Should().Be(TraktMediaAudioChannel.Channels_5_1);
+            episodes[0].Metadata.MediaResolution.Should().Be(TraktMediaResolution.HD_1080p);
+            episodes[0].Metadata.MediaType.Should().Be(TraktMediaType.Digital);
+            episodes[0].Metadata.ThreeDimensional.Should().BeFalse();
         }
     }
 }
