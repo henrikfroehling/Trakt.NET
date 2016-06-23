5ba78d5af53cb543b11909d8167c05ec7c3404de
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-23 02:18:17 +0200

    Remove get reply method from comments module - same as get comment method.

--- Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Comments/TraktCommentReplySummaryRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TraktApiSharp.Requests.WithoutOAuth.Comments
-{
-    internal class TraktCommentReplySummaryRequest : TraktCommentSummaryRequest
-    {
-        internal TraktCommentReplySummaryRequest(TraktClient client) : base(client) { }
-    }
-}
