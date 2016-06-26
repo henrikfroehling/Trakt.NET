91e4435f43f7e373c117a79f6ff937fdd207c8c6
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-26 23:56:13 +0200

    Add unit tests for enum access scope read and write.

--- Source/Tests/TraktApiSharp.Tests/Enums/TraktAccessScopeTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Enums/TraktAccessScopeTests.cs
@@ -2,11 +2,18 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
     using TraktApiSharp.Enums;
 
     [TestClass]
     public class TraktAccessScopeTests
     {
+        class TestObject
+        {
+            [JsonConverter(typeof(TraktAccessScopeConverter))]
+            public TraktAccessScope Value { get; set; }
+        }
+
         [TestMethod]
         public void TestTraktAccessScopeHasMembers()
         {
@@ -21,6 +28,58 @@
             TraktAccessScope.Private.AsString().Should().Be("private");
             TraktAccessScope.Public.AsString().Should().Be("public");
             TraktAccessScope.Unspecified.AsString().Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod]
+        public void TestTraktAccessScopeWriteAndReadJson_Friends()
+        {
+            var obj = new TestObject { Value = TraktAccessScope.Friends };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktAccessScope.Friends);
+        }
+
+        [TestMethod]
+        public void TestTraktAccessScopeWriteAndReadJson_Private()
+        {
+            var obj = new TestObject { Value = TraktAccessScope.Private };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktAccessScope.Private);
+        }
+
+        [TestMethod]
+        public void TestTraktAccessScopeWriteAndReadJson_Public()
+        {
+            var obj = new TestObject { Value = TraktAccessScope.Public };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktAccessScope.Public);
+        }
+
+        [TestMethod]
+        public void TestTraktAccessScopeWriteAndReadJson_Unspecified()
+        {
+            var obj = new TestObject { Value = TraktAccessScope.Unspecified };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktAccessScope.Unspecified);
         }
     }
 }
