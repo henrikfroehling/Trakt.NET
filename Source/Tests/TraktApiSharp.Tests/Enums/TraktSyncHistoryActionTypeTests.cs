9d243eb6d5c0849fd266a4c6f9b64f4aa03f7f01
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-27 01:22:20 +0200

    Add unit tests for enum sync history action type read and write.

--- Source/Tests/TraktApiSharp.Tests/Enums/TraktSyncHistoryActionTypeTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Enums/TraktSyncHistoryActionTypeTests.cs
@@ -2,11 +2,18 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
     using TraktApiSharp.Enums;
 
     [TestClass]
     public class TraktSyncHistoryActionTypeTests
     {
+        class TestObject
+        {
+            [JsonConverter(typeof(TraktSyncHistoryActionTypeConverter))]
+            public TraktSyncHistoryActionType Value { get; set; }
+        }
+
         [TestMethod]
         public void TestTraktSyncHistoryActionTypeHasMembers()
         {
@@ -21,6 +28,58 @@
             TraktSyncHistoryActionType.Scrobble.AsString().Should().Be("scrobble");
             TraktSyncHistoryActionType.Checkin.AsString().Should().Be("checkin");
             TraktSyncHistoryActionType.Watch.AsString().Should().Be("watch");
+        }
+
+        [TestMethod]
+        public void TestTraktSyncHistoryActionTypeWriteAndReadJson_Scrobble()
+        {
+            var obj = new TestObject { Value = TraktSyncHistoryActionType.Scrobble };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSyncHistoryActionType.Scrobble);
+        }
+
+        [TestMethod]
+        public void TestTraktSyncHistoryActionTypeWriteAndReadJson_Checkin()
+        {
+            var obj = new TestObject { Value = TraktSyncHistoryActionType.Checkin };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSyncHistoryActionType.Checkin);
+        }
+
+        [TestMethod]
+        public void TestTraktSyncHistoryActionTypeWriteAndReadJson_Watch()
+        {
+            var obj = new TestObject { Value = TraktSyncHistoryActionType.Watch };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSyncHistoryActionType.Watch);
+        }
+
+        [TestMethod]
+        public void TestTraktSyncHistoryActionTypeWriteAndReadJson_Unspecified()
+        {
+            var obj = new TestObject { Value = TraktSyncHistoryActionType.Unspecified };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSyncHistoryActionType.Unspecified);
         }
     }
 }
