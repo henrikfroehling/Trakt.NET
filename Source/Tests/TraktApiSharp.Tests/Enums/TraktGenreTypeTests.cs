ef11f31cbe82438d7999ffc6facc1ee31a4725bf
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-27 00:36:05 +0200

    Add unit tests for enum genre type read and write.

--- Source/Tests/TraktApiSharp.Tests/Enums/TraktGenreTypeTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Enums/TraktGenreTypeTests.cs
@@ -2,11 +2,18 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
     using TraktApiSharp.Enums;
 
     [TestClass]
     public class TraktGenreTypeTests
     {
+        class TestObject
+        {
+            [JsonConverter(typeof(TraktGenreTypeConverter))]
+            public TraktGenreType Value { get; set; }
+        }
+
         [TestMethod]
         public void TestTraktGenreTypeHasMembers()
         {
@@ -20,6 +27,45 @@
             TraktGenreType.Shows.AsString().Should().Be("shows");
             TraktGenreType.Movies.AsString().Should().Be("movies");
             TraktGenreType.Unspecified.AsString().Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod]
+        public void TestTraktGenreTypeWriteAndReadJson_Shows()
+        {
+            var obj = new TestObject { Value = TraktGenreType.Shows };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktGenreType.Shows);
+        }
+
+        [TestMethod]
+        public void TestTraktGenreTypeWriteAndReadJson_Movies()
+        {
+            var obj = new TestObject { Value = TraktGenreType.Movies };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktGenreType.Movies);
+        }
+
+        [TestMethod]
+        public void TestTraktGenreTypeWriteAndReadJson_Unspecified()
+        {
+            var obj = new TestObject { Value = TraktGenreType.Unspecified };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktGenreType.Unspecified);
         }
     }
 }
