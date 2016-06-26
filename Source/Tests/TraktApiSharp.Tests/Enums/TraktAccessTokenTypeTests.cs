0f2d128a67f0fe4982e453c7d1f677b098b55aa8
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-27 00:30:45 +0200

    Add unit tests for enum access token type read and write.

--- Source/Tests/TraktApiSharp.Tests/Enums/TraktAccessTokenTypeTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Enums/TraktAccessTokenTypeTests.cs
@@ -2,11 +2,18 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
     using TraktApiSharp.Enums;
 
     [TestClass]
     public class TraktAccessTokenTypeTests
     {
+        class TestObject
+        {
+            [JsonConverter(typeof(TraktAccessTokenTypeConverter))]
+            public TraktAccessTokenType Value { get; set; }
+        }
+
         [TestMethod]
         public void TestTraktAccessTokenTypeHasMembers()
         {
@@ -19,6 +26,32 @@
         {
             TraktAccessTokenType.Bearer.AsString().Should().Be("bearer");
             TraktAccessTokenType.Unspecified.AsString().Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod]
+        public void TestTraktAccessTokenTypeWriteAndReadJson_Bearer()
+        {
+            var obj = new TestObject { Value = TraktAccessTokenType.Bearer };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktAccessTokenType.Bearer);
+        }
+
+        [TestMethod]
+        public void TestTraktAccessTokenTypeWriteAndReadJson_Unspecified()
+        {
+            var obj = new TestObject { Value = TraktAccessTokenType.Unspecified };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktAccessTokenType.Unspecified);
         }
     }
 }
