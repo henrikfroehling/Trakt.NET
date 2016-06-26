6606bf2b248b416aea54c70cb76ad556883ff14f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-27 01:19:26 +0200

    Add unit tests for enum show status read and write.

--- Source/Tests/TraktApiSharp.Tests/Enums/TraktShowStatusTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Enums/TraktShowStatusTests.cs
@@ -2,11 +2,18 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
     using TraktApiSharp.Enums;
 
     [TestClass]
     public class TraktShowStatusTests
     {
+        class TestObject
+        {
+            [JsonConverter(typeof(TraktShowStatusConverter))]
+            public TraktShowStatus Value { get; set; }
+        }
+
         [TestMethod]
         public void TestTraktShowStatusHasMembers()
         {
@@ -22,6 +29,71 @@
             TraktShowStatus.InProduction.AsString().Should().Be("in production");
             TraktShowStatus.Canceled.AsString().Should().Be("canceled");
             TraktShowStatus.Ended.AsString().Should().Be("ended");
+        }
+
+        [TestMethod]
+        public void TestTraktShowStatusWriteAndReadJson_ReturningSeries()
+        {
+            var obj = new TestObject { Value = TraktShowStatus.ReturningSeries };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktShowStatus.ReturningSeries);
+        }
+
+        [TestMethod]
+        public void TestTraktShowStatusWriteAndReadJson_InProduction()
+        {
+            var obj = new TestObject { Value = TraktShowStatus.InProduction };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktShowStatus.InProduction);
+        }
+
+        [TestMethod]
+        public void TestTraktShowStatusWriteAndReadJson_Canceled()
+        {
+            var obj = new TestObject { Value = TraktShowStatus.Canceled };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktShowStatus.Canceled);
+        }
+
+        [TestMethod]
+        public void TestTraktShowStatusWriteAndReadJson_Ended()
+        {
+            var obj = new TestObject { Value = TraktShowStatus.Ended };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktShowStatus.Ended);
+        }
+
+        [TestMethod]
+        public void TestTraktShowStatusWriteAndReadJson_Unspecified()
+        {
+            var obj = new TestObject { Value = TraktShowStatus.Unspecified };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktShowStatus.Unspecified);
         }
     }
 }
