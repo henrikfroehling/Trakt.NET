107f9a1d841e1c14e75ea1d838f86862ea7bf9c2
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-27 00:43:28 +0200

    Add unit tests for enum list item type read and write.

--- Source/Tests/TraktApiSharp.Tests/Enums/TraktListItemTypeTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Enums/TraktListItemTypeTests.cs
@@ -2,11 +2,18 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
     using TraktApiSharp.Enums;
 
     [TestClass]
     public class TraktListItemTypeTests
     {
+        class TestObject
+        {
+            [JsonConverter(typeof(TraktListItemTypeConverter))]
+            public TraktListItemType Value { get; set; }
+        }
+
         [TestMethod]
         public void TestTraktListItemTypeHasMembers()
         {
@@ -34,6 +41,84 @@
             TraktListItemType.Season.AsStringUriParameter().Should().Be("seasons");
             TraktListItemType.Episode.AsStringUriParameter().Should().Be("episodes");
             TraktListItemType.Person.AsStringUriParameter().Should().Be("people");
+        }
+
+        [TestMethod]
+        public void TestTraktListItemTypeWriteAndReadJson_Movie()
+        {
+            var obj = new TestObject { Value = TraktListItemType.Movie };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktListItemType.Movie);
+        }
+
+        [TestMethod]
+        public void TestTraktListItemTypeWriteAndReadJson_Show()
+        {
+            var obj = new TestObject { Value = TraktListItemType.Show };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktListItemType.Show);
+        }
+
+        [TestMethod]
+        public void TestTraktListItemTypeWriteAndReadJson_Season()
+        {
+            var obj = new TestObject { Value = TraktListItemType.Season };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktListItemType.Season);
+        }
+
+        [TestMethod]
+        public void TestTraktListItemTypeWriteAndReadJson_Episode()
+        {
+            var obj = new TestObject { Value = TraktListItemType.Episode };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktListItemType.Episode);
+        }
+
+        [TestMethod]
+        public void TestTraktListItemTypeWriteAndReadJson_Person()
+        {
+            var obj = new TestObject { Value = TraktListItemType.Person };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktListItemType.Person);
+        }
+
+        [TestMethod]
+        public void TestTraktListItemTypeWriteAndReadJson_Unspecified()
+        {
+            var obj = new TestObject { Value = TraktListItemType.Unspecified };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktListItemType.Unspecified);
         }
     }
 }
