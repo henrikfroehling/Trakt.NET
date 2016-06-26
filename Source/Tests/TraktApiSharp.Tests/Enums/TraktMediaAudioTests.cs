447622a19660b127ff0dd1b563f918c9959d2759
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-27 00:54:08 +0200

    Add unit tests for enum media audio read and write.

--- Source/Tests/TraktApiSharp.Tests/Enums/TraktMediaAudioTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Enums/TraktMediaAudioTests.cs
@@ -2,11 +2,18 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
     using TraktApiSharp.Enums;
 
     [TestClass]
     public class TraktMediaAudioTests
     {
+        class TestObject
+        {
+            [JsonConverter(typeof(TraktMediaAudioConverter))]
+            public TraktMediaAudio Value { get; set; }
+        }
+
         [TestMethod]
         public void TestTraktMediaAudioHasMembers()
         {
@@ -30,6 +37,162 @@
             TraktMediaAudio.DolbyDigital.AsString().Should().Be("dolby_digital");
             TraktMediaAudio.DolbyDigitalPlus.AsString().Should().Be("dolby_digital_plus");
             TraktMediaAudio.DolbyTrueHD.AsString().Should().Be("dolby_truehd");
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_LPCM()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.LPCM };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.LPCM);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_MP3()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.MP3 };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.MP3);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_AAC()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.AAC };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.AAC);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_OGG()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.OGG };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.OGG);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_WMA()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.WMA };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.WMA);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_DTS()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.DTS };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.DTS);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_DTS_MA()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.DTS_MA };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.DTS_MA);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_DolbyPrologic()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.DolbyPrologic };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.DolbyPrologic);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_DolbyDigital()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.DolbyDigital };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.DolbyDigital);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_DolbyDigitalPlus()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.DolbyDigitalPlus };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.DolbyDigitalPlus);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_DolbyTrueHD()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.DolbyTrueHD };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.DolbyTrueHD);
+        }
+
+        [TestMethod]
+        public void TestTraktMediaAudioWriteAndReadJson_Unspecified()
+        {
+            var obj = new TestObject { Value = TraktMediaAudio.Unspecified };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktMediaAudio.Unspecified);
         }
     }
 }
