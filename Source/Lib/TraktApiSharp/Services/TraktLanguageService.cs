c84d51c1a52498813d060959b3f4e3cc189eacb9
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-26 23:40:24 +0200

    Rename language service.

--- Source/Lib/TraktApiSharp/Services/TraktLanguageService.cs
+++ Source/Lib/TraktApiSharp/Services/TraktLanguageService.cs
@@ -4,7 +4,7 @@
     using System.Globalization;
     using System.Linq;
 
-    public static class LanguageService
+    public static class TraktLanguageService
     {
         public static string GetLanguage(string languageCode)
         {
