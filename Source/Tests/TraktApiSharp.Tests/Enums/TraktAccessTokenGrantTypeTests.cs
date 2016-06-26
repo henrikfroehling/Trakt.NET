93b616af6fb208715016869c28ada5a953d70471
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-27 00:24:26 +0200

    Add unit tests for enum access token grant type read and write.

--- Source/Tests/TraktApiSharp.Tests/Enums/TraktAccessTokenGrantTypeTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Enums/TraktAccessTokenGrantTypeTests.cs
@@ -2,11 +2,18 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
     using TraktApiSharp.Enums;
 
     [TestClass]
     public class TraktAccessTokenGrantTypeTests
     {
+        class TestObject
+        {
+            [JsonConverter(typeof(TraktAccessTokenGrantTypeConverter))]
+            public TraktAccessTokenGrantType Value { get; set; }
+        }
+
         [TestMethod]
         public void TestTraktAccessTokenGrantTypeHasMembers()
         {
@@ -20,6 +27,45 @@
             TraktAccessTokenGrantType.AuthorizationCode.AsString().Should().Be("authorization_code");
             TraktAccessTokenGrantType.RefreshToken.AsString().Should().Be("refresh_token");
             TraktAccessTokenGrantType.Unspecified.AsString().Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod]
+        public void TestTraktAccessTokenGrantTypeWriteAndReadJson_AuthorizationCode()
+        {
+            var obj = new TestObject { Value = TraktAccessTokenGrantType.AuthorizationCode };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktAccessTokenGrantType.AuthorizationCode);
+        }
+
+        [TestMethod]
+        public void TestTraktAccessTokenGrantTypeWriteAndReadJson_RefreshToken()
+        {
+            var obj = new TestObject { Value = TraktAccessTokenGrantType.RefreshToken };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktAccessTokenGrantType.RefreshToken);
+        }
+
+        [TestMethod]
+        public void TestTraktAccessTokenGrantTypeWriteAndReadJson_Unspecified()
+        {
+            var obj = new TestObject { Value = TraktAccessTokenGrantType.Unspecified };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktAccessTokenGrantType.Unspecified);
         }
     }
 }
