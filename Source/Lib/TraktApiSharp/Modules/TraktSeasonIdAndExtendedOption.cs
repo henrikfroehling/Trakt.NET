c101f72191108835280e7decabb6a4885acbdef5
Henrik Fröhling <henrik.froehling@gmail.com>
2016-06-29 23:07:13 +0200

    Add implementation for querying multiple different seasons of different shows at once.

---
+++ Source/Lib/TraktApiSharp/Modules/TraktSeasonIdAndExtendedOption.cs
@@ -0,0 +1,13 @@
+﻿namespace TraktApiSharp.Modules
+{
+    using Requests;
+
+    public class TraktSeasonIdAndExtendedOption
+    {
+        public string ShowId { get; set; }
+
+        public int Season { get; set; }
+
+        public TraktExtendedOption ExtendedOption { get; set; }
+    }
+}
