f6a24eb63adca340e6ca44a47b13b631d7b0dc2d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-02 00:48:04 +0200

    Add implementation for querying multiple different user lists.

---
+++ Source/Lib/TraktApiSharp/Modules/TraktUsersListId.cs
@@ -0,0 +1,10 @@
+namespace TraktApiSharp.Modules
+{
+i    public class TrakUserstListId
+    {
+        public string Username { get; set; }
+
+        public string ListId { get; set; }
+    }
+}
+
