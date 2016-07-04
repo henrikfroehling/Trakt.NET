d0e46de7d20d7a896e1014821bdbd83663ce7722
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-04 02:46:35 +0200

    Add filter property to popular movies request.

--- Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Movies/Common/TraktMoviesPopularRequest.cs
+++ Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Movies/Common/TraktMoviesPopularRequest.cs
@@ -1,5 +1,6 @@
 ﻿namespace TraktApiSharp.Requests.WithoutOAuth.Movies.Common
 {
+    using Base;
     using Base.Get;
     using Objects.Basic;
     using Objects.Get.Movies;
@@ -8,9 +9,11 @@
     {
         internal TraktMoviesPopularRequest(TraktClient client) : base(client) { }
 
-        protected override string UriTemplate => "movies/popular{?extended,page,limit}";
+        protected override string UriTemplate => "movies/popular{?extended,page,limit,query,years,genres,languages,countries,runtimes,ratings,certifications}";
 
         protected override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        internet TraktMovieFilter Filter { get; set; }
 
         protected override bool SupportsPagination => true;
 
