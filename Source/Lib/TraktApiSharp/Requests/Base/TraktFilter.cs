1a33fd37c55c7ed973671c9ae68b2f4d957ccd5c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-05 23:38:44 +0200

    Add unit tests for common filter.

--- Source/Lib/TraktApiSharp/Requests/Base/TraktFilter.cs
+++ Source/Lib/TraktApiSharp/Requests/Base/TraktFilter.cs
@@ -1,108 +1,101 @@
 ﻿namespace TraktApiSharp.Requests.Base
 {
+    using System;
     using System.Collections.Generic;
 
     public class TraktFilter
     {
-        public string Query { get; set; }
+        public string Query { get; protected set; }
 
-        public int Years { get; set; }
+        public int Years { get; protected set; }
 
-        public string[] Genres { get; set; }
+        public string[] Genres { get; protected set; }
 
-        public string[] Languages { get; set; }
+        public string[] Languages { get; protected set; }
 
-        public string[] Countries { get; set; }
+        public string[] Countries { get; protected set; }
 
-        public Range<int> Runtimes { get; set; }
+        public Range<int> Runtimes { get; protected set; }
 
-        public Range<int> Ratings { get; set; }
+        public Range<int> Ratings { get; protected set; }
+
+        public TraktFilter WithQuery(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+                throw new ArgumentException("query not valid", nameof(query));
+
+            Query = query;
+            return this;
+        }
+
+        public TraktFilter WithYears(int years)
+        {
+            var length = years.ToString().Length;
+
+            if (years < 0 || length > 4 || length < 4)
+                throw new ArgumentException("years not valid", nameof(years));
+
+            Years = years;
+            return this;
+        }
 
         public TraktFilter AddGenres(string genre, params string[] genres)
         {
-            var genresList = new List<string>();
-
-            genresList.AddRange(this.Genres);
-            genresList.Add(genre);
-            genresList.AddRange(genres);
-
-            this.Genres = genresList.ToArray();
-
-            return this;
+            return AddGenres(true, genre, genres);
         }
 
         public TraktFilter WithGenres(string genre, params string[] genres)
         {
-            this.Genres = new string[genres.Length + 1];
-
-            this.Genres[0] = genre;
-
-            for (int i = 0; i < genres.Length; i++)
-                this.Genres[i + 1] = genres[i];
-
-            return this;
+            return AddGenres(false, genre, genres);
         }
 
         public TraktFilter AddLanguages(string language, params string[] languages)
         {
-            var languagesList = new List<string>();
-
-            languagesList.AddRange(this.Languages);
-            languagesList.Add(language);
-            languagesList.AddRange(languages);
-
-            this.Languages = languagesList.ToArray();
-
-            return this;
+            return AddLanguages(true, language, languages);
         }
 
         public TraktFilter WithLanguages(string language, params string[] languages)
         {
-            this.Languages = new string[languages.Length + 1];
-
-            this.Languages[0] = language;
-
-            for (int i = 0; i < languages.Length; i++)
-                this.Languages[i + 1] = languages[i];
-
-            return this;
+            return AddLanguages(false, language, languages);
         }
 
         public TraktFilter AddCountries(string country, params string[] countries)
         {
-            var countriesList = new List<string>();
-
-            countriesList.AddRange(this.Countries);
-            countriesList.Add(country);
-            countriesList.AddRange(countries);
-
-            this.Countries = countriesList.ToArray();
-
-            return this;
+            return AddCountries(true, country, countries);
         }
 
         public TraktFilter WithCountries(string country, params string[] countries)
         {
-            this.Countries = new string[countries.Length + 1];
-
-            this.Countries[0] = country;
-
-            for (int i = 0; i < countries.Length; i++)
-                this.Countries[i + 1] = countries[i];
-
-            return this;
+            return AddCountries(false, country, countries);
         }
 
         public TraktFilter WithRuntimes(int begin, int end)
         {
+            if (begin < 0 || end < 0 || end < begin)
+                throw new ArgumentException("runtimes not valid");
+
             Runtimes = new Range<int>(begin, end);
             return this;
         }
 
         public TraktFilter WithRatings(int begin, int end)
         {
+            if (begin < 0 || end < 0 || end < begin || end > 100)
+                throw new ArgumentException("ratings not valid");
+
             Ratings = new Range<int>(begin, end);
             return this;
+        }
+
+        public virtual void Clear()
+        {
+            Query = null;
+            Years = 0;
+            Genres = null;
+            Languages = null;
+            Countries = null;
+            Runtimes = null;
+            Ratings = null;
         }
 
         public override string ToString()
@@ -131,6 +124,86 @@
                 parameters.Add($"ratings={Ratings.Begin.ToString()}-{Ratings.End.ToString()}");
 
             return parameters.Count > 0 ? string.Join("&", parameters) : string.Empty;
+        }
+
+        private TraktFilter AddGenres(bool keepExisting, string genre, params string[] genres)
+        {
+            var genresList = new List<string>();
+
+            if (keepExisting && this.Genres != null && this.Genres.Length > 0)
+                genresList.AddRange(this.Genres);
+
+            if (!string.IsNullOrEmpty(genre))
+                genresList.Add(genre);
+
+            if (genres != null && genres.Length > 0)
+                genresList.AddRange(genres);
+
+            this.Genres = genresList.ToArray();
+
+            return this;
+        }
+
+        private TraktFilter AddLanguages(bool keepExisting, string language, params string[] languages)
+        {
+            var languagesList = new List<string>();
+
+            if (keepExisting && this.Languages != null && this.Languages.Length > 0)
+                languagesList.AddRange(this.Languages);
+
+            if (!string.IsNullOrEmpty(language))
+            {
+                if (language.Length > 2 || language.Length < 2)
+                    throw new ArgumentException("language not valid", nameof(language));
+
+                languagesList.Add(language);
+            }
+
+            if (languages != null && languages.Length > 0)
+            {
+                for (int i = 0; i < languages.Length; i++)
+                {
+                    if (languages[i].Length > 2 || languages[i].Length < 2)
+                        throw new ArgumentException("language not valid", nameof(languages));
+                }
+
+                languagesList.AddRange(languages);
+            }
+
+            this.Languages = languagesList.ToArray();
+
+            return this;
+        }
+
+        private TraktFilter AddCountries(bool keepExisting, string country, params string[] countries)
+        {
+            var countriesList = new List<string>();
+
+            if (keepExisting && this.Countries != null && this.Countries.Length > 0)
+                countriesList.AddRange(this.Countries);
+
+            if (!string.IsNullOrEmpty(country))
+            {
+                if (country.Length > 2 || country.Length < 2)
+                    throw new ArgumentException("country not valid", nameof(country));
+
+                countriesList.Add(country);
+            }
+
+            if (countries != null && countries.Length > 0)
+            {
+                for (int i = 0; i < countries.Length; i++)
+                {
+                    if (countries[i].Length > 2 || countries[i].Length < 2)
+                        throw new ArgumentException("country not valid", nameof(countries));
+                }
+
+                countriesList.AddRange(countries);
+            }
+
+            this.Countries = countriesList.ToArray();
+
+            return this;
         }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Requests/Base/TraktFilterTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Requests/Base/TraktFilterTests.cs
@@ -1,6 +1,9 @@
 ﻿namespace TraktApiSharp.Tests.Requests.Base
 {
+    using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using TraktApiSharp.Requests.Base;
 
     [TestClass]
     public class TraktFilterTests
@@ -8,61 +11,389 @@
         [TestMethod]
         public void TestTraktFilterDefaultConstructor()
         {
-            Assert.Fail();
+            var filter = new TraktFilter();
+
+            filter.Query.Should().BeNull();
+            filter.Years.Should().Be(0);
+            filter.Genres.Should().BeNull();
+            filter.Languages.Should().BeNull();
+            filter.Countries.Should().BeNull();
+            filter.Runtimes.Should().BeNull();
+            filter.Ratings.Should().BeNull();
+            filter.ToString().Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod]
+        public void TestTraktFilterWithQuery()
+        {
+            var filter = new TraktFilter();
+
+            Action act = () => filter.WithQuery(null);
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithQuery(string.Empty);
+            act.ShouldThrow<ArgumentException>();
+
+            filter.WithQuery("query");
+            filter.Query.Should().Be("query");
+        }
+
+        [TestMethod]
+        public void TestTraktFilterWithYears()
+        {
+            var filter = new TraktFilter();
+
+            Action act = () => filter.WithYears(-1);
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithYears(1);
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithYears(12);
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithYears(123);
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithYears(12345);
+            act.ShouldThrow<ArgumentException>();
+
+            filter.WithYears(2016);
+            filter.Years.Should().Be(2016);
         }
 
         [TestMethod]
         public void TestTraktFilterAddGenres()
         {
-            Assert.Fail();
+            var filter = new TraktFilter();
+
+            filter.AddGenres(null);
+            filter.Genres.Should().NotBeNull().And.BeEmpty();
+
+            filter.AddGenres(null, "action");
+            filter.Genres.Should().NotBeNull().And.HaveCount(1);
+
+            filter.AddGenres("action", "fantasy", "science-fiction");
+            filter.Genres.Should().NotBeNull().And.HaveCount(4);
+
+            filter.AddGenres("drama");
+            filter.Genres.Should().NotBeNull().And.HaveCount(5);
+
+            filter.AddGenres(null);
+            filter.Genres.Should().NotBeNull().And.HaveCount(5);
+
+            filter.AddGenres("comedy", "mystery");
+            filter.Genres.Should().NotBeNull().And.HaveCount(7);
         }
 
         [TestMethod]
         public void TestTraktFilterWithGenres()
         {
-            Assert.Fail();
+            var filter = new TraktFilter();
+
+            filter.WithGenres(null);
+            filter.Genres.Should().NotBeNull().And.BeEmpty();
+
+            filter.WithGenres(null, "action");
+            filter.Genres.Should().NotBeNull().And.HaveCount(1);
+
+            filter.AddGenres("action", "fantasy");
+            filter.Genres.Should().NotBeNull().And.HaveCount(3);
+
+            filter.WithGenres(null);
+            filter.Genres.Should().NotBeNull().And.BeEmpty();
+
+            filter.WithGenres(null, "action");
+            filter.Genres.Should().NotBeNull().And.HaveCount(1);
+
+            filter.WithGenres("action", "fantasy");
+            filter.Genres.Should().NotBeNull().And.HaveCount(2);
+
+            filter.WithGenres("action", "fantasy", "drama", "comedy");
+            filter.Genres.Should().NotBeNull().And.HaveCount(4);
+
+            filter.WithGenres("mystery");
+            filter.Genres.Should().NotBeNull().And.HaveCount(1);
         }
 
         [TestMethod]
         public void TestTraktFilterAddLanguages()
         {
-            Assert.Fail();
+            var filter = new TraktFilter();
+
+            Action act = () => filter.AddLanguages("deu");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.AddLanguages("d");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.AddLanguages(null, "deu", "en");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.AddLanguages(null, "en", "de", "f");
+            act.ShouldThrow<ArgumentException>();
+
+            filter.AddLanguages(null);
+            filter.Languages.Should().NotBeNull().And.BeEmpty();
+
+            filter.AddLanguages(null, "de");
+            filter.Languages.Should().NotBeNull().And.HaveCount(1);
+
+            filter.AddLanguages("de", "en", "fr");
+            filter.Languages.Should().NotBeNull().And.HaveCount(4);
+
+            filter.AddLanguages("es");
+            filter.Languages.Should().NotBeNull().And.HaveCount(5);
+
+            filter.AddLanguages(null);
+            filter.Languages.Should().NotBeNull().And.HaveCount(5);
+
+            filter.AddLanguages("nl", "us");
+            filter.Languages.Should().NotBeNull().And.HaveCount(7);
         }
 
         [TestMethod]
         public void TestTraktFilterWithLanguages()
         {
-            Assert.Fail();
+            var filter = new TraktFilter();
+
+            Action act = () => filter.WithLanguages("deu");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithLanguages("d");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithLanguages(null, "deu", "en");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithLanguages(null, "en", "de", "f");
+            act.ShouldThrow<ArgumentException>();
+
+            filter.WithLanguages(null);
+            filter.Languages.Should().NotBeNull().And.BeEmpty();
+
+            filter.WithLanguages(null, "de");
+            filter.Languages.Should().NotBeNull().And.HaveCount(1);
+
+            filter.AddLanguages("de", "en");
+            filter.Languages.Should().NotBeNull().And.HaveCount(3);
+
+            filter.WithLanguages(null);
+            filter.Languages.Should().NotBeNull().And.BeEmpty();
+
+            filter.WithLanguages(null, "de");
+            filter.Languages.Should().NotBeNull().And.HaveCount(1);
+
+            filter.WithLanguages("de", "en");
+            filter.Languages.Should().NotBeNull().And.HaveCount(2);
+
+            filter.WithLanguages("de", "en", "es", "fr");
+            filter.Languages.Should().NotBeNull().And.HaveCount(4);
+
+            filter.WithLanguages("us");
+            filter.Languages.Should().NotBeNull().And.HaveCount(1);
         }
 
         [TestMethod]
         public void TestTraktFilterAddCountries()
         {
-            Assert.Fail();
+            var filter = new TraktFilter();
+
+            Action act = () => filter.AddCountries("ger");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.AddCountries("g");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.AddCountries(null, "ger", "gb");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.AddCountries(null, "gb", "ge", "f");
+            act.ShouldThrow<ArgumentException>();
+
+            filter.AddCountries(null);
+            filter.Countries.Should().NotBeNull().And.BeEmpty();
+
+            filter.AddCountries(null, "ge");
+            filter.Countries.Should().NotBeNull().And.HaveCount(1);
+
+            filter.AddCountries("ge", "gb", "fr");
+            filter.Countries.Should().NotBeNull().And.HaveCount(4);
+
+            filter.AddCountries("sp");
+            filter.Countries.Should().NotBeNull().And.HaveCount(5);
+
+            filter.AddCountries(null);
+            filter.Countries.Should().NotBeNull().And.HaveCount(5);
+
+            filter.AddCountries("nl", "us");
+            filter.Countries.Should().NotBeNull().And.HaveCount(7);
         }
 
         [TestMethod]
         public void TestTraktFilterWithCountries()
         {
-            Assert.Fail();
+            var filter = new TraktFilter();
+
+            Action act = () => filter.WithCountries("ger");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithCountries("g");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithCountries(null, "ger", "gb");
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithCountries(null, "gb", "ge", "f");
+            act.ShouldThrow<ArgumentException>();
+
+            filter.WithCountries(null);
+            filter.Countries.Should().NotBeNull().And.BeEmpty();
+
+            filter.WithCountries(null, "ge");
+            filter.Countries.Should().NotBeNull().And.HaveCount(1);
+
+            filter.AddCountries("ge", "gb");
+            filter.Countries.Should().NotBeNull().And.HaveCount(3);
+
+            filter.WithCountries(null);
+            filter.Countries.Should().NotBeNull().And.BeEmpty();
+
+            filter.WithCountries(null, "ge");
+            filter.Countries.Should().NotBeNull().And.HaveCount(1);
+
+            filter.WithCountries("ge", "gb");
+            filter.Countries.Should().NotBeNull().And.HaveCount(2);
+
+            filter.WithCountries("ge", "gb", "sp", "fr");
+            filter.Countries.Should().NotBeNull().And.HaveCount(4);
+
+            filter.WithCountries("us");
+            filter.Countries.Should().NotBeNull().And.HaveCount(1);
         }
 
         [TestMethod]
         public void TestTraktFilterWithRuntimes()
         {
-            Assert.Fail();
+            var filter = new TraktFilter();
+
+            Action act = () => filter.WithRuntimes(-1, 0);
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithRuntimes(0, -1);
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithRuntimes(75, 74);
+            act.ShouldThrow<ArgumentException>();
+
+            filter.WithRuntimes(45, 120);
+            filter.Runtimes.Should().NotBeNull();
+            filter.Runtimes.Begin.Should().Be(45);
+            filter.Runtimes.End.Should().Be(120);
         }
 
         [TestMethod]
         public void TestTraktFilterWithRatings()
         {
-            Assert.Fail();
+            var filter = new TraktFilter();
+
+            Action act = () => filter.WithRatings(-1, 0);
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithRatings(0, -1);
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithRatings(0, 101);
+            act.ShouldThrow<ArgumentException>();
+
+            act = () => filter.WithRatings(75, 74);
+            act.ShouldThrow<ArgumentException>();
+
+            filter.WithRatings(40, 80);
+            filter.Ratings.Should().NotBeNull();
+            filter.Ratings.Begin.Should().Be(40);
+            filter.Ratings.End.Should().Be(80);
+        }
+
+        [TestMethod]
+        public void TestTraktFilterClear()
+        {
+            var filter = new TraktFilter();
+
+            filter.WithQuery("query");
+            filter.Query.Should().Be("query");
+
+            filter.WithYears(2016);
+            filter.Years.Should().Be(2016);
+
+            filter.WithGenres("action", "drama");
+            filter.Genres.Should().NotBeNull().And.HaveCount(2);
+
+            filter.WithLanguages("de", "en");
+            filter.Languages.Should().NotBeNull().And.HaveCount(2);
+
+            filter.WithCountries("gb", "us");
+            filter.Countries.Should().NotBeNull().And.HaveCount(2);
+
+            filter.WithRuntimes(30, 180);
+            filter.Runtimes.Should().NotBeNull();
+            filter.Runtimes.Begin.Should().Be(30);
+            filter.Runtimes.End.Should().Be(180);
+
+            filter.WithRatings(60, 90);
+            filter.Ratings.Should().NotBeNull();
+            filter.Ratings.Begin.Should().Be(60);
+            filter.Ratings.End.Should().Be(90);
+
+            filter.Clear();
+
+            filter.Query.Should().BeNull();
+            filter.Years.Should().Be(0);
+            filter.Genres.Should().BeNull();
+            filter.Languages.Should().BeNull();
+            filter.Countries.Should().BeNull();
+            filter.Runtimes.Should().BeNull();
+            filter.Ratings.Should().BeNull();
+            filter.ToString().Should().NotBeNull().And.BeEmpty();
         }
 
         [TestMethod]
         public void TestTraktFilterToString()
         {
-            Assert.Fail();
+            var filter = new TraktFilter();
+
+            filter.ToString().Should().NotBeNull().And.BeEmpty();
+
+            filter.WithQuery("query");
+            filter.ToString().Should().Be("query=query");
+
+            var year = 2016;
+
+            filter.WithYears(year);
+            filter.ToString().Should().Be($"query=query&years={year}");
+
+            filter.WithGenres("action", "drama", "fantasy");
+            filter.ToString().Should().Be($"query=query&years={year}&genres=action,drama,fantasy");
+
+            filter.WithLanguages("de", "en", "es");
+            filter.ToString().Should().Be($"query=query&years={year}&genres=action,drama,fantasy&languages=de,en,es");
+
+            filter.WithCountries("gb", "us", "fr");
+            filter.ToString().Should().Be($"query=query&years={year}&genres=action,drama,fantasy&languages=de,en,es&countries=gb,us,fr");
+
+            var runtimeBegin = 50;
+            var runtimeEnd = 100;
+
+            filter.WithRuntimes(runtimeBegin, runtimeEnd);
+            filter.ToString().Should().Be($"query=query&years={year}&genres=action,drama,fantasy&languages=de,en,es&countries=gb,us,fr" +
+                                          $"&runtimes={runtimeBegin}-{runtimeEnd}");
+
+            var ratingBegin = 70;
+            var ratingEnd = 90;
+
+            filter.WithRatings(ratingBegin, ratingEnd);
+            filter.ToString().Should().Be($"query=query&years={year}&genres=action,drama,fantasy&languages=de,en,es&countries=gb,us,fr" +
+                                          $"&runtimes={runtimeBegin}-{runtimeEnd}&ratings={ratingBegin}-{ratingEnd}");
         }
     }
 }
