c52221e9ce98606bcf4cd9314d2ffce2307b92b2
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-11 17:37:20 +0200

    Setup example project.

---
+++ Examples/TraktApiSharp.Example.UWP/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using TraktApiSharp.Example.UWP.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace TraktApiSharp.Example.UWP.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ Examples/TraktApiSharp.Example.UWP/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using TraktApiSharp.Example.UWP.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace TraktApiSharp.Example.UWP.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
