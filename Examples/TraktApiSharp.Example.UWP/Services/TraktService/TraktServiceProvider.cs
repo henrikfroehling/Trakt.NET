9a06e334eb0bf05d1bd16f2e74762995f0fe390d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-11 17:58:15 +0200

    Add trakt client provider.

---
+++ Examples/TraktApiSharp.Example.UWP/Services/TraktService/TraktServiceProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Example.UWP.Services.TraktService
+{
+    public class TraktServiceProvider
+    {
+        private static readonly string CLIENT_ID = "ENTER YOUR TRAKT CLIENT ID HERE!!!";
+
+        public static TraktServiceProvider Instance { get; } = new TraktServiceProvider();
+
+        public TraktClient Client { get; } = new TraktClient(CLIENT_ID);
+    }
+}
