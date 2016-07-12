3a1e0c56f0cef9b2bfaf7d6f5fe129dae63c1278
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-13 01:21:49 +0200

    Setup viewmodels for movies- and shows-page.

--- Examples/TraktApiSharp.Example.UWP/ViewModels/MoviesPageViewModel.cs
+++ Examples/TraktApiSharp.Example.UWP/ViewModels/MoviesPageViewModel.cs
@@ -4,6 +4,22 @@
 
     public class MoviesPageViewModel : ViewModelBase
     {
+        public TrendingMoviesPartViewModel TrendingMoviesViewModel { get; } = new TrendingMoviesPartViewModel();
+
+        public PopularMoviesPartViewModel PopularMoviesViewModel { get; } = new PopularMoviesPartViewModel();
+
+        public MostPlayedMoviesPartViewModel MostPlayedMoviesViewModel { get; } = new MostPlayedMoviesPartViewModel();
+
+        public MostWatchedMoviesPartViewModel MostWatchedMoviesViewModel { get; } = new MostWatchedMoviesPartViewModel();
+
+        public MostCollectedMoviesPartViewModel MostCollectedMoviesViewModel { get; } = new MostCollectedMoviesPartViewModel();
+
+        public MostAnticipatedMoviesPartViewModel MostAnticipatedMoviesViewModel { get; } = new MostAnticipatedMoviesPartViewModel();
+
+        public BoxOfficeMoviesPartViewModel BoxOfficeMoviesViewModel { get; } = new BoxOfficeMoviesPartViewModel();
+
+        public RecentlyUpdatedMoviesPartViewModel RecentlyUpdatedMoviesViewModel { get; } = new RecentlyUpdatedMoviesPartViewModel();
+
         public void GotoSettings() =>
             NavigationService.Navigate(typeof(Views.SettingsPage), 0);
 
@@ -12,5 +28,45 @@
 
         public void GotoAbout() =>
             NavigationService.Navigate(typeof(Views.SettingsPage), 2);
+    }
+
+    public class TrendingMoviesPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class PopularMoviesPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class MostPlayedMoviesPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class MostWatchedMoviesPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class MostCollectedMoviesPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class MostAnticipatedMoviesPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class BoxOfficeMoviesPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class RecentlyUpdatedMoviesPartViewModel : ViewModelBase
+    {
+
     }
 }
--- Examples/TraktApiSharp.Example.UWP/ViewModels/ShowsPageViewModel.cs
+++ Examples/TraktApiSharp.Example.UWP/ViewModels/ShowsPageViewModel.cs
@@ -4,6 +4,20 @@
 
     public class ShowsPageViewModel : ViewModelBase
     {
+        public TrendingShowsPartViewModel TrendingShowsViewModel { get; } = new TrendingShowsPartViewModel();
+
+        public PopularShowsPartViewModel PopularShowsViewModel { get; } = new PopularShowsPartViewModel();
+
+        public MostPlayedShowsPartViewModel MostPlayedShowsViewModel { get; } = new MostPlayedShowsPartViewModel();
+
+        public MostWatchedShowsPartViewModel MostWatchedShowsViewModel { get; } = new MostWatchedShowsPartViewModel();
+
+        public MostCollectedShowsPartViewModel MostCollectedShowsViewModel { get; } = new MostCollectedShowsPartViewModel();
+
+        public MostAnticipatedShowsPartViewModel MostAnticipatedShowsViewModel { get; } = new MostAnticipatedShowsPartViewModel();
+
+        public RecentlyUpdatedShowsPartViewModel RecentlyUpdatedShowsViewModel { get; } = new RecentlyUpdatedShowsPartViewModel();
+
         public void GotoSettings() =>
             NavigationService.Navigate(typeof(Views.SettingsPage), 0);
 
@@ -12,5 +26,40 @@
 
         public void GotoAbout() =>
             NavigationService.Navigate(typeof(Views.SettingsPage), 2);
+    }
+
+    public class TrendingShowsPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class PopularShowsPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class MostPlayedShowsPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class MostWatchedShowsPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class MostCollectedShowsPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class MostAnticipatedShowsPartViewModel : ViewModelBase
+    {
+
+    }
+
+    public class RecentlyUpdatedShowsPartViewModel : ViewModelBase
+    {
+
     }
 }
