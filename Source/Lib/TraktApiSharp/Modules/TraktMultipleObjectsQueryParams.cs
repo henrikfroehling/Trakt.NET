f3b121aa6e90ede497190d5d40e2b27146f731a9
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-13 20:01:12 +0200

    Improve query parameters for multiple movies request.

---
+++ Source/Lib/TraktApiSharp/Modules/TraktMultipleObjectsQueryParams.cs
@@ -0,0 +1,26 @@
+﻿namespace TraktApiSharp.Modules
+{
+    using Requests.Params;
+    using Utils;
+
+    public class TraktMultipleObjectsQueryParams : TraktMultipleQueryParams<TraktObjectsQueryParams>
+    {
+        public void Add(string id, TraktExtendedOption extended = null)
+        {
+            Add(new TraktObjectsQueryParams(id, extended));
+        }
+    }
+
+    public struct TraktObjectsQueryParams
+    {
+        public TraktObjectsQueryParams(string id, TraktExtendedOption extended)
+        {
+            Id = id;
+            ExtendedOption = extended;
+        }
+
+        public string Id { get; }
+
+        public TraktExtendedOption ExtendedOption { get; }
+    }
+}
