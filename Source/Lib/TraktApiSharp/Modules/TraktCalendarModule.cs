d5c408e927290125a373524b99b7c4c2fe0d5d1e
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-22 00:09:31 +0200

    Add documentation for calendars module.

--- Source/Lib/TraktApiSharp/Modules/TraktCalendarModule.cs
+++ Source/Lib/TraktApiSharp/Modules/TraktCalendarModule.cs
@@ -8,12 +8,35 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Provides access to data retrieving methods specific to calendars.
+    /// <para>
+    /// This module contains all methods of the <a href ="http://docs.trakt.apiary.io/#reference/calendars">"Trakt API Doc - Calendars"</a> section.
+    /// </para>
+    /// </summary>
     public class TraktCalendarModule : TraktBaseModule
     {
         internal TraktCalendarModule(TraktClient client) : base(client) { }
 
+        /// <summary>
+        /// Gets all users <see cref="TraktCalendarShow" />s airing during the given time period.
+        /// <para>OAuth authorization required.</para>
+        /// <para>
+        /// See <a href="http://docs.trakt.apiary.io/#reference/calendars/my-shows/get-shows">"Trakt API Doc - Calendars: My Shows"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="startDate">The date, on which the time period should start. Defaults to today. Will be converted to the Trakt date-format.</param>
+        /// <param name="days">1 - 31 days, specifying the length of the time period. Defaults to 7 days.</param>
+        /// <param name="extendedOption">
+        /// The extended option, which determines how much data about the shows should be queried.
+        /// See also <seealso cref="TraktExtendedOption" />.
+        /// </param>
+        /// <param name="filter">Optional filters for genres, languages, year, runtimes, ratings, etc. See also <seealso cref="TraktCalendarFilter" />.</param>
+        /// <returns>A list of <see cref="TraktCalendarShow" /> instances.</returns>
+        /// <exception cref="Exceptions.TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown, if the given days value is not between 1 and 31.</exception>
         public async Task<IEnumerable<TraktCalendarShow>> GetUserShowsAsync(DateTime? startDate = null, int? days = null,
-                                                                            TraktExtendedOption extended = null,
+                                                                            TraktExtendedOption extendedOption = null,
                                                                             TraktCalendarFilter filter = null)
         {
             ValidateDays(days);
@@ -22,13 +45,30 @@
             {
                 StartDate = startDate,
                 Days = days,
-                ExtendedOption = extended,
+                ExtendedOption = extendedOption,
                 Filter = filter
             });
         }
 
+        /// <summary>
+        /// Gets all new users <see cref="TraktCalendarShow" />s airing during the given time period.
+        /// <para>OAuth authorization required.</para>
+        /// <para>
+        /// See <a href="http://docs.trakt.apiary.io/#reference/calendars/my-new-shows/get-new-shows">"Trakt API Doc - Calendars: My New Shows"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="startDate">The date, on which the time period should start. Defaults to today. Will be converted to the Trakt date-format.</param>
+        /// <param name="days">1 - 31 days, specifying the length of the time period. Defaults to 7 days.</param>
+        /// <param name="extendedOption">
+        /// The extended option, which determines how much data about the shows should be queried.
+        /// See also <seealso cref="TraktExtendedOption" />.
+        /// </param>
+        /// <param name="filter">Optional filters for genres, languages, year, runtimes, ratings, etc. See also <seealso cref="TraktCalendarFilter" />.</param>
+        /// <returns>A list of <see cref="TraktCalendarShow" /> instances.</returns>
+        /// <exception cref="Exceptions.TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown, if the given days value is not between 1 and 31.</exception>
         public async Task<IEnumerable<TraktCalendarShow>> GetUserNewShowsAsync(DateTime? startDate = null, int? days = null,
-                                                                               TraktExtendedOption extended = null,
+                                                                               TraktExtendedOption extendedOption = null,
                                                                                TraktCalendarFilter filter = null)
         {
             ValidateDays(days);
@@ -37,13 +77,30 @@
             {
                 StartDate = startDate,
                 Days = days,
-                ExtendedOption = extended,
+                ExtendedOption = extendedOption,
                 Filter = filter
             });
         }
 
+        /// <summary>
+        /// Gets all users season premieres airing during the given time period.
+        /// <para>OAuth authorization required.</para>
+        /// <para>
+        /// See <a href="http://docs.trakt.apiary.io/#reference/calendars/my-season-premieres/get-season-premieres">"Trakt API Doc - Calendars: My Season Premieres"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="startDate">The date, on which the time period should start. Defaults to today. Will be converted to the Trakt date-format.</param>
+        /// <param name="days">1 - 31 days, specifying the length of the time period. Defaults to 7 days.</param>
+        /// <param name="extendedOption">
+        /// The extended option, which determines how much data about the shows should be queried.
+        /// See also <seealso cref="TraktExtendedOption" />.
+        /// </param>
+        /// <param name="filter">Optional filters for genres, languages, year, runtimes, ratings, etc. See also <seealso cref="TraktCalendarFilter" />.</param>
+        /// <returns>A list of <see cref="TraktCalendarShow" /> instances.</returns>
+        /// <exception cref="Exceptions.TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown, if the given days value is not between 1 and 31.</exception>
         public async Task<IEnumerable<TraktCalendarShow>> GetUserSeasonPremieresAsync(DateTime? startDate = null, int? days = null,
-                                                                                      TraktExtendedOption extended = null,
+                                                                                      TraktExtendedOption extendedOption = null,
                                                                                       TraktCalendarFilter filter = null)
         {
             ValidateDays(days);
@@ -52,13 +109,30 @@
             {
                 StartDate = startDate,
                 Days = days,
-                ExtendedOption = extended,
+                ExtendedOption = extendedOption,
                 Filter = filter
             });
         }
 
+        /// <summary>
+        /// Gets all users <see cref="TraktCalendarMovie" />s airing during the given time period.
+        /// <para>OAuth authorization required.</para>
+        /// <para>
+        /// See <a href="http://docs.trakt.apiary.io/#reference/calendars/my-movies/get-movies">"Trakt API Doc - Calendars: My Movies"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="startDate">The date, on which the time period should start. Defaults to today. Will be converted to the Trakt date-format.</param>
+        /// <param name="days">1 - 31 days, specifying the length of the time period. Defaults to 7 days.</param>
+        /// <param name="extendedOption">
+        /// The extended option, which determines how much data about the movies should be queried.
+        /// See also <seealso cref="TraktExtendedOption" />.
+        /// </param>
+        /// <param name="filter">Optional filters for genres, languages, year, runtimes, ratings, etc. See also <seealso cref="TraktCalendarFilter" />.</param>
+        /// <returns>A list of <see cref="TraktCalendarMovie" /> instances.</returns>
+        /// <exception cref="Exceptions.TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown, if the given days value is not between 1 and 31.</exception>
         public async Task<IEnumerable<TraktCalendarMovie>> GetUserMoviesAsync(DateTime? startDate = null, int? days = null,
-                                                                              TraktExtendedOption extended = null,
+                                                                              TraktExtendedOption extendedOption = null,
                                                                               TraktCalendarFilter filter = null)
         {
             ValidateDays(days);
@@ -67,13 +141,30 @@
             {
                 StartDate = startDate,
                 Days = days,
-                ExtendedOption = extended,
+                ExtendedOption = extendedOption,
                 Filter = filter
             });
         }
 
+        /// <summary>
+        /// Gets all <see cref="TraktCalendarShow" />s airing during the given time period.
+        /// <para>OAuth authorization NOT required.</para>
+        /// <para>
+        /// See <a href="http://docs.trakt.apiary.io/#reference/calendars/all-shows/get-shows">"Trakt API Doc - Calendars: All Shows"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="startDate">The date, on which the time period should start. Defaults to today. Will be converted to the Trakt date-format.</param>
+        /// <param name="days">1 - 31 days, specifying the length of the time period. Defaults to 7 days.</param>
+        /// <param name="extendedOption">
+        /// The extended option, which determines how much data about the shows should be queried.
+        /// See also <seealso cref="TraktExtendedOption" />.
+        /// </param>
+        /// <param name="filter">Optional filters for genres, languages, year, runtimes, ratings, etc. See also <seealso cref="TraktCalendarFilter" />.</param>
+        /// <returns>A list of <see cref="TraktCalendarShow" /> instances.</returns>
+        /// <exception cref="Exceptions.TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown, if the given days value is not between 1 and 31.</exception>
         public async Task<IEnumerable<TraktCalendarShow>> GetAllShowsAsync(DateTime? startDate = null, int? days = null,
-                                                                           TraktExtendedOption extended = null,
+                                                                           TraktExtendedOption extendedOption = null,
                                                                            TraktCalendarFilter filter = null)
         {
             ValidateDays(days);
@@ -82,13 +173,30 @@
             {
                 StartDate = startDate,
                 Days = days,
-                ExtendedOption = extended,
+                ExtendedOption = extendedOption,
                 Filter = filter
             });
         }
 
+        /// <summary>
+        /// Gets all new <see cref="TraktCalendarShow" />s airing during the given time period.
+        /// <para>OAuth authorization NOT required.</para>
+        /// <para>
+        /// See <a href="http://docs.trakt.apiary.io/#reference/calendars/all-new-shows/get-new-shows">"Trakt API Doc - Calendars: All New Shows"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="startDate">The date, on which the time period should start. Defaults to today. Will be converted to the Trakt date-format.</param>
+        /// <param name="days">1 - 31 days, specifying the length of the time period. Defaults to 7 days.</param>
+        /// <param name="extendedOption">
+        /// The extended option, which determines how much data about the shows should be queried.
+        /// See also <seealso cref="TraktExtendedOption" />.
+        /// </param>
+        /// <param name="filter">Optional filters for genres, languages, year, runtimes, ratings, etc. See also <seealso cref="TraktCalendarFilter" />.</param>
+        /// <returns>A list of <see cref="TraktCalendarShow" /> instances.</returns>
+        /// <exception cref="Exceptions.TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown, if the given days value is not between 1 and 31.</exception>
         public async Task<IEnumerable<TraktCalendarShow>> GetAllNewShowsAsync(DateTime? startDate = null, int? days = null,
-                                                                              TraktExtendedOption extended = null,
+                                                                              TraktExtendedOption extendedOption = null,
                                                                               TraktCalendarFilter filter = null)
         {
             ValidateDays(days);
@@ -97,13 +205,30 @@
             {
                 StartDate = startDate,
                 Days = days,
-                ExtendedOption = extended,
+                ExtendedOption = extendedOption,
                 Filter = filter
             });
         }
 
+        /// <summary>
+        /// Gets all season premieres airing during the given time period.
+        /// <para>OAuth authorization NOT required.</para>
+        /// <para>
+        /// See <a href="http://docs.trakt.apiary.io/#reference/calendars/all-season-premieres/get-season-premieres">"Trakt API Doc - Calendars: All Season Premieres"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="startDate">The date, on which the time period should start. Defaults to today. Will be converted to the Trakt date-format.</param>
+        /// <param name="days">1 - 31 days, specifying the length of the time period. Defaults to 7 days.</param>
+        /// <param name="extendedOption">
+        /// The extended option, which determines how much data about the shows should be queried.
+        /// See also <seealso cref="TraktExtendedOption" />.
+        /// </param>
+        /// <param name="filter">Optional filters for genres, languages, year, runtimes, ratings, etc. See also <seealso cref="TraktCalendarFilter" />.</param>
+        /// <returns>A list of <see cref="TraktCalendarShow" /> instances.</returns>
+        /// <exception cref="Exceptions.TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown, if the given days value is not between 1 and 31.</exception>
         public async Task<IEnumerable<TraktCalendarShow>> GetAllSeasonPremieresAsync(DateTime? startDate = null, int? days = null,
-                                                                                     TraktExtendedOption extended = null,
+                                                                                     TraktExtendedOption extendedOption = null,
                                                                                      TraktCalendarFilter filter = null)
         {
             ValidateDays(days);
@@ -112,13 +237,30 @@
             {
                 StartDate = startDate,
                 Days = days,
-                ExtendedOption = extended,
+                ExtendedOption = extendedOption,
                 Filter = filter
             });
         }
 
+        /// <summary>
+        /// Gets all <see cref="TraktCalendarMovie" />s airing during the given time period.
+        /// <para>OAuth authorization NOT required.</para>
+        /// <para>
+        /// See <a href="http://docs.trakt.apiary.io/#reference/calendars/my-movies/get-movies">"Trakt API Doc - Calendars: My Movies"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="startDate">The date, on which the time period should start. Defaults to today. Will be converted to the Trakt date-format.</param>
+        /// <param name="days">1 - 31 days, specifying the length of the time period. Defaults to 7 days.</param>
+        /// <param name="extendedOption">
+        /// The extended option, which determines how much data about the movies should be queried.
+        /// See also <seealso cref="TraktExtendedOption" />.
+        /// </param>
+        /// <param name="filter">Optional filters for genres, languages, year, runtimes, ratings, etc. See also <seealso cref="TraktCalendarFilter" />.</param>
+        /// <returns>A list of <see cref="TraktCalendarMovie" /> instances.</returns>
+        /// <exception cref="Exceptions.TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown, if the given days value is not between 1 and 31.</exception>
         public async Task<IEnumerable<TraktCalendarMovie>> GetAllMoviesAsync(DateTime? startDate = null, int? days = null,
-                                                                             TraktExtendedOption extended = null,
+                                                                             TraktExtendedOption extendedOption = null,
                                                                              TraktCalendarFilter filter = null)
         {
             ValidateDays(days);
@@ -127,7 +269,7 @@
             {
                 StartDate = startDate,
                 Days = days,
-                ExtendedOption = extended,
+                ExtendedOption = extendedOption,
                 Filter = filter
             });
         }
