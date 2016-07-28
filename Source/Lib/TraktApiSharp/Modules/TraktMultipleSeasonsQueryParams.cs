69722ed73ab47f3d8c05dde8a665d9c431610b71
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-28 02:20:26 +0200

    Add documentation for multiple query params.

--- Source/Lib/TraktApiSharp/Modules/TraktMultipleSeasonsQueryParams.cs
+++ Source/Lib/TraktApiSharp/Modules/TraktMultipleSeasonsQueryParams.cs
@@ -2,6 +2,21 @@
 {
     using Requests.Params;
 
+    /// <summary>
+    /// Collection containing multiple different combinations of show ids, season-numbers and extended options.
+    /// </summary>
+    /// <example>
+    /// This example shows an instantiation of this class.
+    /// <code>
+    /// new TraktMultipleSeasonsQueryParams
+    /// {
+    ///     // { show-id, seasonnumber[, extended option] }
+    ///     { "show-id-1", 1 },
+    ///     { "show-id-2", 3, new TraktExtendedOption { Full = true } },
+    ///     { "show-id-3", 2 }
+    /// };
+    /// </code>
+    /// </example>
     public class TraktMultipleSeasonsQueryParams : TraktMultipleQueryParams<TraktSeasonsQueryParams>
     {
         public void Add(string showId, int season, TraktExtendedOption extended = null)
