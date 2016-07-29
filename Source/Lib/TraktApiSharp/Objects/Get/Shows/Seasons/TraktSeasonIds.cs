35ae4e07c8dc9821e14b9fe278f99967a3552569
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-29 04:46:33 +0200

    Add get best id method for season ids.

--- Source/Lib/TraktApiSharp/Objects/Get/Shows/Seasons/TraktSeasonIds.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/Shows/Seasons/TraktSeasonIds.cs
@@ -24,5 +24,24 @@
         /// <summary>Returns, whether any id has been set.</summary>
         [JsonIgnore]
         public bool HasAnyId => Trakt > 0 || Tvdb > 0 || Tvdb > 0 || TvRage > 0;
+
+        /// <summary>Gets the most reliable id from those that have been set.</summary>
+        /// <returns>The id as a string or an empty string, if no id is set.</returns>
+        public string GetBestId()
+        {
+            if (Trakt > 0)
+                return Trakt.ToString();
+
+            if (Tvdb.HasValue && Tvdb.Value > 0)
+                return Tvdb.Value.ToString();
+
+            if (Tmdb.HasValue && Tmdb.Value > 0)
+                return Tmdb.Value.ToString();
+
+            if (TvRage.HasValue && TvRage.Value > 0)
+                return TvRage.Value.ToString();
+
+            return string.Empty;
+        }
     }
 }
