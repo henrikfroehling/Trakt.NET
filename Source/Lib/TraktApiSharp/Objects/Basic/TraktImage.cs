f42d7ce594d842a42b85a39426ebf2ecd13d57e6
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-29 04:14:47 +0200

    Update existing documentation for basic objects.

--- Source/Lib/TraktApiSharp/Objects/Basic/TraktImage.cs
+++ Source/Lib/TraktApiSharp/Objects/Basic/TraktImage.cs
@@ -2,14 +2,10 @@
 {
     using Newtonsoft.Json;
 
-    /// <summary>
-    /// An image for an item available on only one size.
-    /// </summary>
+    /// <summary>An image for an item available in only one size.</summary>
     public class TraktImage
     {
-        /// <summary>
-        /// The address to the full size image.
-        /// </summary>
+        /// <summary>The address to the full size image.</summary>
         [JsonProperty(PropertyName = "full")]
         public string Full { get; set; }
     }
