5f0c0594c6b566eeb210a3716cfdef6f81340715
Henrik Fröhling <henrik.froehling@gmail.com>
2016-07-29 04:30:24 +0200

    Update existing documentation for show images.

--- Source/Lib/TraktApiSharp/Objects/Get/Shows/TraktShowImages.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/Shows/TraktShowImages.cs
@@ -3,44 +3,30 @@
     using Basic;
     using Newtonsoft.Json;
 
-    /// <summary>
-    /// A collection of images for a Trakt show.
-    /// </summary>
+    /// <summary>A collection of images and image sets for a Trakt show.</summary>
     public class TraktShowImages
     {
-        /// <summary>
-        /// A fanart image set for various sizes.
-        /// </summary>
+        /// <summary>Gets or sets the fan art image set.</summary>
         [JsonProperty(PropertyName = "fanart")]
         public TraktImageSet FanArt { get; set; }
 
-        /// <summary>
-        /// A poster image set for various sizes.
-        /// </summary>
+        /// <summary>Gets or sets the poster image set.</summary>
         [JsonProperty(PropertyName = "poster")]
         public TraktImageSet Poster { get; set; }
 
-        /// <summary>
-        /// A logo image.
-        /// </summary>
+        /// <summary>Gets or sets the loge image.</summary>
         [JsonProperty(PropertyName = "logo")]
         public TraktImage Logo { get; set; }
 
-        /// <summary>
-        /// A clearart image.
-        /// </summary>
+        /// <summary>Gets or sets the clear art image.</summary>
         [JsonProperty(PropertyName = "clearart")]
         public TraktImage ClearArt { get; set; }
 
-        /// <summary>
-        /// A banner image.
-        /// </summary>
+        /// <summary>Gets or sets the banner image.</summary>
         [JsonProperty(PropertyName = "banner")]
         public TraktImage Banner { get; set; }
 
-        /// <summary>
-        /// A thumbnail image.
-        /// </summary>
+        /// <summary>Gets or sets the thumb image.</summary>
         [JsonProperty(PropertyName = "thumb")]
         public TraktImage Thumb { get; set; }
     }
