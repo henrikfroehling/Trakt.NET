ef20ce571a895560f667bec8e9d7f75ced390cdb
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-04 22:10:56 +0200

    Implement not null attribute.

---
+++ Source/Lib/TraktApiSharp/Attributes/NotNullAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace TraktApiSharp.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Delegate | AttributeTargets.Event
+                    | AttributeTargets.Field | AttributeTargets.Parameter
+                    | AttributeTargets.Property | AttributeTargets.ReturnValue)]
+    public sealed class NotNullAttribute : Attribute
+    {
+        public NotNullAttribute() : this("element must not be null") { }
+
+        public NotNullAttribute(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+    }
+}
