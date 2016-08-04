52bd04fdb36c51d4e2fe4ec733f13ce523bfd438
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-04 22:23:14 +0200

    Implement oauth authorization optional attribute.

---
+++ Source/Lib/TraktApiSharp/Attributes/OAuthAuthorizationOptionalAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class OAuthAuthorizationOptionalAttribute : Attribute
+    {
+        public OAuthAuthorizationOptionalAttribute() : this("OAuth authorization is optional") { }
+
+        public OAuthAuthorizationOptionalAttribute(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+    }
+}
