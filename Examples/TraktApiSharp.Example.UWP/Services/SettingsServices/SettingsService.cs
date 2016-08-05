484d3625fef1bfb982d98c625f4e6d3d6939332b
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-05 06:24:08 +0200

    Add setting properties for trakt configurations.

--- Examples/TraktApiSharp.Example.UWP/Services/SettingsServices/SettingsService.cs
+++ Examples/TraktApiSharp.Example.UWP/Services/SettingsServices/SettingsService.cs
@@ -8,7 +8,9 @@ namespace TraktApiSharp.Example.UWP.Services.SettingsServices
     public class SettingsService
     {
         public static SettingsService Instance { get; } = new SettingsService();
-        Template10.Services.SettingsService.ISettingsHelper _helper;
+
+        private Template10.Services.SettingsService.ISettingsHelper _helper;
+
         private SettingsService()
         {
             _helper = new Template10.Services.SettingsService.SettingsHelper();
@@ -17,9 +19,11 @@ namespace TraktApiSharp.Example.UWP.Services.SettingsServices
         public bool UseShellBackButton
         {
             get { return _helper.Read<bool>(nameof(UseShellBackButton), true); }
+
             set
             {
                 _helper.Write(nameof(UseShellBackButton), value);
+
                 BootStrapper.Current.NavigationService.Dispatcher.Dispatch(() =>
                 {
                     BootStrapper.Current.ShowShellBackButton = value;
@@ -29,14 +33,46 @@ namespace TraktApiSharp.Example.UWP.Services.SettingsServices
             }
         }
 
+        public string TraktClientId
+        {
+            get { return _helper.Read<string>(nameof(TraktClientId), string.Empty); }
+            set { _helper.Write(nameof(TraktClientId), value); }
+        }
+
+        public string TraktClientSecret
+        {
+            get { return _helper.Read<string>(nameof(TraktClientSecret), string.Empty); }
+            set { _helper.Write(nameof(TraktClientSecret), value); }
+        }
+
+        public string TraktClientAccessToken
+        {
+            get { return _helper.Read<string>(nameof(TraktClientAccessToken), string.Empty); }
+            set { _helper.Write(nameof(TraktClientAccessToken), value); }
+        }
+
+        public bool TraktUseStagingUrl
+        {
+            get { return _helper.Read<bool>(nameof(TraktUseStagingUrl), false); }
+            set { _helper.Write(nameof(TraktUseStagingUrl), value); }
+        }
+
+        public bool TraktForceAuthorization
+        {
+            get { return _helper.Read<bool>(nameof(TraktForceAuthorization), false); }
+            set { _helper.Write(nameof(TraktForceAuthorization), value); }
+        }
+
         public ApplicationTheme AppTheme
         {
             get
             {
                 var theme = ApplicationTheme.Light;
                 var value = _helper.Read<string>(nameof(AppTheme), theme.ToString());
+
                 return Enum.TryParse<ApplicationTheme>(value, out theme) ? theme : ApplicationTheme.Dark;
             }
+
             set
             {
                 _helper.Write(nameof(AppTheme), value.ToString());
@@ -48,6 +84,7 @@ namespace TraktApiSharp.Example.UWP.Services.SettingsServices
         public TimeSpan CacheMaxDuration
         {
             get { return _helper.Read<TimeSpan>(nameof(CacheMaxDuration), TimeSpan.FromDays(2)); }
+
             set
             {
                 _helper.Write(nameof(CacheMaxDuration), value);
@@ -56,4 +93,3 @@ namespace TraktApiSharp.Example.UWP.Services.SettingsServices
         }
     }
 }
-
