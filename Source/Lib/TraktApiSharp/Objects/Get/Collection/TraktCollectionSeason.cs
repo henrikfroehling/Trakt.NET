0c21cc2de53ae284736055f8197975167466fc58
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-10 18:12:02 +0200

    Add source documentation for collection objects.

--- Source/Lib/TraktApiSharp/Objects/Get/Collection/TraktCollectionSeason.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/Collection/TraktCollectionSeason.cs
@@ -4,11 +4,18 @@
     using Newtonsoft.Json;
     using System.Collections.Generic;
 
+    /// <summary>Contains information about a collected Trakt season.</summary>
     public class TraktCollectionSeason
     {
+        /// <summary>Gets or sets the number of the collected season.</summary>
         [JsonProperty(PropertyName = "number")]
         public int? Number { get; set; }
 
+        /// <summary>
+        /// Gets or sets a list of collected episodes in the collected season.
+        /// See also <seealso cref="TraktCollectionEpisode" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "episodes")]
         [Nullable]
         public IEnumerable<TraktCollectionEpisode> Episodes { get; set; }
