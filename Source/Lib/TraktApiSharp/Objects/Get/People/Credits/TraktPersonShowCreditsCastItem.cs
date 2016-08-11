1d6c19f7c2394fe9fb29056e304e5f9751a87cd5
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-12 01:44:13 +0200

    Add source documentation for person show credtis.

--- Source/Lib/TraktApiSharp/Objects/Get/People/Credits/TraktPersonShowCreditsCastItem.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/People/Credits/TraktPersonShowCreditsCastItem.cs
@@ -4,12 +4,18 @@
     using Newtonsoft.Json;
     using Shows;
 
+    /// <summary>Contains information about a Trakt person's cast position.</summary>
     public class TraktPersonShowCreditsCastItem
     {
+        /// <summary>Gets or sets the character name of the cast position.<para>Nullable</para></summary>
         [JsonProperty(PropertyName = "character")]
         [Nullable]
         public string Character { get; set; }
 
+        /// <summary>
+        /// Gets or sets the show of the cast position. See also <seealso cref="TraktShow" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "show")]
         [Nullable]
         public TraktShow Show { get; set; }
--- Source/Lib/TraktApiSharp/Objects/Get/People/Credits/TraktPersonShowCreditsCrewItem.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/People/Credits/TraktPersonShowCreditsCrewItem.cs
@@ -4,12 +4,18 @@
     using Newtonsoft.Json;
     using Shows;
 
+    /// <summary>Contains information about a Trakt person's crew position.</summary>
     public class TraktPersonShowCreditsCrewItem
     {
+        /// <summary>Gets or sets the job name of the crew position.<para>Nullable</para></summary>
         [JsonProperty(PropertyName = "job")]
         [Nullable]
         public string Job { get; set; }
 
+        /// <summary>
+        /// Gets or sets the show of the crew position. See also <seealso cref="TraktShow" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "show")]
         [Nullable]
         public TraktShow Show { get; set; }
