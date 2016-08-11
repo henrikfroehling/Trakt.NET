74330e0333c7a57c590d4d7ae14258867e12fc04
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-12 01:35:44 +0200

    Add source documentation for person movie credtis.

--- Source/Lib/TraktApiSharp/Objects/Get/People/Credits/TraktPersonMovieCreditsCastItem.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/People/Credits/TraktPersonMovieCreditsCastItem.cs
@@ -4,12 +4,18 @@
     using Movies;
     using Newtonsoft.Json;
 
+    /// <summary>Contains information about a Trakt person's cast position.</summary>
     public class TraktPersonMovieCreditsCastItem
     {
+        /// <summary>Gets or sets the character name of the cast position.<para>Nullable</para></summary>
         [JsonProperty(PropertyName = "character")]
         [Nullable]
         public string Character { get; set; }
 
+        /// <summary>
+        /// Gets or sets the movie of the cast position. See also <seealso cref="TraktMovie" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "movie")]
         [Nullable]
         public TraktMovie Movie { get; set; }
--- Source/Lib/TraktApiSharp/Objects/Get/People/Credits/TraktPersonMovieCreditsCrewItem.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/People/Credits/TraktPersonMovieCreditsCrewItem.cs
@@ -4,12 +4,18 @@
     using Movies;
     using Newtonsoft.Json;
 
+    /// <summary>Contains information about a Trakt person's crew position.</summary>
     public class TraktPersonMovieCreditsCrewItem
     {
+        /// <summary>Gets or sets the job name of the crew position.<para>Nullable</para></summary>
         [JsonProperty(PropertyName = "job")]
         [Nullable]
         public string Job { get; set; }
 
+        /// <summary>
+        /// Gets or sets the movie of the crew position. See also <seealso cref="TraktMovie" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "movie")]
         [Nullable]
         public TraktMovie Movie { get; set; }
