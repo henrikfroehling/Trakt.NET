ad684ec651cf819802cfa3f30d89a95bd87751cf
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-13 01:45:36 +0200

    Remove async helper methods for json serialization.

--- Source/Lib/TraktApiSharp/Utils/Json.cs
+++ Source/Lib/TraktApiSharp/Utils/Json.cs
@@ -1,7 +1,6 @@
 ﻿namespace TraktApiSharp.Utils
 {
     using Newtonsoft.Json;
-    using System.Threading.Tasks;
 
     internal static class Json
     {
@@ -20,16 +19,6 @@
         internal static TResult Deserialize<TResult>(string value)
         {
             return JsonConvert.DeserializeObject<TResult>(value, DEFAULT_JSON_SETTINGS);
-        }
-
-        internal static async Task<string> SerializeAsync(object value)
-        {
-            return await Task.Run(() => JsonConvert.SerializeObject(value, DEFAULT_JSON_SETTINGS));
-        }
-
-        internal static async Task<TResult> DeserializeAsync<TResult>(string value)
-        {
-            return await Task.Run(() => JsonConvert.DeserializeObject<TResult>(value, DEFAULT_JSON_SETTINGS));
         }
     }
 }
