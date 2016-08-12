806b6e531586ca7da55a53b07be4c4f04b3886df
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-12 02:31:19 +0200

    Add source documentation for sync activities.

--- Source/Lib/TraktApiSharp/Objects/Get/Syncs/Activities/TraktSyncSeasonsLastActivities.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/Syncs/Activities/TraktSyncSeasonsLastActivities.cs
@@ -3,14 +3,18 @@
     using Newtonsoft.Json;
     using System;
 
+    /// <summary>A collection of UTC datetimes of last activities for seasons.</summary>
     public class TraktSyncSeasonsLastActivities
     {
+        /// <summary>Gets or sets the UTC datetime, when a season was lastly rated.</summary>
         [JsonProperty(PropertyName = "rated_at")]
         public DateTime? RatedAt { get; set; }
 
+        /// <summary>Gets or sets the UTC datetime, when a season was lastly added to the watchlist.</summary>
         [JsonProperty(PropertyName = "watchlisted_at")]
         public DateTime? WatchlistedAt { get; set; }
 
+        /// <summary>Gets or sets the UTC datetime, when a season was lastly commented.</summary>
         [JsonProperty(PropertyName = "commented_at")]
         public DateTime? CommentedAt { get; set; }
     }
