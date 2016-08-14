d995c2302ecfb83ff976ad9a629f20b399191d9f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-14 17:48:19 +0200

    Add source documentation for remaining user objects.

--- Source/Lib/TraktApiSharp/Objects/Get/Users/TraktUserLikeItem.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/TraktUserLikeItem.cs
@@ -7,20 +7,37 @@
     using Newtonsoft.Json;
     using System;
 
+    /// <summary>Contains information about an item a Trakt user has liked, including the corresponding comment or list.</summary>
     public class TraktUserLikeItem
     {
+        /// <summary>Gets or sets the UTC datetime, when the comment or list was liked.</summary>
         [JsonProperty(PropertyName = "liked_at")]
         public DateTime? LikedAt { get; set; }
 
+        /// <summary>
+        /// Gets or sets the object type, which this liked item contains.
+        /// See also <seealso cref="TraktUserLikeType" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "type")]
         [JsonConverter(typeof(TraktEnumerationConverter<TraktUserLikeType>))]
         [Nullable]
         public TraktUserLikeType Type { get; set; }
 
+        /// <summary>
+        /// Gets or sets the comment, if <see cref="Type" /> is <see cref="TraktUserLikeType.Comment" />.
+        /// See also <seealso cref="TraktComment" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "comment")]
         [Nullable]
         public TraktComment Comment { get; set; }
 
+        /// <summary>
+        /// Gets or sets the list, if <see cref="Type" /> is <see cref="TraktUserLikeType.List" />.
+        /// See also <seealso cref="TraktList" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "list")]
         [Nullable]
         public TraktList List { get; set; }
--- Source/Lib/TraktApiSharp/Objects/Get/Users/TraktUserWatchingItem.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/TraktUserWatchingItem.cs
@@ -8,32 +8,60 @@
     using Shows.Episodes;
     using System;
 
+    /// <summary>Contains information about a movie or an episode a Trakt user is currently watching.</summary>
     public class TraktUserWatchingItem
     {
-        [JsonProperty(PropertyName = "expires_at")]
-        public DateTime? ExpiresAt { get; set; }
-
+        /// <summary>Gets or sets the UTC datetime, when the movie or episode started.</summary>
         [JsonProperty(PropertyName = "started_at")]
         public DateTime? StartedAt { get; set; }
 
+        /// <summary>Gets or sets the UTC datetime, when the movie or episode expires.</summary>
+        [JsonProperty(PropertyName = "expires_at")]
+        public DateTime? ExpiresAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action type for the movie or episode.
+        /// See also <seealso cref="TraktHistoryActionType" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "action")]
         [JsonConverter(typeof(TraktEnumerationConverter<TraktHistoryActionType>))]
         [Nullable]
         public TraktHistoryActionType Action { get; set; }
 
+        /// <summary>
+        /// Gets or sets the object type, which this watching item contains.
+        /// See also <seealso cref="TraktSyncType" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "type")]
         [JsonConverter(typeof(TraktEnumerationConverter<TraktSyncType>))]
         [Nullable]
         public TraktSyncType Type { get; set; }
 
+        /// <summary>
+        /// Gets or sets the movie, if <see cref="Type" /> is <see cref="TraktSyncType.Movie" />.
+        /// See also <seealso cref="TraktMovie" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "movie")]
         [Nullable]
         public TraktMovie Movie { get; set; }
 
+        /// <summary>
+        /// Gets or sets the show, if <see cref="Type" /> is <see cref="TraktSyncType.Episode" />.
+        /// See also <seealso cref="TraktShow" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "show")]
         [Nullable]
         public TraktShow Show { get; set; }
 
+        /// <summary>
+        /// Gets or sets the episode, if <see cref="Type" /> is <see cref="TraktSyncType.Episode" />.
+        /// See also <seealso cref="TraktEpisode" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "episode")]
         [Nullable]
         public TraktEpisode Episode { get; set; }
