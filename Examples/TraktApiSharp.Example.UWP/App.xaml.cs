04b025eb3f930656db5d9c2934cd231f0625e9f6
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-14 19:50:13 +0200

    Remove startup delay from project template.

--- Examples/TraktApiSharp.Example.UWP/App.xaml.cs
+++ Examples/TraktApiSharp.Example.UWP/App.xaml.cs
@@ -1,11 +1,8 @@
-using Windows.UI.Xaml;
 using System.Threading.Tasks;
+using Template10.Controls;
 using TraktApiSharp.Example.UWP.Services.SettingsServices;
 using Windows.ApplicationModel.Activation;
-using Template10.Controls;
-using Template10.Common;
-using System;
-using System.Linq;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 
 namespace TraktApiSharp.Example.UWP
@@ -21,14 +18,10 @@ namespace TraktApiSharp.Example.UWP
             InitializeComponent();
             SplashFactory = (e) => new Views.Splash(e);
 
-            #region App settings
-
-            var _settings = SettingsService.Instance;
-            RequestedTheme = _settings.AppTheme;
-            CacheMaxDuration = _settings.CacheMaxDuration;
-            ShowShellBackButton = _settings.UseShellBackButton;
-
-            #endregion
+            var settings = SettingsService.Instance;
+            RequestedTheme = settings.AppTheme;
+            CacheMaxDuration = settings.CacheMaxDuration;
+            ShowShellBackButton = settings.UseShellBackButton;
         }
 
         public override async Task OnInitializeAsync(IActivatedEventArgs args)
@@ -46,17 +39,14 @@ namespace TraktApiSharp.Example.UWP
                     ModalContent = new Views.Busy(),
                 };
             }
+
             await Task.CompletedTask;
         }
 
         public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
         {
-            // long-running startup tasks go here
-            await Task.Delay(5000);
-
             NavigationService.Navigate(typeof(Views.MainPage));
             await Task.CompletedTask;
         }
     }
 }
-
