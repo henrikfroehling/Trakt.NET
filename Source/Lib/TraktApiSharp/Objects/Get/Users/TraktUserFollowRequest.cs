96c29238ed8ce24e1c7185e9f5814c9cd9300bbe
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-14 17:28:19 +0200

    Add source documentation for user friends and followers.

--- Source/Lib/TraktApiSharp/Objects/Get/Users/TraktUserFollowRequest.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/TraktUserFollowRequest.cs
@@ -4,14 +4,22 @@
     using Newtonsoft.Json;
     using System;
 
+    /// <summary>Represents a Trakt user follow request.</summary>
     public class TraktUserFollowRequest
     {
+        /// <summary>Gets or sets the id of the follow request.</summary>
         [JsonProperty(PropertyName = "id")]
         public int Id { get; set; }
 
+        /// <summary>Gets or sets the UTC datetime, when the request was made.</summary>
         [JsonProperty(PropertyName = "requested_at")]
         public DateTime? RequestedAt { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Trakt user, who is requesting.
+        /// See also <seealso cref="TraktUser" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "user")]
         [Nullable]
         public TraktUser User { get; set; }
