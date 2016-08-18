cae5d1e93e3b23d8e5186e44ce5f059139edfdac
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-18 05:07:09 +0200

    Add source documentation for checkin post objects.

--- Source/Lib/TraktApiSharp/Objects/Post/Checkins/Responses/TraktCheckinPostErrorResponse.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Checkins/Responses/TraktCheckinPostErrorResponse.cs
@@ -3,8 +3,10 @@
     using Newtonsoft.Json;
     using System;
 
+    /// <summary>Represents a checkin error response.</summary>
     public class TraktCheckinPostErrorResponse
     {
+        /// <summary>Gets or sets the UTC datetime, when the current checkin expires.</summary>
         [JsonProperty(PropertyName = "expires_at")]
         public DateTime? ExpiresAt { get; set; }
     }
--- Source/Lib/TraktApiSharp/Objects/Post/Checkins/Responses/TraktCheckinPostResponse.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Checkins/Responses/TraktCheckinPostResponse.cs
@@ -7,9 +7,15 @@
 
     public abstract class TraktCheckinPostResponse
     {
+        /// <summary>Gets or sets the UTC datetime, when the checked in movie or episode was watched.</summary>
         [JsonProperty(PropertyName = "watched_at")]
         public DateTime? WatchedAt { get; set; }
 
+        /// <summary>
+        /// Gets or sets the sharing options for the checkin response.
+        /// See also <seealso cref="TraktSharing" />.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "sharing")]
         [Nullable]
         public TraktSharing Sharing { get; set; }
