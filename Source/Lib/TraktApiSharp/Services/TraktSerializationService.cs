9535db6a9a22851f084966da55fe862fc9a8afd6
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-22 03:55:35 +0200

    Add serialization service prototype.

---
+++ Source/Lib/TraktApiSharp/Services/TraktSerializationService.cs
@@ -0,0 +1,18 @@
+﻿namespace TraktApiSharp.Services
+{
+    using Authentication;
+
+    /// <summary>Provides helper methods for serializing and deserializing Trakt objects.</summary>
+    public static class TraktSerializationService
+    {
+        public static string Serialize(TraktAuthorization authorization)
+        {
+            return string.Empty;
+        }
+
+        public static TraktAuthorization Deserialize(string authorization)
+        {
+            return null;
+        }
+    }
+}
