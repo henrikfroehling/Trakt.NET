5cbd45fa4af7fe6303615898114f46238e0698b2
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-22 04:48:14 +0200

    Add implementation for argument exceptions for serializing.

--- Source/Lib/TraktApiSharp/Services/TraktSerializationService.cs
+++ Source/Lib/TraktApiSharp/Services/TraktSerializationService.cs
@@ -1,12 +1,16 @@
 ﻿namespace TraktApiSharp.Services
 {
     using Authentication;
+    using System;
 
     /// <summary>Provides helper methods for serializing and deserializing Trakt objects.</summary>
     public static class TraktSerializationService
     {
         public static string Serialize(TraktAuthorization authorization)
         {
+            if (authorization == null)
+                throw new ArgumentNullException(nameof(authorization), "authorization must not be null");
+
             return string.Empty;
         }
 
