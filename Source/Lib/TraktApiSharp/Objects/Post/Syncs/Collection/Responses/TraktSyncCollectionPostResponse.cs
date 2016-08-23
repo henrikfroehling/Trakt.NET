b644b42a4a5b4ae9176c6a1f5583bb1a9fe02243
Henrik Fröhling <henrik.froehling@gmail.com>
2016-08-23 05:22:57 +0200

    Add source documentation for collection post.

--- Source/Lib/TraktApiSharp/Objects/Post/Syncs/Collection/Responses/TraktSyncCollectionPostResponse.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Syncs/Collection/Responses/TraktSyncCollectionPostResponse.cs
@@ -4,20 +4,40 @@
     using Newtonsoft.Json;
     using Syncs.Responses;
 
+    /// <summary>
+    /// Represents the response for a collection post. See also <see cref="TraktSyncCollectionPost" />.
+    /// <para>Contains the number of added, updated, existing and not found movies, shows, seasons and episodes.</para>
+    /// </summary>
     public class TraktSyncCollectionPostResponse
     {
+        /// <summary>
+        /// A collection containing the number of added movies, shows, seasons and episodes.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "added")]
         [Nullable]
         public TraktSyncPostResponseGroup Added { get; set; }
 
+        /// <summary>
+        /// A collection containing the number of updated movies, shows, seasons and episodes.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "updated")]
         [Nullable]
         public TraktSyncPostResponseGroup Updated { get; set; }
 
+        /// <summary>
+        /// A collection containing the number of existing movies, shows, seasons and episodes.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "existing")]
         [Nullable]
         public TraktSyncPostResponseGroup Existing { get; set; }
 
+        /// <summary>
+        /// A collection containing the ids of movies, shows, seasons and episodes, which were not found.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "not_found")]
         [Nullable]
         public TraktSyncPostResponseNotFound NotFound { get; set; }
--- Source/Lib/TraktApiSharp/Objects/Post/Syncs/Responses/TraktSyncPostResponseNotFound.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Syncs/Responses/TraktSyncPostResponseNotFound.cs
@@ -8,20 +8,37 @@
     using Newtonsoft.Json;
     using System.Collections.Generic;
 
+    /// <summary>A collection containing the ids of movies, shows, seasons and episodes, which were not found.</summary>
     public class TraktSyncPostResponseNotFound
     {
+        /// <summary>
+        /// A list of <see cref="TraktSyncPostResponseNotFoundItem{T}" />, containing the ids of movies, which were not found.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "movies")]
         [Nullable]
         public IEnumerable<TraktSyncPostResponseNotFoundItem<TraktMovieIds>> Movies { get; set; }
 
+        /// <summary>
+        /// A list of <see cref="TraktSyncPostResponseNotFoundItem{T}" />, containing the ids of shows, which were not found.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "shows")]
         [Nullable]
         public IEnumerable<TraktSyncPostResponseNotFoundItem<TraktShowIds>> Shows { get; set; }
 
+        /// <summary>
+        /// A list of <see cref="TraktSyncPostResponseNotFoundItem{T}" />, containing the ids of seasons, which were not found.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "seasons")]
         [Nullable]
         public IEnumerable<TraktSyncPostResponseNotFoundItem<TraktSeasonIds>> Seasons { get; set; }
 
+        /// <summary>
+        /// A list of <see cref="TraktSyncPostResponseNotFoundItem{T}" />, containing the ids of episodes, which were not found.
+        /// <para>Nullable</para>
+        /// </summary>
         [JsonProperty(PropertyName = "episodes")]
         [Nullable]
         public IEnumerable<TraktSyncPostResponseNotFoundItem<TraktEpisodeIds>> Episodes { get; set; }
