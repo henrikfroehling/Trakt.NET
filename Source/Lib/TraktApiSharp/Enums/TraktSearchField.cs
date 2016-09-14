5684476fcd4929991a7de89fbb28be4da75329a9
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-14 03:44:47 +0200

    Add enumeration values for search field.

--- Source/Lib/TraktApiSharp/Enums/TraktSearchField.cs
+++ Source/Lib/TraktApiSharp/Enums/TraktSearchField.cs
@@ -2,6 +2,30 @@
 {
     public sealed class TraktSearchField : TraktEnumeration
     {
+        public static TraktSearchField Unspecified { get; } = new TraktSearchField();
+
+        public static TraktSearchField Title { get; } = new TraktSearchField(1, "title", "title", "Title");
+
+        public static TraktSearchField Tagline { get; } = new TraktSearchField(2, "tagline", "tagline", "Tagline");
+
+        public static TraktSearchField Overview { get; } = new TraktSearchField(4, "overview", "overview", "Overview");
+
+        public static TraktSearchField People { get; } = new TraktSearchField(8, "people", "people", "People");
+
+        public static TraktSearchField Translations { get; } = new TraktSearchField(16, "translations", "translations", "Translations");
+
+        public static TraktSearchField Aliases { get; } = new TraktSearchField(32, "aliases", "aliases", "Aliases");
+
+        public static TraktSearchField Name { get; } = new TraktSearchField(64, "name", "name", "Name");
+
+        public static TraktSearchField Biography { get; } = new TraktSearchField(128, "biography", "biography", "Biography");
+
+        public static TraktSearchField Description { get; } = new TraktSearchField(256, "description", "description", "Description");
+
+        public TraktSearchField() : base() { }
+
+        private TraktSearchField(int value, string objectName, string uriName, string displayName)
+            : base(value, objectName, uriName, displayName) { }
 
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Enums/TraktSearchFieldTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Enums/TraktSearchFieldTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Newtonsoft.Json;
+    using System.Collections.Generic;
     using TraktApiSharp.Enums;
 
     [TestClass]
@@ -27,11 +28,11 @@
             var allValues = TraktEnumeration.GetAll<TraktSearchField>();
 
             allValues.Should().NotBeNull().And.HaveCount(10);
-            //allValues.Should().Contain(new List<TraktSearchField>() { TraktSearchField.Unspecified, TraktSearchField.Title,
-            //                                                          TraktSearchField.Tagline, TraktSearchField.Overview,
-            //                                                          TraktSearchField.People, TraktSearchField.Translations,
-            //                                                          TraktSearchField.Aliases, TraktSearchField.Name,
-            //                                                          TraktSearchField.Biography, TraktSearchField.Description });
+            allValues.Should().Contain(new List<TraktSearchField>() { TraktSearchField.Unspecified, TraktSearchField.Title,
+                                                                      TraktSearchField.Tagline, TraktSearchField.Overview,
+                                                                      TraktSearchField.People, TraktSearchField.Translations,
+                                                                      TraktSearchField.Aliases, TraktSearchField.Name,
+                                                                      TraktSearchField.Biography, TraktSearchField.Description });
         }
 
         [TestMethod]
@@ -43,61 +44,131 @@
         [TestMethod]
         public void TestTraktSearchFieldWriteAndReadJson_Title()
         {
-            Assert.Fail();
+            var obj = new TestObject { Value = TraktSearchField.Title };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSearchField.Title);
         }
 
         [TestMethod]
         public void TestTraktSearchFieldWriteAndReadJson_Tagline()
         {
-            Assert.Fail();
+            var obj = new TestObject { Value = TraktSearchField.Tagline };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSearchField.Tagline);
         }
 
         [TestMethod]
         public void TestTraktSearchFieldWriteAndReadJson_Overview()
         {
-            Assert.Fail();
+            var obj = new TestObject { Value = TraktSearchField.Overview };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSearchField.Overview);
         }
 
         [TestMethod]
         public void TestTraktSearchFieldWriteAndReadJson_People()
         {
-            Assert.Fail();
+            var obj = new TestObject { Value = TraktSearchField.People };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSearchField.People);
         }
 
         [TestMethod]
         public void TestTraktSearchFieldWriteAndReadJson_Translations()
         {
-            Assert.Fail();
+            var obj = new TestObject { Value = TraktSearchField.Translations };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSearchField.Translations);
         }
 
         [TestMethod]
         public void TestTraktSearchFieldWriteAndReadJson_Aliases()
         {
-            Assert.Fail();
+            var obj = new TestObject { Value = TraktSearchField.Aliases };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSearchField.Aliases);
         }
 
         [TestMethod]
         public void TestTraktSearchFieldWriteAndReadJson_Name()
         {
-            Assert.Fail();
+            var obj = new TestObject { Value = TraktSearchField.Name };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSearchField.Name);
         }
 
         [TestMethod]
         public void TestTraktSearchFieldWriteAndReadJson_Biography()
         {
-            Assert.Fail();
+            var obj = new TestObject { Value = TraktSearchField.Biography };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSearchField.Biography);
         }
 
         [TestMethod]
         public void TestTraktSearchFieldWriteAndReadJson_Description()
         {
-            Assert.Fail();
+            var obj = new TestObject { Value = TraktSearchField.Description };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSearchField.Description);
         }
 
         [TestMethod]
         public void TestTraktSearchFieldWriteAndReadJson_Unspecified()
         {
-            Assert.Fail();
+            var obj = new TestObject { Value = TraktSearchField.Unspecified };
+
+            var objWritten = JsonConvert.SerializeObject(obj);
+            objWritten.Should().NotBeNullOrEmpty();
+
+            var objRead = JsonConvert.DeserializeObject<TestObject>(objWritten);
+            objRead.Should().NotBeNull();
+            objRead.Value.Should().Be(TraktSearchField.Unspecified);
         }
     }
 }
