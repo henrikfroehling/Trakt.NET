f4a2450f1714e0be18eec500d6403b5a9ec4feff
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 00:43:21 +0200

    Add base classes for post requests.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Post/ATraktPaginationPostRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base.Post
+{
+    using Interfaces.Requests;
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class ATraktPaginationPostRequest<TItem, TRequestBody> : ATraktPaginationRequest<TItem>, ITraktRequest, ITraktHasRequestBody<TRequestBody>
+    {
+        public ATraktPaginationPostRequest(TraktClient client) : base(client)
+        {
+            RequestBody = new TraktRequestBody<TRequestBody>();
+        }
+
+        public TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
+
+        public HttpMethod Method => HttpMethod.Post;
+
+        public TraktRequestBody<TRequestBody> RequestBody { get; set; }
+
+        public TRequestBody RequestBodyContent
+        {
+            get { return RequestBody.RequestBody; }
+            set { RequestBody.RequestBody = value; }
+        }
+    }
+}
