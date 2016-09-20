2946d06f3bbdeb1096a5e2e19d7bac0a54f65abe
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 01:41:20 +0200

    Add uri properties to abstract base request.

--- Source/Lib/TraktApiSharp/Experimental/Requests/Base/ATraktBaseRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/ATraktBaseRequest.cs
@@ -1,12 +1,27 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Base
 {
-    internal abstract class ATraktBaseRequest
+    using Interfaces;
+    using System;
+    using System.Collections.Generic;
+
+    internal abstract class ATraktBaseRequest : ITraktUriBuildable
     {
         internal ATraktBaseRequest(TraktClient client)
         {
             Client = client;
         }
 
+        public abstract string UriTemplate { get; }
+
+        public string Url => BuildUrl();
+
         internal TraktClient Client { get; }
+
+        public string BuildUrl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IDictionary<string, object> GetUriPathParameters() => new Dictionary<string, object>();
     }
 }
