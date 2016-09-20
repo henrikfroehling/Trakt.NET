152075049aad2fe1a73a9afef4027899a7cc1686
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 00:29:35 +0200

    Add base classes for put requests.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Put/ATraktListPutRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base.Put
+{
+    using Interfaces.Requests;
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class ATraktListPutRequest<TItem, TRequestBody> : ATraktListRequest<TItem>, ITraktRequest, ITraktHasRequestBody<TRequestBody>
+    {
+        public ATraktListPutRequest(TraktClient client) : base(client)
+        {
+            RequestBody = new TraktRequestBody<TRequestBody>();
+        }
+
+        public TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
+
+        public HttpMethod Method => HttpMethod.Put;
+
+        public TraktRequestBody<TRequestBody> RequestBody { get; set; }
+
+        public TRequestBody RequestBodyContent
+        {
+            get { return RequestBody.RequestBody; }
+            set { RequestBody.RequestBody = value; }
+        }
+    }
+}
---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Put/ATraktPaginationPutRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base.Put
+{
+    using Interfaces.Requests;
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class ATraktPaginationPutRequest<TItem, TRequestBody> : ATraktPaginationRequest<TItem>, ITraktRequest, ITraktHasRequestBody<TRequestBody>
+    {
+        public ATraktPaginationPutRequest(TraktClient client) : base(client)
+        {
+            RequestBody = new TraktRequestBody<TRequestBody>();
+        }
+
+        public TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
+
+        public HttpMethod Method => HttpMethod.Put;
+
+        public TraktRequestBody<TRequestBody> RequestBody { get; set; }
+
+        public TRequestBody RequestBodyContent
+        {
+            get { return RequestBody.RequestBody; }
+            set { RequestBody.RequestBody = value; }
+        }
+    }
+}
