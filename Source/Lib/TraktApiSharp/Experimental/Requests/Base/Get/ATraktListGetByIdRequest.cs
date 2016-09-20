9fad030f4091d5953dc18e2d454bb246d2c041a3
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 01:07:37 +0200

    Add base classes for get by id requests.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Get/ATraktListGetByIdRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base.Get
+{
+    using Interfaces.Requests;
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class ATraktListGetByIdRequest<TItem> : ATraktListRequest<TItem>, ITraktRequest, ITraktHasId
+    {
+        public ATraktListGetByIdRequest(TraktClient client) : base(client)
+        {
+            RequestId = new TraktRequestId();
+        }
+
+        public abstract TraktAuthorizationRequirement AuthorizationRequirement { get; }
+
+        public HttpMethod Method => HttpMethod.Get;
+
+        public string Id
+        {
+            get { return RequestId.Id; }
+            set { RequestId.Id = value; }
+        }
+
+        public TraktRequestId RequestId { get; set; }
+    }
+}
---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/TraktRequestId.cs
@@ -0,0 +1,19 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base
+{
+    using Interfaces;
+    using System;
+    using System.Collections.Generic;
+
+    internal sealed class TraktRequestId : ITraktId
+    {
+        public string Id { get; set; }
+
+        public IDictionary<string, object> GetIdPathParameters() => new Dictionary<string, object> { ["id"] = Id };
+
+        public void ValidateId()
+        {
+            if (string.IsNullOrEmpty(Id))
+                throw new ArgumentException("id not set");
+        }
+    }
+}
