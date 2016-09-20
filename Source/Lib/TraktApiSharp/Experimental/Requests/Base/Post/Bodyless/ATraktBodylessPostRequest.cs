8cd2ff9848ee187dd6efeb6f7f8ed98cd3a2360d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-20 02:31:58 +0200

    Add base classes for bodyless post requests.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Post/Bodyless/ATraktBodylessPostRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base.Post
+{
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class ATraktBodylessPostRequest<TItem> : ATraktRequest<TItem, object>
+    {
+        protected override HttpMethod Method => HttpMethod.Put;
+
+        protected override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
+    }
+}
