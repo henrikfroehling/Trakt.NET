1353e67bcc7bc1dc807664636753a787802aef6c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 01:10:50 +0200

    Add base class for delete by id requests.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Delete/ATraktNoContentDeleteByIdRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base.Delete
+{
+    using Interfaces.Requests;
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class ATraktNoContentDeleteByIdRequest : ATraktNoContentRequest, ITraktRequest, ITraktHasId
+    {
+        public ATraktNoContentDeleteByIdRequest(TraktClient client) : base(client)
+        {
+            RequestId = new TraktRequestId();
+        }
+
+        public TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
+
+        public HttpMethod Method => HttpMethod.Delete;
+
+        public string Id
+        {
+            get { return RequestId.Id; }
+            set { RequestId.Id = value; }
+        }
+
+        public TraktRequestId RequestId { get; set; }
+    }
+}
