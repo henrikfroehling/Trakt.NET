005e8f14d94dc377de147d3c345f8d9cbd632b34
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-20 02:27:40 +0200

    Add base classes for post requests.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Post/ATraktPostRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base.Post
+{
+    using System;
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class ATraktPostRequest<TItem, TRequestBody> : ATraktRequest<TItem, TRequestBody>
+    {
+        protected override HttpMethod Method => HttpMethod.Put;
+
+        protected override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
+
+        protected override void Validate()
+        {
+            base.Validate();
+
+            if (RequestBody == null)
+                throw new ArgumentException("request body not valid");
+        }
+    }
+}
