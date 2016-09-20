d4944d38a9405afd0c0cdc30d1d2f92e72a8a555
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-20 02:43:27 +0200

    Add base classes for get by id requests.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Get/ATraktPaginationGetByIdRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base.Get
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal abstract class ATraktPaginationGetByIdRequest<TItem> : ATraktPaginationGetRequest<TItem>
+    {
+        protected override IDictionary<string, object> GetUriPathParameters()
+        {
+            var uriParams = base.GetUriPathParameters();
+            uriParams.Add("id", Id);
+            return uriParams;
+        }
+
+        protected override void Validate()
+        {
+            base.Validate();
+
+            if (string.IsNullOrEmpty(Id))
+                throw new ArgumentException("id not valid");
+        }
+    }
+}
