c4201a33edc4e4241df9a1428187e179d89b678d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 01:27:35 +0200

    Add base classes for bodyless post by id requests.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Post/Bodyless/ATraktListBodylessPostByIdRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base.Post.Bodyless
+{
+    using Interfaces.Requests;
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class ATraktListBodylessPostByIdRequest<TItem> : ATraktListRequest<TItem>, ITraktRequest, ITraktHasId
+    {
+        public ATraktListBodylessPostByIdRequest(TraktClient client) : base(client)
+        {
+            RequestId = new TraktRequestId();
+        }
+
+        public TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
+
+        public HttpMethod Method => HttpMethod.Post;
+
+        public string Id
+        {
+            get { return RequestId.Id; }
+            set { RequestId.Id = value; }
+        }
+
+        public TraktRequestId RequestId { get; set; }
+    }
+}
