ec0b38d8b50bf9544df28498b0f76be30b49b047
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 19:54:35 +0200

    Add tests for TraktResponse class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Responses/TraktResponseTests.cs
@@ -0,0 +1,35 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Responses
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Responses;
+
+    [TestClass]
+    public class TraktResponseTests
+    {
+        [TestMethod]
+        public void TestTraktResponseIsNotAbstract()
+        {
+            typeof(TraktResponse<>).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void TestTraktResponseIsSubclassOfATraktResponse()
+        {
+            typeof(TraktResponse<int>).IsSubclassOf(typeof(ATraktResponse<int>)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestTraktResponseHasGenericTypeParameter()
+        {
+            typeof(TraktResponse<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(TraktResponse<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod]
+        public void TestTraktResponseImplementsITraktResponseHeadersInterface()
+        {
+            typeof(TraktResponse<>).GetInterfaces().Should().Contain(typeof(ITraktResponseHeaders));
+        }
+    }
+}
