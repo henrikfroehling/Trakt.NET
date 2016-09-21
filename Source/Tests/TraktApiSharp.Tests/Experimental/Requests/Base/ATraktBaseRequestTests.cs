50398772cb97538579e0c81296cacb594d98dd00
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 00:54:05 +0200

    Add tests for ATraktBaseRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/ATraktBaseRequestTests.cs
@@ -0,0 +1,38 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Reflection;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktBaseRequestTests
+    {
+        [TestMethod]
+        public void TestATraktBaseRequestIsAbstract()
+        {
+            typeof(ATraktBaseRequest).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktBaseRequestImplementsITraktUriBuildableInterface()
+        {
+            typeof(ATraktBaseRequest).GetInterfaces().Should().Contain(typeof(ITraktUriBuildable));
+        }
+
+        [TestMethod]
+        public void TestATraktBaseRequestHasClientProperty()
+        {
+            var clientPropertyInfo = typeof(ATraktBaseRequest)
+                .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                .Where(p => p.Name == "Client")
+                .FirstOrDefault();
+
+            clientPropertyInfo.CanRead.Should().BeTrue();
+            clientPropertyInfo.CanWrite.Should().BeFalse();
+            clientPropertyInfo.PropertyType.Should().Be(typeof(TraktClient));
+        }
+    }
+}
