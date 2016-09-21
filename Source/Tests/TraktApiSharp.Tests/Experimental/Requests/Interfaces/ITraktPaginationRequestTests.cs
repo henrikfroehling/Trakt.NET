c8311be97b1cf874e870a44144c53dabed8e8606
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 23:50:04 +0200

    Add tests for ITraktPaginationRequest interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktPaginationRequestTests.cs
@@ -0,0 +1,37 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+    using TraktApiSharp.Experimental.Responses;
+
+    [TestClass]
+    public class ITraktPaginationRequestTests
+    {
+        [TestMethod]
+        public void TestITraktPaginationRequestIsInterface()
+        {
+            typeof(ITraktPaginationRequest<>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktPaginationRequestHasGenericTypeParameter()
+        {
+            typeof(ITraktPaginationRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktPaginationRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod]
+        public void TestITraktPaginationRequestHasQueryAsyncMethod()
+        {
+            var methodInfo = typeof(ITraktPaginationRequest<int>).GetMethods()
+                                                           .Where(m => m.Name == "QueryAsync")
+                                                           .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(Task<TraktPaginationResponse<int>>));
+            methodInfo.GetParameters().Should().BeEmpty();
+        }
+    }
+}
