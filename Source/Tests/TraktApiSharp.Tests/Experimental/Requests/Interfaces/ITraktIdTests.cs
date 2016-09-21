4585745f746c55a53e36a129d3cbd54ae31e1883
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 23:31:21 +0200

    Add tests for ITraktId interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktIdTests.cs
@@ -0,0 +1,52 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ITraktIdTests
+    {
+        [TestMethod]
+        public void TestITraktIdIsInterface()
+        {
+            typeof(ITraktId).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktIdHasIdProperty()
+        {
+            var idPropertyInfo = typeof(ITraktId).GetProperties()
+                                                 .Where(p => p.Name == "Id")
+                                                 .FirstOrDefault();
+
+            idPropertyInfo.CanRead.Should().BeTrue();
+            idPropertyInfo.CanWrite.Should().BeTrue();
+            idPropertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [TestMethod]
+        public void TestITraktIdHasGetIdPathParametersMethod()
+        {
+            var methodInfo = typeof(ITraktId).GetMethods()
+                                             .Where(m => m.Name == "GetIdPathParameters")
+                                             .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
+            methodInfo.GetParameters().Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void TestITraktIdHasValidateIdMethod()
+        {
+            var methodInfo = typeof(ITraktId).GetMethods()
+                                             .Where(m => m.Name == "ValidateId")
+                                             .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(void));
+            methodInfo.GetParameters().Should().BeEmpty();
+        }
+    }
+}
