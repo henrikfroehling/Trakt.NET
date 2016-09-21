deed64e8ca1c1dc4aee4746c5ecb7ea509e1b72e
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 01:56:56 +0200

    Add tests for ATraktNoContentGetRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Get/ATraktNoContentGetRequestTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Get
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktNoContentGetRequestTests
+    {
+        [TestMethod]
+        public void TestATraktNoContentGetRequestIsAbstract()
+        {
+            typeof(ATraktNoContentGetRequest).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentGetRequestIsSubclassOfATraktNoContentRequest()
+        {
+            typeof(ATraktNoContentGetRequest).IsSubclassOf(typeof(ATraktNoContentRequest)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentGetRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktNoContentGetRequest).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+    }
+}
