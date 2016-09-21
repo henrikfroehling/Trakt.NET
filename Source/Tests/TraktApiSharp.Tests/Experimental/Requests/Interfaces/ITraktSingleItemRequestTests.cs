15e7b172445648fbfef7f59ddda1b6357599adf0
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 00:29:14 +0200

    Add tests for ITraktSingleItemRequest interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktSingleItemRequestTests.cs
@@ -0,0 +1,37 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+    using TraktApiSharp.Experimental.Responses;
+
+    [TestClass]
+    public class ITraktSingleItemRequestTests
+    {
+        [TestMethod]
+        public void TestITraktSingleItemRequestIsInterface()
+        {
+            typeof(ITraktSingleItemRequest<>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktSingleItemRequestHasGenericTypeParameter()
+        {
+            typeof(ITraktSingleItemRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktSingleItemRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod]
+        public void TestITraktSingleItemRequestHasQueryAsyncMethod()
+        {
+            var methodInfo = typeof(ITraktSingleItemRequest<int>).GetMethods()
+                                                           .Where(m => m.Name == "QueryAsync")
+                                                           .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(Task<TraktResponse<int>>));
+            methodInfo.GetParameters().Should().BeEmpty();
+        }
+    }
+}
