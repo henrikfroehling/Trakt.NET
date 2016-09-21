f313ca17b96df0537f2c60d71bef50ffe4ecfe3a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 18:15:39 +0200

    Add tests of ITraktResponseHeaders interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Responses/ITraktResponseHeadersTests.cs
@@ -0,0 +1,53 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Responses
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Responses;
+
+    [TestClass]
+    public class ITraktResponseHeadersTests
+    {
+        [TestMethod]
+        public void TestITraktResponseHeadersIsInterface()
+        {
+            typeof(ITraktResponseHeaders).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktResponseHeadersHasUserCountProperty()
+        {
+            var userCountPropertyInfo = typeof(ITraktResponseHeaders).GetProperties()
+                                                                     .Where((p) => { return p.Name == "UserCount"; })
+                                                                     .FirstOrDefault();
+
+            userCountPropertyInfo.CanRead.Should().BeTrue();
+            userCountPropertyInfo.CanWrite.Should().BeTrue();
+            userCountPropertyInfo.PropertyType.Should().Be(typeof(int?));
+        }
+
+        [TestMethod]
+        public void TestITraktResponseHeadersHasSortByProperty()
+        {
+            var userCountPropertyInfo = typeof(ITraktResponseHeaders).GetProperties()
+                                                                     .Where((p) => { return p.Name == "SortBy"; })
+                                                                     .FirstOrDefault();
+
+            userCountPropertyInfo.CanRead.Should().BeTrue();
+            userCountPropertyInfo.CanWrite.Should().BeTrue();
+            userCountPropertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [TestMethod]
+        public void TestITraktResponseHeadersHasSortHowProperty()
+        {
+            var userCountPropertyInfo = typeof(ITraktResponseHeaders).GetProperties()
+                                                                     .Where((p) => { return p.Name == "SortHow"; })
+                                                                     .FirstOrDefault();
+
+            userCountPropertyInfo.CanRead.Should().BeTrue();
+            userCountPropertyInfo.CanWrite.Should().BeTrue();
+            userCountPropertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+    }
+}
