3717cf06b30d8e6acc7425e3b7c881d55aa5868c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 18:20:04 +0200

    Add tests for ITraktPaginationResponseHeaders interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Responses/ITraktPaginationResponseHeadersTests.cs
@@ -0,0 +1,65 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Responses
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Responses;
+
+    [TestClass]
+    public class ITraktPaginationResponseHeadersTests
+    {
+        [TestMethod]
+        public void TestITraktPaginationResponseHeadersIsInterface()
+        {
+            typeof(ITraktPaginationResponseHeaders).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktPaginationResponseHeadersHasPageProperty()
+        {
+            var userCountPropertyInfo = typeof(ITraktPaginationResponseHeaders).GetProperties()
+                                                                               .Where((p) => { return p.Name == "Page"; })
+                                                                               .FirstOrDefault();
+
+            userCountPropertyInfo.CanRead.Should().BeTrue();
+            userCountPropertyInfo.CanWrite.Should().BeTrue();
+            userCountPropertyInfo.PropertyType.Should().Be(typeof(int?));
+        }
+
+        [TestMethod]
+        public void TestITraktPaginationResponseHeadersHasLimitProperty()
+        {
+            var userCountPropertyInfo = typeof(ITraktPaginationResponseHeaders).GetProperties()
+                                                                               .Where((p) => { return p.Name == "Limit"; })
+                                                                               .FirstOrDefault();
+
+            userCountPropertyInfo.CanRead.Should().BeTrue();
+            userCountPropertyInfo.CanWrite.Should().BeTrue();
+            userCountPropertyInfo.PropertyType.Should().Be(typeof(int?));
+        }
+
+        [TestMethod]
+        public void TestITraktPaginationResponseHeadersHasPageCountProperty()
+        {
+            var userCountPropertyInfo = typeof(ITraktPaginationResponseHeaders).GetProperties()
+                                                                               .Where((p) => { return p.Name == "PageCount"; })
+                                                                               .FirstOrDefault();
+
+            userCountPropertyInfo.CanRead.Should().BeTrue();
+            userCountPropertyInfo.CanWrite.Should().BeTrue();
+            userCountPropertyInfo.PropertyType.Should().Be(typeof(int?));
+        }
+
+        [TestMethod]
+        public void TestITraktPaginationResponseHeadersHasItemCountProperty()
+        {
+            var userCountPropertyInfo = typeof(ITraktPaginationResponseHeaders).GetProperties()
+                                                                               .Where((p) => { return p.Name == "ItemCount"; })
+                                                                               .FirstOrDefault();
+
+            userCountPropertyInfo.CanRead.Should().BeTrue();
+            userCountPropertyInfo.CanWrite.Should().BeTrue();
+            userCountPropertyInfo.PropertyType.Should().Be(typeof(int?));
+        }
+    }
+}
