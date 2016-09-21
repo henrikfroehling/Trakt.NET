85ebc02fe37b4139b6c90f81fec3273f13c57f5e
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 00:20:31 +0200

    Add tests for ITraktRequest interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktRequestTests.cs
@@ -0,0 +1,28 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ITraktRequestTests
+    {
+        [TestMethod]
+        public void TestITraktRequestIsInterface()
+        {
+            typeof(ITraktRequest).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktRequestImplementsITraktHttpRequestInterface()
+        {
+            typeof(ITraktRequest).GetInterfaces().Should().Contain(typeof(ITraktHttpRequest));
+        }
+
+        [TestMethod]
+        public void TestITraktRequestImplementsITraktRequestAuthorizationInterface()
+        {
+            typeof(ITraktRequest).GetInterfaces().Should().Contain(typeof(ITraktRequestAuthorization));
+        }
+    }
+}
