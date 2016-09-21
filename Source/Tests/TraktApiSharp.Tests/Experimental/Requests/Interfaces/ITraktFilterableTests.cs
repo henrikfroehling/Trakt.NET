7593070b31b6f8116e77c855c2cfa4c5f5b7c6e8
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 23:23:32 +0200

    Add tests for ITraktFilterable interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktFilterableTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+    using TraktApiSharp.Requests.Params;
+
+    [TestClass]
+    public class ITraktFilterableTests
+    {
+        [TestMethod]
+        public void TestITraktFilterableIsInterface()
+        {
+            typeof(ITraktFilterable).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktFilterableHasFilterProperty()
+        {
+            var filterPropertyInfo = typeof(ITraktFilterable).GetProperties()
+                                                             .Where(p => p.Name == "Filter")
+                                                             .FirstOrDefault();
+
+            filterPropertyInfo.CanRead.Should().BeTrue();
+            filterPropertyInfo.CanWrite.Should().BeTrue();
+            filterPropertyInfo.PropertyType.Should().Be(typeof(TraktCommonFilter));
+        }
+    }
+}
