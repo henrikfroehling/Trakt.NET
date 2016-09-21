7f76d0d69ef70c2eeb81da15e411e9401207ce38
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 00:37:09 +0200

    Add tests for ITraktValidatable interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktValidatableTests.cs
@@ -0,0 +1,28 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ITraktValidatableTests
+    {
+        [TestMethod]
+        public void TestITraktValidatableIsInterface()
+        {
+            typeof(ITraktValidatable).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktValidatableHasValidateMethod()
+        {
+            var methodInfo = typeof(ITraktValidatable).GetMethods()
+                                                      .Where(m => m.Name == "Validate")
+                                                      .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(void));
+            methodInfo.GetParameters().Should().BeEmpty();
+        }
+    }
+}
