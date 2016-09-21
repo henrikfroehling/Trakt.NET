e28db3136dd658b27c1836d9dd78e590dba1a007
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 00:24:02 +0200

    Add tests for ITraktRequestAuthorization interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktRequestAuthorizationTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+    using TraktApiSharp.Requests;
+
+    [TestClass]
+    public class ITraktRequestAuthorizationTests
+    {
+        [TestMethod]
+        public void TestITraktRequestAuthorizationIsInterface()
+        {
+            typeof(ITraktRequestAuthorization).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktRequestAuthorizationHasAuthorizationRequirementProperty()
+        {
+            var authorizationRequirementPropertyInfo = typeof(ITraktRequestAuthorization).GetProperties()
+                                                                                         .Where(p => p.Name == "AuthorizationRequirement")
+                                                                                         .FirstOrDefault();
+
+            authorizationRequirementPropertyInfo.CanRead.Should().BeTrue();
+            authorizationRequirementPropertyInfo.CanWrite.Should().BeFalse();
+            authorizationRequirementPropertyInfo.PropertyType.Should().Be(typeof(TraktAuthorizationRequirement));
+        }
+    }
+}
