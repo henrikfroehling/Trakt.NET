a4fd4db26f2389769bbf308e1c8e72a763f11a84
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 01:35:35 +0200

    Add tests for ATraktNoContentDeleteByIdRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Delete/ATraktNoContentDeleteByIdRequestTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Delete
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Delete;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktNoContentDeleteByIdRequestTests
+    {
+        [TestMethod]
+        public void TestATraktNoContentDeleteByIdRequestIsAbstract()
+        {
+            typeof(ATraktNoContentDeleteByIdRequest).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentDeleteByIdRequestIsSubclassOfATraktNoContentRequest()
+        {
+            typeof(ATraktNoContentDeleteByIdRequest).IsSubclassOf(typeof(ATraktNoContentRequest)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentDeleteByIdRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktNoContentDeleteByIdRequest).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentDeleteByIdRequestImplementsITraktHasIdInterface()
+        {
+            typeof(ATraktNoContentDeleteByIdRequest).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
