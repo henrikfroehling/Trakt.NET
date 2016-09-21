4a26e0d9d205ed50457bb452eb85aa4f8403f994
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 00:34:54 +0200

    Add tests for ITraktUriBuildable interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktUriBuildableTests.cs
@@ -0,0 +1,64 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ITraktUriBuildableTests
+    {
+        [TestMethod]
+        public void TestITraktUriBuildableIsInterface()
+        {
+            typeof(ITraktUriBuildable).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktUriBuildableHasUriTemplateProperty()
+        {
+            var uriTemplatePropertyInfo = typeof(ITraktUriBuildable).GetProperties()
+                                                                    .Where(p => p.Name == "UriTemplate")
+                                                                    .FirstOrDefault();
+
+            uriTemplatePropertyInfo.CanRead.Should().BeTrue();
+            uriTemplatePropertyInfo.CanWrite.Should().BeFalse();
+            uriTemplatePropertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [TestMethod]
+        public void TestITraktUriBuildableHasUrlProperty()
+        {
+            var urlPropertyInfo = typeof(ITraktUriBuildable).GetProperties()
+                                                            .Where(p => p.Name == "Url")
+                                                            .FirstOrDefault();
+
+            urlPropertyInfo.CanRead.Should().BeTrue();
+            urlPropertyInfo.CanWrite.Should().BeFalse();
+            urlPropertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [TestMethod]
+        public void TestITraktUriBuildableHasBuildUrlMethod()
+        {
+            var methodInfo = typeof(ITraktUriBuildable).GetMethods()
+                                                       .Where(m => m.Name == "BuildUrl")
+                                                       .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(string));
+            methodInfo.GetParameters().Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void TestITraktUriBuildableHasGetUriPathParametersMethod()
+        {
+            var methodInfo = typeof(ITraktUriBuildable).GetMethods()
+                                                       .Where(m => m.Name == "GetUriPathParameters")
+                                                       .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
+            methodInfo.GetParameters().Should().BeEmpty();
+        }
+    }
+}
