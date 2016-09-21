5714c6bf063421e9d9ebae5c72c8228b9e4881ca
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 20:08:09 +0200

    Add tests for TraktPaginationResponse class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Responses/TraktPaginationResponseTests.cs
@@ -0,0 +1,35 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Responses
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Responses;
+
+    [TestClass]
+    public class TraktPaginationResponseTests
+    {
+        [TestMethod]
+        public void TestTraktPaginationResponseIsNotAbstract()
+        {
+            typeof(TraktPaginationResponse<>).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void TestTraktPaginationResponseIsSubclassOfTraktListResponse()
+        {
+            typeof(TraktPaginationResponse<int>).IsSubclassOf(typeof(TraktListResponse<int>)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestTraktPaginationResponseHasGenericTypeParameter()
+        {
+            typeof(TraktPaginationResponse<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(TraktPaginationResponse<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod]
+        public void TestTraktPaginationResponseImplementsITraktResponseHeadersInterface()
+        {
+            typeof(TraktPaginationResponse<>).GetInterfaces().Should().Contain(typeof(ITraktPaginationResponseHeaders));
+        }
+    }
+}
