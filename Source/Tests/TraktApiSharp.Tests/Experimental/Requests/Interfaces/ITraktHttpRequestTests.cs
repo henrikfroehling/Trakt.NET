4348bf9a1d258bc541a754c26bee07e864281062
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 23:23:43 +0200

    Add tests for ITraktHttpRequest interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktHttpRequestTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Net.Http;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ITraktHttpRequestTests
+    {
+        [TestMethod]
+        public void TestITraktHttpRequestIsInterface()
+        {
+            typeof(ITraktHttpRequest).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktHttpRequestHasMethodProperty()
+        {
+            var methodPropertyInfo = typeof(ITraktHttpRequest).GetProperties()
+                                                              .Where(p => p.Name == "Method")
+                                                              .FirstOrDefault();
+
+            methodPropertyInfo.CanRead.Should().BeTrue();
+            methodPropertyInfo.CanWrite.Should().BeFalse();
+            methodPropertyInfo.PropertyType.Should().Be(typeof(HttpMethod));
+        }
+    }
+}
