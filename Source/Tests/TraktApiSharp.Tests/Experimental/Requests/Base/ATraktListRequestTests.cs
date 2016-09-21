c36aa2ea4ae89b91058d805850e40e26ddbdec55
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 01:24:10 +0200

    Add tests for ATraktListRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/ATraktListRequestTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktListRequestTests
+    {
+        [TestMethod]
+        public void TestATraktListRequestIsAbstract()
+        {
+            typeof(ATraktListRequest<>).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktListRequestIsSubclassOfATraktBaseRequest()
+        {
+            typeof(ATraktListRequest<>).IsSubclassOf(typeof(ATraktBaseRequest)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktListRequestHasGenericTypeParameter()
+        {
+            typeof(ATraktListRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ATraktListRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod]
+        public void TestATraktListRequestImplementsITraktListRequestInterface()
+        {
+            typeof(ATraktListRequest<int>).GetInterfaces().Should().Contain(typeof(ITraktListRequest<int>));
+        }
+    }
+}
