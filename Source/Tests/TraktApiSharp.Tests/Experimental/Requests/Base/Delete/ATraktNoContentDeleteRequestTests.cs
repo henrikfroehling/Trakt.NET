0714a38289594d815fe04c87ee7fd4a970e1ff0f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 01:33:17 +0200

    Add tests for ATraktNoContentDeleteRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Delete/ATraktNoContentDeleteRequestTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Delete
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Delete;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktNoContentDeleteRequestTests
+    {
+        [TestMethod]
+        public void TestATraktNoContentDeleteRequestIsAbstract()
+        {
+            typeof(ATraktNoContentDeleteRequest).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentDeleteRequestIsSubclassOfATraktNoContentRequest()
+        {
+            typeof(ATraktNoContentDeleteRequest).IsSubclassOf(typeof(ATraktNoContentRequest)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentDeleteRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktNoContentDeleteRequest).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+    }
+}
