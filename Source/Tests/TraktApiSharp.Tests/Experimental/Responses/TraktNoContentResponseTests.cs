57e20f95fd6e62d475c120746eee689bf2836e15
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 19:12:25 +0200

    Add tests for class TraktNoContentResponse.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Responses/TraktNoContentResponseTests.cs
@@ -0,0 +1,42 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Responses
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Exceptions;
+    using TraktApiSharp.Experimental.Responses;
+
+    [TestClass]
+    public class TraktNoContentResponseTests
+    {
+        [TestMethod]
+        public void TestTraktNoContentResponseIsNotAbstract()
+        {
+            typeof(TraktNoContentResponse).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void TestTraktNoContentResponseHasIsSuccessProperty()
+        {
+            var isSuccessPropertyInfo = typeof(ATraktResponse<>).GetProperties()
+                                                                .Where(p => p.Name == "IsSuccess")
+                                                                .FirstOrDefault();
+
+            isSuccessPropertyInfo.CanRead.Should().BeTrue();
+            isSuccessPropertyInfo.CanWrite.Should().BeTrue();
+            isSuccessPropertyInfo.PropertyType.Should().Be(typeof(bool));
+        }
+
+        [TestMethod]
+        public void TestTraktNoContentResponseHasExceptionProperty()
+        {
+            var exceptionPropertyInfo = typeof(ATraktResponse<>).GetProperties()
+                                                                .Where(p => p.Name == "Exception")
+                                                                .FirstOrDefault();
+
+            exceptionPropertyInfo.CanRead.Should().BeTrue();
+            exceptionPropertyInfo.CanWrite.Should().BeTrue();
+            exceptionPropertyInfo.PropertyType.Should().Be(typeof(TraktException));
+        }
+    }
+}
