9dd5c9817264cada4aa91ae3aa6bba0a7acd7410
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 23:43:33 +0200

    Add tests for ITraktObjectRequest interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktObjectRequestTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+    using TraktApiSharp.Requests;
+
+    [TestClass]
+    public class ITraktObjectRequestTests
+    {
+        [TestMethod]
+        public void TestITraktObjectRequestIsInterface()
+        {
+            typeof(ITraktObjectRequest).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktObjectRequestHasRequestObjectTypeProperty()
+        {
+            var requestObjectTypePropertyInfo = typeof(ITraktObjectRequest).GetProperties()
+                                                                           .Where(p => p.Name == "RequestObjectType")
+                                                                           .FirstOrDefault();
+
+            requestObjectTypePropertyInfo.CanRead.Should().BeTrue();
+            requestObjectTypePropertyInfo.CanWrite.Should().BeFalse();
+            requestObjectTypePropertyInfo.PropertyType.Should().Be(typeof(TraktRequestObjectType?));
+        }
+    }
+}
