8f1a4cefdf7da292df57ed98041b53df07944146
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 01:07:36 +0200

    Add tests for TraktRequestBody class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/TraktRequestBodyTests.cs
@@ -0,0 +1,35 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class TraktRequestBodyTests
+    {
+        [TestMethod]
+        public void TestTraktRequestBodyIsNotAbstract()
+        {
+            typeof(TraktRequestBody<>).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void TestTraktRequestBodyIsSealed()
+        {
+            typeof(TraktRequestBody<>).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestTraktRequestBodyImplementsITraktPostableInterface()
+        {
+            typeof(TraktRequestBody<int>).GetInterfaces().Should().Contain(typeof(ITraktPostable<int>));
+        }
+
+        [TestMethod]
+        public void TestTraktRequestBodyImplementsITraktValidatableInterface()
+        {
+            typeof(TraktRequestBody<>).GetInterfaces().Should().Contain(typeof(ITraktValidatable));
+        }
+    }
+}
