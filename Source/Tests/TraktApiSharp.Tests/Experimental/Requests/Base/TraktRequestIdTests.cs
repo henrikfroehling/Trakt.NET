7445fb10a083888f50029658d03b7e64c0e1047f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 00:58:17 +0200

    Add tests for TraktRequestId class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/TraktRequestIdTests.cs
@@ -0,0 +1,29 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class TraktRequestIdTests
+    {
+        [TestMethod]
+        public void TestTraktRequestIdIsNotAbstract()
+        {
+            typeof(TraktRequestId).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void TestTraktRequestIdIsSealed()
+        {
+            typeof(TraktRequestId).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestTraktRequestIdImplementsITraktIdInterface()
+        {
+            typeof(TraktRequestId).GetInterfaces().Should().Contain(typeof(ITraktId));
+        }
+    }
+}
