79becdecb42d92e36f5ff7bea99b65c9ab7a3725
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-21 23:12:29 +0200

    Add tests for IsCheckinRequest interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktCheckinRequestTests.cs
@@ -0,0 +1,29 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ITraktCheckinRequestTests
+    {
+        [TestMethod]
+        public void TestITraktCheckinRequestIsInterface()
+        {
+            typeof(ITraktCheckinRequest).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktCheckinRequestHasIsCheckinRequestProperty()
+        {
+            var isCheckinRequestPropertyInfo = typeof(ITraktCheckinRequest).GetProperties()
+                                                                           .Where(p => p.Name == "IsCheckinRequest")
+                                                                           .FirstOrDefault();
+
+            isCheckinRequestPropertyInfo.CanRead.Should().BeTrue();
+            isCheckinRequestPropertyInfo.CanWrite.Should().BeFalse();
+            isCheckinRequestPropertyInfo.PropertyType.Should().Be(typeof(bool));
+        }
+    }
+}
