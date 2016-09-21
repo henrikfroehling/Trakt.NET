1e9f9aebc035648a775bf54552fdbecb3163e4dd
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 01:12:11 +0200

    Add tests for ITraktHasRequestBody interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/ITraktHasRequestBodyTests.cs
@@ -0,0 +1,41 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Base;
+
+    [TestClass]
+    public class ITraktHasRequestBodyTests
+    {
+        [TestMethod]
+        public void TestITraktHasRequestBodyIsInterface()
+        {
+            typeof(ITraktHasRequestBody<>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktHasRequestBodyHasRequestBodyContentProperty()
+        {
+            var requestBodyContentPropertyInfo = typeof(ITraktHasRequestBody<int>).GetProperties()
+                                                                                  .Where(p => p.Name == "RequestBodyContent")
+                                                                                  .FirstOrDefault();
+
+            requestBodyContentPropertyInfo.CanRead.Should().BeTrue();
+            requestBodyContentPropertyInfo.CanWrite.Should().BeTrue();
+            requestBodyContentPropertyInfo.PropertyType.Should().Be(typeof(int));
+        }
+
+        [TestMethod]
+        public void TestITraktHasRequestBodyHasRequestBodyProperty()
+        {
+            var requestBodyPropertyInfo = typeof(ITraktHasRequestBody<int>).GetProperties()
+                                                                           .Where(p => p.Name == "RequestBody")
+                                                                           .FirstOrDefault();
+
+            requestBodyPropertyInfo.CanRead.Should().BeTrue();
+            requestBodyPropertyInfo.CanWrite.Should().BeTrue();
+            requestBodyPropertyInfo.PropertyType.Should().Be(typeof(TraktRequestBody<int>));
+        }
+    }
+}
