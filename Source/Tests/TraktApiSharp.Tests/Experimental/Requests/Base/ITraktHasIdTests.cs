b5a8e8e0922bde32cf7066d0292fe44ddf3a5bcf
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 01:02:59 +0200

    Add tests for ITraktHasId interface.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/ITraktHasIdTests.cs
@@ -0,0 +1,41 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Base;
+
+    [TestClass]
+    public class ITraktHasIdTests
+    {
+        [TestMethod]
+        public void TestITraktHasIdIsInterface()
+        {
+            typeof(ITraktHasId).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestITraktHasIdHasIdProperty()
+        {
+            var idPropertyInfo = typeof(ITraktHasId).GetProperties()
+                                                    .Where(p => p.Name == "Id")
+                                                    .FirstOrDefault();
+
+            idPropertyInfo.CanRead.Should().BeTrue();
+            idPropertyInfo.CanWrite.Should().BeTrue();
+            idPropertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [TestMethod]
+        public void TestITraktHasIdHasRequestIdProperty()
+        {
+            var requestIdPropertyInfo = typeof(ITraktHasId).GetProperties()
+                                                           .Where(p => p.Name == "RequestId")
+                                                           .FirstOrDefault();
+
+            requestIdPropertyInfo.CanRead.Should().BeTrue();
+            requestIdPropertyInfo.CanWrite.Should().BeTrue();
+            requestIdPropertyInfo.PropertyType.Should().Be(typeof(TraktRequestId));
+        }
+    }
+}
