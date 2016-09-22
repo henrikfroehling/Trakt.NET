6fecbc4495388c88abbb7f5a3bc7ada3626e2431
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 02:48:11 +0200

    Add tests for ATraktPaginationPutByIdRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Put/ATraktPaginationPutByIdRequestTests.cs
@@ -0,0 +1,49 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Put
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Put;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktPaginationPutByIdRequestTests
+    {
+        [TestMethod]
+        public void TestATraktPaginationPutByIdRequestIsAbstract()
+        {
+            typeof(ATraktPaginationPutByIdRequest<,>).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktPaginationPutByIdRequestIsSubclassOfATraktPaginationRequest()
+        {
+            typeof(ATraktPaginationPutByIdRequest<int, float>).IsSubclassOf(typeof(ATraktPaginationRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktPaginationPutByIdRequestHasGenericTypeParameter()
+        {
+            typeof(ATraktPaginationPutByIdRequest<,>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ATraktPaginationPutByIdRequest<int, float>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(2);
+        }
+
+        [TestMethod]
+        public void TestATraktPaginationPutByIdRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktPaginationPutByIdRequest<,>).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+
+        [TestMethod]
+        public void TestATraktPaginationPutByIdRequestImplementsITraktHasRequestBodyInterface()
+        {
+            typeof(ATraktPaginationPutByIdRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasRequestBody<float>));
+        }
+
+        [TestMethod]
+        public void TestATraktPaginationPutByIdRequestImplementsITraktHasIdInterface()
+        {
+            typeof(ATraktPaginationPutByIdRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
