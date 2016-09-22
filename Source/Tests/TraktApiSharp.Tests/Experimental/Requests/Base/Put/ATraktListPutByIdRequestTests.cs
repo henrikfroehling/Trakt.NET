f2d4b97e014687475f0379d3c16f81db7ae97ee8
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 02:40:36 +0200

    Add tests for ATraktListPutByIdRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Put/ATraktListPutByIdRequestTests.cs
@@ -0,0 +1,49 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Put
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Put;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktListPutByIdRequestTests
+    {
+        [TestMethod]
+        public void TestATraktListPutByIdRequestIsAbstract()
+        {
+            typeof(ATraktListPutByIdRequest<,>).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktListPutByIdRequestIsSubclassOfATraktListRequest()
+        {
+            typeof(ATraktListPutByIdRequest<int, float>).IsSubclassOf(typeof(ATraktListRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktListPutByIdRequestHasGenericTypeParameter()
+        {
+            typeof(ATraktListPutByIdRequest<,>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ATraktListPutByIdRequest<int, float>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(2);
+        }
+
+        [TestMethod]
+        public void TestATraktListPutByIdRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktListPutByIdRequest<,>).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+
+        [TestMethod]
+        public void TestATraktListPutByIdRequestImplementsITraktHasRequestBodyInterface()
+        {
+            typeof(ATraktListPutByIdRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasRequestBody<float>));
+        }
+
+        [TestMethod]
+        public void TestATraktListPutByIdRequestImplementsITraktHasIdInterface()
+        {
+            typeof(ATraktListPutByIdRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
