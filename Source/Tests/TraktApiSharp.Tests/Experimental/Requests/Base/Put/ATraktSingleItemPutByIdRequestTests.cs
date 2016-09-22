beb766d808dbd9726eb18233b9e9fd7cfce1a10d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 02:50:30 +0200

    Add tests for ATraktSingleItemPutByIdRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Put/ATraktSingleItemPutByIdRequestTests.cs
@@ -0,0 +1,49 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Put
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Put;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktSingleItemPutByIdRequestTests
+    {
+        [TestMethod]
+        public void TestATraktSingleItemPutByIdRequestIsAbstract()
+        {
+            typeof(ATraktSingleItemPutByIdRequest<,>).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemPutByIdRequestIsSubclassOfATraktSingleItemRequest()
+        {
+            typeof(ATraktSingleItemPutByIdRequest<int, float>).IsSubclassOf(typeof(ATraktSingleItemRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemPutByIdRequestHasGenericTypeParameter()
+        {
+            typeof(ATraktSingleItemPutByIdRequest<,>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ATraktSingleItemPutByIdRequest<int, float>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(2);
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemPutByIdRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktSingleItemPutByIdRequest<,>).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemPutByIdRequestImplementsITraktHasRequestBodyInterface()
+        {
+            typeof(ATraktSingleItemPutByIdRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasRequestBody<float>));
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemPutByIdRequestImplementsITraktHasIdInterface()
+        {
+            typeof(ATraktSingleItemPutByIdRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
