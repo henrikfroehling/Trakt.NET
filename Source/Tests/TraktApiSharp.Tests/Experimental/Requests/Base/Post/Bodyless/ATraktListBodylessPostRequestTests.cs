767578b7e51e8c6987e127e8f41d06175ab4d236
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 03:32:48 +0200

    Add tests for ATraktListBodylessPostRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Post/Bodyless/ATraktListBodylessPostRequestTests.cs
@@ -0,0 +1,37 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Post.Bodyless
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Post.Bodyless;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktListBodylessPostRequestTests
+    {
+        [TestMethod]
+        public void TestATraktListBodylessPostRequestIsAbstract()
+        {
+            typeof(ATraktListBodylessPostRequest<>).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktListBodylessPostRequestIsSubclassOfATraktListRequest()
+        {
+            typeof(ATraktListBodylessPostRequest<int>).IsSubclassOf(typeof(ATraktListRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktListBodylessPostRequestHasGenericTypeParameter()
+        {
+            typeof(ATraktListBodylessPostRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ATraktListBodylessPostRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod]
+        public void TestATraktListBodylessPostRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktListBodylessPostRequest<>).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+    }
+}
