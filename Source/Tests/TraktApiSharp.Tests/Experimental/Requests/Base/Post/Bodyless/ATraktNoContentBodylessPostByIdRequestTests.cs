668a47400e9e5ee80f501ce2b0bad2c8800848f8
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 03:43:24 +0200

    Add tests for ATraktNoContentBodylessPostByIdRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Post/Bodyless/ATraktNoContentBodylessPostByIdRequestTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Post.Bodyless
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Post.Bodyless;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktNoContentBodylessPostByIdRequestTests
+    {
+        [TestMethod]
+        public void TestATraktNoContentBodylessPostByIdRequestIsAbstract()
+        {
+            typeof(ATraktNoContentBodylessPostByIdRequest).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentBodylessPostByIdRequestIsSubclassOfATraktNoContentRequest()
+        {
+            typeof(ATraktNoContentBodylessPostByIdRequest).IsSubclassOf(typeof(ATraktNoContentRequest)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentBodylessPostByIdRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktNoContentBodylessPostByIdRequest).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentBodylessPostByIdRequestImplementsITraktHasIdInterface()
+        {
+            typeof(ATraktNoContentBodylessPostByIdRequest).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
