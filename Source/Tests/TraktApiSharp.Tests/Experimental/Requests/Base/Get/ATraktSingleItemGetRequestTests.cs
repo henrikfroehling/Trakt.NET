1c0f704f383e4da0538bced9f88fa2b9fb3a18d3
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 02:04:41 +0200

    Add  tests for ATraktSingleItemGetRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Get/ATraktSingleItemGetRequestTests.cs
@@ -0,0 +1,37 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Get
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktSingleItemGetRequestTests
+    {
+        [TestMethod]
+        public void TestATraktSingleItemGetRequestIsAbstract()
+        {
+            typeof(ATraktSingleItemGetRequest<>).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemGetRequestIsSubclassOfATraktSingleItemRequest()
+        {
+            typeof(ATraktSingleItemGetRequest<int>).IsSubclassOf(typeof(ATraktSingleItemRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemGetRequestHasGenericTypeParameter()
+        {
+            typeof(ATraktSingleItemGetRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ATraktSingleItemGetRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemGetRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktSingleItemGetRequest<>).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+    }
+}
