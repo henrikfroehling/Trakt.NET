bf82ccfb071558e21c232509fdb5f6c82411524f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 02:10:34 +0200

    Add tests for ATraktNoContentGetByIdRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Get/ATraktNoContentGetByIdRequestTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Get
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktNoContentGetByIdRequestTests
+    {
+        [TestMethod]
+        public void TestATraktNoContentGetByIdRequestIsAbstract()
+        {
+            typeof(ATraktNoContentGetByIdRequest).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentGetByIdRequestIsSubclassOfATraktNoContentRequest()
+        {
+            typeof(ATraktNoContentGetByIdRequest).IsSubclassOf(typeof(ATraktNoContentRequest)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentGetByIdRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktNoContentGetByIdRequest).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+
+        [TestMethod]
+        public void TestATraktNoContentGetByIdRequestImplementsITraktHasIdInterface()
+        {
+            typeof(ATraktNoContentGetByIdRequest).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
