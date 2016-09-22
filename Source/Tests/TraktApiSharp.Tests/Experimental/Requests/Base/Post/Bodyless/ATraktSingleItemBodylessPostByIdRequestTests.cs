47311d8a6995cb76ad856e33cda374325d321a65
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 03:47:49 +0200

    Add tests for ATraktSingleItemBodylessPostByIdRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Post/Bodyless/ATraktSingleItemBodylessPostByIdRequestTests.cs
@@ -0,0 +1,43 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Post.Bodyless
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Post.Bodyless;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktSingleItemBodylessPostByIdRequestTests
+    {
+        [TestMethod]
+        public void TestATraktSingleItemBodylessPostByIdRequestIsAbstract()
+        {
+            typeof(ATraktSingleItemBodylessPostByIdRequest<>).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemBodylessPostByIdRequestIsSubclassOfATraktSingleItemRequest()
+        {
+            typeof(ATraktSingleItemBodylessPostByIdRequest<int>).IsSubclassOf(typeof(ATraktSingleItemRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemBodylessPostByIdRequestHasGenericTypeParameter()
+        {
+            typeof(ATraktSingleItemBodylessPostByIdRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ATraktSingleItemBodylessPostByIdRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemBodylessPostByIdRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktSingleItemBodylessPostByIdRequest<>).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+
+        [TestMethod]
+        public void TestATraktSingleItemBodylessPostByIdRequestImplementsITraktHasIdInterface()
+        {
+            typeof(ATraktSingleItemBodylessPostByIdRequest<>).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
