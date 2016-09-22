45e16eaf59bc201a5e3325dd74381498871cd1ce
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 02:30:13 +0200

    Add tests for ATraktListPutRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Put/ATraktListPutRequestTests.cs
@@ -0,0 +1,43 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Put
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Put;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktListPutRequestTests
+    {
+        [TestMethod]
+        public void TestATraktListPutRequestIsAbstract()
+        {
+            typeof(ATraktListPutRequest<,>).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktListPutRequestIsSubclassOfATraktListRequest()
+        {
+            typeof(ATraktListPutRequest<int, float>).IsSubclassOf(typeof(ATraktListRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktListPutRequestHasGenericTypeParameter()
+        {
+            typeof(ATraktListPutRequest<,>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ATraktListPutRequest<int, float>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(2);
+        }
+
+        [TestMethod]
+        public void TestATraktListPutRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktListPutRequest<,>).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+
+        [TestMethod]
+        public void TestATraktListPutRequestImplementsITraktHasRequestBodyInterface()
+        {
+            typeof(ATraktListPutRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasRequestBody<float>));
+        }
+    }
+}
