d384506b89f223580d5873662a84244d45558780
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-22 02:12:34 +0200

    Add tests for ATraktPaginationGetByIdRequest class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Get/ATraktPaginationGetByIdRequestTests.cs
@@ -0,0 +1,43 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Base.Get
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ATraktPaginationGetByIdRequestTests
+    {
+        [TestMethod]
+        public void TestATraktPaginationGetByIdRequestIsAbstract()
+        {
+            typeof(ATraktPaginationGetByIdRequest<>).IsAbstract.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktPaginationGetByIdRequestIsSubclassOfATraktPaginationRequest()
+        {
+            typeof(ATraktPaginationGetByIdRequest<int>).IsSubclassOf(typeof(ATraktPaginationRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void TestATraktPaginationGetByIdRequestHasGenericTypeParameter()
+        {
+            typeof(ATraktPaginationGetByIdRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ATraktPaginationGetByIdRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod]
+        public void TestATraktPaginationGetByIdRequestImplementsITraktRequestInterface()
+        {
+            typeof(ATraktPaginationGetByIdRequest<>).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+
+        [TestMethod]
+        public void TestATraktPaginationGetByIdRequestImplementsITraktHasIdInterface()
+        {
+            typeof(ATraktPaginationGetByIdRequest<>).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
