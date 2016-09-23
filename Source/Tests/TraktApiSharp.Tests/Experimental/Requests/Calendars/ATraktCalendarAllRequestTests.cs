3eb607c72b1ba0960b163092f9ddddb9a5701042
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-23 02:10:45 +0200

    Add base class for calendar requests without oauth.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/ATraktCalendarAllRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Calendars
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Calendars;
+
+    [TestClass]
+    public class ATraktCalendarAllRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("Without OAuth")]
+        public void TestATraktCalendarAllRequestIsAbstract()
+        {
+            typeof(ATraktCalendarAllRequest).IsAbstract.Should().BeTrue();
+        }
+    }
+}
