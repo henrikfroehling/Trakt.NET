1f99a4dd0a90a2d5ad6bf40ce4f48b886937630c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-23 21:51:58 +0200

    Add start date property.

--- Source/Lib/TraktApiSharp/Experimental/Requests/Calendars/OAuth/ATraktCalendarUserRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Calendars/OAuth/ATraktCalendarUserRequest.cs
@@ -1,9 +1,12 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Calendars.OAuth
 {
     using Base.Get;
+    using System;
 
     internal abstract class ATraktCalendarUserRequest<T> : ATraktListGetRequest<T>
     {
         public ATraktCalendarUserRequest(TraktClient client) : base(client) { }
+
+        internal DateTime? StartDate { get; set; }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/OAuth/ATraktCalendarUserRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/OAuth/ATraktCalendarUserRequestTests.cs
@@ -2,11 +2,14 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Linq;
+    using System.Reflection;
     using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Calendars.OAuth;
 
     [TestClass]
-    public class TraktCalendarUserRequestTests
+    public class ATraktCalendarUserRequestTests
     {
         [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("With OAuth")]
         public void TestATraktCalendarUserRequestIsAbstract()
@@ -25,6 +28,19 @@
         public void TestATraktCalendarUserRequestIsSubclassOfATraktListGetRequest()
         {
             typeof(ATraktCalendarUserRequest<int>).IsSubclassOf(typeof(ATraktListGetRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("With OAuth")]
+        public void TestATraktCalendarUserRequestHasStartDateProperty()
+        {
+            var startDatePropertyInfo = typeof(ATraktCalendarUserRequest<>)
+                    .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.Name == "StartDate")
+                    .FirstOrDefault();
+
+            startDatePropertyInfo.CanRead.Should().BeTrue();
+            startDatePropertyInfo.CanWrite.Should().BeTrue();
+            startDatePropertyInfo.PropertyType.Should().Be(typeof(DateTime?));
         }
     }
 }
