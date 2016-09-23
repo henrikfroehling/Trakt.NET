3f6b7b326358cc4b0b90320d3d97008124be0ad2
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-23 05:29:46 +0200

    Add calendar all season premieres request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/TraktCalendarAllSeasonPremieresRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Calendars
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Calendars;
+
+    [TestClass]
+    public class TraktCalendarAllSeasonPremieresRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("Without OAuth"), TestCategory("Seasons")]
+        public void TestTraktCalendarAllSeasonPremieresRequestIsNotAbstract()
+        {
+            typeof(TraktCalendarAllSeasonPremieresRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
