13ce469994807b6ae574a376e27b359732147b7c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-23 22:13:19 +0200

    Add calendar user new shows request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/OAuth/TraktCalendarUserNewShowsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Calendars.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Calendars.OAuth;
+
+    [TestClass]
+    public class TraktCalendarUserNewShowsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("With OAuth"), TestCategory("Shows")]
+        public void TestTraktCalendarUserNewShowsRequestIsNotAbstract()
+        {
+            typeof(TraktCalendarUserNewShowsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
