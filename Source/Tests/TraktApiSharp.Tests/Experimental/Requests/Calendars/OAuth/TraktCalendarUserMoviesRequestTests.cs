abba1c06d82fad0e7930c8ee32df1e811a71d0df
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-23 21:59:33 +0200

    Add calendar user movies request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/OAuth/TraktCalendarUserMoviesRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Calendars.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Calendars.OAuth;
+
+    [TestClass]
+    public class TraktCalendarUserMoviesRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("With OAuth"), TestCategory("Movies")]
+        public void TestTraktCalendarUserMoviesRequestIsNotAbstract()
+        {
+            typeof(TraktCalendarUserMoviesRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
