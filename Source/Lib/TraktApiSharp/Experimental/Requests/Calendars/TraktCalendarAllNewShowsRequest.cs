2041d5a2f97fa243a023052f0c98eaac32f2f405
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-23 05:15:02 +0200

    Add calendar all new shows request class.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Calendars/TraktCalendarAllNewShowsRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Calendars
+{
+    internal class TraktCalendarAllNewShowsRequest
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/TraktCalendarAllNewShowsRequestTests.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Calendars
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Calendars;
+
+    [TestClass]
+    public class TraktCalendarAllNewShowsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("Without OAuth"), TestCategory("Shows")]
+        public void TestTraktCalendarAllNewShowsRequestIsNotAbstract()
+        {
+            typeof(TraktCalendarAllNewShowsRequest).IsAbstract.Should().BeFalse();
+        }
+
+    }
+}
