a0a396e3b0e5f03f58005f00653955cb1df6f56e
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-23 22:30:13 +0200

    Derive TraktCalendarUserSeasonPremieresRequest from ATraktCalendarUserRequest.

--- Source/Lib/TraktApiSharp/Experimental/Requests/Calendars/OAuth/TraktCalendarUserSeasonPremieresRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Calendars/OAuth/TraktCalendarUserSeasonPremieresRequest.cs
@@ -1,7 +1,27 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Calendars.OAuth
 {
-    internal sealed class TraktCalendarUserSeasonPremieresRequest
-    {
+    using Objects.Get.Calendars;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktCalendarUserSeasonPremieresRequest : ATraktCalendarUserRequest<TraktCalendarShow>
+    {
+        public TraktCalendarUserSeasonPremieresRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
