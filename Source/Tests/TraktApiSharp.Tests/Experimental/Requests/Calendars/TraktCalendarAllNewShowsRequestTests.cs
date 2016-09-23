48caf532f6a3dfd8e5689a6688c173b30673ed4b
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-23 05:22:13 +0200

    Set authorization of TraktCalendarAllNewShowsRequest not required.

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/TraktCalendarAllNewShowsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/TraktCalendarAllNewShowsRequestTests.cs
@@ -4,6 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Calendars;
     using TraktApiSharp.Objects.Get.Calendars;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktCalendarAllNewShowsRequestTests
@@ -24,6 +25,13 @@
         public void TestTraktCalendarAllNewShowsRequestIsSubclassOfATraktCalendarAllRequest()
         {
             typeof(TraktCalendarAllNewShowsRequest).IsSubclassOf(typeof(ATraktCalendarAllRequest<TraktCalendarShow>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("Without OAuth"), TestCategory("Shows")]
+        public void TestTraktCalendarAllNewShowsRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktCalendarAllNewShowsRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
 
     }
