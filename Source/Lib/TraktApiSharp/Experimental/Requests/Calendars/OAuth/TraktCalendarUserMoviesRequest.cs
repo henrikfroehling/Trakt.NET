8e55c30b6fd950fbf129de5df9059bdbaf0eb0fc
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-23 22:04:03 +0200

    Derive TraktCalendarUserMoviesRequest from ATraktCalendarUserRequest.

--- Source/Lib/TraktApiSharp/Experimental/Requests/Calendars/OAuth/TraktCalendarUserMoviesRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Calendars/OAuth/TraktCalendarUserMoviesRequest.cs
@@ -1,7 +1,27 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Calendars.OAuth
 {
-    internal sealed class TraktCalendarUserMoviesRequest
-    {
+    using Objects.Get.Calendars;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktCalendarUserMoviesRequest : ATraktCalendarUserRequest<TraktCalendarMovie>
+    {
+        public TraktCalendarUserMoviesRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/OAuth/TraktCalendarUserMoviesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/OAuth/TraktCalendarUserMoviesRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Calendars.OAuth;
+    using TraktApiSharp.Objects.Get.Calendars;
 
     [TestClass]
     public class TraktCalendarUserMoviesRequestTests
@@ -17,6 +18,12 @@
         public void TestTraktCalendarUserMoviesRequestIsSealed()
         {
             typeof(TraktCalendarUserMoviesRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("With OAuth"), TestCategory("Movies")]
+        public void TestTraktCalendarUserMoviesRequestIsSubclassOfATraktCalendarAllRequest()
+        {
+            typeof(TraktCalendarUserMoviesRequest).IsSubclassOf(typeof(ATraktCalendarUserRequest<TraktCalendarMovie>)).Should().BeTrue();
         }
     }
 }
