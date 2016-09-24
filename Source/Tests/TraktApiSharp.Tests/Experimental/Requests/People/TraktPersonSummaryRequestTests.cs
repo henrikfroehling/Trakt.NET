5bef7d7e3814dddb00056e2055d0714dfe69b4d7
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-24 04:45:33 +0200

    Add people summary request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/People/TraktPersonSummaryRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.People
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.People;
+
+    [TestClass]
+    public class TraktPersonSummaryRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("People")]
+        public void TestTraktPersonSummaryRequestIsNotAbstract()
+        {
+            typeof(TraktPersonSummaryRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
