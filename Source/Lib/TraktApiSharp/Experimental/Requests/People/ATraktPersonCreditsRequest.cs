e7b8a843e5714e455af88fe2b9dc302229653c57
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-24 05:25:07 +0200

    Implement ITraktObjectRequest in ATraktPersonCreditsRequest.

--- Source/Lib/TraktApiSharp/Experimental/Requests/People/ATraktPersonCreditsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/People/ATraktPersonCreditsRequest.cs
@@ -1,9 +1,20 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.People
 {
     using Base.Get;
+    using Interfaces;
+    using System;
+    using TraktApiSharp.Requests;
 
-    internal abstract class ATraktPersonCreditsRequest<T> : ATraktSingleItemGetByIdRequest<T>
+    internal abstract class ATraktPersonCreditsRequest<T> : ATraktSingleItemGetByIdRequest<T>, ITraktObjectRequest
     {
         public ATraktPersonCreditsRequest(TraktClient client) : base(client) { }
+
+        public TraktRequestObjectType? RequestObjectType
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
