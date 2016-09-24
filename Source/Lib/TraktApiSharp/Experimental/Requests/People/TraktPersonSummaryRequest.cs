099d527c5dd47c7281ec604ca6726f5440ec2981
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-24 04:50:32 +0200

    Derive TraktPersonSummaryRequest from ATraktSingleItemGetByIdRequest.

--- Source/Lib/TraktApiSharp/Experimental/Requests/People/TraktPersonSummaryRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/People/TraktPersonSummaryRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.People
 {
-    internal sealed class TraktPersonSummaryRequest
-    {
+    using Base.Get;
+    using Objects.Get.People;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktPersonSummaryRequest : ATraktSingleItemGetByIdRequest<TraktPerson>
+    {
+        public TraktPersonSummaryRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/People/TraktPersonSummaryRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/People/TraktPersonSummaryRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.People;
+    using TraktApiSharp.Objects.Get.People;
 
     [TestClass]
     public class TraktPersonSummaryRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktPersonSummaryRequestIsSealed()
         {
             typeof(TraktPersonSummaryRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("People")]
+        public void TestTraktPersonSummaryRequestIsSubclassOfATraktSingleItemGetByIdRequest()
+        {
+            typeof(TraktPersonSummaryRequest).IsSubclassOf(typeof(ATraktSingleItemGetByIdRequest<TraktPerson>)).Should().BeTrue();
         }
     }
 }
