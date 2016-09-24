357b587b345759512d49dd03947a1e25aa98fbfd
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-24 04:17:55 +0200

    Add genres shows request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Genres/TraktGenresShowsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Genres
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Genres;
+
+    [TestClass]
+    public class TraktGenresShowsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Genres"), TestCategory("Shows")]
+        public void TestTraktGenresShowsRequestIsNotAbstract()
+        {
+            typeof(TraktGenresShowsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
