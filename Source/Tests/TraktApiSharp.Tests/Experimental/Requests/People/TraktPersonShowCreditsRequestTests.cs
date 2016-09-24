03eca6c865959880b1456062db16f17763723e3b
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-24 05:49:18 +0200

    Add people show credits request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/People/TraktPersonShowCreditsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.People
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.People;
+
+    [TestClass]
+    public class TraktPersonShowCreditsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("People"), TestCategory("Credits"), TestCategory("Show")]
+        public void TestTraktPersonShowCreditsRequestIsNotAbstract()
+        {
+            typeof(TraktPersonShowCreditsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
