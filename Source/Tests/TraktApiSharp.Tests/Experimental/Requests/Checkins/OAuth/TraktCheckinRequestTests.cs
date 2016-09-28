1cb4953f916aa12b8a5867c43d39696fb4bb4f07
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-29 01:41:21 +0200

    Add checkin request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Checkins/OAuth/TraktCheckinRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Checkins.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Checkins.OAuth;
+
+    [TestClass]
+    public class TraktCheckinRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Checkins"), TestCategory("With OAuth")]
+        public void TestTraktCheckinRequestIsNotAbstract()
+        {
+            typeof(TraktCheckinRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
