7b20955dc374e9c78f41acf2006cef1439c8ec54
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-29 02:26:52 +0200

    Add checkin delete request class.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Checkins/OAuth/TraktCheckinsDeleteRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Checkins.OAuth
+{
+    internal class TraktCheckinsDeleteRequest
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Checkins/OAuth/TraktCheckinsDeleteRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Checkins.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Checkins.OAuth;
+
+    [TestClass]
+    public class TraktCheckinsDeleteRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Checkins"), TestCategory("With OAuth")]
+        public void TestTraktCheckinsDeleteRequestIsNotAbstract()
+        {
+            typeof(TraktCheckinsDeleteRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
