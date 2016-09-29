965886c273157b062d462a9c506a1da8257b79ac
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-29 05:16:13 +0200

    Add movie recommendation hide request class.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Recommendations/OAuth/TraktUserRecommendationHideMovieRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Recommendations.OAuth
+{
+    internal class TraktUserRecommendationHideMovieRequest
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Recommendations/OAuth/TraktUserRecommendationHideMovieRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Recommendations.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Recommendations.OAuth;
+
+    [TestClass]
+    public class TraktUserRecommendationHideMovieRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Recommendations"), TestCategory("With OAuth"), TestCategory("Movies")]
+        public void TestTraktUserRecommendationHideMovieRequestIsNotAbstract()
+        {
+            typeof(TraktUserRecommendationHideMovieRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
