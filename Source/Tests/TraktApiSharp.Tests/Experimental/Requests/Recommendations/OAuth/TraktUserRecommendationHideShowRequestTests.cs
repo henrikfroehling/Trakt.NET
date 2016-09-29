931950b29da64d64c791ee91e16b03269b4d9794
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-29 05:31:58 +0200

    Add show recommendation hide request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Recommendations/OAuth/TraktUserRecommendationHideShowRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Recommendations.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Recommendations.OAuth;
+
+    [TestClass]
+    public class TraktUserRecommendationHideShowRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Recommendations"), TestCategory("With OAuth"), TestCategory("Shows")]
+        public void TestTraktUserRecommendationHideShowRequestIsNotAbstract()
+        {
+            typeof(TraktUserRecommendationHideShowRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
