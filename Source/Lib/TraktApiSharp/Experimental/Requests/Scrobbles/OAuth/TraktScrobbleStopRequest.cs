3db19f069fbe63e9227856b4fc659d29a5358b70
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-29 06:27:13 +0200

    Add scrobble stop request class.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Scrobbles/OAuth/TraktScrobbleStopRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Scrobbles.OAuth
+{
+    internal class TraktScrobbleStopRequest
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Scrobbles/OAuth/TraktScrobbleStopRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Scrobbles.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Scrobbles.OAuth;
+
+    [TestClass]
+    public class TraktScrobbleStopRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Scrobbles"), TestCategory("With OAuth"), TestCategory("Stop")]
+        public void TestTraktScrobbleStopRequestIsNotAbstract()
+        {
+            typeof(TraktScrobbleStopRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
