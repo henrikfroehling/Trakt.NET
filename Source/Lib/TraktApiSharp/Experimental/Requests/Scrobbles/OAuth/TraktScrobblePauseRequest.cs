013c83d7ea3aa2104f82ccf25deb21fda5515af2
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-29 06:22:24 +0200

    Derive TraktScrobblePauseRequest from ATraktSingleItemPostRequest.

--- Source/Lib/TraktApiSharp/Experimental/Requests/Scrobbles/OAuth/TraktScrobblePauseRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Scrobbles/OAuth/TraktScrobblePauseRequest.cs
@@ -1,7 +1,18 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Scrobbles.OAuth
 {
-    internal sealed class TraktScrobblePauseRequest<TItem, TRequestBody>
-    {
+    using Base.Post;
+    using System;
 
+    internal sealed class TraktScrobblePauseRequest<TItem, TRequestBody> : ATraktSingleItemPostRequest<TItem, TRequestBody>
+    {
+        public TraktScrobblePauseRequest(TraktClient client) : base(client) { }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
