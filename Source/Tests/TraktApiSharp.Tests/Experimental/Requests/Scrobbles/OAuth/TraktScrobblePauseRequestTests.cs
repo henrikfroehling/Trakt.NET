4bf9072a21135032b37e5e67b084cf3c90d6bbb6
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-29 06:18:16 +0200

    Add scrobble pause request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Scrobbles/OAuth/TraktScrobblePauseRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Scrobbles.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Scrobbles.OAuth;
+
+    [TestClass]
+    public class TraktScrobblePauseRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Scrobbles"), TestCategory("With OAuth"), TestCategory("Pause")]
+        public void TestTraktScrobblePauseRequestIsNotAbstract()
+        {
+            typeof(TraktScrobblePauseRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
