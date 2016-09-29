d4f3f0df48944f8926ec8ed8048e5a1d1e28b7a3
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-29 04:43:25 +0200

    Implement ITraktExtendedInfo in ATraktUserRecommendationsRequest.

--- Source/Lib/TraktApiSharp/Experimental/Requests/Recommendations/OAuth/ATraktUserRecommendationsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Recommendations/OAuth/ATraktUserRecommendationsRequest.cs
@@ -1,9 +1,13 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Recommendations.OAuth
 {
     using Base.Get;
+    using Interfaces;
+    using TraktApiSharp.Requests.Params;
 
-    internal abstract class ATraktUserRecommendationsRequest<TItem> : ATraktPaginationGetRequest<TItem>
+    internal abstract class ATraktUserRecommendationsRequest<TItem> : ATraktPaginationGetRequest<TItem>, ITraktExtendedInfo
     {
         public ATraktUserRecommendationsRequest(TraktClient client) : base(client) { }
+
+        public TraktExtendedOption ExtendedOption { get; set; }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Recommendations/OAuth/ATraktUserRecommendationsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Recommendations/OAuth/ATraktUserRecommendationsRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Recommendations.OAuth;
 
     [TestClass]
@@ -25,6 +26,12 @@
         public void TestATraktUserRecommendationsRequestIsSubclassOfATraktPaginationGetRequest()
         {
             typeof(ATraktUserRecommendationsRequest<int>).IsSubclassOf(typeof(ATraktPaginationGetRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Recommendations"), TestCategory("With OAuth")]
+        public void TestATraktUserRecommendationsRequestImplementsITraktExtendedInfoInterface()
+        {
+            typeof(ATraktUserRecommendationsRequest<>).GetInterfaces().Should().Contain(typeof(ITraktExtendedInfo));
         }
     }
 }
