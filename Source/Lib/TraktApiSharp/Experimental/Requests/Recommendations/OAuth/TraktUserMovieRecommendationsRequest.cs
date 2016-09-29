ac26a55057230b2630d4ff61092d8f751c40c0eb
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-29 04:49:40 +0200

    Add movie recommendations request class.

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Recommendations/OAuth/TraktUserMovieRecommendationsRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Recommendations.OAuth
+{
+    internal class TraktUserMovieRecommendationsRequest
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Recommendations/OAuth/TraktUserMovieRecommendationsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Recommendations.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Recommendations.OAuth;
+
+    [TestClass]
+    public class TraktUserMovieRecommendationsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Recommendations"), TestCategory("With OAuth"), TestCategory("Movies")]
+        public void TestTraktUserMovieRecommendationsRequestIsNotAbstract()
+        {
+            typeof(TraktUserMovieRecommendationsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
