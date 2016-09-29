4f9f3af024451013b38c3a202fb874bc5a9feb8d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-29 06:06:53 +0200

    Add scrobble start request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Scrobbles/OAuth/TraktScrobbleStartRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Scrobbles.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Scrobbles.OAuth;
+
+    [TestClass]
+    public class TraktScrobbleStartRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Scrobbles"), TestCategory("With OAuth"), TestCategory("Start")]
+        public void TestTraktScrobbleStartRequestIsNotAbstract()
+        {
+            typeof(TraktScrobbleStartRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
