fd61dbc300e238adf4ecb9b7a822c07a60618595
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-30 21:38:31 +0200

    Set authorization of TraktSearchTextQueryRequest not required.

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Search/TraktSearchTextQueryRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Search/TraktSearchTextQueryRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Search;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktSearchTextQueryRequestTests
@@ -23,6 +24,13 @@
         public void TestTraktSearchTextQueryRequestIsSubclassOfATraktSearchRequest()
         {
             typeof(TraktSearchTextQueryRequest).IsSubclassOf(typeof(ATraktSearchRequest)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Search"), TestCategory("Without OAuth"), TestCategory("Search Text Query")]
+        public void TestTraktSearchTextQueryRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktSearchTextQueryRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
     }
 }
