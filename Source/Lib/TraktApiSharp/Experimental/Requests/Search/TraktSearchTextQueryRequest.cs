7f57a16774d4e0a677c7f41c5b441317d412ffb3
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-30 22:03:14 +0200

    Override GetUriPathParameters method in TraktSearchTextQueryRequest.

--- Source/Lib/TraktApiSharp/Experimental/Requests/Search/TraktSearchTextQueryRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Search/TraktSearchTextQueryRequest.cs
@@ -2,6 +2,8 @@
 {
     using Enums;
     using Interfaces;
+    using System;
+    using System.Collections.Generic;
     using TraktApiSharp.Requests;
     using TraktApiSharp.Requests.Params;
 
@@ -16,6 +18,25 @@
         internal TraktSearchField SearchFields { get; set; }
 
         internal string Query { get; set; }
+
+        public override IDictionary<string, object> GetUriPathParameters()
+        {
+            var uriParams = base.GetUriPathParameters();
+
+            if (ResultTypes == null)
+                throw new ArgumentNullException(nameof(ResultTypes));
+
+            if (string.IsNullOrEmpty(Query))
+                throw new ArgumentException("query must not be empty", nameof(Query));
+
+            uriParams.Add("type", ResultTypes.UriName);
+            uriParams.Add("query", Query);
+
+            if (SearchFields != null && SearchFields != TraktSearchField.Unspecified)
+                uriParams.Add("fields", SearchFields.UriName);
+
+            return uriParams;
+        }
 
         public override string UriTemplate => "search/{type}{?query,fields,years,genres,languages,countries,runtimes,ratings,extended,page,limit}";
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Search/TraktSearchTextQueryRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Search/TraktSearchTextQueryRequestTests.cs
@@ -2,6 +2,8 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using TraktApiSharp.Enums;
@@ -74,6 +76,76 @@
             startDatePropertyInfo.CanRead.Should().BeTrue();
             startDatePropertyInfo.CanWrite.Should().BeTrue();
             startDatePropertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Search"), TestCategory("Without OAuth"), TestCategory("Search Text Query")]
+        public void TestTraktSearchTextQueryRequestHasGetUriPathParametersMethod()
+        {
+            var methodInfo = typeof(TraktSearchTextQueryRequest).GetMethods()
+                                                                .Where(m => m.Name == "GetUriPathParameters")
+                                                                .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
+            methodInfo.GetParameters().Should().BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Search"), TestCategory("Without OAuth"), TestCategory("Search Text Query")]
+        public void TestTraktSearchTextQueryRequestUriParamsWithoutResultTypes()
+        {
+            var request = new TraktSearchTextQueryRequest(null)
+            {
+                ResultTypes = null
+            };
+
+            Action act = () => request.GetUriPathParameters();
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Search"), TestCategory("Without OAuth"), TestCategory("Search Text Query")]
+        public void TestTraktSearchTextQueryRequestUriParamsWithoutQuery()
+        {
+            var request = new TraktSearchTextQueryRequest(null)
+            {
+                ResultTypes = TraktSearchResultType.Movie,
+                Query = null
+            };
+
+            Action act = () => request.GetUriPathParameters();
+            act.ShouldThrow<ArgumentException>();
+
+            request = new TraktSearchTextQueryRequest(null)
+            {
+                ResultTypes = TraktSearchResultType.Movie,
+                Query = string.Empty
+            };
+
+            act = () => request.GetUriPathParameters();
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("Without OAuth"), TestCategory("Movies")]
+        public void TestTraktSearchTextQueryRequestUriParamsWithSearchFields()
+        {
+            var resultTypes = TraktSearchResultType.Movie | TraktSearchResultType.Show;
+            var query = "the big bang theory";
+            var fields = TraktSearchField.Name | TraktSearchField.Overview;
+
+            var request = new TraktSearchTextQueryRequest(null)
+            {
+                ResultTypes = resultTypes,
+                Query = query,
+                SearchFields = fields
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["type"] = resultTypes.UriName,
+                ["query"] = query,
+                ["fields"] = fields.UriName
+            });
         }
     }
 }
