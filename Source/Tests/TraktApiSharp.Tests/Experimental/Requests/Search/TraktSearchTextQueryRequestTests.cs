ac49db0cb645ac2473cc3c34698dae26083281a0
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-30 21:32:32 +0200

    Add search text query request class.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Search/TraktSearchTextQueryRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Search
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Search;
+
+    [TestClass]
+    public class TraktSearchTextQueryRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Search"), TestCategory("Without OAuth"), TestCategory("Search Text Query")]
+        public void TestTraktSearchTextQueryRequestIsNotAbstract()
+        {
+            typeof(TraktSearchTextQueryRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
