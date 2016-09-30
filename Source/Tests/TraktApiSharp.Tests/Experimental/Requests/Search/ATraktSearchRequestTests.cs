0d494e3109ae6b2ec6c76409eeac8ea554644c9a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-30 21:18:19 +0200

    Add base class for search requests.

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Search/ATraktSearchRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Search
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Search;
+
+    [TestClass]
+    public class ATraktSearchRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Search"), TestCategory("Without OAuth")]
+        public void TestATraktSearchRequestIsAbstract()
+        {
+            typeof(ATraktSearchRequest).IsAbstract.Should().BeTrue();
+        }
+    }
+}
