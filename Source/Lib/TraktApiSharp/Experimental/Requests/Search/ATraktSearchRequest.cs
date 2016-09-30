9f94a17d791cb87a3d7f77fdcf3eaeaf5102acde
Henrik Fröhling <henrik.froehling@gmail.com>
2016-09-30 21:52:13 +0200

    Add ResultTypes property in ATraktSearchRequest.

--- Source/Lib/TraktApiSharp/Experimental/Requests/Search/ATraktSearchRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Search/ATraktSearchRequest.cs
@@ -1,6 +1,7 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Search
 {
     using Base.Get;
+    using Enums;
     using Interfaces;
     using Objects.Basic;
     using TraktApiSharp.Requests.Params;
@@ -10,5 +11,7 @@
         public ATraktSearchRequest(TraktClient client) : base(client) { }
 
         public TraktExtendedOption ExtendedOption { get; set; }
+
+        internal TraktSearchResultType ResultTypes { get; set; }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Search/ATraktSearchRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Search/ATraktSearchRequestTests.cs
@@ -2,6 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Reflection;
+    using TraktApiSharp.Enums;
     using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Search;
@@ -26,6 +29,19 @@
         public void TestATraktSearchRequestImplementsITraktExtendedInfoInterface()
         {
             typeof(ATraktSearchRequest).GetInterfaces().Should().Contain(typeof(ITraktExtendedInfo));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Search"), TestCategory("Without OAuth")]
+        public void TestATraktSearchRequestHasResultTypesProperty()
+        {
+            var startDatePropertyInfo = typeof(ATraktSearchRequest)
+                    .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.Name == "ResultTypes")
+                    .FirstOrDefault();
+
+            startDatePropertyInfo.CanRead.Should().BeTrue();
+            startDatePropertyInfo.CanWrite.Should().BeTrue();
+            startDatePropertyInfo.PropertyType.Should().Be(typeof(TraktSearchResultType));
         }
     }
 }
