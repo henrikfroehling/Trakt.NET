c09f820eb8172fc011a4db63065882d9d6a2f8eb
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-07 22:09:01 +0200

    Add method overload for CreateWith() in TraktAuthorization

--- Source/Lib/TraktApiSharp/Authentication/TraktAuthorization.cs
+++ Source/Lib/TraktApiSharp/Authentication/TraktAuthorization.cs
@@ -119,6 +119,26 @@
                 RefreshToken = refreshToken ?? string.Empty
             };
 
+        /// <summary>
+        /// Creates a new <see cref="TraktAuthorization" /> instance with the given values.
+        /// <see cref="ExpiresInSeconds" /> of the created <see cref="TraktAuthorization" /> instance will have the default
+        /// value of 3600 * 24 * 90 seconds, equal to 90 days.
+        /// </summary>
+        /// <param name="createdAt">The datetime, when the given access token was created. Will be converted to UTC datetime.</param>
+        /// <param name="accessToken">The access token for the new <see cref="TraktAuthorization" /> instance.</param>
+        /// <param name="refreshToken">The optional refresh token for the new <see cref="TraktAuthorization" /> instance.</param>
+        /// <returns>A new <see cref="TraktAuthorization" /> instance with the given values.</returns>
+        public static TraktAuthorization CreateWith(DateTime createdAt, string accessToken, string refreshToken = null)
+            => new TraktAuthorization
+            {
+                Created = createdAt.ToUniversalTime(),
+                ExpiresInSeconds = 3600 * 24 * 90,
+                AccessScope = TraktAccessScope.Public,
+                TokenType = TraktAccessTokenType.Bearer,
+                AccessToken = accessToken ?? string.Empty,
+                RefreshToken = refreshToken ?? string.Empty
+            };
+
         /// <summary>Creates a new <see cref="TraktAuthorization" /> instance with the given values.</summary>
         /// <param name="createdAt">The datetime, when the given access token was created. Will be converted to UTC datetime.</param>
         /// <param name="expiresInSeconds">The seconds, after which the given access token will expire.</param>
--- Source/Tests/TraktApiSharp.Tests/Authentication/TraktAuthorizationTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Authentication/TraktAuthorizationTests.cs
@@ -193,6 +193,46 @@
         }
 
         [TestMethod]
+        public void TestTraktAuthorizationCreateWithCreatedAtAndAccessToken()
+        {
+            var createdAt = DateTime.Now;
+            var expiresIn = 3600 * 24 * 90;
+            var accessToken = "accessToken";
+
+            var authorization = TraktAuthorization.CreateWith(createdAt, accessToken);
+
+            authorization.Should().NotBeNull();
+            authorization.AccessToken.Should().Be(accessToken);
+            authorization.RefreshToken.Should().NotBeNull().And.BeEmpty();
+            authorization.AccessScope.Should().Be(TraktAccessScope.Public);
+            authorization.TokenType.Should().Be(TraktAccessTokenType.Bearer);
+            authorization.IgnoreExpiration.Should().BeFalse();
+            authorization.Created.Should().Be(createdAt.ToUniversalTime());
+            authorization.ExpiresInSeconds.Should().Be(expiresIn);
+        }
+
+        [TestMethod]
+        public void TestTraktAuthorizationCreateWithCreatedAtAndAccessTokenAndRefreshToken()
+        {
+            var createdAt = DateTime.Now;
+            var expiresIn = 3600 * 24 * 90;
+
+            var accessToken = "accessToken";
+            var refreshToken = "refreshToken";
+
+            var authorization = TraktAuthorization.CreateWith(createdAt, accessToken, refreshToken);
+
+            authorization.Should().NotBeNull();
+            authorization.AccessToken.Should().Be(accessToken);
+            authorization.RefreshToken.Should().Be(refreshToken);
+            authorization.AccessScope.Should().Be(TraktAccessScope.Public);
+            authorization.TokenType.Should().Be(TraktAccessTokenType.Bearer);
+            authorization.IgnoreExpiration.Should().BeFalse();
+            authorization.Created.Should().Be(createdAt.ToUniversalTime());
+            authorization.ExpiresInSeconds.Should().Be(expiresIn);
+        }
+
+        [TestMethod]
         public void TestTraktAuthorizationCreateWithCreatedAtAndExpiresInAndAccessToken()
         {
             var createdAt = DateTime.Now;
@@ -253,13 +293,9 @@
             authorization.AccessToken.Should().Be(accessToken);
             authorization.RefreshToken.Should().NotBeNull().And.BeEmpty();
 
+            // ------------------------------------------------------
+
             authorization = TraktAuthorization.CreateWith(expiresIn, null, refreshToken);
-
-            authorization.Should().NotBeNull();
-            authorization.AccessToken.Should().NotBeNull().And.BeEmpty();
-            authorization.RefreshToken.Should().Be(refreshToken);
-
-            authorization = TraktAuthorization.CreateWith(createdAt, expiresIn, null, refreshToken);
 
             authorization.Should().NotBeNull();
             authorization.AccessToken.Should().NotBeNull().And.BeEmpty();
@@ -270,6 +306,28 @@
             authorization.Should().NotBeNull();
             authorization.AccessToken.Should().Be(accessToken);
             authorization.RefreshToken.Should().NotBeNull().And.BeEmpty();
+
+            // ------------------------------------------------------
+
+            authorization = TraktAuthorization.CreateWith(createdAt, null, refreshToken);
+
+            authorization.Should().NotBeNull();
+            authorization.AccessToken.Should().NotBeNull().And.BeEmpty();
+            authorization.RefreshToken.Should().Be(refreshToken);
+
+            authorization = TraktAuthorization.CreateWith(createdAt, accessToken);
+
+            authorization.Should().NotBeNull();
+            authorization.AccessToken.Should().Be(accessToken);
+            authorization.RefreshToken.Should().NotBeNull().And.BeEmpty();
+
+            // ------------------------------------------------------
+
+            authorization = TraktAuthorization.CreateWith(createdAt, expiresIn, null, refreshToken);
+
+            authorization.Should().NotBeNull();
+            authorization.AccessToken.Should().NotBeNull().And.BeEmpty();
+            authorization.RefreshToken.Should().Be(refreshToken);
 
             authorization = TraktAuthorization.CreateWith(createdAt, expiresIn, accessToken);
 
