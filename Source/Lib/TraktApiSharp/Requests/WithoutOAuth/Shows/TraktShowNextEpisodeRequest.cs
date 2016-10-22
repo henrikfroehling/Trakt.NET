f221eb804617c946b37bf01bed9b780c6d30bf84
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-22 23:00:21 +0200

    Add request implementation for next episode

---
+++ Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Shows/TraktShowNextEpisodeRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Requests.WithoutOAuth.Shows
+{
+    using Base.Get;
+    using Objects.Get.Shows.Episodes;
+
+    internal class TraktShowNextEpisodeRequest : TraktGetByIdRequest<TraktEpisode, TraktEpisode>
+    {
+        internal TraktShowNextEpisodeRequest(TraktClient client) : base(client) { }
+
+        protected override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        protected override string UriTemplate => "shows/{id}/next_episode{?extended}";
+
+        protected override TraktRequestObjectType? RequestObjectType => TraktRequestObjectType.Shows;
+    }
+}
