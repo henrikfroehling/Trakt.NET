da9c35d8ec93e00f72fc17eed22df6ffad7dc79a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-25 23:51:55 +0200

    Set authorization of TraktCommentSummaryRequest not required

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/TraktCommentSummaryRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/TraktCommentSummaryRequestTests.cs
@@ -5,6 +5,7 @@
     using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Comments;
     using TraktApiSharp.Objects.Basic;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktCommentSummaryRequestTests
@@ -25,6 +26,13 @@
         public void TestTraktCommentSummaryRequestIsSubclassOfATraktSingleItemGetByIdRequest()
         {
             typeof(TraktCommentSummaryRequest).IsSubclassOf(typeof(ATraktSingleItemGetByIdRequest<TraktComment>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Comments"), TestCategory("Without OAuth")]
+        public void TestTraktCommentSummaryRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktCommentSummaryRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
     }
 }
