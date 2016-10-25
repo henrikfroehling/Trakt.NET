dc781e27dd78d1bd1a2c8457ce57652071b8af00
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-25 23:37:54 +0200

    Add comment summary request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/TraktCommentSummaryRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Comments
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Comments;
+
+    [TestClass]
+    public class TraktCommentSummaryRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Comments"), TestCategory("Without OAuth")]
+        public void TestTraktCommentSummaryRequestIsNotAbstract()
+        {
+            typeof(TraktCommentSummaryRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
