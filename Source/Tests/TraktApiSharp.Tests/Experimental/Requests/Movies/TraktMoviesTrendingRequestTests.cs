1f64cae92815ebe12b553657d2ce9c3ace4b5d25
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 01:23:35 +0200

    Add trending movie request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesTrendingRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Movies
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Movies;
+
+    [TestClass]
+    public class TraktMoviesTrendingRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestTraktMoviesTrendingRequestIsNotAbstract()
+        {
+            typeof(TraktMoviesTrendingRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
