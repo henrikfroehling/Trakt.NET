27a771f69e79416d49066aa8d51b68a604e774b0
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 01:11:21 +0200

    Add popular movies request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesPopularRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Movies
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Movies;
+
+    [TestClass]
+    public class TraktMoviesPopularRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestTraktMoviesPopularRequestIsNotAbstract()
+        {
+            typeof(TraktMoviesPopularRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
