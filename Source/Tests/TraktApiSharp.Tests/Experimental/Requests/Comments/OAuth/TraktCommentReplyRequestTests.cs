c40dcc4172d38dda81ddf1f589b4e7da2e9a9d59
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-26 03:13:56 +0200

    Add comment reply request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/OAuth/TraktCommentReplyRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Comments.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Comments.OAuth;
+
+    [TestClass]
+    public class TraktCommentReplyRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Comments"), TestCategory("With OAuth")]
+        public void TestTraktCommentReplyRequestIsNotAbstract()
+        {
+            typeof(TraktCommentReplyRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
