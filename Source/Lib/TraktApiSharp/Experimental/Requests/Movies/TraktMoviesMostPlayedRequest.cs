8ba60fc01c7289cbbbc85b9bce35ae29572a6b69
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 01:57:41 +0200

    Derive TraktMoviesMostPlayedRequest from ATraktMoviesMostPWCRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMoviesMostPlayedRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMoviesMostPlayedRequest.cs
@@ -1,7 +1,27 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Movies
 {
-    internal sealed class TraktMoviesMostPlayedRequest
-    {
+    using Objects.Get.Movies.Common;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktMoviesMostPlayedRequest : ATraktMoviesMostPWCRequest<TraktMostPlayedMovie>
+    {
+        internal TraktMoviesMostPlayedRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesMostPlayedRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesMostPlayedRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Movies;
+    using TraktApiSharp.Objects.Get.Movies.Common;
 
     [TestClass]
     public class TraktMoviesMostPlayedRequestTests
@@ -17,6 +18,12 @@
         public void TestTraktMoviesMostPlayedRequestIsSealed()
         {
             typeof(TraktMoviesMostPlayedRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestTraktMoviesMostPlayedRequestIsSubclassOfATraktMoviesMostPWCRequest()
+        {
+            typeof(TraktMoviesMostPlayedRequest).IsSubclassOf(typeof(ATraktMoviesMostPWCRequest<TraktMostPlayedMovie>)).Should().BeTrue();
         }
     }
 }
