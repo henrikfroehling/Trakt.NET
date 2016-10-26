d02eaeab40f293e69145bce4c629a27288aec538
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-26 05:04:01 +0200

    Add show comment post request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/OAuth/TraktShowCommentPostRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Comments.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Comments.OAuth;
+
+    [TestClass]
+    public class TraktShowCommentPostRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Comments"), TestCategory("With OAuth")]
+        public void TestTraktShowCommentPostRequestIsNotAbstract()
+        {
+            typeof(TraktShowCommentPostRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
