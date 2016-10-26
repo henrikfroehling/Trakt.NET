7107e21230b900c380040ac58c51f0c76210680a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-26 04:14:20 +0200

    Implement ITraktObjectRequest in TraktCommentUpdateRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Comments/OAuth/TraktCommentUpdateRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Comments/OAuth/TraktCommentUpdateRequest.cs
@@ -1,12 +1,16 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Comments.OAuth
 {
     using Base.Put;
+    using Interfaces;
     using Objects.Post.Comments;
     using Objects.Post.Comments.Responses;
+    using TraktApiSharp.Requests;
 
-    internal sealed class TraktCommentUpdateRequest : ATraktSingleItemPutByIdRequest<TraktCommentPostResponse, TraktCommentUpdatePost>
+    internal sealed class TraktCommentUpdateRequest : ATraktSingleItemPutByIdRequest<TraktCommentPostResponse, TraktCommentUpdatePost>, ITraktObjectRequest
     {
         internal TraktCommentUpdateRequest(TraktClient client) : base(client) { }
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Comments;
 
         public override string UriTemplate => "comments/{id}";
     }
