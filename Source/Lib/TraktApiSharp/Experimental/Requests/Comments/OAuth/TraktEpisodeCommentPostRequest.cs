a70b408918fec4159b01c74d58a5a3406f34f9b5
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-26 05:24:56 +0200

    Derive TraktEpisodeCommentPostRequest from ATraktCommentPostRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Comments/OAuth/TraktEpisodeCommentPostRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Comments/OAuth/TraktEpisodeCommentPostRequest.cs
@@ -1,7 +1,18 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Comments.OAuth
 {
-    internal class TraktEpisodeCommentPostRequest
-    {
+    using Objects.Post.Comments;
+    using System;
 
+    internal sealed class TraktEpisodeCommentPostRequest : ATraktCommentPostRequest<TraktEpisodeCommentPost>
+    {
+        internal TraktEpisodeCommentPostRequest(TraktClient client) : base(client) { }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/OAuth/TraktEpisodeCommentPostRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/OAuth/TraktEpisodeCommentPostRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Comments.OAuth;
+    using TraktApiSharp.Objects.Post.Comments;
 
     [TestClass]
     public class TraktEpisodeCommentPostRequestTests
@@ -11,6 +12,18 @@
         public void TestTraktEpisodeCommentPostRequestIsNotAbstract()
         {
             typeof(TraktEpisodeCommentPostRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Comments"), TestCategory("With OAuth")]
+        public void TestTraktEpisodeCommentPostRequestIsSealed()
+        {
+            typeof(TraktEpisodeCommentPostRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Comments"), TestCategory("With OAuth")]
+        public void TestTraktEpisodeCommentPostRequestIsSubclassOfATraktCommentPostRequest()
+        {
+            typeof(TraktEpisodeCommentPostRequest).IsSubclassOf(typeof(ATraktCommentPostRequest<TraktEpisodeCommentPost>)).Should().BeTrue();
         }
     }
 }
