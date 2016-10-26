cc64f3791008efb64971c7489393d9fe22888154
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 01:38:47 +0200

    Add base class for most played|watched|collected movies requests

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/ATraktMoviesMostPWCRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Movies
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Movies;
+
+    [TestClass]
+    public class ATraktMoviesMostPWCRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestATraktMoviesMostPWCRequestIsAbstract()
+        {
+            typeof(ATraktMoviesMostPWCRequest).IsAbstract.Should().BeTrue();
+        }
+    }
+}
