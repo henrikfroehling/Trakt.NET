55c9c2ac68a7d7793ed043820d3c84cff6cc2958
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-26 05:13:14 +0200

    Add season comment post request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/OAuth/TraktSeasonCommentPostRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Comments.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Comments.OAuth;
+
+    [TestClass]
+    public class TraktSeasonCommentPostRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Comments"), TestCategory("With OAuth")]
+        public void TestTraktSeasonCommentPostRequestIsNotAbstract()
+        {
+            typeof(TraktSeasonCommentPostRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
