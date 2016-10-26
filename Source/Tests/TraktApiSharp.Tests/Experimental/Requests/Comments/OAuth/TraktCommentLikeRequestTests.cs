0e4ffd43311585d9ef107d14f26751f25420e2bd
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-26 02:47:36 +0200

    Add comment like request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/OAuth/TraktCommentLikeRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Comments.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Comments.OAuth;
+
+    [TestClass]
+    public class TraktCommentLikeRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Comments"), TestCategory("With OAuth")]
+        public void TestTraktCommentLikeRequestIsNotAbstract()
+        {
+            typeof(TraktCommentLikeRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
