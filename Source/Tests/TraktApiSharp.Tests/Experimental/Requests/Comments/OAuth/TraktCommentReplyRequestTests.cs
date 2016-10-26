591829af9ceb8a69b726ef8589874c3d5dd43eef
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-26 03:33:58 +0200

    Implement ITraktObjectRequest in TraktCommentReplyRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/OAuth/TraktCommentReplyRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Comments/OAuth/TraktCommentReplyRequestTests.cs
@@ -4,6 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Post;
     using TraktApiSharp.Experimental.Requests.Comments.OAuth;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Objects.Post.Comments;
     using TraktApiSharp.Objects.Post.Comments.Responses;
 
@@ -33,6 +34,12 @@
         {
             var request = new TraktCommentReplyRequest(null);
             request.UriTemplate.Should().Be("comments/{id}/replies");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Comments"), TestCategory("With OAuth")]
+        public void TestTraktCommentReplyRequestImplementsITraktObjectRequestInterface()
+        {
+            typeof(TraktCommentReplyRequest).GetInterfaces().Should().Contain(typeof(ITraktObjectRequest));
         }
     }
 }
