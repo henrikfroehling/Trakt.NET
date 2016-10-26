4e49e207f8b5431776c156e42c90234419d3f394
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 01:28:35 +0200

    Set authorization of TraktMoviesTrendingRequest not required

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesTrendingRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesTrendingRequestTests.cs
@@ -4,6 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Movies;
     using TraktApiSharp.Objects.Get.Movies.Common;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktMoviesTrendingRequestTests
@@ -24,6 +25,13 @@
         public void TestTraktMoviesTrendingRequestIsSubclassOfATraktMoviesRequest()
         {
             typeof(TraktMoviesTrendingRequest).IsSubclassOf(typeof(ATraktMoviesRequest<TraktTrendingMovie>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestTraktMoviesTrendingRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktMoviesTrendingRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
     }
 }
