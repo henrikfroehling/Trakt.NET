f71244d2c9b04604bb35d0e6861a8df8075e3c58
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 01:49:44 +0200

    Add overload for GetUriPathParameters method in ATraktMoviesMostPWCRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/ATraktMoviesMostPWCRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/ATraktMoviesMostPWCRequest.cs
@@ -1,11 +1,22 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Movies
 {
     using Enums;
+    using System.Collections.Generic;
 
     internal abstract class ATraktMoviesMostPWCRequest<TItem> : ATraktMoviesRequest<TItem>
     {
         internal ATraktMoviesMostPWCRequest(TraktClient client) : base(client) { }
 
         internal TraktTimePeriod Period { get; set; }
+
+        public override IDictionary<string, object> GetUriPathParameters()
+        {
+            var uriParams = base.GetUriPathParameters();
+
+            if (Period != null && Period != TraktTimePeriod.Unspecified)
+                uriParams.Add("period", Period.UriName);
+
+            return uriParams;
+        }
     }
 }
