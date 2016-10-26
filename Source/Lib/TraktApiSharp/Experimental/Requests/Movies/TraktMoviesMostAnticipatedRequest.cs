bd808374ffab936a672dbba806e89ab1b2b0d7ea
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 00:58:58 +0200

    Derive TraktMoviesMostAnticipatedRequest from ATraktMoviesRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMoviesMostAnticipatedRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMoviesMostAnticipatedRequest.cs
@@ -1,7 +1,27 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Movies
 {
-    internal sealed class TraktMoviesMostAnticipatedRequest
-    {
+    using Objects.Get.Movies.Common;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktMoviesMostAnticipatedRequest : ATraktMoviesRequest<TraktMostAnticipatedMovie>
+    {
+        internal TraktMoviesMostAnticipatedRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesMostAnticipatedRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesMostAnticipatedRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Movies;
+    using TraktApiSharp.Objects.Get.Movies.Common;
 
     [TestClass]
     public class TraktMoviesMostAnticipatedRequestTests
@@ -17,6 +18,12 @@
         public void TestTraktMoviesMostAnticipatedRequestIsSealed()
         {
             typeof(TraktMoviesMostAnticipatedRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestTraktMoviesMostAnticipatedRequestIsSubclassOfATraktMoviesRequest()
+        {
+            typeof(TraktMoviesMostAnticipatedRequest).IsSubclassOf(typeof(ATraktMoviesRequest<TraktMostAnticipatedMovie>)).Should().BeTrue();
         }
     }
 }
