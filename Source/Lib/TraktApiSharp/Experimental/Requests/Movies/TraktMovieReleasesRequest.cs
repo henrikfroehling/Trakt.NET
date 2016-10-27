42eb1e7efb6649d00fb0971e8fdcf80bed80c7e1
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 05:57:24 +0200

    Derive TraktMovieReleasesRequest from ATraktListGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieReleasesRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieReleasesRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Movies
 {
-    internal sealed class TraktMovieReleasesRequest
-    {
+    using Base.Get;
+    using Objects.Get.Movies;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktMovieReleasesRequest : ATraktListGetByIdRequest<TraktMovieRelease>
+    {
+        internal TraktMovieReleasesRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieReleasesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieReleasesRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Movies;
+    using TraktApiSharp.Objects.Get.Movies;
 
     [TestClass]
     public class TraktMovieReleasesRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktMovieReleasesRequestIsSealed()
         {
             typeof(TraktMovieReleasesRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieReleasesRequestIsSubclassOfATraktListGetByIdRequest()
+        {
+            typeof(TraktMovieReleasesRequest).IsSubclassOf(typeof(ATraktListGetByIdRequest<TraktMovieRelease>)).Should().BeTrue();
         }
     }
 }
