f8ea7ec5c4e17e3c3ba01429840864d5f3c39398
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 02:23:04 +0200

    Add most collected movies request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesMostCollectedRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Movies
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Movies;
+
+    [TestClass]
+    public class TraktMoviesMostCollectedRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestTraktMoviesMostCollectedRequestIsNotAbstract()
+        {
+            typeof(TraktMoviesMostCollectedRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
