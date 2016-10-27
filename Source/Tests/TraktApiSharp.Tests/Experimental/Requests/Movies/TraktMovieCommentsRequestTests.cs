28bd66ff2e56cd297d93fca18fff5fcf19e177af
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 04:33:54 +0200

    Add movie comments request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieCommentsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Movies
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Movies;
+
+    [TestClass]
+    public class TraktMovieCommentsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieCommentsRequestIsNotAbstract()
+        {
+            typeof(TraktMovieCommentsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
