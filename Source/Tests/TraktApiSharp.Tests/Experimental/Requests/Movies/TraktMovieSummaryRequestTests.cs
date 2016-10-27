4838ec8bc3f3cd837788798c71355938179d34e9
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 06:53:47 +0200

    Add movie summary request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieSummaryRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Movies
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Movies;
+
+    [TestClass]
+    public class TraktMovieSummaryRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieSummaryRequestIsNotAbstract()
+        {
+            typeof(TraktMovieSummaryRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
