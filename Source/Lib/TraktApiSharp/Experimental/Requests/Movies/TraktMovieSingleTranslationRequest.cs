23a76454d09c93f26fee266ba06d7e6da4b2bd48
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 06:30:27 +0200

    Derive TraktMovieSingleTranslationRequest from ATraktSingleItemGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieSingleTranslationRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieSingleTranslationRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Movies
 {
-    internal sealed class TraktMovieSingleTranslationRequest
-    {
+    using Base.Get;
+    using Objects.Get.Movies;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktMovieSingleTranslationRequest : ATraktSingleItemGetByIdRequest<TraktMovieTranslation>
+    {
+        internal TraktMovieSingleTranslationRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieSingleTranslationRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieSingleTranslationRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Movies;
+    using TraktApiSharp.Objects.Get.Movies;
 
     [TestClass]
     public class TraktMovieSingleTranslationRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktMovieSingleTranslationRequestIsSealed()
         {
             typeof(TraktMovieSingleTranslationRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieSingleTranslationRequestIsSubclassOfATraktSingleItemGetByIdRequest()
+        {
+            typeof(TraktMovieSingleTranslationRequest).IsSubclassOf(typeof(ATraktSingleItemGetByIdRequest<TraktMovieTranslation>)).Should().BeTrue();
         }
     }
 }
