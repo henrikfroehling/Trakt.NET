ed9c59e2fc7a118ac08075cd7e6f577bae6681ce
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 04:28:15 +0200

    Set uri template for TraktMovieAliasesRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieAliasesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieAliasesRequestTests.cs
@@ -34,5 +34,12 @@
             var request = new TraktMovieAliasesRequest(null);
             request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieAliasesRequestHasValidUriTemplate()
+        {
+            var request = new TraktMovieAliasesRequest(null);
+            request.UriTemplate.Should().Be("movies/{id}/aliases");
+        }
     }
 }
