d3253f443647d273605e13098bffa87f75a213e2
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 05:14:46 +0200

    Make TraktMovieRatingsRequest sealed

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieRatingsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieRatingsRequestTests.cs
@@ -12,5 +12,11 @@
         {
             typeof(TraktMovieRatingsRequest).IsAbstract.Should().BeFalse();
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieRatingsRequestIsSealed()
+        {
+            typeof(TraktMovieRatingsRequest).IsSealed.Should().BeTrue();
+        }
     }
 }
