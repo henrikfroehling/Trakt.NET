ad7cde6a296dd83254ba62eb601d0b626bd2fe3e
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 05:03:13 +0200

    Set authorization of TraktMoviePeopleRequest not required

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviePeopleRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviePeopleRequestTests.cs
@@ -5,6 +5,7 @@
     using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Movies;
     using TraktApiSharp.Objects.Basic;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktMoviePeopleRequestTests
@@ -25,6 +26,13 @@
         public void TestTraktMoviePeopleRequestIsSubclassOfATraktSingleItemGetByIdRequest()
         {
             typeof(TraktMoviePeopleRequest).IsSubclassOf(typeof(ATraktSingleItemGetByIdRequest<TraktCastAndCrew>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMoviePeopleRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktMoviePeopleRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
     }
 }
