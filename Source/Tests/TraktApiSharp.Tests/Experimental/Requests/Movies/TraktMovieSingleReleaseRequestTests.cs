02178ba062430aa861257203c5a888e43eb36aed
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 06:23:27 +0200

    Add implementation for GetUriPathParameters method in TraktMovieSingleReleaseRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieSingleReleaseRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieSingleReleaseRequestTests.cs
@@ -75,5 +75,21 @@
             methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
             methodInfo.GetParameters().Should().BeEmpty();
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieSingleReleaseRequestUriParamsWithLanguageCode()
+        {
+            var languageCode = "de";
+
+            var request = new TraktMovieSingleReleaseRequest(null)
+            {
+                LanguageCode = languageCode
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("language", languageCode);
+        }
     }
 }
