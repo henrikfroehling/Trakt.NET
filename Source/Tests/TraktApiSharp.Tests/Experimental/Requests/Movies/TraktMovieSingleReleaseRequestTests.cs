f50178d5d0eda3217137f6c1f78b16d0bda06ea5
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 06:06:08 +0200

    Add movie single release request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieSingleReleaseRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Movies
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Movies;
+
+    [TestClass]
+    public class TraktMovieSingleReleaseRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieSingleReleaseRequestIsNotAbstract()
+        {
+            typeof(TraktMovieSingleReleaseRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
