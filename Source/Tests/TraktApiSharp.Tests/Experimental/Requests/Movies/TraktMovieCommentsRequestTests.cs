711f37ec49f57e71214203dd0728eff71c98ef27
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 04:40:16 +0200

    Set authorization of TraktMovieCommentsRequest not required

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieCommentsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieCommentsRequestTests.cs
@@ -5,6 +5,7 @@
     using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Movies;
     using TraktApiSharp.Objects.Basic;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktMovieCommentsRequestTests
@@ -25,6 +26,13 @@
         public void TestTraktMovieCommentsRequestIsSubclassOfATraktPaginationGetByIdRequest()
         {
             typeof(TraktMovieCommentsRequest).IsSubclassOf(typeof(ATraktPaginationGetByIdRequest<TraktComment>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieCommentsRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktMovieCommentsRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
     }
 }
