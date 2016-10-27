4639cac3e5c417ad5dd8f225325aa0e36e28da77
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 06:03:09 +0200

    Implement ITraktObjectRequest in TraktMovieReleasesRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieReleasesRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieReleasesRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Movies
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Get.Movies;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktMovieReleasesRequest : ATraktListGetByIdRequest<TraktMovieRelease>
+    internal sealed class TraktMovieReleasesRequest : ATraktListGetByIdRequest<TraktMovieRelease>, ITraktObjectRequest
     {
         internal TraktMovieReleasesRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Movies;
 
         public override string UriTemplate => "movies/{id}/releases";
     }
