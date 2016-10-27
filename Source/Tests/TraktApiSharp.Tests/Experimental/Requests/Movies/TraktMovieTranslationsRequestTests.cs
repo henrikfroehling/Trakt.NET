8fce002a721530c3e079043be23a90d0c26739a7
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 07:12:18 +0200

    Implement ITraktObjectRequest in TraktMovieTranslationsRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieTranslationsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieTranslationsRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Movies;
     using TraktApiSharp.Objects.Get.Movies;
     using TraktApiSharp.Requests;
@@ -40,6 +41,12 @@
         {
             var request = new TraktMovieTranslationsRequest(null);
             request.UriTemplate.Should().Be("movies/{id}/translations");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieTranslationsRequestImplementsITraktObjectRequestInterface()
+        {
+            typeof(TraktMovieTranslationsRequest).GetInterfaces().Should().Contain(typeof(ITraktObjectRequest));
         }
     }
 }
