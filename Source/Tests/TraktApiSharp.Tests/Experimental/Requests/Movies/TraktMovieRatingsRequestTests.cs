f1dba648294802c3663c320eeeaa2517fc2b358b
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 05:13:35 +0200

    Add movie ratings request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieRatingsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Movies
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Movies;
+
+    [TestClass]
+    public class TraktMovieRatingsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieRatingsRequestIsNotAbstract()
+        {
+            typeof(TraktMovieRatingsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
