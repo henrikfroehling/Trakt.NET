398b94129e68461eb4e3b6b61a10f690009f816c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 06:56:44 +0200

    Derive TraktMovieSummaryRequest from ATraktSingleItemGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieSummaryRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieSummaryRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Movies
 {
-    internal sealed class TraktMovieSummaryRequest
-    {
+    using Base.Get;
+    using Objects.Get.Movies;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktMovieSummaryRequest : ATraktSingleItemGetByIdRequest<TraktMovie>
+    {
+        internal TraktMovieSummaryRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
