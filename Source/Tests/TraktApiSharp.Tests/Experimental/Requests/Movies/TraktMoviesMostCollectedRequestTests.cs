7a358331afd4744530a2d61ab6ef554bf5db5ab7
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 02:33:26 +0200

    Add tests for GetUriPathParameters method implementation in TraktMoviesMostCollectedRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesMostCollectedRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesMostCollectedRequestTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Enums;
     using TraktApiSharp.Experimental.Requests.Movies;
     using TraktApiSharp.Objects.Get.Movies.Common;
     using TraktApiSharp.Requests;
@@ -39,6 +40,46 @@
         {
             var request = new TraktMoviesMostCollectedRequest(null);
             request.UriTemplate.Should().Be("movies/collected{/period}{?extended,page,limit,query,years,genres,languages,countries,runtimes,ratings,certifications}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestTraktMoviesMostCollectedRequestUriParamsWithoutPeriod()
+        {
+            var request = new TraktMoviesMostCollectedRequest(null);
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestTraktMoviesMostCollectedRequestUriParamsWithUnspecifiedPeriod()
+        {
+            var period = TraktTimePeriod.Unspecified;
+
+            var request = new TraktMoviesMostCollectedRequest(null)
+            {
+                Period = period
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestTraktMoviesMostCollectedRequestUriParamsWithPeriod()
+        {
+            var period = TraktTimePeriod.Monthly;
+
+            var request = new TraktMoviesMostCollectedRequest(null)
+            {
+                Period = period
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("period", period.UriName);
         }
     }
 }
