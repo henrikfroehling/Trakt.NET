9320456fd3ce00429b2dcf7a66d99acfe02fed4b
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 07:21:52 +0200

    Implement ITraktObjectRequest in TraktMovieWatchingUsersRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieWatchingUsersRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieWatchingUsersRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Movies
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Get.Users;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktMovieWatchingUsersRequest : ATraktListGetByIdRequest<TraktUser>
+    internal sealed class TraktMovieWatchingUsersRequest : ATraktListGetByIdRequest<TraktUser>, ITraktObjectRequest
     {
         internal TraktMovieWatchingUsersRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Movies;
 
         public override string UriTemplate => "movies/{id}/watching{?extended}";
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieWatchingUsersRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieWatchingUsersRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Movies;
     using TraktApiSharp.Objects.Get.Users;
     using TraktApiSharp.Requests;
@@ -40,6 +41,12 @@
         {
             var request = new TraktMovieWatchingUsersRequest(null);
             request.UriTemplate.Should().Be("movies/{id}/watching{?extended}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieWatchingUsersRequestImplementsITraktObjectRequestInterface()
+        {
+            typeof(TraktMovieWatchingUsersRequest).GetInterfaces().Should().Contain(typeof(ITraktObjectRequest));
         }
     }
 }
