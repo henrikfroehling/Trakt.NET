9096c9d6133cf9bbbd2b020105ffa042e6ef07ca
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 04:42:34 +0200

    Set uri template for TraktMovieCommentsRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieCommentsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieCommentsRequestTests.cs
@@ -34,5 +34,12 @@
             var request = new TraktMovieCommentsRequest(null);
             request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieCommentsRequestHasValidUriTemplate()
+        {
+            var request = new TraktMovieCommentsRequest(null);
+            request.UriTemplate.Should().Be("movies/{id}/comments{/sorting}{?page,limit}");
+        }
     }
 }
