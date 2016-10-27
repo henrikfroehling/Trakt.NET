3a32f131df4789b5c3959309d217e43fd43160c1
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 04:44:34 +0200

    Implement ITraktObjectRequest in TraktMovieCommentsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieCommentsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieCommentsRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Movies
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Basic;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktMovieCommentsRequest : ATraktPaginationGetByIdRequest<TraktComment>
+    internal sealed class TraktMovieCommentsRequest : ATraktPaginationGetByIdRequest<TraktComment>, ITraktObjectRequest
     {
         internal TraktMovieCommentsRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Movies;
 
         public override string UriTemplate => "movies/{id}/comments{/sorting}{?page,limit}";
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieCommentsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieCommentsRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Movies;
     using TraktApiSharp.Objects.Basic;
     using TraktApiSharp.Requests;
@@ -40,6 +41,12 @@
         {
             var request = new TraktMovieCommentsRequest(null);
             request.UriTemplate.Should().Be("movies/{id}/comments{/sorting}{?page,limit}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieCommentsRequestImplementsITraktObjectRequestInterface()
+        {
+            typeof(TraktMovieCommentsRequest).GetInterfaces().Should().Contain(typeof(ITraktObjectRequest));
         }
     }
 }
