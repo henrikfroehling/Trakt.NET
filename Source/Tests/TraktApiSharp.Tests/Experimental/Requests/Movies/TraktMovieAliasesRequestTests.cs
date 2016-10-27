2ec6b3051bbed848e58dd6083a2ee3f79bc954da
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 04:19:00 +0200

    Make TraktMovieAliasesRequest sealed

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieAliasesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieAliasesRequestTests.cs
@@ -12,5 +12,11 @@
         {
             typeof(TraktMovieAliasesRequest).IsAbstract.Should().BeFalse();
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieAliasesRequestIsSealed()
+        {
+            typeof(TraktMovieAliasesRequest).IsSealed.Should().BeTrue();
+        }
     }
 }
