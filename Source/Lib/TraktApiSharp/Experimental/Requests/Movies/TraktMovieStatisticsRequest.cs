4dc089fb358572ebbb49441064656341072316b2
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 06:51:17 +0200

    Implement ITraktObjectRequest in TraktMovieStatisticsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieStatisticsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieStatisticsRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Movies
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Basic;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktMovieStatisticsRequest : ATraktSingleItemGetByIdRequest<TraktStatistics>
+    internal sealed class TraktMovieStatisticsRequest : ATraktSingleItemGetByIdRequest<TraktStatistics>, ITraktObjectRequest
     {
         internal TraktMovieStatisticsRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Movies;
 
         public override string UriTemplate => "movies/{id}/stats";
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieStatisticsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieStatisticsRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Movies;
     using TraktApiSharp.Objects.Basic;
     using TraktApiSharp.Requests;
@@ -40,6 +41,12 @@
         {
             var request = new TraktMovieStatisticsRequest(null);
             request.UriTemplate.Should().Be("movies/{id}/stats");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieStatisticsRequestImplementsITraktObjectRequestInterface()
+        {
+            typeof(TraktMovieStatisticsRequest).GetInterfaces().Should().Contain(typeof(ITraktObjectRequest));
         }
     }
 }
