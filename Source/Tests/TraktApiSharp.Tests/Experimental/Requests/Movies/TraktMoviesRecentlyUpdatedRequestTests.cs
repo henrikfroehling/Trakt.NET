9b2b6c7cfb36e1f205a0a3bfcf2acee11710b416
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 02:54:11 +0200

    Derive TraktMoviesRecentlyUpdatedRequest from ATraktPaginationGetRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesRecentlyUpdatedRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMoviesRecentlyUpdatedRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Movies;
+    using TraktApiSharp.Objects.Get.Movies.Common;
 
     [TestClass]
     public class TraktMoviesRecentlyUpdatedRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktMoviesRecentlyUpdatedRequestIsSealed()
         {
             typeof(TraktMoviesRecentlyUpdatedRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies"), TestCategory("Lists")]
+        public void TestTraktMoviesRecentlyUpdatedRequestIsSubclassOfATraktPaginationGetRequest()
+        {
+            typeof(TraktMoviesRecentlyUpdatedRequest).IsSubclassOf(typeof(ATraktPaginationGetRequest<TraktRecentlyUpdatedMovie>)).Should().BeTrue();
         }
     }
 }
