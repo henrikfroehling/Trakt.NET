b11ae10bf2d79f68ac47e1bdf9aafb1fa6282ef8
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 05:43:43 +0200

    Set authorization of TraktMovieRelatedMoviesRequest not required

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieRelatedMoviesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieRelatedMoviesRequestTests.cs
@@ -5,6 +5,7 @@
     using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Movies;
     using TraktApiSharp.Objects.Get.Movies;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktMovieRelatedMoviesRequestTests
@@ -25,6 +26,13 @@
         public void TestTraktMovieRelatedMoviesRequestIsSubclassOfATraktPaginationGetByIdRequest()
         {
             typeof(TraktMovieRelatedMoviesRequest).IsSubclassOf(typeof(ATraktPaginationGetByIdRequest<TraktMovie>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieRelatedMoviesRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktMovieRelatedMoviesRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
     }
 }
