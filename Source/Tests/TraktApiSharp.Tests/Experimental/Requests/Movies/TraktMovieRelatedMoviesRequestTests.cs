11f786a050da2a5bfd02f9ca1f17333f2854b37d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 05:38:08 +0200

    Add movie related movies request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieRelatedMoviesRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Movies
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Movies;
+
+    [TestClass]
+    public class TraktMovieRelatedMoviesRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieRelatedMoviesRequestIsNotAbstract()
+        {
+            typeof(TraktMovieRelatedMoviesRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
