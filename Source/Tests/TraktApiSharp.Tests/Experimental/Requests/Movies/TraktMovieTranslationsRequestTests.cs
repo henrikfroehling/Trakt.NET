5a07e9767596f86ab7701f61495a122f4ed1773f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 07:08:00 +0200

    Derive TraktMovieTranslationsRequest from ATraktListGetByIdRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieTranslationsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieTranslationsRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Movies;
+    using TraktApiSharp.Objects.Get.Movies;
 
     [TestClass]
     public class TraktMovieTranslationsRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktMovieTranslationsRequestIsSealed()
         {
             typeof(TraktMovieTranslationsRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieTranslationsRequestIsSubclassOfATraktListGetByIdRequest()
+        {
+            typeof(TraktMovieTranslationsRequest).IsSubclassOf(typeof(ATraktListGetByIdRequest<TraktMovieTranslation>)).Should().BeTrue();
         }
     }
 }
