644b805b0575156dfd6ff1510961e42ad4701255
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 06:19:42 +0200

    Add overload for GetUriPathParameters method in TraktMovieSingleReleaseRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieSingleReleaseRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieSingleReleaseRequest.cs
@@ -3,6 +3,7 @@
     using Base.Get;
     using Interfaces;
     using Objects.Get.Movies;
+    using System.Collections.Generic;
     using TraktApiSharp.Requests;
 
     internal sealed class TraktMovieSingleReleaseRequest : ATraktSingleItemGetByIdRequest<TraktMovieRelease>, ITraktObjectRequest
@@ -10,6 +11,11 @@
         internal TraktMovieSingleReleaseRequest(TraktClient client) : base(client) { }
 
         internal string LanguageCode { get; set; }
+
+        public override IDictionary<string, object> GetUriPathParameters()
+        {
+            return base.GetUriPathParameters();
+        }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
 
