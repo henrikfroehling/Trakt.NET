905c6e0dac0e26f91e1eec75321179d062ddcf7a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-10-27 05:53:47 +0200

    Add movie releases request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Movies/TraktMovieReleasesRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Movies
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Movies;
+
+    [TestClass]
+    public class TraktMovieReleasesRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Movies")]
+        public void TestTraktMovieReleasesRequestIsNotAbstract()
+        {
+            typeof(TraktMovieReleasesRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
