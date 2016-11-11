d6225c2e6e3c4c5fc349226e50bfc35ea614c292
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-12 00:46:21 +0100

    Add updated shows request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsRecentlyUpdatedRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowsRecentlyUpdatedRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsRecentlyUpdatedRequestIsNotAbstract()
+        {
+            typeof(TraktShowsRecentlyUpdatedRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
