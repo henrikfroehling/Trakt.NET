f1d4f219cf98edcca4cdb62533e46dae56201907
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-11 22:28:17 +0100

    Set authorization of TraktShowsPopularRequest not required

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsPopularRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsPopularRequestTests.cs
@@ -4,6 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Shows;
     using TraktApiSharp.Objects.Get.Shows;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktShowsPopularRequestTests
@@ -24,6 +25,13 @@
         public void TestTraktShowsPopularRequestIsSubclassOfATraktShowsRequest()
         {
             typeof(TraktShowsPopularRequest).IsSubclassOf(typeof(ATraktShowsRequest<TraktShow>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsPopularRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktShowsPopularRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
     }
 }
