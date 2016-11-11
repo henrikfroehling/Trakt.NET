05b542497b031f21a798aad47bf9f6753b4fa60e
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-11 22:58:47 +0100

    Add trending shows request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsTrendingRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowsTrendingRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsTrendingRequestIsNotAbstract()
+        {
+            typeof(TraktShowsTrendingRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
