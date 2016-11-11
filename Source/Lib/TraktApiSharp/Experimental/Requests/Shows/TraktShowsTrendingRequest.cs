34a3bf585847194b7ae250d7ceea1ad507b34cb5
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-11 23:01:25 +0100

    Derive TraktShowsTrendingRequest from ATraktShowsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowsTrendingRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowsTrendingRequest.cs
@@ -1,7 +1,27 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
-    internal sealed class TraktShowsTrendingRequest
-    {
+    using Objects.Get.Shows.Common;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktShowsTrendingRequest : ATraktShowsRequest<TraktTrendingShow>
+    {
+        public TraktShowsTrendingRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsTrendingRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsTrendingRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Shows;
+    using TraktApiSharp.Objects.Get.Shows.Common;
 
     [TestClass]
     public class TraktShowsTrendingRequestTests
@@ -17,6 +18,12 @@
         public void TestTraktShowsTrendingRequestIsSealed()
         {
             typeof(TraktShowsTrendingRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsTrendingRequestIsSubclassOfATraktShowsRequest()
+        {
+            typeof(TraktShowsTrendingRequest).IsSubclassOf(typeof(ATraktShowsRequest<TraktTrendingShow>)).Should().BeTrue();
         }
     }
 }
