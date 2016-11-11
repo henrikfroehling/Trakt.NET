5c6d9ed0610806927781c3a683cce98c392fc4b4
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-11 21:41:26 +0100

    Add base class for show lists requests

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/ATraktShowsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class ATraktShowsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestATraktShowsRequestIsAbstract()
+        {
+            typeof(ATraktShowsRequest).IsAbstract.Should().BeTrue();
+        }
+    }
+}
