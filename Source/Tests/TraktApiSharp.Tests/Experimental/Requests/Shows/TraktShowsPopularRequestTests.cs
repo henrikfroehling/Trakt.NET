3d32999e31111b81ee260584612ee7f98b5cfad0
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-11 22:21:14 +0100

    Add popular shows request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsPopularRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowsPopularRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsPopularRequestIsNotAbstract()
+        {
+            typeof(TraktShowsPopularRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
