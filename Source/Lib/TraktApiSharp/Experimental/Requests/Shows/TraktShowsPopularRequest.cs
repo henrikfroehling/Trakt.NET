bb41762142babc7789fa7d054a6ff247b883b161
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-11 22:26:41 +0100

    Derive TraktShowsPopularRequest from ATraktShowsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowsPopularRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowsPopularRequest.cs
@@ -1,7 +1,27 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
-    internal sealed class TraktShowsPopularRequest
-    {
+    using Objects.Get.Shows;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktShowsPopularRequest : ATraktShowsRequest<TraktShow>
+    {
+        public TraktShowsPopularRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsPopularRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsPopularRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Shows;
+    using TraktApiSharp.Objects.Get.Shows;
 
     [TestClass]
     public class TraktShowsPopularRequestTests
@@ -17,6 +18,12 @@
         public void TestTraktShowsPopularRequestIsSealed()
         {
             typeof(TraktShowsPopularRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsPopularRequestIsSubclassOfATraktShowsRequest()
+        {
+            typeof(TraktShowsPopularRequest).IsSubclassOf(typeof(ATraktShowsRequest<TraktShow>)).Should().BeTrue();
         }
     }
 }
