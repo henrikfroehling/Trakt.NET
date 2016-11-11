88bfffc7261b41b95866a094a744816eed9a372c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-11 21:49:48 +0100

    Implement ITraktExtendedInfo in ATraktShowsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/ATraktShowsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/ATraktShowsRequest.cs
@@ -1,9 +1,13 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
     using Base.Get;
+    using Interfaces;
+    using TraktApiSharp.Requests.Params;
 
-    internal abstract class ATraktShowsRequest<TItem> : ATraktPaginationGetRequest<TItem>
+    internal abstract class ATraktShowsRequest<TItem> : ATraktPaginationGetRequest<TItem>, ITraktExtendedInfo
     {
         public ATraktShowsRequest(TraktClient client) : base(client) { }
+
+        public TraktExtendedInfo ExtendedInfo { get; set; }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/ATraktShowsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/ATraktShowsRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Shows;
 
     [TestClass]
@@ -25,6 +26,12 @@
         public void TestATraktShowsRequestIsSubclassOfATraktPaginationGetRequest()
         {
             typeof(ATraktShowsRequest<int>).IsSubclassOf(typeof(ATraktPaginationGetRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestATraktShowsRequestImplementsITraktExtendedInfoInterface()
+        {
+            typeof(ATraktShowsRequest<>).GetInterfaces().Should().Contain(typeof(ITraktExtendedInfo));
         }
     }
 }
