42ab875a4306b3de7dbd04627ca264f3a458346d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-12 01:49:50 +0100

    Add most watched shows request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsMostWatchedRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowsMostWatchedRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsMostWatchedRequestIsNotAbstract()
+        {
+            typeof(TraktShowsMostWatchedRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
