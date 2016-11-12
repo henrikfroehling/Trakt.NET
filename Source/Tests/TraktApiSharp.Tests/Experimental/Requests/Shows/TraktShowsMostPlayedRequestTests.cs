5bade015568cf394ca59405e32bd9eec3149c027
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-12 01:33:18 +0100

    Add most played shows request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsMostPlayedRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowsMostPlayedRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsMostPlayedRequestIsNotAbstract()
+        {
+            typeof(TraktShowsMostPlayedRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
