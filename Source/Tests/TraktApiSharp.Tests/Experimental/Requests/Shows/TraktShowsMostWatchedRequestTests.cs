6068e3eb47a4db6071161fb726fe90846ae1b7b5
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-12 01:59:30 +0100

    Add tests for GetUriPathParameters method implementation in TraktShowsMostWatchedRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsMostWatchedRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsMostWatchedRequestTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Enums;
     using TraktApiSharp.Experimental.Requests.Shows;
     using TraktApiSharp.Objects.Get.Shows.Common;
     using TraktApiSharp.Requests;
@@ -39,6 +40,46 @@
         {
             var request = new TraktShowsMostWatchedRequest(null);
             request.UriTemplate.Should().Be("shows/watched{/period}{?extended,page,limit,query,years,genres,languages,countries,runtimes,ratings,certifications,networks,status}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsMostWatchedRequestUriParamsWithoutPeriod()
+        {
+            var request = new TraktShowsMostWatchedRequest(null);
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsMostWatchedRequestUriParamsWithUnspecifiedPeriod()
+        {
+            var period = TraktTimePeriod.Unspecified;
+
+            var request = new TraktShowsMostWatchedRequest(null)
+            {
+                Period = period
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsMostWatchedRequestUriParamsWithPeriod()
+        {
+            var period = TraktTimePeriod.Monthly;
+
+            var request = new TraktShowsMostWatchedRequest(null)
+            {
+                Period = period
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("period", period.UriName);
         }
     }
 }
