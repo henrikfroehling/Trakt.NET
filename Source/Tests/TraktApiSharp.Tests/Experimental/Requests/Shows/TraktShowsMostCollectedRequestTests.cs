dcd41abe9a609e9a219d850b8d905257939dbfc7
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-12 02:12:48 +0100

    Add tests for GetUriPathParameters method implementation in TraktShowsMostCollectedRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsMostCollectedRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsMostCollectedRequestTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Enums;
     using TraktApiSharp.Experimental.Requests.Shows;
     using TraktApiSharp.Objects.Get.Shows.Common;
     using TraktApiSharp.Requests;
@@ -39,6 +40,46 @@
         {
             var request = new TraktShowsMostCollectedRequest(null);
             request.UriTemplate.Should().Be("shows/collected{/period}{?extended,page,limit,query,years,genres,languages,countries,runtimes,ratings,certifications,networks,status}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsMostCollectedRequestUriParamsWithoutPeriod()
+        {
+            var request = new TraktShowsMostCollectedRequest(null);
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsMostCollectedRequestUriParamsWithUnspecifiedPeriod()
+        {
+            var period = TraktTimePeriod.Unspecified;
+
+            var request = new TraktShowsMostCollectedRequest(null)
+            {
+                Period = period
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsMostCollectedRequestUriParamsWithPeriod()
+        {
+            var period = TraktTimePeriod.Monthly;
+
+            var request = new TraktShowsMostCollectedRequest(null)
+            {
+                Period = period
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("period", period.UriName);
         }
     }
 }
