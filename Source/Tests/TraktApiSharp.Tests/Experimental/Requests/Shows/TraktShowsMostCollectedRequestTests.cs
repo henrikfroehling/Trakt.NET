5c9e04d7290eba840b7a31a9b82e17f9746afe04
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-12 02:11:14 +0100

    Set uri template for TraktShowsMostCollectedRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsMostCollectedRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowsMostCollectedRequestTests.cs
@@ -33,5 +33,12 @@
             var request = new TraktShowsMostCollectedRequest(null);
             request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("Lists")]
+        public void TestTraktShowsMostCollectedRequestHasValidUriTemplate()
+        {
+            var request = new TraktShowsMostCollectedRequest(null);
+            request.UriTemplate.Should().Be("shows/collected{/period}{?extended,page,limit,query,years,genres,languages,countries,runtimes,ratings,certifications,networks,status}");
+        }
     }
 }
