88996aaceb5571d743c75ce110d0a07454156a75
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-12 18:35:14 +0100

    Convert Pair<T, U> to struct

--- Source/Lib/TraktApiSharp/Utils/Pair.cs
+++ Source/Lib/TraktApiSharp/Utils/Pair.cs
@@ -3,12 +3,9 @@
     /// <summary>A small container containing two values of different types.</summary>
     /// <typeparam name="T">The type of the first element in this pair.</typeparam>
     /// <typeparam name="U">The type of the second element in this pair.</typeparam>
-    public sealed class Pair<T, U>
+    public struct Pair<T, U>
     {
-        /// <summary>Initializes a new instance of the <see cref="Pair{T,U}" /> class.</summary>
-        public Pair() { }
-
-        /// <summary>Initializes a new instance of the <see cref="Pair{T,U}" /> class.</summary>
+        /// <summary>Initializes a new instance of the <see cref="Pair{T,U}" /> struct.</summary>
         /// <param name="first">The pair's first value.</param>
         /// <param name="second">The pair's second value.</param>
         public Pair(T first, U second)
