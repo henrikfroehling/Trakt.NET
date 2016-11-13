5b73102d4643074ec9b71849588060c146f7956f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-13 17:52:12 +0100

    Add calendar all dvd movies request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/TraktCalendarAllDVDMoviesRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Calendars
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Calendars;
+
+    [TestClass]
+    public class TraktCalendarAllDVDMoviesRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("Without OAuth"), TestCategory("Movies")]
+        public void TestTraktCalendarAllDVDMoviesRequestIsNotAbstract()
+        {
+            typeof(TraktCalendarAllDVDMoviesRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
