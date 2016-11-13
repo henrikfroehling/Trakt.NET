3d4d2ccc1a98417143f99d16bda2c7d29c97ff59
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-13 18:03:38 +0100

    Add tests for GetUriPathParameters() in TraktCalendarAllDVDMoviesRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/TraktCalendarAllDVDMoviesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Calendars/TraktCalendarAllDVDMoviesRequestTests.cs
@@ -2,7 +2,10 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
     using TraktApiSharp.Experimental.Requests.Calendars;
+    using TraktApiSharp.Extensions;
     using TraktApiSharp.Objects.Get.Calendars;
     using TraktApiSharp.Requests;
 
@@ -39,6 +42,76 @@
         {
             var request = new TraktCalendarAllDVDMoviesRequest(null);
             request.UriTemplate.Should().Be("calendars/all/dvd{/start_date}{/days}{?extended,query,years,genres,languages,countries,runtimes,ratings}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("Without OAuth"), TestCategory("Movies")]
+        public void TestTraktCalendarAllDVDMoviesRequestUriParamsWithoutStartDateAndDays()
+        {
+            var request = new TraktCalendarAllDVDMoviesRequest(null);
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("Without OAuth"), TestCategory("Movies")]
+        public void TestTraktCalendarAllDVDMoviesRequestUriParamsWithStartDate()
+        {
+            var startDate = DateTime.Now;
+
+            var request = new TraktCalendarAllDVDMoviesRequest(null)
+            {
+                StartDate = startDate
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("start_date", startDate.ToTraktDateString());
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("Without OAuth"), TestCategory("Movies")]
+        public void TestTraktCalendarAllDVDMoviesRequestUriParamsWithStartDateAndDays()
+        {
+            var startDate = DateTime.Now;
+            var days = 14;
+
+            var request = new TraktCalendarAllDVDMoviesRequest(null)
+            {
+                StartDate = startDate,
+                Days = days
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["start_date"] = startDate.ToTraktDateString(),
+                ["days"] = days
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Calendars"), TestCategory("Without OAuth"), TestCategory("Movies")]
+        public void TestTraktCalendarAllDVDMoviesRequestUriParamsWithDays()
+        {
+            var startDate = DateTime.Now;
+            var days = 14;
+
+            var request = new TraktCalendarAllDVDMoviesRequest(null)
+            {
+                Days = days
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["start_date"] = startDate.ToTraktDateString(),
+                ["days"] = days
+            });
         }
     }
 }
