0f0673431648031f811fd16cb567bc9224ab25f0
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-13 02:50:34 +0100

    Add request implementation for calendars-my-dvd

---
+++ Source/Lib/TraktApiSharp/Requests/WithOAuth/Calendars/TraktCalendarUserDVDMoviesRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Requests.WithOAuth.Calendars
+{
+    using Objects.Get.Calendars;
+
+    internal class TraktCalendarUserDVDMoviesRequest : TraktCalendarUserRequest<TraktCalendarMovie>
+    {
+        public TraktCalendarUserDVDMoviesRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "calendars/my/dvd{/start_date}{/days}{?extended,query,years,genres,languages,countries,runtimes,ratings}";
+    }
+}
