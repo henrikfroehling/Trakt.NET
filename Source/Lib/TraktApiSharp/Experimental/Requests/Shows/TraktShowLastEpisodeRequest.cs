467bff637ac61cc557c0f699f9d68740a56f4023
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-18 22:16:32 +0100

    Implement ITraktExtendedInfo in TraktShowLastEpisodeRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowLastEpisodeRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowLastEpisodeRequest.cs
@@ -4,12 +4,15 @@
     using Interfaces;
     using Objects.Get.Shows.Episodes;
     using TraktApiSharp.Requests;
+    using TraktApiSharp.Requests.Params;
 
-    internal sealed class TraktShowLastEpisodeRequest : ATraktSingleItemGetByIdRequest<TraktEpisode>, ITraktObjectRequest
+    internal sealed class TraktShowLastEpisodeRequest : ATraktSingleItemGetByIdRequest<TraktEpisode>, ITraktObjectRequest, ITraktExtendedInfo
     {
         public TraktShowLastEpisodeRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktExtendedInfo ExtendedInfo { get; set; }
 
         public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Shows;
 
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowLastEpisodeRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowLastEpisodeRequestTests.cs
@@ -48,5 +48,11 @@
         {
             typeof(TraktShowLastEpisodeRequest).GetInterfaces().Should().Contain(typeof(ITraktObjectRequest));
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowLastEpisodeRequestImplementsITraktExtendedInfoInterface()
+        {
+            typeof(TraktShowLastEpisodeRequest).GetInterfaces().Should().Contain(typeof(ITraktExtendedInfo));
+        }
     }
 }
