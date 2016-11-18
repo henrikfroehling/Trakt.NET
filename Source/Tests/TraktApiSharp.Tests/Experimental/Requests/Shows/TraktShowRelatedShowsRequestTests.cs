a1a9f8522481e702e4d1484bc22bdaf652dab85e
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 00:52:58 +0100

    Add show related shows request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowRelatedShowsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowRelatedShowsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowRelatedShowsRequestIsNotAbstract()
+        {
+            typeof(TraktShowRelatedShowsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
