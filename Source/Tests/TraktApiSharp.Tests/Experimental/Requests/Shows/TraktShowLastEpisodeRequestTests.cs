f5f331cc23dc8daad72cf4abcd16ab87232b8348
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-18 21:52:12 +0100

    Add show last episode request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowLastEpisodeRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowLastEpisodeRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowLastEpisodeRequestIsNotAbstract()
+        {
+            typeof(TraktShowLastEpisodeRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
