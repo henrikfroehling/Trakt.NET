cd8ed8a41af45e8bab4bf1388c8709828e076704
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-18 22:19:10 +0100

    Add show next episode request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowNextEpisodeRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowNextEpisodeRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowNextEpisodeRequestIsNotAbstract()
+        {
+            typeof(TraktShowNextEpisodeRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
