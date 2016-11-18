5766425e3efd0c3e27ea5a575711cd74f93108c2
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-18 21:15:19 +0100

    Set authorization of TraktShowAliasesRequest not required

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowAliasesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowAliasesRequestTests.cs
@@ -5,6 +5,7 @@
     using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Shows;
     using TraktApiSharp.Objects.Get.Shows;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktShowAliasesRequestTests
@@ -25,6 +26,13 @@
         public void TestTraktShowAliasesRequestIsSubclassOfATraktListGetByIdRequest()
         {
             typeof(TraktShowAliasesRequest).IsSubclassOf(typeof(ATraktListGetByIdRequest<TraktShowAlias>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowAliasesRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktShowAliasesRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
     }
 }
