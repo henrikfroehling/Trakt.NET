e79182866487f77743950003ae08c34ddc44764d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-18 21:07:07 +0100

    Add show aliases request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowAliasesRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowAliasesRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowAliasesRequestIsNotAbstract()
+        {
+            typeof(TraktShowAliasesRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
