f88f98d562231dda5911f50877c0750ed7f66924
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-18 22:25:07 +0100

    Implement ITraktObjectRequest in TraktShowNextEpisodeRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowNextEpisodeRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowNextEpisodeRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Get.Shows.Episodes;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktShowNextEpisodeRequest : ATraktSingleItemGetByIdRequest<TraktEpisode>
+    internal sealed class TraktShowNextEpisodeRequest : ATraktSingleItemGetByIdRequest<TraktEpisode>, ITraktObjectRequest
     {
         public TraktShowNextEpisodeRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Shows;
 
         public override string UriTemplate => "shows/{id}/next_episode{?extended}";
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowNextEpisodeRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowNextEpisodeRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Shows;
     using TraktApiSharp.Objects.Get.Shows.Episodes;
     using TraktApiSharp.Requests;
@@ -40,6 +41,12 @@
         {
             var request = new TraktShowNextEpisodeRequest(null);
             request.UriTemplate.Should().Be("shows/{id}/next_episode{?extended}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowNextEpisodeRequestImplementsITraktObjectRequestInterface()
+        {
+            typeof(TraktShowNextEpisodeRequest).GetInterfaces().Should().Contain(typeof(ITraktObjectRequest));
         }
     }
 }
