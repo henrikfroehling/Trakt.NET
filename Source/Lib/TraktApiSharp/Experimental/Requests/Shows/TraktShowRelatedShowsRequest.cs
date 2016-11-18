4f6b5700fd7061acae5c2a61983b07d0578e34b9
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 00:55:52 +0100

    Derive TraktShowRelatedShowsRequest from ATraktPaginationGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowRelatedShowsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowRelatedShowsRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
-    internal sealed class TraktShowRelatedShowsRequest
-    {
+    using Base.Get;
+    using Objects.Get.Shows;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktShowRelatedShowsRequest : ATraktPaginationGetByIdRequest<TraktShow>
+    {
+        public TraktShowRelatedShowsRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowRelatedShowsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowRelatedShowsRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Shows;
+    using TraktApiSharp.Objects.Get.Shows;
 
     [TestClass]
     public class TraktShowRelatedShowsRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktShowRelatedShowsRequestIsSealed()
         {
             typeof(TraktShowRelatedShowsRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowRelatedShowsRequestIsSubclassOfATraktPaginationGetByIdRequest()
+        {
+            typeof(TraktShowRelatedShowsRequest).IsSubclassOf(typeof(ATraktPaginationGetByIdRequest<TraktShow>)).Should().BeTrue();
         }
     }
 }
