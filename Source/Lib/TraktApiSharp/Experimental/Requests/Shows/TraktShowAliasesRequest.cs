0e1450ff4f76650d6576c6a54a826f0ceb9d99dd
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-18 21:19:27 +0100

    Implement ITraktObjectRequest in TraktShowAliasesRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowAliasesRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowAliasesRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Get.Shows;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktShowAliasesRequest : ATraktListGetByIdRequest<TraktShowAlias>
+    internal sealed class TraktShowAliasesRequest : ATraktListGetByIdRequest<TraktShowAlias>, ITraktObjectRequest
     {
         public TraktShowAliasesRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Shows;
 
         public override string UriTemplate => "shows/{id}/aliases";
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowAliasesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowAliasesRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Shows;
     using TraktApiSharp.Objects.Get.Shows;
     using TraktApiSharp.Requests;
@@ -40,6 +41,12 @@
         {
             var request = new TraktShowAliasesRequest(null);
             request.UriTemplate.Should().Be("shows/{id}/aliases");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowAliasesRequestImplementsITraktObjectRequestInterface()
+        {
+            typeof(TraktShowAliasesRequest).GetInterfaces().Should().Contain(typeof(ITraktObjectRequest));
         }
     }
 }
