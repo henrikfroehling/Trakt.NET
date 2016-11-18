abeee08c1262b753e38765d4741403af1dc08c26
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-18 21:30:48 +0100

    Derive TraktShowCommentsRequest from ATraktPaginationGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowCommentsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowCommentsRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
-    internal sealed class TraktShowCommentsRequest
-    {
+    using Base.Get;
+    using Objects.Basic;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktShowCommentsRequest : ATraktPaginationGetByIdRequest<TraktComment>
+    {
+        public TraktShowCommentsRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowCommentsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowCommentsRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Shows;
+    using TraktApiSharp.Objects.Basic;
 
     [TestClass]
     public class TraktShowCommentsRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktShowCommentsRequestIsSealed()
         {
             typeof(TraktShowCommentsRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowCommentsRequestIsSubclassOfATraktPaginationGetByIdRequest()
+        {
+            typeof(TraktShowCommentsRequest).IsSubclassOf(typeof(ATraktPaginationGetByIdRequest<TraktComment>)).Should().BeTrue();
         }
     }
 }
