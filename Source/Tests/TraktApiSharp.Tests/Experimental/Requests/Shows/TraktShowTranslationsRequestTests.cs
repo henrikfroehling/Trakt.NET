3db3a02020d1abb352fa7aa921d9341d1820eb07
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 02:01:13 +0100

    Add show translations request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowTranslationsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowTranslationsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowTranslationsRequestIsNotAbstract()
+        {
+            typeof(TraktShowTranslationsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
