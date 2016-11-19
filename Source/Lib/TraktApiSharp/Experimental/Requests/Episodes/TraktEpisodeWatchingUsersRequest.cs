be1b00f33739059a61590bd90360f6699480b62f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-20 00:05:04 +0100

    Add episode watching users request class

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Episodes/TraktEpisodeWatchingUsersRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Episodes
+{
+    internal class TraktEpisodeWatchingUsersRequest
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Episodes/TraktEpisodeWatchingUsersRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Episodes
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Episodes;
+
+    [TestClass]
+    public class TraktEpisodeWatchingUsersRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Episodes")]
+        public void TestTraktEpisodeWatchingUsersRequestIsNotAbstract()
+        {
+            typeof(TraktEpisodeWatchingUsersRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
