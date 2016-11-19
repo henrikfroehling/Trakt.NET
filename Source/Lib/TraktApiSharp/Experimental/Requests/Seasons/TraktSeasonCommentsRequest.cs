bf0b319125feb858f47c96aa244cd3b712c226c5
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 05:13:43 +0100

    Derive TraktSeasonCommentsRequest from ATraktPaginationGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Seasons/TraktSeasonCommentsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Seasons/TraktSeasonCommentsRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Seasons
 {
-    internal sealed class TraktSeasonCommentsRequest
-    {
+    using Base.Get;
+    using Objects.Basic;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktSeasonCommentsRequest : ATraktPaginationGetByIdRequest<TraktComment>
+    {
+        public TraktSeasonCommentsRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonCommentsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonCommentsRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Seasons;
+    using TraktApiSharp.Objects.Basic;
 
     [TestClass]
     public class TraktSeasonCommentsRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktSeasonCommentsRequestIsSealed()
         {
             typeof(TraktSeasonCommentsRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Seasons")]
+        public void TestTraktSeasonCommentsRequestIsSubclassOfATraktPaginationGetByIdRequest()
+        {
+            typeof(TraktSeasonCommentsRequest).IsSubclassOf(typeof(ATraktPaginationGetByIdRequest<TraktComment>)).Should().BeTrue();
         }
     }
 }
