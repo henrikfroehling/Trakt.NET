888d64fce899284e6739e410a947a749d3bb1cd9
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 08:28:39 +0100

    Derive TraktSeasonWatchingUsersRequest from ATraktListGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Seasons/TraktSeasonWatchingUsersRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Seasons/TraktSeasonWatchingUsersRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Seasons
 {
-    internal sealed class TraktSeasonWatchingUsersRequest
-    {
+    using Base.Get;
+    using Objects.Get.Users;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktSeasonWatchingUsersRequest : ATraktListGetByIdRequest<TraktUser>
+    {
+        public TraktSeasonWatchingUsersRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonWatchingUsersRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonWatchingUsersRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Seasons;
+    using TraktApiSharp.Objects.Get.Users;
 
     [TestClass]
     public class TraktSeasonWatchingUsersRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktSeasonWatchingUsersRequestIsSealed()
         {
             typeof(TraktSeasonWatchingUsersRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Seasons")]
+        public void TestTraktSeasonWatchingUsersRequestIsSubclassOfATraktListGetByIdRequest()
+        {
+            typeof(TraktSeasonWatchingUsersRequest).IsSubclassOf(typeof(ATraktListGetByIdRequest<TraktUser>)).Should().BeTrue();
         }
     }
 }
