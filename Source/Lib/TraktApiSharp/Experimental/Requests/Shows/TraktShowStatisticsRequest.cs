9353656ad49b4e44dc7e246b083995457f228c90
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 01:29:16 +0100

    Derive TraktShowStatisticsRequest from ATraktSingleItemGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowStatisticsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowStatisticsRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
-    internal sealed class TraktShowStatisticsRequest
-    {
+    using Base.Get;
+    using Objects.Basic;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktShowStatisticsRequest : ATraktSingleItemGetByIdRequest<TraktStatistics>
+    {
+        public TraktShowStatisticsRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowStatisticsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowStatisticsRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Shows;
+    using TraktApiSharp.Objects.Basic;
 
     [TestClass]
     public class TraktShowStatisticsRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktShowStatisticsRequestIsSealed()
         {
             typeof(TraktShowStatisticsRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowStatisticsRequestIsSubclassOfATraktSingleItemGetByIdRequest()
+        {
+            typeof(TraktShowStatisticsRequest).IsSubclassOf(typeof(ATraktSingleItemGetByIdRequest<TraktStatistics>)).Should().BeTrue();
         }
     }
 }
