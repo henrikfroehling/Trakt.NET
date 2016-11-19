c91cc6ed515badc381f72981c7a84810363f7533
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 02:49:28 +0100

    Add show watched progress request class

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/OAuth/TraktShowWatchedProgressRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Shows.OAuth
+{
+    internal class TraktShowWatchedProgressRequest
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/OAuth/TraktShowWatchedProgressRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows.OAuth;
+
+    [TestClass]
+    public class TraktShowWatchedProgressRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("With OAuth")]
+        public void TestTraktShowWatchedProgressRequestIsNotAbstract()
+        {
+            typeof(TraktShowWatchedProgressRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
