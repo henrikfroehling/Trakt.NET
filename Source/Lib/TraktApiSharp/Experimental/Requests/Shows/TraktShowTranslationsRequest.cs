2595308caef82318cc452022319cee1e969576b8
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 02:05:50 +0100

    Derive TraktShowTranslationsRequest from ATraktListGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowTranslationsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowTranslationsRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
-    internal sealed class TraktShowTranslationsRequest
-    {
+    using Base.Get;
+    using Objects.Get.Shows;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktShowTranslationsRequest : ATraktListGetByIdRequest<TraktShowTranslation>
+    {
+        public TraktShowTranslationsRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowTranslationsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowTranslationsRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Shows;
+    using TraktApiSharp.Objects.Get.Shows;
 
     [TestClass]
     public class TraktShowTranslationsRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktShowTranslationsRequestIsSealed()
         {
             typeof(TraktShowTranslationsRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowTranslationsRequestIsSubclassOfATraktListGetByIdRequest()
+        {
+            typeof(TraktShowTranslationsRequest).IsSubclassOf(typeof(ATraktListGetByIdRequest<TraktShowTranslation>)).Should().BeTrue();
         }
     }
 }
