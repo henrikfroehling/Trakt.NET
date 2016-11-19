ab73dada0a945e514da16709ac0f20d3ab77c065
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 23:46:36 +0100

    Add episode summary request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Episodes/TraktEpisodeSummaryRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Episodes
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Episodes;
+
+    [TestClass]
+    public class TraktEpisodeSummaryRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Episodes")]
+        public void TestTraktEpisodeSummaryRequestIsNotAbstract()
+        {
+            typeof(TraktEpisodeSummaryRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
