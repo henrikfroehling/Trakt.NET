e8b6bb696ef1c32b3697ae6c219a41d5655f421c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 07:48:19 +0100

    Add season single request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonSingleRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Seasons
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Seasons;
+
+    [TestClass]
+    public class TraktSeasonSingleRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Seasons")]
+        public void TestTraktSeasonSingleRequestIsNotAbstract()
+        {
+            typeof(TraktSeasonSingleRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
