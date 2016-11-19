9902220f15b015f8eec1c7c987662388f414678c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 02:26:12 +0100

    Add base class for show progress requests

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/OAuth/ATraktShowProgressRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows.OAuth;
+
+    [TestClass]
+    public class ATraktShowProgressRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("With OAuth")]
+        public void TestATraktShowProgressRequestIsAbstract()
+        {
+            typeof(ATraktShowProgressRequest).IsAbstract.Should().BeTrue();
+        }
+    }
+}
