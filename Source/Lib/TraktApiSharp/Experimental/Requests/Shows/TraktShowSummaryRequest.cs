9594bd047cd106da462381899a0e5c3c45c305bc
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 01:56:35 +0100

    Implement ITraktObjectRequest in TraktShowSummaryRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowSummaryRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowSummaryRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Get.Shows;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktShowSummaryRequest : ATraktSingleItemGetByIdRequest<TraktShow>
+    internal sealed class TraktShowSummaryRequest : ATraktSingleItemGetByIdRequest<TraktShow>, ITraktObjectRequest
     {
         public TraktShowSummaryRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Shows;
 
         public override string UriTemplate => "shows/{id}{?extended}";
     }
