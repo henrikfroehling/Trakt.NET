acdcf1f8cfc497beed484bd575ac49cdde9e50d9
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 08:15:32 +0100

    Implement ITraktObjectRequest in TraktSeasonStatisticsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Seasons/TraktSeasonStatisticsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Seasons/TraktSeasonStatisticsRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Seasons
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Basic;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktSeasonStatisticsRequest : ATraktSingleItemGetByIdRequest<TraktStatistics>
+    internal sealed class TraktSeasonStatisticsRequest : ATraktSingleItemGetByIdRequest<TraktStatistics>, ITraktObjectRequest
     {
         internal TraktSeasonStatisticsRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Seasons;
 
         public override string UriTemplate => "shows/{id}/seasons/{season}/stats";
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonStatisticsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonStatisticsRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Seasons;
     using TraktApiSharp.Objects.Basic;
     using TraktApiSharp.Requests;
@@ -40,6 +41,12 @@
         {
             var request = new TraktSeasonStatisticsRequest(null);
             request.UriTemplate.Should().Be("shows/{id}/seasons/{season}/stats");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Seasons")]
+        public void TestTraktSeasonStatisticsRequestImplementsITraktObjectRequestInterface()
+        {
+            typeof(TraktSeasonStatisticsRequest).GetInterfaces().Should().Contain(typeof(ITraktObjectRequest));
         }
     }
 }
