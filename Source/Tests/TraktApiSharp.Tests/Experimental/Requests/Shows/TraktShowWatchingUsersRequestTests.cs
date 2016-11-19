5643c2aecb31226cfad83f9e03e9c4ad5dcff52f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 02:11:39 +0100

    Add show watching users request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowWatchingUsersRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows;
+
+    [TestClass]
+    public class TraktShowWatchingUsersRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowWatchingUsersRequestIsNotAbstract()
+        {
+            typeof(TraktShowWatchingUsersRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
