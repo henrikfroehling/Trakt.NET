192e52d7231c2a30f943ee2b50b55290457843be
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 20:40:25 +0100

    Add episode comments request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Episodes/TraktEpisodeCommentsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Episodes
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Episodes;
+
+    [TestClass]
+    public class TraktEpisodeCommentsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Episodes")]
+        public void TestTraktEpisodeCommentsRequestIsNotAbstract()
+        {
+            typeof(TraktEpisodeCommentsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
