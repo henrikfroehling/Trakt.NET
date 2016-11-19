2208e074aa13f0f7eee0d5d2911d58e04dc26868
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 01:33:35 +0100

    Implement ITraktObjectRequest in TraktShowStatisticsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowStatisticsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowStatisticsRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Basic;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktShowStatisticsRequest : ATraktSingleItemGetByIdRequest<TraktStatistics>
+    internal sealed class TraktShowStatisticsRequest : ATraktSingleItemGetByIdRequest<TraktStatistics>, ITraktObjectRequest
     {
         public TraktShowStatisticsRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Shows;
 
         public override string UriTemplate => "shows/{id}/stats";
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowStatisticsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowStatisticsRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Shows;
     using TraktApiSharp.Objects.Basic;
     using TraktApiSharp.Requests;
@@ -40,6 +41,12 @@
         {
             var request = new TraktShowStatisticsRequest(null);
             request.UriTemplate.Should().Be("shows/{id}/stats");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowStatisticsRequestImplementsITraktObjectRequestInterface()
+        {
+            typeof(TraktShowStatisticsRequest).GetInterfaces().Should().Contain(typeof(ITraktObjectRequest));
         }
     }
 }
