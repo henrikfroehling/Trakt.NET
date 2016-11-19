071e589746099c4be8dc1b19c7d90e4871f4c5ad
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 23:48:46 +0100

    Derive TraktEpisodeSummaryRequest from ATraktSingleItemGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Episodes/TraktEpisodeSummaryRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Episodes/TraktEpisodeSummaryRequest.cs
@@ -1,7 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Episodes
 {
-    internal sealed class TraktEpisodeSummaryRequest
-    {
+    using Base.Get;
+    using Objects.Get.Shows.Episodes;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktEpisodeSummaryRequest : ATraktSingleItemGetByIdRequest<TraktEpisode>
+    {
+        internal TraktEpisodeSummaryRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Episodes/TraktEpisodeSummaryRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Episodes/TraktEpisodeSummaryRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Episodes;
+    using TraktApiSharp.Objects.Get.Shows.Episodes;
 
     [TestClass]
     public class TraktEpisodeSummaryRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktEpisodeSummaryRequestIsSealed()
         {
             typeof(TraktEpisodeSummaryRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Episodes")]
+        public void TestTraktEpisodeSummaryRequestIsSubclassOfATraktSingleItemGetByIdRequest()
+        {
+            typeof(TraktEpisodeSummaryRequest).IsSubclassOf(typeof(ATraktSingleItemGetByIdRequest<TraktEpisode>)).Should().BeTrue();
         }
     }
 }
