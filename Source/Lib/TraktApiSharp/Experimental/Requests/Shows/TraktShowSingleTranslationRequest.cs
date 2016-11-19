ac31c4bc2c57475164e6147a2eb44ba31d47f284
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 01:15:33 +0100

    Implement ITraktObjectRequest in TraktShowSingleTranslationRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowSingleTranslationRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowSingleTranslationRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Get.Shows;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktShowSingleTranslationRequest : ATraktSingleItemGetByIdRequest<TraktShowTranslation>
+    internal sealed class TraktShowSingleTranslationRequest : ATraktSingleItemGetByIdRequest<TraktShowTranslation>, ITraktObjectRequest
     {
         public TraktShowSingleTranslationRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Shows;
 
         public override string UriTemplate => "shows/{id}/translations/{language}";
     }
