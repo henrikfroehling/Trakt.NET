dc316235178636bdea2d4594b5c11effe8d22173
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 01:30:46 +0100

    Set authorization of TraktShowStatisticsRequest not required

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowStatisticsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowStatisticsRequestTests.cs
@@ -5,6 +5,7 @@
     using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Shows;
     using TraktApiSharp.Objects.Basic;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktShowStatisticsRequestTests
@@ -25,6 +26,13 @@
         public void TestTraktShowStatisticsRequestIsSubclassOfATraktSingleItemGetByIdRequest()
         {
             typeof(TraktShowStatisticsRequest).IsSubclassOf(typeof(ATraktSingleItemGetByIdRequest<TraktStatistics>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowStatisticsRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktShowStatisticsRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
     }
 }
