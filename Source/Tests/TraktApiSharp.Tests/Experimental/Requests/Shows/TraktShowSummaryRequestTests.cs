194c49907b640785a6db3b019375aeb5ff419ec7
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 01:55:26 +0100

    Set uri template for TraktShowSummaryRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowSummaryRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowSummaryRequestTests.cs
@@ -34,5 +34,12 @@
             var request = new TraktShowSummaryRequest(null);
             request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowSummaryRequestHasValidUriTemplate()
+        {
+            var request = new TraktShowSummaryRequest(null);
+            request.UriTemplate.Should().Be("shows/{id}{?extended}");
+        }
     }
 }
