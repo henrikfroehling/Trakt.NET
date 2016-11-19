0eefdd3baf128620b01a6abc6baccdeda1454d8b
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 06:30:54 +0100

    Set uri template for TraktSeasonRatingsRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonRatingsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonRatingsRequestTests.cs
@@ -34,5 +34,12 @@
             var request = new TraktSeasonRatingsRequest(null);
             request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Seasons")]
+        public void TestTraktSeasonRatingsRequestHasValidUriTemplate()
+        {
+            var request = new TraktSeasonRatingsRequest(null);
+            request.UriTemplate.Should().Be("shows/{id}/seasons/{season}/ratings");
+        }
     }
 }
