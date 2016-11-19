14fb4f370c7683f406d347faa3c3cafcda1ecd76
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 21:30:32 +0100

    Add EpisodeNumber property in TraktEpisodeRatingsRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Episodes/TraktEpisodeRatingsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Episodes/TraktEpisodeRatingsRequestTests.cs
@@ -63,5 +63,18 @@
             sortingPropertyInfo.CanWrite.Should().BeTrue();
             sortingPropertyInfo.PropertyType.Should().Be(typeof(uint));
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Episodes")]
+        public void TestTraktEpisodeRatingsRequestHasEpisodeNumberProperty()
+        {
+            var sortingPropertyInfo = typeof(TraktEpisodeRatingsRequest)
+                    .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.Name == "EpisodeNumber")
+                    .FirstOrDefault();
+
+            sortingPropertyInfo.CanRead.Should().BeTrue();
+            sortingPropertyInfo.CanWrite.Should().BeTrue();
+            sortingPropertyInfo.PropertyType.Should().Be(typeof(uint));
+        }
     }
 }
