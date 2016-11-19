59ecd8201e839a8c662fd76221622b5e4803b819
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 06:25:02 +0100

    Add season ratings request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonRatingsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Seasons
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Seasons;
+
+    [TestClass]
+    public class TraktSeasonRatingsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Seasons")]
+        public void TestTraktSeasonRatingsRequestIsNotAbstract()
+        {
+            typeof(TraktSeasonRatingsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
