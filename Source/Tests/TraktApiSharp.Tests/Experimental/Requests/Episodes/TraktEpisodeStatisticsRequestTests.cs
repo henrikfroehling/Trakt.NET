3694edea334b16008ed453aea0502d16023887b3
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 23:28:02 +0100

    Add episode statistics request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Episodes/TraktEpisodeStatisticsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Episodes
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Episodes;
+
+    [TestClass]
+    public class TraktEpisodeStatisticsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Episodes")]
+        public void TestTraktEpisodeStatisticsRequestIsNotAbstract()
+        {
+            typeof(TraktEpisodeStatisticsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
