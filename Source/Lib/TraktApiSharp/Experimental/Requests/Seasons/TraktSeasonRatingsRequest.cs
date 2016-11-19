29ccafbcaccb213d36c165a1c3b9a7e3ecea76ae
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 06:34:01 +0100

    Implement ITraktObjectRequest in TraktSeasonRatingsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Seasons/TraktSeasonRatingsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Seasons/TraktSeasonRatingsRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Seasons
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Basic;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktSeasonRatingsRequest : ATraktSingleItemGetByIdRequest<TraktRating>
+    internal sealed class TraktSeasonRatingsRequest : ATraktSingleItemGetByIdRequest<TraktRating>, ITraktObjectRequest
     {
         public TraktSeasonRatingsRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Seasons;
 
         public override string UriTemplate => "shows/{id}/seasons/{season}/ratings";
     }
