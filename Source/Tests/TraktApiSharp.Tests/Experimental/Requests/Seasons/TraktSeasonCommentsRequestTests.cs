842bdc4a19b1acfc01572820a6e48e10eb6c0abe
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 05:05:11 +0100

    Add seasons comments request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonCommentsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Seasons
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Seasons;
+
+    [TestClass]
+    public class TraktSeasonCommentsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Seasons")]
+        public void TestTraktSeasonCommentsRequestIsNotAbstract()
+        {
+            typeof(TraktSeasonCommentsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
