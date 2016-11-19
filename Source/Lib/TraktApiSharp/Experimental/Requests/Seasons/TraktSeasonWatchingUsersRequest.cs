3f15ecb49d24198a0d4ccfdbebf1994703de5ed5
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 08:25:41 +0100

    Add season watching users request class

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Seasons/TraktSeasonWatchingUsersRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Seasons
+{
+    internal class TraktSeasonWatchingUsersRequest
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonWatchingUsersRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Seasons
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Seasons;
+
+    [TestClass]
+    public class TraktSeasonWatchingUsersRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Seasons")]
+        public void TestTraktSeasonWatchingUsersRequestIsNotAbstract()
+        {
+            typeof(TraktSeasonWatchingUsersRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
