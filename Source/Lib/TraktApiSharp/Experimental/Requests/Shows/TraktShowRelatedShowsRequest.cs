fea8d16989e87ee061a19e834dab573746405ab0
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 01:01:18 +0100

    Implement ITraktObjectRequest in TraktShowRelatedShowsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowRelatedShowsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Shows/TraktShowRelatedShowsRequest.cs
@@ -1,14 +1,17 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Shows
 {
     using Base.Get;
+    using Interfaces;
     using Objects.Get.Shows;
     using TraktApiSharp.Requests;
 
-    internal sealed class TraktShowRelatedShowsRequest : ATraktPaginationGetByIdRequest<TraktShow>
+    internal sealed class TraktShowRelatedShowsRequest : ATraktPaginationGetByIdRequest<TraktShow>, ITraktObjectRequest
     {
         public TraktShowRelatedShowsRequest(TraktClient client) : base(client) { }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+
+        public TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Shows;
 
         public override string UriTemplate => "shows/{id}/related{?extended,page,limit}";
     }
