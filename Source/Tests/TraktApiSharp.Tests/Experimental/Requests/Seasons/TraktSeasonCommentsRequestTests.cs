6340c7a54a5520dbf2234d49789172b1685a1405
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 05:18:36 +0100

    Set uri template for TraktSeasonCommentsRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonCommentsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonCommentsRequestTests.cs
@@ -34,5 +34,12 @@
             var request = new TraktSeasonCommentsRequest(null);
             request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Seasons")]
+        public void TestTraktSeasonCommentsRequestHasValidUriTemplate()
+        {
+            var request = new TraktSeasonCommentsRequest(null);
+            request.UriTemplate.Should().Be("shows/{id}/seasons/{season}/comments{/sorting}{?page,limit}");
+        }
     }
 }
