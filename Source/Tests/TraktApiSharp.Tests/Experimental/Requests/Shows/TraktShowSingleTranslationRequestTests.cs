f07e954a2ca067e7e98d74dd7879f47019154d34
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 01:22:17 +0100

    Add implementation for GetUriPathParameters method in TraktShowSingleTranslationRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowSingleTranslationRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowSingleTranslationRequestTests.cs
@@ -75,5 +75,21 @@
             methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
             methodInfo.GetParameters().Should().BeEmpty();
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowSingleTranslationRequestUriParamsWithLanguageCode()
+        {
+            var languageCode = "de";
+
+            var request = new TraktShowSingleTranslationRequest(null)
+            {
+                LanguageCode = languageCode
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("language", languageCode);
+        }
     }
 }
