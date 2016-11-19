e29e4038490a8e677cd71a8e90751c56a7448bca
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 01:09:42 +0100

    Set authorization of TraktShowSingleTranslationRequest not required

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowSingleTranslationRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/TraktShowSingleTranslationRequestTests.cs
@@ -5,6 +5,7 @@
     using TraktApiSharp.Experimental.Requests.Base.Get;
     using TraktApiSharp.Experimental.Requests.Shows;
     using TraktApiSharp.Objects.Get.Shows;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktShowSingleTranslationRequestTests
@@ -25,6 +26,13 @@
         public void TestTraktShowSingleTranslationRequestIsSubclassOfATraktSingleItemGetByIdRequest()
         {
             typeof(TraktShowSingleTranslationRequest).IsSubclassOf(typeof(ATraktSingleItemGetByIdRequest<TraktShowTranslation>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows")]
+        public void TestTraktShowSingleTranslationRequestHasAuthorizationNotRequired()
+        {
+            var request = new TraktShowSingleTranslationRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.NotRequired);
         }
     }
 }
