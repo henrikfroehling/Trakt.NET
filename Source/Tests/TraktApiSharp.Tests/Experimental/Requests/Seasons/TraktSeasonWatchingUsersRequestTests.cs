8334e52d0aea08c4d622c02fde84d445c161d8f6
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 08:42:15 +0100

    Add overload for GetUriPathParameters method in TraktSeasonWatchingUsersRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonWatchingUsersRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Seasons/TraktSeasonWatchingUsersRequestTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using TraktApiSharp.Experimental.Requests.Base.Get;
@@ -68,6 +69,17 @@
             sortingPropertyInfo.CanRead.Should().BeTrue();
             sortingPropertyInfo.CanWrite.Should().BeTrue();
             sortingPropertyInfo.PropertyType.Should().Be(typeof(uint));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Seasons")]
+        public void TestTraktSeasonWatchingUsersRequestHasGetUriPathParametersMethod()
+        {
+            var methodInfo = typeof(TraktSeasonWatchingUsersRequest).GetMethods()
+                                                                    .Where(m => m.Name == "GetUriPathParameters")
+                                                                    .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
+            methodInfo.GetParameters().Should().BeEmpty();
         }
     }
 }
