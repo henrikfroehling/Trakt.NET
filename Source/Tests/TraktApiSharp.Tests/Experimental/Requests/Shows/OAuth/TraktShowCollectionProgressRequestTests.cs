64695a4e2010fc59a11853b99e2f46b5bf051174
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-19 02:41:08 +0100

    Add show collection progress request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Shows/OAuth/TraktShowCollectionProgressRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Shows.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Shows.OAuth;
+
+    [TestClass]
+    public class TraktShowCollectionProgressRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Shows"), TestCategory("With OAuth")]
+        public void TestTraktShowCollectionProgressRequestIsNotAbstract()
+        {
+            typeof(TraktShowCollectionProgressRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
