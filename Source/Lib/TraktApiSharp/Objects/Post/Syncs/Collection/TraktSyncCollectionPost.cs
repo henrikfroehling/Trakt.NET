574b0335fb88163f1a7159f93fcc0536ad83b16a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-25 07:36:58 +0100

    Add implementation for AddShows method in TraktSyncCollectionPostBuilder

--- Source/Lib/TraktApiSharp/Objects/Post/Syncs/Collection/TraktSyncCollectionPost.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Syncs/Collection/TraktSyncCollectionPost.cs
@@ -185,6 +185,15 @@
 
         public TraktSyncCollectionPostBuilder AddShows(IEnumerable<TraktShow> shows)
         {
+            if (shows == null)
+                throw new ArgumentNullException(nameof(shows));
+
+            if (shows.Count() == 0)
+                return this;
+
+            foreach (var show in shows)
+                AddShow(show);
+
             return this;
         }
 
