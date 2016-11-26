5827397c3f472d97f90bd9c2078cf4629dcf6fe0
Henrik Fröhling <henrik.froehling@gmail.com>
2016-11-26 06:43:59 +0100

    Add documentation for AddEpisodes method in TraktSyncHistoryPostBuilder

--- Source/Lib/TraktApiSharp/Objects/Post/Syncs/History/TraktSyncHistoryPost.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Syncs/History/TraktSyncHistoryPost.cs
@@ -398,6 +398,15 @@
             return AddEpisodeOrIgnore(episode);
         }
 
+        /// <summary>Adds a collection of <see cref="TraktEpisode" />s, which will be added to the history post.</summary>
+        /// <param name="episodes">A collection of Trakt episodes, which will be added.</param>
+        /// <returns>The current <see cref="TraktSyncHistoryPostBuilder" /> instance.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown, if the given episodes collection is null.
+        /// Thrown, if one of the given episodes is null.
+        /// Thrown, if one of the given episodes' ids are null.
+        /// </exception>
+        /// <exception cref="ArgumentException">Thrown, if one of the given episodes has no valid ids set.</exception>
         public TraktSyncHistoryPostBuilder AddEpisodes(IEnumerable<TraktEpisode> episodes)
         {
             if (episodes == null)
