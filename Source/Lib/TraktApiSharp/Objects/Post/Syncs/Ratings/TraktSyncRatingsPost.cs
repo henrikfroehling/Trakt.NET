a6cf0696a4e5046a2f5feeae42bd93c127b8470a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-05 15:25:39 +0100

    Add implementation for AddEpisodes method in TraktSyncRatingsPostBuilder

--- Source/Lib/TraktApiSharp/Objects/Post/Syncs/Ratings/TraktSyncRatingsPost.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Syncs/Ratings/TraktSyncRatingsPost.cs
@@ -566,6 +566,15 @@
 
         public TraktSyncRatingsPostBuilder AddEpisodes(IEnumerable<TraktEpisode> episodes)
         {
+            if (episodes == null)
+                throw new ArgumentNullException(nameof(episodes));
+
+            if (episodes.Count() == 0)
+                return this;
+
+            foreach (var episode in episodes)
+                AddEpisode(episode);
+
             return this;
         }
 
