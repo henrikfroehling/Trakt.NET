6269a242c88308c111e9201664fcff8c013d81a1
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-05 15:15:20 +0100

    Add implementation for AddShows method in TraktSyncRatingsPostBuilder

--- Source/Lib/TraktApiSharp/Objects/Post/Syncs/Ratings/TraktSyncRatingsPost.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Syncs/Ratings/TraktSyncRatingsPost.cs
@@ -168,6 +168,15 @@
 
         public TraktSyncRatingsPostBuilder AddShows(IEnumerable<TraktShow> shows)
         {
+            if (shows == null)
+                throw new ArgumentNullException(nameof(shows));
+
+            if (shows.Count() == 0)
+                return this;
+
+            foreach (var show in shows)
+                AddShow(show);
+
             return this;
         }
 
