bfac360323cdc44253e60b225a0bddf094a7c1e3
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-06 16:12:23 +0100

    Implement ITraktExtendedInfo in TraktSyncWatchedMoviesRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedMoviesRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedMoviesRequest.cs
@@ -1,10 +1,12 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Syncs.OAuth
 {
+    using Interfaces;
     using Objects.Get.Watched;
     using System;
     using TraktApiSharp.Requests;
+    using TraktApiSharp.Requests.Params;
 
-    internal sealed class TraktSyncWatchedMoviesRequest : ATraktSyncListRequest<TraktWatchedMovie>
+    internal sealed class TraktSyncWatchedMoviesRequest : ATraktSyncListRequest<TraktWatchedMovie>, ITraktExtendedInfo
     {
         internal TraktSyncWatchedMoviesRequest(TraktClient client) : base(client) { }
 
@@ -15,6 +17,8 @@
                 throw new NotImplementedException();
             }
         }
+
+        public TraktExtendedInfo ExtendedInfo { get; set; }
 
         public override string UriTemplate
         {
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedMoviesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedMoviesRequestTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
     using TraktApiSharp.Objects.Get.Watched;
 
@@ -24,6 +25,12 @@
         public void TestTraktSyncWatchedMoviesRequestIsSubclassOfATraktSyncListRequest()
         {
             typeof(TraktSyncWatchedMoviesRequest).IsSubclassOf(typeof(ATraktSyncListRequest<TraktWatchedMovie>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedMoviesRequestImplementsITraktExtendedInfoInterface()
+        {
+            typeof(TraktSyncWatchedMoviesRequest).GetInterfaces().Should().Contain(typeof(ITraktExtendedInfo));
         }
     }
 }
