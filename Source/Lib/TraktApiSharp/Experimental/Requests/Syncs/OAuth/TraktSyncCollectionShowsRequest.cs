e52233fb676043eb5602055c027a27fa0e86574c
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-06 10:33:24 +0100

    Derive TraktSyncCollectionShowsRequest from ATraktSyncListRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionShowsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionShowsRequest.cs
@@ -1,7 +1,27 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Syncs.OAuth
 {
-    internal sealed class TraktSyncCollectionShowsRequest
-    {
+    using Objects.Get.Collection;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktSyncCollectionShowsRequest : ATraktSyncListRequest<TraktCollectionShow>
+    {
+        internal TraktSyncCollectionShowsRequest(TraktClient client) : base(client) { }
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionShowsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionShowsRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
+    using TraktApiSharp.Objects.Get.Collection;
 
     [TestClass]
     public class TraktSyncCollectionShowsRequestTests
@@ -17,6 +18,12 @@
         public void TestTraktSyncCollectionShowsRequestIsSealed()
         {
             typeof(TraktSyncCollectionShowsRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncCollectionShowsRequestIsSubclassOfATraktSyncListRequest()
+        {
+            typeof(TraktSyncCollectionShowsRequest).IsSubclassOf(typeof(ATraktSyncListRequest<TraktCollectionShow>)).Should().BeTrue();
         }
     }
 }
