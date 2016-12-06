c9cdec0ccaa2556ae58444a3e1d15aef568fcdc4
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-06 13:05:53 +0100

    Add overload for GetUriPathParameters method in TraktSyncPlaybackProgressRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncPlaybackProgressRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncPlaybackProgressRequest.cs
@@ -2,6 +2,7 @@
 {
     using Enums;
     using Objects.Get.Syncs.Playback;
+    using System.Collections.Generic;
     using TraktApiSharp.Requests;
 
     internal sealed class TraktSyncPlaybackProgressRequest : ATraktSyncListRequest<TraktSyncPlaybackProgressItem>
@@ -11,6 +12,11 @@
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
 
         internal TraktSyncType Type { get; set; }
+
+        public override IDictionary<string, object> GetUriPathParameters()
+        {
+            return base.GetUriPathParameters();
+        }
 
         public override string UriTemplate => "sync/playback{/type}{?limit}";
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncPlaybackProgressRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncPlaybackProgressRequestTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using TraktApiSharp.Enums;
@@ -55,6 +56,17 @@
             sortingPropertyInfo.CanRead.Should().BeTrue();
             sortingPropertyInfo.CanWrite.Should().BeTrue();
             sortingPropertyInfo.PropertyType.Should().Be(typeof(TraktSyncType));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncPlaybackProgressRequestHasGetUriPathParametersMethod()
+        {
+            var methodInfo = typeof(TraktSyncPlaybackProgressRequest).GetMethods()
+                                                                     .Where(m => m.Name == "GetUriPathParameters")
+                                                                     .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
+            methodInfo.GetParameters().Should().BeEmpty();
         }
     }
 }
