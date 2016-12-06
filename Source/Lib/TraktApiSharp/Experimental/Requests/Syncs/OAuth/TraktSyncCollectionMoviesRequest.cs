dfcf6185892b0c3c1b88f43b53fb9e10ede9096d
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-06 10:12:19 +0100

    Make TraktSyncCollectionMoviesRequest sealed

--- Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionMoviesRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionMoviesRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Syncs.OAuth
 {
-    internal class TraktSyncCollectionMoviesRequest
+    internal sealed class TraktSyncCollectionMoviesRequest
     {
 
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionMoviesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionMoviesRequestTests.cs
@@ -12,5 +12,11 @@
         {
             typeof(TraktSyncCollectionMoviesRequest).IsAbstract.Should().BeFalse();
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncCollectionMoviesRequestIsSealed()
+        {
+            typeof(TraktSyncCollectionMoviesRequest).IsSealed.Should().BeTrue();
+        }
     }
 }
