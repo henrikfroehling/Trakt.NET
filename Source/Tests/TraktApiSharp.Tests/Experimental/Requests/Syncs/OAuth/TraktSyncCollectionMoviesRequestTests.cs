d09e14f0f413f1b601abcf034d634b1d72e1a428
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-06 10:10:55 +0100

    Add sync collection movies request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionMoviesRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Syncs.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
+
+    [TestClass]
+    public class TraktSyncCollectionMoviesRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncCollectionMoviesRequestIsNotAbstract()
+        {
+            typeof(TraktSyncCollectionMoviesRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
