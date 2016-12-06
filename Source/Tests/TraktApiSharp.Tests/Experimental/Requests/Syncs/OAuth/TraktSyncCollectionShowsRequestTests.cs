03a2139e9caaba4800385b5e7414a3e4d4145fbf
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-06 10:30:28 +0100

    Add sync collection shows request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionShowsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Syncs.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
+
+    [TestClass]
+    public class TraktSyncCollectionShowsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncCollectionShowsRequestIsNotAbstract()
+        {
+            typeof(TraktSyncCollectionShowsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
