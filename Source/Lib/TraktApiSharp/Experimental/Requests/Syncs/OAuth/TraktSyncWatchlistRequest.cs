7f6b304b843fc9058bb3c85abb108146131ae944
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-08 04:11:50 +0100

    Add Type property in TraktSyncWatchlistRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncWatchlistRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncWatchlistRequest.cs
@@ -1,10 +1,13 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Syncs.OAuth
 {
+    using Enums;
     using Objects.Get.Watchlist;
 
     internal sealed class TraktSyncWatchlistRequest : ATraktSyncPaginationRequest<TraktWatchlistItem>
     {
         internal TraktSyncWatchlistRequest(TraktClient client) : base(client) { }
+
+        internal TraktSyncItemType Type { get; set; }
 
         public override string UriTemplate => "sync/watchlist{/type}{?extended,page,limit}";
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncWatchlistRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncWatchlistRequestTests.cs
@@ -2,6 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Reflection;
+    using TraktApiSharp.Enums;
     using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
     using TraktApiSharp.Objects.Get.Watchlist;
     using TraktApiSharp.Requests;
@@ -39,6 +42,19 @@
         {
             var request = new TraktSyncWatchlistRequest(null);
             request.UriTemplate.Should().Be("sync/watchlist{/type}{?extended,page,limit}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchlistRequestHasTypeProperty()
+        {
+            var sortingPropertyInfo = typeof(TraktSyncWatchlistRequest)
+                    .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.Name == "Type")
+                    .FirstOrDefault();
+
+            sortingPropertyInfo.CanRead.Should().BeTrue();
+            sortingPropertyInfo.CanWrite.Should().BeTrue();
+            sortingPropertyInfo.PropertyType.Should().Be(typeof(TraktSyncItemType));
         }
     }
 }
