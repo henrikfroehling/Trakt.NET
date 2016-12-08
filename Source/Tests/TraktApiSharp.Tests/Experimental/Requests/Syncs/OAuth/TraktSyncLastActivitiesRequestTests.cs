26e71b5179d240eca4b97406b094deb2b45893b5
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-08 04:19:35 +0100

    Add sync last activities request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncLastActivitiesRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Syncs.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
+
+    [TestClass]
+    public class TraktSyncLastActivitiesRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncLastActivitiesRequestIsNotAbstract()
+        {
+            typeof(TraktSyncLastActivitiesRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
