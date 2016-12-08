5adabb45f5994b0f7914a33153f1d30ace2d8a48
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-08 03:53:25 +0100

    Add implementation for GetUriPathParameters method in TraktSyncWatchedHistoryRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedHistoryRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedHistoryRequest.cs
@@ -1,6 +1,7 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Syncs.OAuth
 {
     using Enums;
+    using Extensions;
     using Objects.Get.History;
     using System;
     using System.Collections.Generic;
@@ -22,7 +23,29 @@
 
         public override IDictionary<string, object> GetUriPathParameters()
         {
-            return base.GetUriPathParameters();
+            var uriParams = base.GetUriPathParameters();
+
+            var isTypeSetAndValid = Type != null && Type != TraktSyncItemType.Unspecified;
+
+            if (isTypeSetAndValid)
+                uriParams.Add("type", Type.UriName);
+
+            if (isTypeSetAndValid && ItemId.HasValue && ItemId.Value > 0)
+                uriParams.Add("item_id", ItemId.ToString());
+
+            if (StartAt.HasValue)
+            {
+                var startAt = StartAt.Value.ToUniversalTime();
+                uriParams.Add("start_at", startAt.ToTraktLongDateTimeString());
+            }
+
+            if (EndAt.HasValue)
+            {
+                var endAt = EndAt.Value.ToUniversalTime();
+                uriParams.Add("end_at", endAt.ToTraktLongDateTimeString());
+            }
+
+            return uriParams;
         }
 
         public override string UriTemplate => "sync/history{/type}{/item_id}{?start_at,end_at,extended,page,limit}";
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedHistoryRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedHistoryRequestTests.cs
@@ -8,6 +8,7 @@
     using System.Reflection;
     using TraktApiSharp.Enums;
     using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
+    using TraktApiSharp.Extensions;
     using TraktApiSharp.Objects.Get.History;
     using TraktApiSharp.Requests;
 
@@ -107,6 +108,228 @@
 
             methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
             methodInfo.GetParameters().Should().BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithoutAnyParameters()
+        {
+            var request = new TraktSyncWatchedHistoryRequest(null);
+            var uriParams = request.GetUriPathParameters();
+            uriParams.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithUnspecifiedType()
+        {
+            var type = TraktSyncItemType.Unspecified;
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                Type = type
+            };
+
+            var uriParams = request.GetUriPathParameters();
+            uriParams.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithType()
+        {
+            var type = TraktSyncItemType.Episode;
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                Type = type
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("type", type.UriName);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithItemIdAndWithoutType()
+        {
+            var itemId = 1394U;
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                ItemId = itemId
+            };
+
+            var uriParams = request.GetUriPathParameters();
+            uriParams.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithItemIdAndWithType()
+        {
+            var type = TraktSyncItemType.Show;
+            var itemId = 1394U;
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                ItemId = itemId,
+                Type = type
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["type"] = type.UriName,
+                ["item_id"] = itemId.ToString()
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithStartAt()
+        {
+            var startAt = new DateTime(2016, 11, 1);
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                StartAt = startAt
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("start_at", startAt.ToTraktLongDateTimeString());
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithEndAt()
+        {
+            var endAt = new DateTime(2016, 11, 30);
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                EndAt = endAt
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("end_at", endAt.ToTraktLongDateTimeString());
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithTypeAndStartAt()
+        {
+            var type = TraktSyncItemType.Episode;
+            var startAt = new DateTime(2016, 11, 1);
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                Type = type,
+                StartAt = startAt
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["type"] = type.UriName,
+                ["start_at"] = startAt.ToTraktLongDateTimeString()
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithTypeAndEndAt()
+        {
+            var type = TraktSyncItemType.Episode;
+            var endAt = new DateTime(2016, 11, 30);
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                Type = type,
+                EndAt = endAt
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["type"] = type.UriName,
+                ["end_at"] = endAt.ToTraktLongDateTimeString()
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithItemIdAndWithTypeAndStartAt()
+        {
+            var type = TraktSyncItemType.Show;
+            var itemId = 1394U;
+            var startAt = new DateTime(2016, 11, 1);
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                ItemId = itemId,
+                Type = type,
+                StartAt = startAt
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["type"] = type.UriName,
+                ["item_id"] = itemId.ToString(),
+                ["start_at"] = startAt.ToTraktLongDateTimeString()
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithItemIdAndWithTypeAndEndAt()
+        {
+            var type = TraktSyncItemType.Show;
+            var itemId = 1394U;
+            var endAt = new DateTime(2016, 11, 30);
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                ItemId = itemId,
+                Type = type,
+                EndAt = endAt
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["type"] = type.UriName,
+                ["item_id"] = itemId.ToString(),
+                ["end_at"] = endAt.ToTraktLongDateTimeString()
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryRequestUriParamsWithStartAtAndEndAt()
+        {
+            var startAt = new DateTime(2016, 11, 1);
+            var endAt = new DateTime(2016, 11, 30);
+
+            var request = new TraktSyncWatchedHistoryRequest(null)
+            {
+                StartAt = startAt,
+                EndAt = endAt
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["start_at"] = startAt.ToTraktLongDateTimeString(),
+                ["end_at"] = endAt.ToTraktLongDateTimeString()
+            });
         }
     }
 }
