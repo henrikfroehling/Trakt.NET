427c2faa9e7ea2ee831d2137a438b50eadb72dff
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-11 12:03:46 +0100

    Derive TraktSyncWatchedHistoryRemoveRequest from ATraktSyncSingleItemPostRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedHistoryRemoveRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedHistoryRemoveRequest.cs
@@ -1,7 +1,19 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Syncs.OAuth
 {
-    internal sealed class TraktSyncWatchedHistoryRemoveRequest
-    {
+    using Objects.Post.Syncs.History;
+    using Objects.Post.Syncs.History.Responses;
+    using System;
 
+    internal sealed class TraktSyncWatchedHistoryRemoveRequest : ATraktSyncSingleItemPostRequest<TraktSyncHistoryRemovePostResponse, TraktSyncHistoryRemovePost>
+    {
+        internal TraktSyncWatchedHistoryRemoveRequest(TraktClient client) : base(client) { }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
