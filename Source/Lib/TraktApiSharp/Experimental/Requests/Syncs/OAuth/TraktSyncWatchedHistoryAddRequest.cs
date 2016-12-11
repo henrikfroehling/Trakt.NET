c2028dccfb147aebc57d54f24d0720830536e525
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-11 11:56:27 +0100

    Derive TraktSyncWatchedHistoryAddRequest from ATraktSyncSingleItemPostRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedHistoryAddRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedHistoryAddRequest.cs
@@ -1,7 +1,19 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Syncs.OAuth
 {
-    internal sealed class TraktSyncWatchedHistoryAddRequest
-    {
+    using Objects.Post.Syncs.History;
+    using Objects.Post.Syncs.History.Responses;
+    using System;
 
+    internal sealed class TraktSyncWatchedHistoryAddRequest : ATraktSyncSingleItemPostRequest<TraktSyncHistoryPostResponse, TraktSyncHistoryPost>
+    {
+        internal TraktSyncWatchedHistoryAddRequest(TraktClient client) : base(client) { }
+
+        public override string UriTemplate
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedHistoryAddRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncWatchedHistoryAddRequestTests.cs
@@ -3,6 +3,8 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
+    using TraktApiSharp.Objects.Post.Syncs.History;
+    using TraktApiSharp.Objects.Post.Syncs.History.Responses;
 
     [TestClass]
     public class TraktSyncWatchedHistoryAddRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktSyncWatchedHistoryAddRequestIsSealed()
         {
             typeof(TraktSyncWatchedHistoryAddRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncWatchedHistoryAddRequestIsSubclassOfATraktSyncSingleItemPostRequest()
+        {
+            typeof(TraktSyncWatchedHistoryAddRequest).IsSubclassOf(typeof(ATraktSyncSingleItemPostRequest<TraktSyncHistoryPostResponse, TraktSyncHistoryPost>)).Should().BeTrue();
         }
     }
 }
