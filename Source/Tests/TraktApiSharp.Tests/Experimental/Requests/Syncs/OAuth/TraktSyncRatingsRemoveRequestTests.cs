3876c7ab8e70f85ffcb59c1713f596d68bc6182e
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-11 11:46:42 +0100

    Add sync ratings remove request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncRatingsRemoveRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Syncs.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
+
+    [TestClass]
+    public class TraktSyncRatingsRemoveRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncRatingsRemoveRequestIsNotAbstract()
+        {
+            typeof(TraktSyncRatingsRemoveRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
