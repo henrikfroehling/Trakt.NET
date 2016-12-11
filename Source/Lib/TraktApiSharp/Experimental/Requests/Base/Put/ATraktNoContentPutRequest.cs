c0ed279e38ac7b3d4162e719a42b8a5da17497c9
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-11 15:44:38 +0100

    Remove ITraktRequest interface in ATraktNoContentPutRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Base/Put/ATraktNoContentPutRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Put/ATraktNoContentPutRequest.cs
@@ -1,10 +1,9 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Base.Put
 {
-    using Interfaces;
     using System.Net.Http;
     using TraktApiSharp.Requests;
 
-    internal abstract class ATraktNoContentPutRequest<TRequestBody> : ATraktNoContentRequest, ITraktRequest, ITraktHasRequestBody<TRequestBody>
+    internal abstract class ATraktNoContentPutRequest<TRequestBody> : ATraktNoContentRequest, ITraktHasRequestBody<TRequestBody>
     {
         internal ATraktNoContentPutRequest(TraktClient client) : base(client)
         {
