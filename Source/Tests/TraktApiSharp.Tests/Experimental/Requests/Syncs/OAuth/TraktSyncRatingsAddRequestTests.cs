eab5cd9163494d93598c9e13614b8f380ccf0be7
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-11 11:36:45 +0100

    Add sync ratings add request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncRatingsAddRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Syncs.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
+
+    [TestClass]
+    public class TraktSyncRatingsAddRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncRatingsAddRequestIsNotAbstract()
+        {
+            typeof(TraktSyncRatingsAddRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
