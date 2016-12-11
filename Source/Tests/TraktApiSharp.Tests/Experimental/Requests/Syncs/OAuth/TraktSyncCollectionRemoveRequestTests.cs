36a8288fe9c239f72df586d058e039cdb8fc47f7
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-11 11:25:30 +0100

    Add sync collection remove request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Syncs/OAuth/TraktSyncCollectionRemoveRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Syncs.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Syncs.OAuth;
+
+    [TestClass]
+    public class TraktSyncCollectionRemoveRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestTraktSyncCollectionRemoveRequestIsNotAbstract()
+        {
+            typeof(TraktSyncCollectionRemoveRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
