267c7329fed934931052f34aa1f3fef62939cf3f
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-20 16:25:44 +0100

    Set valid object request type for TraktUserListUnlikeRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserListUnlikeRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserListUnlikeRequest.cs
@@ -7,13 +7,7 @@
     {
         internal TraktUserListUnlikeRequest(TraktClient client) : base(client) { }
 
-        public override TraktRequestObjectType RequestObjectType
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public override TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Lists;
 
         public override string UriTemplate
         {
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserListUnlikeRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserListUnlikeRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktUserListUnlikeRequestTests
@@ -23,6 +24,13 @@
         public void TestTraktUserListUnlikeRequestIsSubclassOfATraktUsersDeleteByIdRequest()
         {
             typeof(TraktUserListUnlikeRequest).IsSubclassOf(typeof(ATraktUsersDeleteByIdRequest)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserListUnlikeRequestHasValidRequestObjectType()
+        {
+            var request = new TraktUserListUnlikeRequest(null);
+            request.RequestObjectType.Should().Be(TraktRequestObjectType.Lists);
         }
     }
 }
