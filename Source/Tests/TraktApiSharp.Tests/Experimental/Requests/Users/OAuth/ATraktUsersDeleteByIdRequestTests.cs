4cc399495cfd413395887d1487ee49444ccc9646
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-20 15:39:44 +0100

    Add base class for users delete requests

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/ATraktUsersDeleteByIdRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class ATraktUsersDeleteByIdRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Syncs")]
+        public void TestATraktUsersDeleteByIdRequestIsAbstract()
+        {
+            typeof(ATraktUsersDeleteByIdRequest).IsAbstract.Should().BeTrue();
+        }
+    }
+}
