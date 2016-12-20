e46d03e2bf36c29b2a42026546d60a7a25ec16f8
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-20 15:49:20 +0100

    Add users custom list delete request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCustomListDeleteRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserCustomListDeleteRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCustomListDeleteRequestIsNotAbstract()
+        {
+            typeof(TraktUserCustomListDeleteRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
