d9b6c4e977e9253e54e463a0f3e65228c0502d58
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-20 01:42:46 +0100

    Add interface for GetUriPathParameters() method

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/ITraktPathParametersTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+
+    [TestClass]
+    public class ITraktPathParametersTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktPathParametersIsInterface()
+        {
+            typeof(ITraktPathParameters).IsInterface.Should().BeTrue();
+        }
+    }
+}
