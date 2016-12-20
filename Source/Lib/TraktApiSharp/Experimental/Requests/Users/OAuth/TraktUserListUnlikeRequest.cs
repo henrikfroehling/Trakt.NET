90d1ddebfbec7e920ef2f59e611de59292834c0a
Henrik Fröhling <henrik.froehling@gmail.com>
2016-12-20 16:30:05 +0100

    Override GetUriPathParameters() method in TraktUserListUnlikeRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserListUnlikeRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserListUnlikeRequest.cs
@@ -1,5 +1,6 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Users.OAuth
 {
+    using System.Collections.Generic;
     using TraktApiSharp.Requests;
 
     internal sealed class TraktUserListUnlikeRequest : ATraktUsersDeleteByIdRequest
@@ -7,6 +8,11 @@
         internal TraktUserListUnlikeRequest(TraktClient client) : base(client) { }
 
         internal string Username { get; set; }
+
+        public override IDictionary<string, object> GetUriPathParameters()
+        {
+            return base.GetUriPathParameters();
+        }
 
         public override TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Lists;
 
