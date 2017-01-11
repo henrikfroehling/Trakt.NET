9a9b8906adab1481e53bfb72b5b0ca5f468bd838
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-11 01:33:57 +0100

    Cleanup

--- Source/Lib/TraktApiSharp/Properties/AssemblyInfo.cs
+++ Source/Lib/TraktApiSharp/Properties/AssemblyInfo.cs
@@ -4,9 +4,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("TraktApiSharp.Tests")]
 
-// Allgemeine Informationen über eine Assembly werden über folgende 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("TraktApiSharp")]
 [assembly: AssemblyDescription("C# wrapper library for the Trakt.tv API.")]
 [assembly: AssemblyConfiguration("")]
@@ -17,15 +14,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.8.0.0")]
 [assembly: AssemblyFileVersion("0.8.0.0")]
