eec7e9e442d8331d0e42d9b8fa4722acb41f143b
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-14 03:46:41 +0100

    Add Username property in TraktUserCollectionMoviesRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCollectionMoviesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCollectionMoviesRequestTests.cs
@@ -2,6 +2,8 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Reflection;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
     using TraktApiSharp.Objects.Get.Collection;
     using TraktApiSharp.Requests;
@@ -39,6 +41,19 @@
         {
             var request = new TraktUserCollectionMoviesRequest(null);
             request.UriTemplate.Should().Be("users/{username}/collection/movies{?extended}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCollectionMoviesRequestHasUsernameProperty()
+        {
+            var sortingPropertyInfo = typeof(TraktUserCollectionMoviesRequest)
+                    .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.Name == "Username")
+                    .FirstOrDefault();
+
+            sortingPropertyInfo.CanRead.Should().BeTrue();
+            sortingPropertyInfo.CanWrite.Should().BeTrue();
+            sortingPropertyInfo.PropertyType.Should().Be(typeof(string));
         }
     }
 }
