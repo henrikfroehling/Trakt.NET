10e7ce73b7dfe6f5e4e553419439d0c886a77422
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-14 03:31:02 +0100

    Add user collection movies request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCollectionMoviesRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserCollectionMoviesRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCollectionMoviesRequestIsNotAbstract()
+        {
+            typeof(TraktUserCollectionMoviesRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
