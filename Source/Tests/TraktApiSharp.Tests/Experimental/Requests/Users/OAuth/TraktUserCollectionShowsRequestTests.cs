b2e249c7a2645cd6a8f362333bf5ec7de06dd64b
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-14 04:07:11 +0100

    Add user collection shows request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCollectionShowsRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserCollectionShowsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCollectionShowsRequestIsNotAbstract()
+        {
+            typeof(TraktUserCollectionShowsRequest).IsAbstract.Should().BeFalse();
+        }
+    }
+}
