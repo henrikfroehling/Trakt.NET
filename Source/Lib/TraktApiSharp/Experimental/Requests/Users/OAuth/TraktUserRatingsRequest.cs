fdf465574b3c8a8eadfcf1919b7bcec8d3290f4e
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-14 22:02:46 +0100

    Add implementation for GetUriPathParameters() method in TraktUserRatingsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserRatingsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserRatingsRequest.cs
@@ -3,6 +3,7 @@
     using Enums;
     using Objects.Get.Ratings;
     using System.Collections.Generic;
+    using System.Linq;
     using TraktApiSharp.Requests;
 
     internal sealed class TraktUserRatingsRequest : ATraktUsersListGetRequest<TraktRatingsItem>
@@ -17,7 +18,27 @@
 
         public override IDictionary<string, object> GetUriPathParameters()
         {
-            return base.GetUriPathParameters();
+            var uriParams = base.GetUriPathParameters();
+
+            uriParams.Add("username", Username);
+
+            var isTypeSetAndValid = Type != null && Type != TraktRatingsItemType.Unspecified;
+
+            if (isTypeSetAndValid)
+                uriParams.Add("type", Type.UriName);
+
+            if (RatingFilter != null && isTypeSetAndValid)
+            {
+                var ratingMin = RatingFilter.Min();
+                var ratingMax = RatingFilter.Max();
+
+                var isRatingsSetAndValid = RatingFilter.Length > 0 && RatingFilter.Length <= 10 && ratingMin >= 1 && ratingMax <= 10;
+
+                if (isRatingsSetAndValid)
+                    uriParams.Add("rating", string.Join(",", RatingFilter));
+            }
+
+            return uriParams;
         }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Optional;
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserRatingsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserRatingsRequestTests.cs
@@ -94,5 +94,445 @@
             methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
             methodInfo.GetParameters().Should().BeEmpty();
         }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsername()
+        {
+            var username = "username";
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("username", username);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndType()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Episode;
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndUnspecifiedType()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Unspecified;
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("username", username);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndRatingFilter()
+        {
+            var username = "username";
+            var ratingFilter = new int[] { 1 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("username", username);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndUnspecifiedTypeAndRatingFilter()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Unspecified;
+            var ratingFilter = new int[] { 1 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(1);
+            uriParams.Should().Contain("username", username);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndValidRatingFilter_1()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName,
+                ["rating"] = string.Join(",", ratingFilter)
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndValidRatingFilter_1_2()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName,
+                ["rating"] = string.Join(",", ratingFilter)
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndValidRatingFilter_1_2_3()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2, 3 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName,
+                ["rating"] = string.Join(",", ratingFilter)
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndValidRatingFilter_1_2_3_4()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2, 3, 4 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName,
+                ["rating"] = string.Join(",", ratingFilter)
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndValidRatingFilter_1_2_3_4_5()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2, 3, 4, 5 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName,
+                ["rating"] = string.Join(",", ratingFilter)
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndValidRatingFilter_1_2_3_4_5_6()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName,
+                ["rating"] = string.Join(",", ratingFilter)
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndValidRatingFilter_1_2_3_4_5_6_7()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName,
+                ["rating"] = string.Join(",", ratingFilter)
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndValidRatingFilter_1_2_3_4_5_6_7_8()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName,
+                ["rating"] = string.Join(",", ratingFilter)
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndValidRatingFilter_1_2_3_4_5_6_7_8_9()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName,
+                ["rating"] = string.Join(",", ratingFilter)
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndValidRatingFilter_1_2_3_4_5_6_7_8_9_10()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(3);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName,
+                ["rating"] = string.Join(",", ratingFilter)
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndInvalidRatingFilter_1_2_3_4_5_6_7_8_9_10_11()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndInvalidRatingFilter_0_1_2_3_4_5_6_7_8_9_10()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndInvalidRatingFilter_1_2_3_4_5_6_7_8_9_11()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName
+            });
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestUriParamsWithUsernameAndTypeAndInvalidRatingFilter_0_1_2_3_4_5_6_7_8_9()
+        {
+            var username = "username";
+            var type = TraktRatingsItemType.Movie;
+            var ratingFilter = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var request = new TraktUserRatingsRequest(null)
+            {
+                Username = username,
+                Type = type,
+                RatingFilter = ratingFilter
+            };
+
+            var uriParams = request.GetUriPathParameters();
+
+            uriParams.Should().NotBeNull().And.NotBeEmpty().And.HaveCount(2);
+            uriParams.Should().Contain(new Dictionary<string, object>
+            {
+                ["username"] = username,
+                ["type"] = type.UriName
+            });
+        }
     }
 }
