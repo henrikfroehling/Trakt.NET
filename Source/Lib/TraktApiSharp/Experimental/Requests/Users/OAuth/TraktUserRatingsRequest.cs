e816e4954f7c9269a8aa94ba391b3f45f45a73aa
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-14 21:11:40 +0100

    Override GetUriPathParameters() method in TraktUserRatingsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserRatingsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserRatingsRequest.cs
@@ -2,6 +2,7 @@
 {
     using Enums;
     using Objects.Get.Ratings;
+    using System.Collections.Generic;
     using TraktApiSharp.Requests;
 
     internal sealed class TraktUserRatingsRequest : ATraktUsersListGetRequest<TraktRatingsItem>
@@ -13,6 +14,11 @@
         internal TraktRatingsItemType Type { get; set; }
 
         internal int[] RatingFilter { get; set; }
+
+        public override IDictionary<string, object> GetUriPathParameters()
+        {
+            return base.GetUriPathParameters();
+        }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Optional;
 
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserRatingsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserRatingsRequestTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using TraktApiSharp.Enums;
@@ -81,6 +82,17 @@
             sortingPropertyInfo.CanRead.Should().BeTrue();
             sortingPropertyInfo.CanWrite.Should().BeTrue();
             sortingPropertyInfo.PropertyType.Should().Be(typeof(int[]));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestHasGetUriPathParametersMethod()
+        {
+            var methodInfo = typeof(TraktUserRatingsRequest).GetMethods()
+                                                            .Where(m => m.Name == "GetUriPathParameters")
+                                                            .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
+            methodInfo.GetParameters().Should().BeEmpty();
         }
     }
 }
