3be7984c1da27e6bf76fd3654d81cb89340b7108
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-14 19:06:28 +0100

    Add user ratings request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserRatingsRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserRatingsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestIsNotAbstract()
+        {
+            typeof(TraktUserRatingsRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserRatingsRequestIsSealed()
+        {
+            typeof(TraktUserRatingsRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
