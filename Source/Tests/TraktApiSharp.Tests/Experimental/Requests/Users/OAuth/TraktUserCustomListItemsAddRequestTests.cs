45c2c8a1875da6e8b64014c5f01a395761f596bf
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 17:33:21 +0100

    Add Username property in TraktUserCustomListItemsAddRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCustomListItemsAddRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCustomListItemsAddRequestTests.cs
@@ -2,6 +2,8 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Reflection;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
     using TraktApiSharp.Objects.Post.Users.CustomListItems;
     using TraktApiSharp.Objects.Post.Users.CustomListItems.Responses;
@@ -47,6 +49,19 @@
         {
             var request = new TraktUserCustomListItemsAddRequest(null);
             request.RequestObjectType.Should().Be(TraktRequestObjectType.Lists);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCustomListItemsAddRequestHasUsernameProperty()
+        {
+            var sortingPropertyInfo = typeof(TraktUserCustomListItemsAddRequest)
+                    .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.Name == "Username")
+                    .FirstOrDefault();
+
+            sortingPropertyInfo.CanRead.Should().BeTrue();
+            sortingPropertyInfo.CanWrite.Should().BeTrue();
+            sortingPropertyInfo.PropertyType.Should().Be(typeof(string));
         }
     }
 }
