243dd9c8d7464f8aeb81124e233a2f6f646f7c5b
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 09:45:44 +0100

    Add Username property  in TraktUserCommentsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserCommentsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserCommentsRequest.cs
@@ -7,6 +7,8 @@
     {
         internal TraktUserCommentsRequest(TraktClient client) : base(client) {}
 
+        internal string Username { get; set; }
+
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Optional;
 
         public override string UriTemplate => "users/{username}/comments{/comment_type}{/type}{?extended,page,limit}";
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCommentsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCommentsRequestTests.cs
@@ -2,6 +2,8 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Reflection;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
     using TraktApiSharp.Objects.Get.Users;
     using TraktApiSharp.Requests;
@@ -39,6 +41,19 @@
         {
             var request = new TraktUserCommentsRequest(null);
             request.UriTemplate.Should().Be("users/{username}/comments{/comment_type}{/type}{?extended,page,limit}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCommentsRequestHasUsernameProperty()
+        {
+            var sortingPropertyInfo = typeof(TraktUserCommentsRequest)
+                    .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.Name == "Username")
+                    .FirstOrDefault();
+
+            sortingPropertyInfo.CanRead.Should().BeTrue();
+            sortingPropertyInfo.CanWrite.Should().BeTrue();
+            sortingPropertyInfo.PropertyType.Should().Be(typeof(string));
         }
     }
 }
