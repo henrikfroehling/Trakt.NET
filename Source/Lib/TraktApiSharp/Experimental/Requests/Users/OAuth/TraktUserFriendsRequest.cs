71b73ed8211a565990e189d30625561e3ff1625c
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 03:43:12 +0100

    Override GetUriPathParameters() method in TraktUserFriendsRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserFriendsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserFriendsRequest.cs
@@ -1,6 +1,7 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Users.OAuth
 {
     using Objects.Get.Users;
+    using System.Collections.Generic;
     using TraktApiSharp.Requests;
 
     internal sealed class TraktUserFriendsRequest : ATraktUsersListGetRequest<TraktUserFriend>
@@ -8,6 +9,11 @@
         internal TraktUserFriendsRequest(TraktClient client) : base(client) {}
 
         internal string Username { get; set; }
+
+        public override IDictionary<string, object> GetUriPathParameters()
+        {
+            return base.GetUriPathParameters();
+        }
 
         public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Optional;
 
