0f8c44b681623aa9d0089c566c9c0b492291fc91
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 00:23:57 +0100

    Add user custom list request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCustomSingleListRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserCustomSingleListRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCustomSingleListRequestIsNotAbstract()
+        {
+            typeof(TraktUserCustomSingleListRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCustomSingleListRequestIsSealed()
+        {
+            typeof(TraktUserCustomSingleListRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
