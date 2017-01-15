5331594acf25fcb2fca2d56b9d4cc1cd4ebf236b
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 04:16:24 +0100

    Add user watched shows request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserWatchedShowsRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserWatchedShowsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserWatchedShowsRequestIsNotAbstract()
+        {
+            typeof(TraktUserWatchedShowsRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserWatchedShowsRequestIsSealed()
+        {
+            typeof(TraktUserWatchedShowsRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
