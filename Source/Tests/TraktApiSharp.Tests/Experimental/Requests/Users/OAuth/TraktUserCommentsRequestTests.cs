16d8d9ccfd85e091d9d891b826b42aaa41305980
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 09:40:09 +0100

    Set authorization of TraktUserCommentsRequest optional

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCommentsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCommentsRequestTests.cs
@@ -4,6 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
     using TraktApiSharp.Objects.Get.Users;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktUserCommentsRequestTests
@@ -24,6 +25,13 @@
         public void TestTraktUserCommentsRequestIsSubclassOfATraktUsersPaginationGetRequest()
         {
             typeof(TraktUserCommentsRequest).IsSubclassOf(typeof(ATraktUsersPaginationGetRequest<TraktUserComment>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCommentsRequestHasAuthorizationOptional()
+        {
+            var request = new TraktUserCommentsRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.Optional);
         }
     }
 }
