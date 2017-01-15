41779f4e98861bb12ff2529eb6b4e00856685aa8
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 18:53:41 +0100

    Add user profile request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserProfileRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserProfileRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserProfileRequestIsNotAbstract()
+        {
+            typeof(TraktUserProfileRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserProfileRequestIsSealed()
+        {
+            typeof(TraktUserProfileRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
