c8a350bf323f1f83bebbcc3b47c21e28e25fae3e
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 10:22:48 +0100

    Set authorization of TraktUserHiddenItemsRequest required

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserHiddenItemsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserHiddenItemsRequest.cs
@@ -2,10 +2,13 @@
 {
     using Objects.Get.Users;
     using System;
+    using TraktApiSharp.Requests;
 
     internal sealed class TraktUserHiddenItemsRequest : ATraktUsersPaginationGetRequest<TraktUserHiddenItem>
     {
         internal TraktUserHiddenItemsRequest(TraktClient client) : base(client) {}
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
 
         public override string UriTemplate => throw new NotImplementedException();
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserHiddenItemsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserHiddenItemsRequestTests.cs
@@ -4,6 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
     using TraktApiSharp.Objects.Get.Users;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktUserHiddenItemsRequestTests
@@ -24,6 +25,13 @@
         public void TestTraktUserHiddenItemsRequestIsSubclassOfATraktUsersPaginationGetRequest()
         {
             typeof(TraktUserHiddenItemsRequest).IsSubclassOf(typeof(ATraktUsersPaginationGetRequest<TraktUserHiddenItem>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserHiddenItemsRequestHasAuthorizationRequired()
+        {
+            var request = new TraktUserHiddenItemsRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.Required);
         }
     }
 }
