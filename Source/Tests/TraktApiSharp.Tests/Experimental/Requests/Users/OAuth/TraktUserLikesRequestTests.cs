745d35d4c8e1c8f4cd2b39a5714e38c29ce296b5
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 23:24:04 +0100

    Add user likes request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserLikesRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserLikesRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserLikesRequestIsNotAbstract()
+        {
+            typeof(TraktUserLikesRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserLikesRequestIsSealed()
+        {
+            typeof(TraktUserLikesRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
