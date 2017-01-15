9b4459dd86ac7af837d2020fa407dda4a541d8b5
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 00:56:47 +0100

    Add user list update request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCustomListUpdateRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserCustomListUpdateRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCustomListUpdateRequestIsNotAbstract()
+        {
+            typeof(TraktUserCustomListUpdateRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCustomListUpdateRequestIsSealed()
+        {
+            typeof(TraktUserCustomListUpdateRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
