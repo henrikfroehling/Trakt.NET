70f1de7764ece6136052f12059e5a058dfef2244
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 19:16:26 +0100

    Add user watching request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserWatchingRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserWatchingRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserWatchingRequestIsNotAbstract()
+        {
+            typeof(TraktUserWatchingRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserWatchingRequestIsSealed()
+        {
+            typeof(TraktUserWatchingRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
