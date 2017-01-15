b3d63354efb579805f9fad0c5ba91471df11805a
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 16:45:29 +0100

    Ádd base class for users post requsts

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/ATraktUsersPostByIdRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class ATraktUsersPostByIdRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestATraktUsersPostByIdRequestIsAbstract()
+        {
+            typeof(ATraktUsersPostByIdRequest).IsAbstract.Should().BeTrue();
+        }
+    }
+}
