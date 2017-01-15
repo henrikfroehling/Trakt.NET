311c00389dc8987cfaf51bb8642e9d4ecc5b0194
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 03:59:51 +0100

    Set authorization of TraktUserWatchedMoviesRequest optional

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserWatchedMoviesRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserWatchedMoviesRequest.cs
@@ -2,10 +2,13 @@
 {
     using Objects.Get.Watched;
     using System;
+    using TraktApiSharp.Requests;
 
     internal sealed class TraktUserWatchedMoviesRequest : ATraktUsersListGetRequest<TraktWatchedMovie>
     {
         internal TraktUserWatchedMoviesRequest(TraktClient client) : base(client) {}
+
+        public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Optional;
 
         public override string UriTemplate => throw new NotImplementedException();
     }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserWatchedMoviesRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserWatchedMoviesRequestTests.cs
@@ -4,6 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
     using TraktApiSharp.Objects.Get.Watched;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktUserWatchedMoviesRequestTests
@@ -24,6 +25,13 @@
         public void TestTraktUserWatchedMoviesRequestIsSubclassOfATraktUsersListGetRequest()
         {
             typeof(TraktUserWatchedMoviesRequest).IsSubclassOf(typeof(ATraktUsersListGetRequest<TraktWatchedMovie>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserWatchedMoviesRequestHasAuthorizationOptional()
+        {
+            var request = new TraktUserWatchedMoviesRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.Optional);
         }
     }
 }
