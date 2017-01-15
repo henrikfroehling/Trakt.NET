bc973227e3698eaf2b6826857d07d3d0f44b108b
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 23:06:06 +0100

    Add user unfollow user request class

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserUnfollowUserRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Users.OAuth
+{
+    internal sealed class TraktUserUnfollowUserRequest
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserUnfollowUserRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserUnfollowUserRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserUnfollowUserRequestIsNotAbstract()
+        {
+            typeof(TraktUserUnfollowUserRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserUnfollowUserRequestIsSealed()
+        {
+            typeof(TraktUserUnfollowUserRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
