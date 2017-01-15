cec1c20dacd6709954136332dc1149261310a701
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 03:55:26 +0100

    Add user watched movies request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserWatchedMoviesRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserWatchedMoviesRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserWatchedMoviesRequestIsNotAbstract()
+        {
+            typeof(TraktUserWatchedMoviesRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserWatchedMoviesRequestIsSealed()
+        {
+            typeof(TraktUserWatchedMoviesRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
