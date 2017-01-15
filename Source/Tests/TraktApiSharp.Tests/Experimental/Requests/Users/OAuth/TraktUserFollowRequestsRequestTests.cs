68d943b3af14c10723aee16b9d40258bd55406a1
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 02:41:15 +0100

    Add user follow requests request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFollowRequestsRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserFollowRequestsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserFollowRequestsRequestIsNotAbstract()
+        {
+            typeof(TraktUserFollowRequestsRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserFollowRequestsRequestIsSealed()
+        {
+            typeof(TraktUserFollowRequestsRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
