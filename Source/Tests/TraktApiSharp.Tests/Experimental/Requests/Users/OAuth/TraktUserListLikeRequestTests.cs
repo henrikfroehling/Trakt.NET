1028614176e2f3d6095b4c621018719bb4780a3e
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 22:45:46 +0100

    Add user list like request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserListLikeRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserListLikeRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserListLikeRequestIsNotAbstract()
+        {
+            typeof(TraktUserListLikeRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserListLikeRequestIsSealed()
+        {
+            typeof(TraktUserListLikeRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
