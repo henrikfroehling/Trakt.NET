329d3cb771f79b385f06956e863778936b90d1bb
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 03:34:15 +0100

    Add Username property in TraktUserFriendsRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFriendsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFriendsRequestTests.cs
@@ -2,6 +2,8 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using System.Reflection;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
     using TraktApiSharp.Objects.Get.Users;
     using TraktApiSharp.Requests;
@@ -39,6 +41,19 @@
         {
             var request = new TraktUserFriendsRequest(null);
             request.UriTemplate.Should().Be("users/{username}/friends{?extended}");
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserFriendsRequestHasUsernameProperty()
+        {
+            var sortingPropertyInfo = typeof(TraktUserFriendsRequest)
+                    .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.Name == "Username")
+                    .FirstOrDefault();
+
+            sortingPropertyInfo.CanRead.Should().BeTrue();
+            sortingPropertyInfo.CanWrite.Should().BeTrue();
+            sortingPropertyInfo.PropertyType.Should().Be(typeof(string));
         }
     }
 }
