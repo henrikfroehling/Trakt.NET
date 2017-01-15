ed5fb4df1891650430bc7dcd598e9d3c53e7ee50
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 19:34:08 +0100

    Add user settings request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserSettingsRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserSettingsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserSettingsRequestIsNotAbstract()
+        {
+            typeof(TraktUserSettingsRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserSettingsRequestIsSealed()
+        {
+            typeof(TraktUserSettingsRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
