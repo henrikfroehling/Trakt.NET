62e99e3193e6e808b6053513ad7accfb1a53979b
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 10:40:21 +0100

    Add user watchlich request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserWatchlistRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserWatchlistRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserWatchlistRequestIsNotAbstract()
+        {
+            typeof(TraktUserWatchlistRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserWatchlistRequestIsSealed()
+        {
+            typeof(TraktUserWatchlistRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
