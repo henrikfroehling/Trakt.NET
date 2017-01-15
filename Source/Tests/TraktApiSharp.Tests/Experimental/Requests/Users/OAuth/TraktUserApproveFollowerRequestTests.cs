4ac97fa61d1de8d63bc74611b0a52a3e25972523
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 23:43:24 +0100

    Add user approve follower request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserApproveFollowerRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserApproveFollowerRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserApproveFollowerRequestIsNotAbstract()
+        {
+            typeof(TraktUserApproveFollowerRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserApproveFollowerRequestIsSealed()
+        {
+            typeof(TraktUserApproveFollowerRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
