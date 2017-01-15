592eb0191062979209f9aa15e3ed22fd609fb93f
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 19:45:23 +0100

    Add user statistics request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserStatisticsRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserStatisticsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserStatisticsRequestIsNotAbstract()
+        {
+            typeof(TraktUserStatisticsRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserStatisticsRequestIsSealed()
+        {
+            typeof(TraktUserStatisticsRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
