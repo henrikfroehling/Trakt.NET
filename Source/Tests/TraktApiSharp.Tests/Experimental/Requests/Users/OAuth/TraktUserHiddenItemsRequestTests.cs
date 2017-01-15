569fe4d6438cca6d10c46a996f3db5dfdae0d75e
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 10:18:34 +0100

    Add user hidden items request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserHiddenItemsRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserHiddenItemsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserHiddenItemsRequestIsNotAbstract()
+        {
+            typeof(TraktUserHiddenItemsRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserHiddenItemsRequestIsSealed()
+        {
+            typeof(TraktUserHiddenItemsRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
