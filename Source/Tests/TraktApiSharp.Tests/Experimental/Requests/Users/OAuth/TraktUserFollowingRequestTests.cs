15e973a0fe3323fc1f3781350e917920691c6b53
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 02:32:20 +0100

    Override GetUriPathParameters() method in TraktUserFollowingRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFollowingRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFollowingRequestTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
@@ -54,6 +55,17 @@
             sortingPropertyInfo.CanRead.Should().BeTrue();
             sortingPropertyInfo.CanWrite.Should().BeTrue();
             sortingPropertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserFollowingRequestHasGetUriPathParametersMethod()
+        {
+            var methodInfo = typeof(TraktUserFollowingRequest).GetMethods()
+                                                              .Where(m => m.Name == "GetUriPathParameters")
+                                                              .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
+            methodInfo.GetParameters().Should().BeEmpty();
         }
     }
 }
