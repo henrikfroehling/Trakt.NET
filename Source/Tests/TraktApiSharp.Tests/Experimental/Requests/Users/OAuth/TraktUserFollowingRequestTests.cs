84c0bacdcc39080ee059d59ec2d03633e4df45c8
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 02:15:32 +0100

    Derive TraktUserFollowingRequest from ATraktUsersListGetRequest

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFollowingRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFollowingRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
+    using TraktApiSharp.Objects.Get.Users;
 
     [TestClass]
     public class TraktUserFollowingRequestTests
@@ -17,6 +18,12 @@
         public void TestTraktUserFollowingRequestIsSealed()
         {
             typeof(TraktUserFollowingRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserFollowingRequestIsSubclassOfATraktUsersListGetRequest()
+        {
+            typeof(TraktUserFollowingRequest).IsSubclassOf(typeof(ATraktUsersListGetRequest<TraktUserFollower>)).Should().BeTrue();
         }
     }
 }
