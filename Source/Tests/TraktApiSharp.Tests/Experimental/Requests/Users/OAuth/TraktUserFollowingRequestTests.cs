11d35c487fcc8a63e5ebb7e227c10442d698bdf9
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 02:19:43 +0100

    Set authorization of TraktUserFollowingRequest optional

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFollowingRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFollowingRequestTests.cs
@@ -4,6 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
     using TraktApiSharp.Objects.Get.Users;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktUserFollowingRequestTests
@@ -24,6 +25,13 @@
         public void TestTraktUserFollowingRequestIsSubclassOfATraktUsersListGetRequest()
         {
             typeof(TraktUserFollowingRequest).IsSubclassOf(typeof(ATraktUsersListGetRequest<TraktUserFollower>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserFollowingRequestHasAuthorizationOptional()
+        {
+            var request = new TraktUserFollowingRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.Optional);
         }
     }
 }
