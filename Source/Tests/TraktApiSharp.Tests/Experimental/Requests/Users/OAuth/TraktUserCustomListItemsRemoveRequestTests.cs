28c7e37f0870a2c2a98b7bf402310ad2cfeda0db
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 17:54:26 +0100

    Add user custom list items remove request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCustomListItemsRemoveRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserCustomListItemsRemoveRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCustomListItemsRemoveRequestIsNotAbstract()
+        {
+            typeof(TraktUserCustomListItemsRemoveRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCustomListItemsRemoveRequestIsSealed()
+        {
+            typeof(TraktUserCustomListItemsRemoveRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
