917f801eaf9e95ff4cb76db701f475e4e2541e7f
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 23:17:37 +0100

    Override GetUriPathParameters() method in TraktUserUnfollowUserRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserUnfollowUserRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserUnfollowUserRequest.cs
@@ -1,12 +1,18 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Users.OAuth
 {
     using Base.Delete;
+    using System.Collections.Generic;
 
     internal sealed class TraktUserUnfollowUserRequest : ATraktNoContentDeleteRequest
     {
         internal TraktUserUnfollowUserRequest(TraktClient client) : base(client) {}
 
         internal string Username { get; set; }
+
+        public override IDictionary<string, object> GetUriPathParameters()
+        {
+            return base.GetUriPathParameters();
+        }
 
         public override string UriTemplate => "users/{username}/follow";
     }
