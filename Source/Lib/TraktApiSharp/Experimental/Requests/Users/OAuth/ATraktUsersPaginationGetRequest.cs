d10629146e12b9dbc111fa005e2de8f7a8f091b1
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 05:20:05 +0100

    Implement ITraktExtendedInfo in ATraktUsersPaginationGetRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/ATraktUsersPaginationGetRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/ATraktUsersPaginationGetRequest.cs
@@ -1,9 +1,13 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Users.OAuth
 {
     using Base.Get;
+    using Interfaces;
+    using TraktApiSharp.Requests.Params;
 
-    internal abstract class ATraktUsersPaginationGetRequest<TItem> : ATraktPaginationGetRequest<TItem>
+    internal abstract class ATraktUsersPaginationGetRequest<TItem> : ATraktPaginationGetRequest<TItem>, ITraktExtendedInfo
     {
         internal ATraktUsersPaginationGetRequest(TraktClient client) : base(client) {}
+
+        public TraktExtendedInfo ExtendedInfo { get; set; }
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/ATraktUsersPaginationGetRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/ATraktUsersPaginationGetRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
 
     [TestClass]
@@ -25,6 +26,12 @@
         public void TestATraktUsersPaginationGetRequestIsSubclassOfATraktPaginationGetRequest()
         {
             typeof(ATraktUsersPaginationGetRequest<int>).IsSubclassOf(typeof(ATraktPaginationGetRequest<int>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestATraktUsersPaginationGetRequestImplementsITraktExtendedInfoInterface()
+        {
+            typeof(ATraktUsersPaginationGetRequest<>).GetInterfaces().Should().Contain(typeof(ITraktExtendedInfo));
         }
     }
 }
