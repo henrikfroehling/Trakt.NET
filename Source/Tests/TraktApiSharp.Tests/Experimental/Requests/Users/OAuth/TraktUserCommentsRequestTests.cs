1f07424c790ac18542351799433e9affc9db8e3a
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 09:34:19 +0100

    Add user comments request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCommentsRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users.OAuth
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users.OAuth;
+
+    [TestClass]
+    public class TraktUserCommentsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCommentsRequestIsNotAbstract()
+        {
+            typeof(TraktUserCommentsRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCommentsRequestIsSealed()
+        {
+            typeof(TraktUserCommentsRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
