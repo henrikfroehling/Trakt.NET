299f29d3420cef5029d74f97caf5d71b490eefb3
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 10:20:42 +0100

    Derive TraktUserHiddenItemsRequest from ATraktUsersPaginationGetRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserHiddenItemsRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserHiddenItemsRequest.cs
@@ -1,7 +1,12 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Users.OAuth
 {
-    internal sealed class TraktUserHiddenItemsRequest
-    {
+    using Objects.Get.Users;
+    using System;
 
+    internal sealed class TraktUserHiddenItemsRequest : ATraktUsersPaginationGetRequest<TraktUserHiddenItem>
+    {
+        internal TraktUserHiddenItemsRequest(TraktClient client) : base(client) {}
+
+        public override string UriTemplate => throw new NotImplementedException();
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserHiddenItemsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserHiddenItemsRequestTests.cs
@@ -3,6 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
+    using TraktApiSharp.Objects.Get.Users;
 
     [TestClass]
     public class TraktUserHiddenItemsRequestTests
@@ -17,6 +18,12 @@
         public void TestTraktUserHiddenItemsRequestIsSealed()
         {
             typeof(TraktUserHiddenItemsRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserHiddenItemsRequestIsSubclassOfATraktUsersPaginationGetRequest()
+        {
+            typeof(TraktUserHiddenItemsRequest).IsSubclassOf(typeof(ATraktUsersPaginationGetRequest<TraktUserHiddenItem>)).Should().BeTrue();
         }
     }
 }
