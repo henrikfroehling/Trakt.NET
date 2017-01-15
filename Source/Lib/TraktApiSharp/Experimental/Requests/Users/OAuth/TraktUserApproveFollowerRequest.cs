556c1ee01878a8a8bcbe048b2ae742f7a712820e
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 23:46:15 +0100

    Derive TraktUserApproveFollowerRequest from ATraktSingleItemBodylessPostByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserApproveFollowerRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserApproveFollowerRequest.cs
@@ -1,7 +1,16 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Users.OAuth
 {
-    internal sealed class TraktUserApproveFollowerRequest
-    {
+    using Base.Post.Bodyless;
+    using Objects.Get.Users;
+    using System;
+    using TraktApiSharp.Requests;
 
+    internal sealed class TraktUserApproveFollowerRequest : ATraktSingleItemBodylessPostByIdRequest<TraktUserFollower>
+    {
+        internal TraktUserApproveFollowerRequest(TraktClient client) : base(client) {}
+
+        public override TraktRequestObjectType RequestObjectType => throw new NotImplementedException();
+
+        public override string UriTemplate => throw new NotImplementedException();
     }
 }
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserApproveFollowerRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserApproveFollowerRequestTests.cs
@@ -2,7 +2,9 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Base.Post.Bodyless;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
+    using TraktApiSharp.Objects.Get.Users;
 
     [TestClass]
     public class TraktUserApproveFollowerRequestTests
@@ -17,6 +19,12 @@
         public void TestTraktUserApproveFollowerRequestIsSealed()
         {
             typeof(TraktUserApproveFollowerRequest).IsSealed.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserApproveFollowerRequestIsSubclassOfATraktSingleItemBodylessPostByIdRequest()
+        {
+            typeof(TraktUserApproveFollowerRequest).IsSubclassOf(typeof(ATraktSingleItemBodylessPostByIdRequest<TraktUserFollower>)).Should().BeTrue();
         }
     }
 }
