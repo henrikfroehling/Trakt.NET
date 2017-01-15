f759b86585f6e8c405d36192ce72e2678314a8e7
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-15 02:57:12 +0100

    Set authorization of TraktUserFollowRequestsRequest required

--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFollowRequestsRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserFollowRequestsRequestTests.cs
@@ -4,6 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Users.OAuth;
     using TraktApiSharp.Objects.Get.Users;
+    using TraktApiSharp.Requests;
 
     [TestClass]
     public class TraktUserFollowRequestsRequestTests
@@ -24,6 +25,13 @@
         public void TestTraktUserFollowRequestsRequestIsSubclassOfATraktUsersListGetRequest()
         {
             typeof(TraktUserFollowRequestsRequest).IsSubclassOf(typeof(ATraktUsersListGetRequest<TraktUserFollowRequest>)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserFollowRequestsRequestHasAuthorizationRequired()
+        {
+            var request = new TraktUserFollowRequestsRequest(null);
+            request.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.Required);
         }
     }
 }
