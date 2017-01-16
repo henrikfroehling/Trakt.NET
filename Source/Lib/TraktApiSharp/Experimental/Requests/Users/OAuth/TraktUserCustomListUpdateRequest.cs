14b5f880dc6e6fe92e8ac6a4c83538640d19b234
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 01:08:36 +0100

    Override GetUriPathParameters() method in TraktUserCustomListUpdateRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserCustomListUpdateRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Users/OAuth/TraktUserCustomListUpdateRequest.cs
@@ -3,6 +3,7 @@
     using Base.Put;
     using Objects.Get.Users.Lists;
     using Objects.Post.Users;
+    using System.Collections.Generic;
     using TraktApiSharp.Requests;
 
     internal sealed class TraktUserCustomListUpdateRequest : ATraktSingleItemPutByIdRequest<TraktList, TraktUserCustomListPost>
@@ -10,6 +11,11 @@
         internal TraktUserCustomListUpdateRequest(TraktClient client) : base(client) {}
 
         internal string Username { get; set; }
+
+        public override IDictionary<string, object> GetUriPathParameters()
+        {
+            return base.GetUriPathParameters();
+        }
 
         public override TraktRequestObjectType RequestObjectType => TraktRequestObjectType.Lists;
 
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCustomListUpdateRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/OAuth/TraktUserCustomListUpdateRequestTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using TraktApiSharp.Experimental.Requests.Base.Put;
@@ -63,6 +64,17 @@
             sortingPropertyInfo.CanRead.Should().BeTrue();
             sortingPropertyInfo.CanWrite.Should().BeTrue();
             sortingPropertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserCustomListUpdateRequestHasGetUriPathParametersMethod()
+        {
+            var methodInfo = typeof(TraktUserCustomListUpdateRequest).GetMethods()
+                                                                     .Where(m => m.Name == "GetUriPathParameters")
+                                                                     .FirstOrDefault();
+
+            methodInfo.ReturnType.Should().Be(typeof(IDictionary<string, object>));
+            methodInfo.GetParameters().Should().BeEmpty();
         }
     }
 }
