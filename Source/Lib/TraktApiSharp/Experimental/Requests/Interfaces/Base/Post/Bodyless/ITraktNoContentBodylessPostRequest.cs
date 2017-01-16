10eb19722dfecc272c7a85e873482f12412dbe04
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 21:13:57 +0100

    Add ITraktNoContentBodylessPostRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Post/Bodyless/ITraktNoContentBodylessPostRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Post.Bodyless
+{
+    internal interface ITraktNoContentBodylessPostRequest : ITraktNoContentRequest
+    {
+
+    }
+}
