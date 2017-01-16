f3a812d27d4ac0d046591450952be9d0707b2d89
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 22:44:07 +0100

    Add ITraktPaginationPostRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Post/ITraktPaginationPostRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Post
+{
+    internal interface ITraktPaginationPostRequest<TItem, TRequestBody> : ITraktPaginationRequest<TItem>, ITraktHasRequestBody<TRequestBody>
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/Base/Post/ITraktPaginationPostRequestTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces.Base.Post
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Post;
+
+    [TestClass]
+    public class ITraktPaginationPostRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktPaginationPostRequestIsInterface()
+        {
+            typeof(ITraktPaginationPostRequest<,>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktPaginationPostRequestHasGenericTypeParameter()
+        {
+            typeof(ITraktPaginationPostRequest<,>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktPaginationPostRequest<int, float>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(2);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktPaginationPostRequestDerivesFromITraktPaginationRequestInterface()
+        {
+            typeof(ITraktPaginationPostRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktPaginationRequest<int>));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktPaginationPostRequestDerivesFromITraktHasRequestBodyInterface()
+        {
+            typeof(ITraktPaginationPostRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasRequestBody<float>));
+        }
+    }
+}
