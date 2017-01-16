3c1afcfe1e776f1e2200635060b38207b6064c20
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 23:24:15 +0100

    Add ITraktNoContentResponse interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Responses/Interfaces/Base/ITraktNoContentResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Experimental.Responses.Interfaces.Base
+{
+    using Exceptions;
+
+    public interface ITraktNoContentResponse
+    {
+        bool IsSuccess { get; set; }
+
+        TraktException Exception { get; set; }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Responses/Interfaces/Base/ITraktNoContentResponseTests.cs
@@ -0,0 +1,42 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Responses.Interfaces.Base
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using TraktApiSharp.Exceptions;
+    using TraktApiSharp.Experimental.Responses.Interfaces.Base;
+
+    [TestClass]
+    public class ITraktNoContentResponseTests
+    {
+        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
+        public void TestITraktNoContentResponseIsInterface()
+        {
+            typeof(ITraktNoContentResponse).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
+        public void TestITraktNoContentResponseHasIsSuccessProperty()
+        {
+            var isSuccessPropertyInfo = typeof(ITraktNoContentResponse).GetProperties()
+                                                                       .Where(p => p.Name == "IsSuccess")
+                                                                       .FirstOrDefault();
+
+            isSuccessPropertyInfo.CanRead.Should().BeTrue();
+            isSuccessPropertyInfo.CanWrite.Should().BeTrue();
+            isSuccessPropertyInfo.PropertyType.Should().Be(typeof(bool));
+        }
+
+        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
+        public void TestITraktNoContentResponseHasExceptionProperty()
+        {
+            var exceptionPropertyInfo = typeof(ITraktNoContentResponse).GetProperties()
+                                                                       .Where(p => p.Name == "Exception")
+                                                                       .FirstOrDefault();
+
+            exceptionPropertyInfo.CanRead.Should().BeTrue();
+            exceptionPropertyInfo.CanWrite.Should().BeTrue();
+            exceptionPropertyInfo.PropertyType.Should().Be(typeof(TraktException));
+        }
+    }
+}
