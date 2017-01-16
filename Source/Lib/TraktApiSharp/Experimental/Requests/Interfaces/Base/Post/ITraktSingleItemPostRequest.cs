45bde793859db2f6ca01a4070f0b372136174fe5
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 22:56:18 +0100

    Add ITraktSingleItemPostRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Post/ITraktSingleItemPostRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Post
+{
+    internal interface ITraktSingleItemPostRequest<TItem, TRequestBody> : ITraktSingleItemRequest<TItem>, ITraktHasRequestBody<TRequestBody>
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/Base/Post/ITraktSingleItemPostRequestTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces.Base.Post
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Post;
+
+    [TestClass]
+    public class ITraktSingleItemPostRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPostRequestIsInterface()
+        {
+            typeof(ITraktSingleItemPostRequest<,>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPostRequestHasGenericTypeParameter()
+        {
+            typeof(ITraktSingleItemPostRequest<,>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktSingleItemPostRequest<int, float>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(2);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPostRequestDerivesFromITraktSingleItemRequestInterface()
+        {
+            typeof(ITraktSingleItemPostRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktSingleItemRequest<int>));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPostRequestDerivesFromITraktHasRequestBodyInterface()
+        {
+            typeof(ITraktSingleItemPostRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasRequestBody<float>));
+        }
+    }
+}
