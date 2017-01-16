92bd3569c4db5cca04548a2269f41e4d4198cc48
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 20:50:20 +0100

    Add ITraktPaginationPutRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Put/ITraktPaginationPutRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Put
+{
+    internal interface ITraktPaginationPutRequest<TItem, TRequestBody> : ITraktPaginationRequest<TItem>, ITraktHasRequestBody<TRequestBody>
+    {
+
+    }
+}
