049a5ace808bed717a27fc5c46517d9a1c71b82c
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 01:13:38 +0100

    Add user list comments request class

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Users/TraktUserListCommentsRequestTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Users
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Users;
+
+    [TestClass]
+    public class TraktUserListCommentsRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserListCommentsRequestIsNotAbstract()
+        {
+            typeof(TraktUserListCommentsRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Users")]
+        public void TestTraktUserListCommentsRequestIsSealed()
+        {
+            typeof(TraktUserListCommentsRequest).IsSealed.Should().BeTrue();
+        }
+    }
+}
