9a0096d362075c1c02d1b6ceef1f4bc8f7b0f618
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 20:13:49 +0100

    Add ITraktPaginationGetByIdRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Get/ITraktPaginationGetByIdRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Get
+{
+    internal interface ITraktPaginationGetByIdRequest<TItem> : ITraktPaginationGetRequest<TItem>, ITraktHasId
+    {
+
+    }
+}
