d35827a98cda1ebc716eeec9bc2c3669bfdf05c5
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 20:59:00 +0100

    Add ITraktSingleItemPutRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Put/ITraktSingleItemPutRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Put
+{
+    internal interface ITraktSingleItemPutRequest<TItem> : ITraktSingleItemRequest<TItem>
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/Base/Put/ITraktSingleItemPutRequestTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces.Base.Put
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Put;
+
+    [TestClass]
+    public class ITraktSingleItemPutRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPutRequestIsInterface()
+        {
+            typeof(ITraktSingleItemPutRequest<>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPutRequestHasGenericTypeParameter()
+        {
+            typeof(ITraktSingleItemPutRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktSingleItemPutRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPutRequestDerivesFromITraktSingleItemRequestInterface()
+        {
+            typeof(ITraktSingleItemPutRequest<int>).GetInterfaces().Should().Contain(typeof(ITraktSingleItemRequest<int>));
+        }
+    }
+}
