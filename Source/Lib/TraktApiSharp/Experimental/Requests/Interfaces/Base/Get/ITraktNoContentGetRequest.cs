2726ad74c4c61d483b4a400e39fd54ee15b15466
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 20:00:23 +0100

    Add ITraktNoContentGetRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Get/ITraktNoContentGetRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Get
+{
+    internal interface ITraktNoContentGetRequest : ITraktNoContentRequest
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/Base/Get/ITraktNoContentGetRequestTests.cs
@@ -0,0 +1,23 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces.Base.Get
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Get;
+
+    [TestClass]
+    public class ITraktNoContentGetRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktNoContentGetRequestIsInterface()
+        {
+            typeof(ITraktNoContentGetRequest).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktNoContentGetRequestDerivesFromITraktNoContentRequestInterface()
+        {
+            typeof(ITraktNoContentGetRequest).GetInterfaces().Should().Contain(typeof(ITraktNoContentRequest));
+        }
+    }
+}
