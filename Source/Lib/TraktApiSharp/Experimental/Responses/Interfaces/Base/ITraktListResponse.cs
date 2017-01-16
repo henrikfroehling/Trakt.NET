c406aeadf63ceba0549dd2d17a6dcbf2d7733784
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-17 00:04:09 +0100

    Add ITraktListResponse interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Responses/Interfaces/Base/ITraktListResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TraktApiSharp.Experimental.Responses.Interfaces.Base
+{
+    using System.Collections.Generic;
+
+    public interface ITraktListResponse<TContentType> : ITraktResponse<IEnumerable<TContentType>>, IEnumerable<TContentType>
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Responses/Interfaces/Base/ITraktListResponseTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Responses.Interfaces.Base
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using TraktApiSharp.Experimental.Responses.Interfaces.Base;
+
+    [TestClass]
+    public class ITraktListResponseTests
+    {
+        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
+        public void TestITraktListResponseIsInterface()
+        {
+            typeof(ITraktListResponse<>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
+        public void TestITraktListResponseHasGenericTypeParameter()
+        {
+            typeof(ITraktListResponse<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktListResponse<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
+        public void TestITraktListResponseDerivesFromITraktResponseInterface()
+        {
+            typeof(ITraktListResponse<int>).GetInterfaces().Should().Contain(typeof(ITraktResponse<IEnumerable<int>>));
+        }
+
+        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
+        public void TestITraktListResponseDerivesFromIEnumerableInterface()
+        {
+            typeof(ITraktListResponse<int>).GetInterfaces().Should().Contain(typeof(IEnumerable<int>));
+        }
+    }
+}
