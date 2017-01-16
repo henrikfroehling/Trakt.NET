042bef69902d23ab2220ec8d89e789ff0679f29c
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 20:29:06 +0100

    Add ITraktListPutRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Put/ITraktListPutRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Put
+{
+    internal interface ITraktListPutRequest<TItem, TRequestBody> : ITraktListRequest<TItem>, ITraktHasRequestBody<TRequestBody>
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/Base/Put/ITraktListPutRequestTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces.Base.Put
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Put;
+
+    [TestClass]
+    public class ITraktListPutRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListPutRequestIsInterface()
+        {
+            typeof(ITraktListPutRequest<,>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListPutRequestHasGenericTypeParameter()
+        {
+            typeof(ITraktListPutRequest<,>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktListPutRequest<int, float>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(2);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListPutRequestDerivesFromITraktListRequestInterface()
+        {
+            typeof(ITraktListPutRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktListRequest<int>));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListPutRequestDerivesFromITraktHasRequestBodyInterface()
+        {
+            typeof(ITraktListPutRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasRequestBody<float>));
+        }
+    }
+}
