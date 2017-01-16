a2c0690370c471bc04e0edad23c210acdd9830cf
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 21:16:31 +0100

    Add ITraktNoContentBodylessPostByIdRequest interface

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/Base/Post/Bodyless/ITraktNoContentBodylessPostByIdRequestTests.cs
@@ -0,0 +1,29 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces.Base.Post.Bodyless
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Post.Bodyless;
+
+    [TestClass]
+    public class ITraktNoContentBodylessPostByIdRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktNoContentBodylessPostByIdRequestIsInterface()
+        {
+            typeof(ITraktNoContentBodylessPostByIdRequest).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktNoContentBodylessPostByIdRequestDerivesFromITraktNoContentBodylessPostRequestInterface()
+        {
+            typeof(ITraktNoContentBodylessPostByIdRequest).GetInterfaces().Should().Contain(typeof(ITraktNoContentBodylessPostRequest));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktNoContentBodylessPostByIdRequestDerivesFromITraktHasIdInterface()
+        {
+            typeof(ITraktNoContentBodylessPostByIdRequest).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
