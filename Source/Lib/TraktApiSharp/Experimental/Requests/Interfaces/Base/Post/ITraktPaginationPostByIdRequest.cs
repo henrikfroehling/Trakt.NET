3e4f3d31e8e0618d7eaf9eec909151ff904fca08
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 22:49:51 +0100

    Add ITraktPaginationPostByIdRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Post/ITraktPaginationPostByIdRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Post
+{
+    internal interface ITraktPaginationPostByIdRequest<TItem, TRequestBody> : ITraktPaginationPostRequest<TItem, TRequestBody>, ITraktHasId
+    {
+
+    }
+}
