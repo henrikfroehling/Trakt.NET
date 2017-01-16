f9d68a70dab98dee460535a46b26a95f552b9cc8
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 20:54:13 +0100

    Add ITraktPaginationPutByIdRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Put/ITraktPaginationPutByIdRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Put
+{
+    internal interface ITraktPaginationPutByIdRequest<TItem, TRequestBody> : ITraktPaginationPutRequest<TItem, TRequestBody>, ITraktHasId
+    {
+
+    }
+}
