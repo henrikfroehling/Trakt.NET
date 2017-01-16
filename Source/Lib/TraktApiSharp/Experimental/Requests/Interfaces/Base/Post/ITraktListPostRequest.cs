04ee89575c06c5e8ce12341d39c34146120a709c
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 21:38:44 +0100

    Add ITraktListPostRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Post/ITraktListPostRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Post
+{
+    internal interface ITraktListPostRequest<TItem, TRequestBody> : ITraktListRequest<TItem>, ITraktHasRequestBody<TRequestBody>
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/Base/Post/ITraktListPostRequestTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces.Base.Post
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Post;
+
+    [TestClass]
+    public class ITraktListPostRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListPostRequestRequestIsInterface()
+        {
+            typeof(ITraktListPostRequest<,>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListPostRequestHasGenericTypeParameter()
+        {
+            typeof(ITraktListPostRequest<,>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktListPostRequest<int, float>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(2);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListPostRequestDerivesFromITraktListRequestInterface()
+        {
+            typeof(ITraktListPostRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktListRequest<int>));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListPostRequestDerivesFromITraktHasRequestBodyInterface()
+        {
+            typeof(ITraktListPostRequest<int, float>).GetInterfaces().Should().Contain(typeof(ITraktHasRequestBody<float>));
+        }
+    }
+}
