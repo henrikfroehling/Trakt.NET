e7d016a4673e35c129b8569df36ac5c6a0ce5aa8
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 21:27:34 +0100

    Add ITraktSingleItemBodylessPostRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Post/Bodyless/ITraktSingleItemBodylessPostRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Post.Bodyless
+{
+    internal interface ITraktSingleItemBodylessPostRequest<TItem> : ITraktSingleItemRequest<TItem>
+    {
+
+    }
+}
