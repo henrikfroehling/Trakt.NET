a6bba0f48c97a6268529065bd431640b9648e43d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 21:07:51 +0100

    Add ITraktListBodylessPostRequest interface

---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/Base/Post/Bodyless/ITraktListBodylessPostRequestTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces.Base.Post.Bodyless
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Post.Bodyless;
+
+    [TestClass]
+    public class ITraktListBodylessPostRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListBodylessPostRequestIsInterface()
+        {
+            typeof(ITraktListBodylessPostRequest<>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListBodylessPostRequestHasGenericTypeParameter()
+        {
+            typeof(ITraktListBodylessPostRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktListBodylessPostRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListBodylessPostRequestDerivesFromITraktListRequestInterface()
+        {
+            typeof(ITraktListBodylessPostRequest<int>).GetInterfaces().Should().Contain(typeof(ITraktListRequest<int>));
+        }
+    }
+}
