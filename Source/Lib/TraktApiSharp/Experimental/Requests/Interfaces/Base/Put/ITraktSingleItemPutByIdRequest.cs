cc40913abf860144f1e317ca3bd341131a8e021f
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 21:02:29 +0100

    Add ITraktSingleItemPutByIdRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Put/ITraktSingleItemPutByIdRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Put
+{
+    internal interface ITraktSingleItemPutByIdRequest<TItem> : ITraktSingleItemPutRequest<TItem>, ITraktHasId
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/Base/Put/ITraktSingleItemPutByIdRequestTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces.Base.Put
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Put;
+
+    [TestClass]
+    public class ITraktSingleItemPutByIdRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPutByIdRequestIsInterface()
+        {
+            typeof(ITraktSingleItemPutByIdRequest<>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPutByIdRequestHasGenericTypeParameter()
+        {
+            typeof(ITraktSingleItemPutByIdRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktSingleItemPutByIdRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPutByIdRequestDerivesFromITraktSingleItemPutRequestInterface()
+        {
+            typeof(ITraktSingleItemPutByIdRequest<int>).GetInterfaces().Should().Contain(typeof(ITraktSingleItemPutRequest<int>));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktSingleItemPutByIdRequestDerivesFromITraktHasIdInterface()
+        {
+            typeof(ITraktSingleItemPutByIdRequest<int>).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
