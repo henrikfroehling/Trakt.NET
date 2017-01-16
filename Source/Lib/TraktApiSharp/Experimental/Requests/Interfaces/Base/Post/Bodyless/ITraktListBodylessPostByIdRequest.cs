2b9a1dbb4c809b0d36e4e26318ec0e7b728019f3
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-16 21:10:38 +0100

    Add ITraktListBodylessPostByIdRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/Post/Bodyless/ITraktListBodylessPostByIdRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base.Post.Bodyless
+{
+    internal interface ITraktListBodylessPostByIdRequest<TItem> : ITraktListBodylessPostRequest<TItem>, ITraktHasId
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Interfaces/Base/Post/Bodyless/ITraktListBodylessPostByIdRequestTests.cs
@@ -0,0 +1,36 @@
+﻿namespace TraktApiSharp.Tests.Experimental.Requests.Interfaces.Base.Post.Bodyless
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Post.Bodyless;
+
+    [TestClass]
+    public class ITraktListBodylessPostByIdRequestTests
+    {
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListBodylessPostByIdRequestIsInterface()
+        {
+            typeof(ITraktListBodylessPostByIdRequest<>).IsInterface.Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListBodylessPostByIdRequestHasGenericTypeParameter()
+        {
+            typeof(ITraktListBodylessPostByIdRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktListBodylessPostByIdRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListBodylessPostByIdRequestDerivesFromITraktListBodylessPostRequestInterface()
+        {
+            typeof(ITraktListBodylessPostByIdRequest<int>).GetInterfaces().Should().Contain(typeof(ITraktListBodylessPostRequest<int>));
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Interfaces")]
+        public void TestITraktListBodylessPostByIdRequestDerivesFromITraktHasIdInterface()
+        {
+            typeof(ITraktListBodylessPostByIdRequest<>).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+        }
+    }
+}
