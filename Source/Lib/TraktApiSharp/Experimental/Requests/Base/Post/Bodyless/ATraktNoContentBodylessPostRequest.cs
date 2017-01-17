13b69e351cf538ab692e9c1028ab1ff416a3764f
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-17 01:41:00 +0100

    Implement ITraktNoContentBodylessPostRequest in ATraktNoContentBodylessPostRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Base/Post/Bodyless/ATraktNoContentBodylessPostRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Post/Bodyless/ATraktNoContentBodylessPostRequest.cs
@@ -1,9 +1,10 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Base.Post.Bodyless
 {
+    using Interfaces.Base.Post.Bodyless;
     using System.Net.Http;
     using TraktApiSharp.Requests;
 
-    internal abstract class ATraktNoContentBodylessPostRequest : ATraktNoContentRequest
+    internal abstract class ATraktNoContentBodylessPostRequest : ATraktNoContentRequest, ITraktNoContentBodylessPostRequest
     {
         internal ATraktNoContentBodylessPostRequest(TraktClient client) : base(client) { }
 
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Post/Bodyless/ATraktNoContentBodylessPostRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Post/Bodyless/ATraktNoContentBodylessPostRequestTests.cs
@@ -4,6 +4,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base;
     using TraktApiSharp.Experimental.Requests.Base.Post.Bodyless;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Post.Bodyless;
 
     [TestClass]
     public class ATraktNoContentBodylessPostRequestTests
@@ -18,6 +19,12 @@
         public void TestATraktNoContentBodylessPostRequestIsSubclassOfATraktNoContentRequest()
         {
             typeof(ATraktNoContentBodylessPostRequest).IsSubclassOf(typeof(ATraktNoContentRequest)).Should().BeTrue();
+        }
+
+        [TestMethod, TestCategory("Requests"), TestCategory("Base"), TestCategory("Delete")]
+        public void TestATraktNoContentBodylessPostRequestImplementsITraktNoContentBodylessPostRequestInterface()
+        {
+            typeof(ATraktNoContentBodylessPostRequest).GetInterfaces().Should().Contain(typeof(ITraktNoContentBodylessPostRequest));
         }
     }
 }
