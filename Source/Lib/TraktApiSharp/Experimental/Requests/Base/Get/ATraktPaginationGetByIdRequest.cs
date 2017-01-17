2eea8b145edd0f0ad123e8beb94dcf22b4fb599a
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-17 01:05:27 +0100

    Implement ITraktPaginationGetByIdRequest in ATraktPaginationGetByIdRequest

--- Source/Lib/TraktApiSharp/Experimental/Requests/Base/Get/ATraktPaginationGetByIdRequest.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/Get/ATraktPaginationGetByIdRequest.cs
@@ -1,10 +1,10 @@
 ﻿namespace TraktApiSharp.Experimental.Requests.Base.Get
 {
-    using Interfaces.Base;
+    using Interfaces.Base.Get;
     using System;
     using TraktApiSharp.Requests;
 
-    internal abstract class ATraktPaginationGetByIdRequest<TItem> : ATraktPaginationGetRequest<TItem>, ITraktHasId
+    internal abstract class ATraktPaginationGetByIdRequest<TItem> : ATraktPaginationGetRequest<TItem>, ITraktPaginationGetByIdRequest<TItem>
     {
         internal ATraktPaginationGetByIdRequest(TraktClient client) : base(client) { }
 
--- Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Get/ATraktPaginationGetByIdRequestTests.cs
+++ Source/Tests/TraktApiSharp.Tests/Experimental/Requests/Base/Get/ATraktPaginationGetByIdRequestTests.cs
@@ -3,7 +3,7 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using TraktApiSharp.Experimental.Requests.Base.Get;
-    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base.Get;
 
     [TestClass]
     public class ATraktPaginationGetByIdRequestTests
@@ -28,9 +28,9 @@
         }
 
         [TestMethod, TestCategory("Requests"), TestCategory("Base"), TestCategory("Get")]
-        public void TestATraktPaginationGetByIdRequestImplementsITraktHasIdInterface()
+        public void TestATraktPaginationGetByIdRequestImplementsITraktPaginationGetByIdRequestInterface()
         {
-            typeof(ATraktPaginationGetByIdRequest<>).GetInterfaces().Should().Contain(typeof(ITraktHasId));
+            typeof(ATraktPaginationGetByIdRequest<int>).GetInterfaces().Should().Contain(typeof(ITraktPaginationGetByIdRequest<int>));
         }
     }
 }
