9a676bd08078cce93881aeab55c222ee2537f36a
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-19 10:04:51 +0100

    Add abstract non generic base class for delete requests

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/TraktDeleteRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base
+{
+    using Interfaces.Base;
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class TraktDeleteRequest : TraktRequest, ITraktDeleteRequest
+    {
+        public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
+
+        public sealed override HttpMethod Method => HttpMethod.Delete;
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Requests/Base/TraktDeleteRequest_Tests.cs
@@ -0,0 +1,74 @@
+﻿namespace TraktApiSharp.Tests.Requests.Base
+{
+    using FluentAssertions;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using TraktApiSharp.Experimental.Requests.Base;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Requests;
+    using TraktApiSharp.Tests.Traits;
+    using Xunit;
+
+    [Category("Requests.Base")]
+    public class TraktDeleteRequest_Tests
+    {
+        internal class TraktDeleteRequestMock : TraktDeleteRequest
+        {
+            public override string UriTemplate => throw new NotImplementedException();
+
+            public override IDictionary<string, object> GetUriPathParameters()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Validate()
+            {
+                throw new NotImplementedException();
+            }
+        };
+
+        [Fact]
+        public void Test_TraktDeleteRequest_Is_AbstractClass()
+        {
+            typeof(TraktDeleteRequest).IsAbstract.Should().BeTrue();
+        }
+        
+        [Fact]
+        public void Test_TraktDeleteRequest_Inherits_TraktRequest()
+        {
+            typeof(TraktDeleteRequest).IsSubclassOf(typeof(TraktRequest)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_TraktDeleteRequest_Implements_ITraktDeleteRequest_Interface()
+        {
+            typeof(TraktDeleteRequest).GetInterfaces().Should().Contain(typeof(ITraktDeleteRequest));
+        }
+
+        [Fact]
+        public void Test_TraktDeleteRequest_Returns_Valid_AuthorizationRequirement()
+        {
+            var requestMock = new TraktDeleteRequestMock();
+            requestMock.AuthorizationRequirement.Should().Be(TraktAuthorizationRequirement.Required);
+        }
+
+        [Fact]
+        public void Test_TraktDeleteRequest_Returns_Valid_Method()
+        {
+            var requestMock = new TraktDeleteRequestMock();
+            requestMock.Method.Should().Be(HttpMethod.Delete);
+        }
+
+        [Fact]
+        public void Test_TraktDeleteRequest_Has_Sealed_Method_Property()
+        {
+            var propertyInfo = typeof(TraktDeleteRequest).GetProperties()
+                                                         .Where(p => p.Name == "Method")
+                                                         .FirstOrDefault();
+
+            propertyInfo.GetMethod.IsFinal.Should().BeTrue();
+        }
+    }
+}
