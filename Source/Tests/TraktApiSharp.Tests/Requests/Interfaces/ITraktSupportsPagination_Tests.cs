296768f5530db5061d3f049593a55d512ca72a8d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-19 01:45:49 +0100

    Move tests for ITraktSupportsPagination into the new test project

---
+++ Source/Tests/TraktApiSharp.Tests/Requests/Interfaces/ITraktSupportsPagination_Tests.cs
@@ -0,0 +1,42 @@
+﻿namespace TraktApiSharp.Tests.Requests.Interfaces
+{
+    using FluentAssertions;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+    using TraktApiSharp.Tests.Traits;
+    using Xunit;
+
+    [Category("Requests.Interfaces")]
+    public class ITraktSupportsPagination_Tests
+    {
+        [Fact]
+        public void Test_ITraktSupportsPagination_IsInterface()
+        {
+            typeof(ITraktSupportsPagination).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktSupportsPagination_Has_Page_Property()
+        {
+            var paginationOptionsPropertyInfo = typeof(ITraktSupportsPagination).GetProperties()
+                                                                                .Where(p => p.Name == "Page")
+                                                                                .FirstOrDefault();
+
+            paginationOptionsPropertyInfo.CanRead.Should().BeTrue();
+            paginationOptionsPropertyInfo.CanWrite.Should().BeTrue();
+            paginationOptionsPropertyInfo.PropertyType.Should().Be(typeof(int?));
+        }
+
+        [Fact]
+        public void Test_ITraktSupportsPagination_Has_Limit_Property()
+        {
+            var paginationOptionsPropertyInfo = typeof(ITraktSupportsPagination).GetProperties()
+                                                                                .Where(p => p.Name == "Limit")
+                                                                                .FirstOrDefault();
+
+            paginationOptionsPropertyInfo.CanRead.Should().BeTrue();
+            paginationOptionsPropertyInfo.CanWrite.Should().BeTrue();
+            paginationOptionsPropertyInfo.PropertyType.Should().Be(typeof(int?));
+        }
+    }
+}
