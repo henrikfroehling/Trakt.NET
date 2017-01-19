9b1ea5a1ba9e357897ce0212c7a3517327665dc9
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-19 01:38:08 +0100

    Move tests for ITraktSupportsFilter into the new test project

---
+++ Source/Tests/TraktApiSharp.Tests/Requests/Interfaces/ITraktSupportsFilter_Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace TraktApiSharp.Tests.Requests.Interfaces
+{
+    using FluentAssertions;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+    using TraktApiSharp.Requests.Params;
+    using TraktApiSharp.Tests.Traits;
+    using Xunit;
+
+    [Category("Requests.Interfaces")]
+    public class ITraktSupportsFilter_Tests
+    {
+        [Fact]
+        public void Test_ITraktSupportsFilter_IsInterface()
+        {
+            typeof(ITraktSupportsFilter).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktSupportsFilter_Has_Filter_Property()
+        {
+            var filterPropertyInfo = typeof(ITraktSupportsFilter).GetProperties()
+                                                                 .Where(p => p.Name == "Filter")
+                                                                 .FirstOrDefault();
+
+            filterPropertyInfo.CanRead.Should().BeTrue();
+            filterPropertyInfo.CanWrite.Should().BeTrue();
+            filterPropertyInfo.PropertyType.Should().Be(typeof(TraktCommonFilter));
+        }
+    }
+}
