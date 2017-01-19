eb01296a75105a8b172887cf5fd6b5c74f6f1db7
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-19 04:24:51 +0100

    Add non generic ITraktDeleteRequest interface

---
+++ Source/Tests/TraktApiSharp.Tests/Requests/Interfaces/Base/ITraktDeleteRequest_Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace TraktApiSharp.Tests.Requests.Interfaces.Base
+{
+    using FluentAssertions;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Tests.Traits;
+    using Xunit;
+
+    [Category("Requests.Interfaces.Base")]
+    public class ITraktDeleteRequest_Tests
+    {
+        [Fact]
+        public void Test_ITraktDeleteRequest_Is_Interface()
+        {
+            typeof(ITraktDeleteRequest).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktDeleteRequest_Inherits_ITraktRequest_Interface()
+        {
+            typeof(ITraktDeleteRequest).GetInterfaces().Should().Contain(typeof(ITraktRequest));
+        }
+    }
+}
