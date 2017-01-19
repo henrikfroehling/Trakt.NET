f74693fefcf86505f8aecacd849473644a894d4d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-19 13:52:36 +0100

    Add abstract generic base classes for put requests

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/ATraktPutRequest'2.cs
@@ -0,0 +1,22 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base
+{
+    using Interfaces.Base;
+    using System;
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class ATraktPutRequest<TContentType, TRequestBody> : ATraktRequest<TContentType>, ITraktPutRequest<TContentType, TRequestBody>
+    {
+        public override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.Required;
+
+        public sealed override HttpMethod Method => HttpMethod.Put;
+
+        public abstract TRequestBody RequestBody { get; set; }
+
+        public override void Validate()
+        {
+            if (RequestBody == null)
+                throw new ArgumentNullException(nameof(RequestBody));
+        }
+    }
+}
