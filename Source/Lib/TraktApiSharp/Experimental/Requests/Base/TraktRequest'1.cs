e4f0b165305f0629c27d962205af0034016510e8
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-19 05:19:46 +0100

    Add abstract generic base class for requests

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Base/TraktRequest'1.cs
@@ -0,0 +1,20 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Base
+{
+    using Interfaces.Base;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using TraktApiSharp.Requests;
+
+    internal abstract class TraktRequest<TContentType> : ITraktRequest<TContentType>
+    {
+        public abstract TraktAuthorizationRequirement AuthorizationRequirement { get; }
+
+        public abstract HttpMethod Method { get; }
+
+        public abstract string UriTemplate { get; }
+
+        public abstract IDictionary<string, object> GetUriPathParameters();
+
+        public abstract void Validate();
+    }
+}
