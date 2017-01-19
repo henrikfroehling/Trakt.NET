c0ea108aa091d3421c8272e9a4fd394c57e3f85f
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-19 04:34:58 +0100

    Add generic ITraktGetRequest interface

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Interfaces/Base/ITraktGetRequest'1.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Interfaces.Base
+{
+    internal interface ITraktGetRequest<TContentType> : ITraktRequest<TContentType>
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Requests/Interfaces/Base/ITraktGetRequest'1_Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Requests.Interfaces.Base
+{
+    using FluentAssertions;
+    using TraktApiSharp.Experimental.Requests.Interfaces.Base;
+    using TraktApiSharp.Tests.Traits;
+    using Xunit;
+
+    [Category("Requests.Interfaces.Base")]
+    public class ITraktGetRequest_1_Tests
+    {
+        [Fact]
+        public void Test_ITraktGetRequest_1_Is_Interface()
+        {
+            typeof(ITraktGetRequest<>).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktGetRequest_1_Has_GenericTypeParameter()
+        {
+            typeof(ITraktGetRequest<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktGetRequest<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [Fact]
+        public void Test_ITraktGetRequest_1_Inherits_ITraktRequest_1_Interface()
+        {
+            typeof(ITraktGetRequest<int>).GetInterfaces().Should().Contain(typeof(ITraktRequest<int>));
+        }
+    }
+}
