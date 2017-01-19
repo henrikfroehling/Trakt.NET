1b9e3ce19e0a0fc4ffa9818908b35114f38c1151
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-19 01:34:29 +0100

    Move tests for ITraktSupportsExtendedInfo into the new test project

---
+++ Source/Tests/TraktApiSharp.Tests/Requests/Interfaces/ITraktSupportsExtendedInfo_Tests.cs
@@ -0,0 +1,31 @@
+﻿namespace TraktApiSharp.Tests.Requests.Interfaces
+{
+    using FluentAssertions;
+    using System.Linq;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+    using TraktApiSharp.Requests.Params;
+    using TraktApiSharp.Tests.Traits;
+    using Xunit;
+
+    [Category("Requests.Interfaces")]
+    public class ITraktSupportsExtendedInfo_Tests
+    {
+        [Fact]
+        public void Test_ITraktSupportsExtendedInfo_IsInterface()
+        {
+            typeof(ITraktSupportsExtendedInfo).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktSupportsExtendedInfo_Has_ExtendedInfo_Property()
+        {
+            var extendedOptionPropertyInfo = typeof(ITraktSupportsExtendedInfo).GetProperties()
+                                                                               .Where(p => p.Name == "ExtendedInfo")
+                                                                               .FirstOrDefault();
+
+            extendedOptionPropertyInfo.CanRead.Should().BeTrue();
+            extendedOptionPropertyInfo.CanWrite.Should().BeTrue();
+            extendedOptionPropertyInfo.PropertyType.Should().Be(typeof(TraktExtendedInfo));
+        }
+    }
+}
