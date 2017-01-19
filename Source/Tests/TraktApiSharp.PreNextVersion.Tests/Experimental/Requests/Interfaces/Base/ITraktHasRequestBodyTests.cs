ffebb807d10735dcfab9b4b4ed11993ee157784a
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-19 03:20:09 +0100

    Remove remaining base classes and interfaces for requests

--- Source/Tests/TraktApiSharp.PreNextVersion.Tests/Experimental/Requests/Interfaces/Base/ITraktHasRequestBodyTests.cs
+++ Source/Tests/TraktApiSharp.PreNextVersion.Tests/Experimental/Requests/Interfaces/Base/ITraktHasRequestBodyTests.cs
@@ -3,7 +3,6 @@
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System.Linq;
-    using TraktApiSharp.Experimental.Requests.Base;
     using TraktApiSharp.Experimental.Requests.Interfaces.Base;
 
     [TestClass]
