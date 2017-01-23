412e11651ec1399241db7f32184a6a3f781d220d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-23 19:08:04 +0100

    Add enumeration for list sort order

---
+++ Source/Tests/TraktApiSharp.Tests/Enums/TraktListSortOrderTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Enums
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using TraktApiSharp.Enums;
+
+    [TestClass]
+    public class TraktListSortOrderTests
+    {
+        [TestMethod]
+        public void TestTraktListSortOrderIsTraktEnumeration()
+        {
+            var enumeration = new TraktListSortOrder();
+            enumeration.Should().BeAssignableTo<TraktEnumeration>();
+        }
+
+        [TestMethod]
+        public void TestTraktListSortOrderGetAll()
+        {
+            var allValues = TraktEnumeration.GetAll<TraktListSortOrder>();
+
+            allValues.Should().NotBeNull().And.HaveCount(7);
+            allValues.Should().Contain(new List<TraktListSortOrder>() { TraktListSortOrder.Unspecified, TraktListSortOrder.Popular,
+                                                                        TraktListSortOrder.Likes, TraktListSortOrder.Comments,
+                                                                        TraktListSortOrder.Items, TraktListSortOrder.Added,
+                                                                        TraktListSortOrder.Updated });
+        }
+    }
+}
