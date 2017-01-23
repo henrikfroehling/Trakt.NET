8b47a8a0bc857cccc1ac800af980a061d855b710
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-23 19:51:59 +0100

    Add request implementation for episode translations

---
+++ Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Shows/Episodes/TraktEpisodeTranslationsRequest.cs
@@ -0,0 +1,26 @@
+﻿namespace TraktApiSharp.Requests.WithoutOAuth.Shows.Episodes
+{
+    using Objects.Get.Shows.Episodes;
+    using System.Collections.Generic;
+
+    internal class TraktEpisodeTranslationsRequest : TraktGetByIdEpisodeRequest<IEnumerable<TraktEpisodeTranslation>, TraktEpisodeTranslation>
+    {
+        public TraktEpisodeTranslationsRequest(TraktClient client) : base(client) { }
+
+        internal string LanguageCode { get; set; }
+
+        protected override IDictionary<string, object> GetUriPathParameters()
+        {
+            var uriParams = base.GetUriPathParameters();
+
+            if (!string.IsNullOrEmpty(LanguageCode))
+                uriParams.Add("language", LanguageCode);
+
+            return uriParams;
+        }
+
+        protected override string UriTemplate => "shows/{id}/seasons/{season}/episodes/{episode}/translations/{language}";
+
+        protected override bool IsListResult => true;
+    }
+}
