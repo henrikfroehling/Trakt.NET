ad73bd914981a1f49bd052b77f3511f66358dfb8
Henrik Fröhling <henrik.froehling@gmail.com>
2017-01-26 04:49:39 +0100

    Add movie lists request

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/Movies/TraktMovieListsRequest.cs
@@ -0,0 +1,41 @@
+﻿namespace TraktApiSharp.Experimental.Requests.Movies
+{
+    using Enums;
+    using Interfaces;
+    using Objects.Get.Users.Lists;
+    using System.Collections.Generic;
+
+    internal sealed class TraktMovieListsRequest : ATraktMovieRequest<TraktList>, ITraktSupportsPagination
+    {
+        internal TraktListType Type { get; set; }
+
+        internal TraktListSortOrder SortOrder { get; set; }
+
+        public int? Page { get; set; }
+
+        public int? Limit { get; set; }
+
+        public override string UriTemplate => "movies/{id}/lists{/type}{/sort_order}{?page,limit}";
+
+        public override IDictionary<string, object> GetUriPathParameters()
+        {
+            var uriParams = base.GetUriPathParameters();
+
+            var isTypeSetAndValid = Type != null && Type != TraktListType.Unspecified;
+
+            if (isTypeSetAndValid)
+                uriParams.Add("type", Type.UriName);
+
+            if (isTypeSetAndValid && SortOrder != null && SortOrder != TraktListSortOrder.Unspecified)
+                uriParams.Add("sort_order", SortOrder.UriName);
+
+            if (Page.HasValue)
+                uriParams.Add("page", Page.Value.ToString());
+
+            if (Limit.HasValue)
+                uriParams.Add("limit", Limit.Value.ToString());
+
+            return uriParams;
+        }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Requests/Movies/TraktMovieListsRequest_Tests.cs
@@ -0,0 +1,356 @@
+﻿namespace TraktApiSharp.Tests.Requests.Movies
+{
+    using FluentAssertions;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using TraktApiSharp.Enums;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+    using TraktApiSharp.Experimental.Requests.Movies;
+    using TraktApiSharp.Objects.Get.Users.Lists;
+    using TraktApiSharp.Tests.Traits;
+    using Xunit;
+
+    [Category("Requests.Movie")]
+    public class TraktMovieListsRequest_Tests
+    {
+        [Fact]
+        public void Test_TraktMovieListsRequest_IsNotAbstract()
+        {
+            typeof(TraktMovieListsRequest).IsAbstract.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test_TraktMovieListsRequest_IsSealed()
+        {
+            typeof(TraktMovieListsRequest).IsSealed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_TraktMovieListsRequest_Inherits_ATraktMovieRequest_1()
+        {
+            typeof(TraktMovieListsRequest).IsSubclassOf(typeof(ATraktMovieRequest<TraktList>)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_TraktMovieListsRequest_Implements_ITraktSupportsPagination_Interface()
+        {
+            typeof(TraktMovieListsRequest).GetInterfaces().Should().Contain(typeof(ITraktSupportsPagination));
+        }
+
+        [Fact]
+        public void Test_TraktMovieListsRequest_Has_Valid_UriTemplate()
+        {
+            var request = new TraktMovieListsRequest();
+            request.UriTemplate.Should().Be("movies/{id}/lists{/type}{/sort_order}{?page,limit}");
+        }
+
+        [Fact]
+        public void Test_TraktMovieListsRequest_Has_Type_Property()
+        {
+            var sortingPropertyInfo = typeof(TraktMovieListsRequest)
+                    .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.Name == "Type")
+                    .FirstOrDefault();
+
+            sortingPropertyInfo.CanRead.Should().BeTrue();
+            sortingPropertyInfo.CanWrite.Should().BeTrue();
+            sortingPropertyInfo.PropertyType.Should().Be(typeof(TraktListType));
+        }
+
+        [Fact]
+        public void Test_TraktMovieListsRequest_Has_SortOrder_Property()
+        {
+            var sortingPropertyInfo = typeof(TraktMovieListsRequest)
+                    .GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.Name == "SortOrder")
+                    .FirstOrDefault();
+
+            sortingPropertyInfo.CanRead.Should().BeTrue();
+            sortingPropertyInfo.CanWrite.Should().BeTrue();
+            sortingPropertyInfo.PropertyType.Should().Be(typeof(TraktListSortOrder));
+        }
+
+        [Fact]
+        public void Test_TraktMovieListsRequest_Validate_Throws_Exceptions()
+        {
+            // id is null
+            var request = new TraktMovieListsRequest();
+
+            Action act = () => request.Validate();
+            act.ShouldThrow<ArgumentNullException>();
+
+            // empty id
+            request = new TraktMovieListsRequest { Id = string.Empty };
+
+            act = () => request.Validate();
+            act.ShouldThrow<ArgumentException>();
+
+            // id with spaces
+            request = new TraktMovieListsRequest { Id = "invalid id" };
+
+            act = () => request.Validate();
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        [Theory, ClassData(typeof(TraktMovieListsRequest_TestData))]
+        public void Test_TraktMovieListsRequest_Returns_Valid_UriPathParameters(IDictionary<string, object> values,
+                                                                                IDictionary<string, object> expected)
+        {
+            values.Should().NotBeNull().And.HaveCount(expected.Count);
+
+            if (expected.Count > 0)
+                values.Should().Contain(expected);
+        }
+
+        public class TraktMovieListsRequest_TestData : IEnumerable<object[]>
+        {
+            private const string _id = "123";
+            private static readonly TraktListType _type = TraktListType.Official;
+            private static readonly TraktListSortOrder _sortOrder = TraktListSortOrder.Comments;
+            private const int _page = 5;
+            private const int _limit = 20;
+
+            private static readonly TraktMovieListsRequest _request1 = new TraktMovieListsRequest
+            {
+                Id = _id
+            };
+
+            private static readonly TraktMovieListsRequest _request2 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Type = _type
+            };
+
+            private static readonly TraktMovieListsRequest _request3 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                SortOrder = _sortOrder
+            };
+
+            private static readonly TraktMovieListsRequest _request4 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Page = _page
+            };
+
+            private static readonly TraktMovieListsRequest _request5 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Limit = _limit
+            };
+
+            private static readonly TraktMovieListsRequest _request6 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Type = _type,
+                SortOrder = _sortOrder
+            };
+
+            private static readonly TraktMovieListsRequest _request7 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Type = _type,
+                Page = _page
+            };
+
+            private static readonly TraktMovieListsRequest _request8 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Type = _type,
+                Limit = _limit
+            };
+
+            private static readonly TraktMovieListsRequest _request9 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Type = _type,
+                Page = _page,
+                Limit = _limit
+            };
+
+            private static readonly TraktMovieListsRequest _request10 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                SortOrder = _sortOrder,
+                Page = _page
+            };
+
+            private static readonly TraktMovieListsRequest _request11 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                SortOrder = _sortOrder,
+                Limit = _limit
+            };
+
+            private static readonly TraktMovieListsRequest _request12 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                SortOrder = _sortOrder,
+                Page = _page,
+                Limit = _limit
+            };
+
+            private static readonly TraktMovieListsRequest _request13 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Page = _page,
+                Limit = _limit
+            };
+
+            private static readonly TraktMovieListsRequest _request14 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Type = _type,
+                SortOrder = _sortOrder,
+                Page = _page
+            };
+
+            private static readonly TraktMovieListsRequest _request15 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Type = _type,
+                SortOrder = _sortOrder,
+                Limit = _limit
+            };
+
+            private static readonly TraktMovieListsRequest _request16 = new TraktMovieListsRequest
+            {
+                Id = _id,
+                Type = _type,
+                SortOrder = _sortOrder,
+                Page = _page,
+                Limit = _limit
+            };
+
+            private static readonly List<object[]> _data = new List<object[]>();
+
+            public TraktMovieListsRequest_TestData()
+            {
+                SetupPathParamters();
+            }
+
+            private void SetupPathParamters()
+            {
+                var strType = _type.UriName;
+                var strSortOrder = _sortOrder.UriName;
+                var strPage = _page.ToString();
+                var strLimit = _limit.ToString();
+
+                _data.Add(new object[] { _request1.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id
+                    }});
+
+                _data.Add(new object[] { _request2.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["type"] = strType
+                    }});
+
+                _data.Add(new object[] { _request3.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id
+                    }});
+
+                _data.Add(new object[] { _request4.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["page"] = strPage
+                    }});
+
+                _data.Add(new object[] { _request5.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["limit"] = strLimit
+                    }});
+
+                _data.Add(new object[] { _request6.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["type"] = strType,
+                        ["sort_order"] = strSortOrder
+                    }});
+
+                _data.Add(new object[] { _request7.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["type"] = strType,
+                        ["page"] = strPage
+                    }});
+
+                _data.Add(new object[] { _request8.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["type"] = strType,
+                        ["limit"] = strLimit
+                    }});
+
+                _data.Add(new object[] { _request9.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["type"] = strType,
+                        ["page"] = strPage,
+                        ["limit"] = strLimit
+                    }});
+
+                _data.Add(new object[] { _request10.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["page"] = strPage
+                    }});
+
+                _data.Add(new object[] { _request11.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["limit"] = strLimit
+                    }});
+
+                _data.Add(new object[] { _request12.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["page"] = strPage,
+                        ["limit"] = strLimit
+                    }});
+
+                _data.Add(new object[] { _request13.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["page"] = strPage,
+                        ["limit"] = strLimit
+                    }});
+
+                _data.Add(new object[] { _request14.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["type"] = strType,
+                        ["sort_order"] = strSortOrder,
+                        ["page"] = strPage
+                    }});
+
+                _data.Add(new object[] { _request15.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["type"] = strType,
+                        ["sort_order"] = strSortOrder,
+                        ["limit"] = strLimit
+                    }});
+
+                _data.Add(new object[] { _request16.GetUriPathParameters(), new Dictionary<string, object>
+                    {
+                        ["id"] = _id,
+                        ["type"] = strType,
+                        ["sort_order"] = strSortOrder,
+                        ["page"] = strPage,
+                        ["limit"] = strLimit
+                    }});
+            }
+
+            public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
