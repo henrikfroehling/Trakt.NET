0161fb7eb6f427840f161523aa737f6a53894c1f
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-03 20:59:18 +0100

    Add helper methods in request handler

--- Source/Lib/TraktApiSharp/Experimental/Requests/TraktRequestHandler.cs
+++ Source/Lib/TraktApiSharp/Experimental/Requests/TraktRequestHandler.cs
@@ -1,13 +1,28 @@
 ﻿namespace TraktApiSharp.Experimental.Requests
 {
+    using Core;
     using Interfaces;
     using Interfaces.Base;
     using Responses.Interfaces;
     using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
     using System.Threading.Tasks;
+    using TraktApiSharp.Exceptions;
+    using TraktApiSharp.Requests;
+    using UriTemplates;
+    using Utils;
 
     internal sealed class TraktRequestHandler : ITraktRequestHandler
     {
+        private TraktClient _client;
+
+        internal TraktRequestHandler(TraktClient client)
+        {
+            _client = client;
+        }
+
         public Task<ITraktNoContentResponse> ExecuteNoContentRequestAsync(ITraktRequest request)
         {
             ValidateRequest(request);
@@ -102,6 +117,74 @@
                 throw new ArgumentNullException(nameof(request));
 
             request.Validate();
+        }
+
+        private string BuildUrl(ITraktRequest request)
+        {
+            var uriTemplate = new UriTemplate(request.UriTemplate);
+            var requestUriParameters = request.GetUriPathParameters();
+
+            foreach (var parameter in requestUriParameters)
+                uriTemplate.AddParameterFromKeyValuePair(parameter.Key, parameter.Value);
+
+            var uri = uriTemplate.Resolve();
+            return $"{_client.Configuration.BaseUrl}{uri}";
+        }
+
+        private HttpContent GetRequestBodyContent<TRequestBody>(ITraktHasRequestBody<TRequestBody> request)
+        {
+            var requestBody = request.RequestBody;
+
+            if (requestBody == null)
+                return null;
+
+            var json = Json.Serialize(requestBody);
+            return !string.IsNullOrEmpty(json) ? new StringContent(json, Encoding.UTF8, "application/json") : null;
+        }
+
+        private HttpRequestMessage CreateRequestMessage(ITraktRequest request)
+        {
+            var url = BuildUrl(request);
+            return new HttpRequestMessage(request.Method, url);
+        }
+
+        private void AddRequestBodyContent<TRequestBody>(HttpRequestMessage requestMessage, ITraktHasRequestBody<TRequestBody> request)
+        {
+            if (requestMessage == null)
+                throw new ArgumentNullException(nameof(requestMessage));
+
+            requestMessage.Content = GetRequestBodyContent(request);
+        }
+
+        private void SetDefaultRequestHeaders(HttpClient httpClient)
+        {
+            var appJsonHeader = new MediaTypeWithQualityHeaderValue("application/json");
+
+            if (!httpClient.DefaultRequestHeaders.Contains(TraktConstants.APIClientIdHeaderKey))
+                httpClient.DefaultRequestHeaders.Add(TraktConstants.APIClientIdHeaderKey, _client.ClientId);
+
+            if (!httpClient.DefaultRequestHeaders.Contains(TraktConstants.APIVersionHeaderKey))
+                httpClient.DefaultRequestHeaders.Add(TraktConstants.APIVersionHeaderKey, $"{_client.Configuration.ApiVersion}");
+
+            if (!httpClient.DefaultRequestHeaders.Accept.Contains(appJsonHeader))
+                httpClient.DefaultRequestHeaders.Accept.Add(appJsonHeader);
+        }
+
+        private void SetRequestHeadersForAuthorization(HttpRequestMessage requestMessage, TraktAuthorizationRequirement authorizationRequirement)
+        {
+            if (requestMessage == null)
+                throw new ArgumentNullException(nameof(requestMessage));
+
+            if (authorizationRequirement == TraktAuthorizationRequirement.Required)
+            {
+                if (!_client.Authentication.IsAuthorized)
+                    throw new TraktAuthorizationException("authorization is required for this request, but the current authorization parameters are invalid");
+
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _client.Authentication.Authorization.AccessToken);
+            }
+
+            if (authorizationRequirement == TraktAuthorizationRequirement.Optional && _client.Configuration.ForceAuthorization && _client.Authentication.IsAuthorized)
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _client.Authentication.Authorization.AccessToken);
         }
     }
 }
