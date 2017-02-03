1a56c38c96934bf63d7cebac5f1d1ef884ffb128
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-03 08:32:14 +0100

    Move tests for ITraktResponse in new xunit test project

--- Source/Tests/TraktApiSharp.Tests/Responses/Interfaces/Base/ITraktResponse'1_Tests.cs
+++ Source/Tests/TraktApiSharp.Tests/Responses/Interfaces/Base/ITraktResponse'1_Tests.cs
@@ -1,40 +1,41 @@
-﻿namespace TraktApiSharp.Tests.Experimental.Responses.Interfaces.Base
+﻿namespace TraktApiSharp.Tests.Responses.Interfaces.Base
 {
     using FluentAssertions;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System.Linq;
     using TraktApiSharp.Experimental.Responses.Interfaces.Base;
+    using TraktApiSharp.Tests.Traits;
+    using Xunit;
 
-    [TestClass]
-    public class ITraktResponseTests
+    [Category("Responses.Interfaces.Base")]
+    public class ITraktResponse_1_Tests
     {
-        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
-        public void TestITraktResponseIsInterface()
+        [Fact]
+        public void Test_ITraktResponse_1_Is_Interface()
         {
             typeof(ITraktResponse<>).IsInterface.Should().BeTrue();
         }
 
-        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
-        public void TestITraktResponseHasGenericTypeParameter()
+        [Fact]
+        public void Test_ITraktResponse_1_Has_GenericTypeParameter()
         {
             typeof(ITraktResponse<>).ContainsGenericParameters.Should().BeTrue();
             typeof(ITraktResponse<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
         }
 
-        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
-        public void TestITraktResponseDerivesFromITraktNoContentResponseInterface()
+        [Fact]
+        public void Test_ITraktResponse_1_Inherits_ITraktNoContentResponse_Interface()
         {
             typeof(ITraktResponse<>).GetInterfaces().Should().Contain(typeof(ITraktNoContentResponse));
         }
 
-        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
-        public void TestITraktResponseDerivesFromITraktResponseHeadersInterface()
+        [Fact]
+        public void Test_ITraktResponse_1_Inherits_ITraktResponseHeaders_Interface()
         {
             typeof(ITraktResponse<>).GetInterfaces().Should().Contain(typeof(ITraktResponseHeaders));
         }
 
-        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
-        public void TestITraktResponseHasHasValueProperty()
+        [Fact]
+        public void Test_ITraktResponse_1_Has_HasValue_Property()
         {
             var hasValuePropertyInfo = typeof(ITraktResponse<>).GetProperties()
                                                                .Where(p => p.Name == "HasValue")
@@ -45,12 +46,12 @@
             hasValuePropertyInfo.PropertyType.Should().Be(typeof(bool));
         }
 
-        [TestMethod, TestCategory("Responses"), TestCategory("Interfaces")]
-        public void TestITraktResponseHasValueProperty()
+        [Fact]
+        public void Test_ITraktResponse_1_Has_Value_Property()
         {
             var valuePropertyInfo = typeof(ITraktResponse<int>).GetProperties()
-                                                            .Where(p => p.Name == "Value")
-                                                            .FirstOrDefault();
+                                                               .Where(p => p.Name == "Value")
+                                                               .FirstOrDefault();
 
             valuePropertyInfo.CanRead.Should().BeTrue();
             valuePropertyInfo.CanWrite.Should().BeFalse();
