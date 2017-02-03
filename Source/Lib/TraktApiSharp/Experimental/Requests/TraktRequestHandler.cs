920c05247f6520df75ea7139314d97f5a22a4371
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-03 19:33:03 +0100

    Add basic request handler implementation

---
+++ Source/Lib/TraktApiSharp/Experimental/Requests/TraktRequestHandler.cs
@@ -0,0 +1,107 @@
+﻿namespace TraktApiSharp.Experimental.Requests
+{
+    using Interfaces;
+    using Interfaces.Base;
+    using Responses.Interfaces;
+    using System;
+    using System.Threading.Tasks;
+
+    internal sealed class TraktRequestHandler : ITraktRequestHandler
+    {
+        public Task<ITraktNoContentResponse> ExecuteNoContentRequestAsync(ITraktRequest request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        public Task<ITraktResponse<TContentType>> ExecuteSingleItemRequestAsync<TContentType>(ITraktRequest<TContentType> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        public Task<ITraktListResponse<TContentType>> ExecuteListRequestAsync<TContentType>(ITraktRequest<TContentType> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        public Task<ITraktPagedResponse<TContentType>> ExecutePagedRequestAsync<TContentType>(ITraktRequest<TContentType> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        // post requests
+
+        public Task<ITraktNoContentResponse> ExecuteNoContentRequestAsync<TRequestBody>(ITraktPostRequest<TRequestBody> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        public Task<ITraktResponse<TContentType>> ExecuteSingleItemRequestAsync<TContentType, TRequestBody>(ITraktPostRequest<TContentType, TRequestBody> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        public Task<ITraktListResponse<TContentType>> ExecuteListRequestAsync<TContentType, TRequestBody>(ITraktPostRequest<TContentType, TRequestBody> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        public Task<ITraktPagedResponse<TContentType>> ExecutePagedRequestAsync<TContentType, TRequestBody>(ITraktPostRequest<TContentType, TRequestBody> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        // put requests
+
+        public Task<ITraktNoContentResponse> ExecuteNoContentRequestAsync<TRequestBody>(ITraktPutRequest<TRequestBody> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        public Task<ITraktResponse<TContentType>> ExecuteSingleItemRequestAsync<TContentType, TRequestBody>(ITraktPutRequest<TContentType, TRequestBody> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        public Task<ITraktListResponse<TContentType>> ExecuteListRequestAsync<TContentType, TRequestBody>(ITraktPutRequest<TContentType, TRequestBody> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        public Task<ITraktPagedResponse<TContentType>> ExecutePagedRequestAsync<TContentType, TRequestBody>(ITraktPutRequest<TContentType, TRequestBody> request)
+        {
+            ValidateRequest(request);
+
+            throw new NotImplementedException();
+        }
+
+        private void ValidateRequest(ITraktRequest request)
+        {
+            if (request == null)
+                throw new ArgumentNullException(nameof(request));
+
+            request.Validate();
+        }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Requests/TraktRequestHandler_Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace TraktApiSharp.Tests.Requests
+{
+    using FluentAssertions;
+    using TraktApiSharp.Experimental.Requests;
+    using TraktApiSharp.Experimental.Requests.Interfaces;
+    using TraktApiSharp.Tests.Traits;
+    using Xunit;
+
+    [Category("Requests")]
+    public class TraktRequestHandler_Tests
+    {
+        [Fact]
+        public void Test_TraktRequestHandler_Is_Not_Abstract()
+        {
+            typeof(TraktRequestHandler).IsAbstract.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Test_TraktRequestHandler_Is_Sealed()
+        {
+            typeof(TraktRequestHandler).IsSealed.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_TraktRequestHandler_Implements_ITraktRequestHandler_Interface()
+        {
+            typeof(TraktRequestHandler).GetInterfaces().Should().Contain(typeof(ITraktRequestHandler));
+        }
+    }
+}
