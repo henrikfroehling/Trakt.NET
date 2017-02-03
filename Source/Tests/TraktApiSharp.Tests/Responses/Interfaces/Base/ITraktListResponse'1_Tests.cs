16158f97995a37088e0e6c1dbe171e3a56d14bb2
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-03 08:39:38 +0100

    Move tests for ITraktListResponse in new xunit test project

---
+++ Source/Tests/TraktApiSharp.Tests/Responses/Interfaces/Base/ITraktListResponse'1_Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace TraktApiSharp.Tests.Responses.Interfaces.Base
+{
+    using FluentAssertions;
+    using System.Collections.Generic;
+    using TraktApiSharp.Experimental.Responses.Interfaces.Base;
+    using TraktApiSharp.Tests.Traits;
+    using Xunit;
+
+    [Category("Responses.Interfaces.Base")]
+    public class ITraktListResponse_1_Tests
+    {
+        [Fact]
+        public void Test_ITraktListResponse_1_Is_Interface()
+        {
+            typeof(ITraktListResponse<>).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktListResponse_1_Has_GenericTypeParameter()
+        {
+            typeof(ITraktListResponse<>).ContainsGenericParameters.Should().BeTrue();
+            typeof(ITraktListResponse<int>).GenericTypeArguments.Should().NotBeEmpty().And.HaveCount(1);
+        }
+
+        [Fact]
+        public void Test_ITraktListResponse_1_Inherits_ITraktResponse_1_Interface()
+        {
+            typeof(ITraktListResponse<int>).GetInterfaces().Should().Contain(typeof(ITraktResponse<IEnumerable<int>>));
+        }
+
+        [Fact]
+        public void Test_ITraktListResponse_1_Inherits_IEnumerable_1_Interface()
+        {
+            typeof(ITraktListResponse<int>).GetInterfaces().Should().Contain(typeof(IEnumerable<int>));
+        }
+    }
+}
