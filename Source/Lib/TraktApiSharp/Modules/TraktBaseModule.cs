9bb9e3efa707c08e75f4a79310070d4acb334467
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-07 16:03:47 +0100

    Remove old base classes for requests

--- Source/Lib/TraktApiSharp/Modules/TraktBaseModule.cs
+++ Source/Lib/TraktApiSharp/Modules/TraktBaseModule.cs
@@ -1,15 +1,10 @@
 ﻿namespace TraktApiSharp.Modules
 {
-    using Requests.Base;
-    using System.Threading.Tasks;
-
     public abstract class TraktBaseModule
     {
         internal TraktBaseModule(TraktClient client) { Client = client; }
 
         /// <summary>Gets a reference to the associated <see cref="TraktClient" /> instance.</summary>
         public TraktClient Client { get; }
-
-        internal async Task<T> QueryAsync<T, U>(ITraktRequest<T, U> request) => await request.QueryAsync();
     }
 }
--- Source/Lib/TraktApiSharp/Requests/Base/ITraktRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TraktApiSharp.Requests.Base
-{
-    using System.Threading.Tasks;
-
-    internal interface ITraktRequest<TResult, TItem>
-    {
-        Task<TResult> QueryAsync();
-    }
-}
