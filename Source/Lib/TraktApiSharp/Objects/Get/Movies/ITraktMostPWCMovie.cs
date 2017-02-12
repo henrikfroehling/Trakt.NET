3e185b9881ffafdd2998f4106e4ed1c849010887
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-12 21:08:00 +0100

    Add interface for TraktMostPlayedMovie

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Movies/ITraktMostPWCMovie.cs
@@ -0,0 +1,13 @@
+﻿namespace TraktApiSharp.Objects.Get.Movies
+{
+    public interface ITraktMostPWCMovie : ITraktMovie
+    {
+        int? WatcherCount { get; set; }
+
+        int? PlayCount { get; set; }
+
+        int? CollectedCount { get; set; }
+
+        ITraktMovie Movie { get; set; }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Movies/TraktMostPlayedMovie_Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Movies
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Movies;
+    using TraktApiSharp.Objects.Get.Movies.Common;
+    using Xunit;
+
+    [Category("Objects.Get.Movies")]
+    public class TraktMostPlayedMovie_Tests
+    {
+        [Fact]
+        public void Test_TraktMostPlayedMovie_Implements_ITraktMostPWCMovie_Interface()
+        {
+            typeof(TraktMostPlayedMovie).GetInterfaces().Should().Contain(typeof(ITraktMostPWCMovie));
+        }
+    }
+}
