6e15575bd7407286656f015e3ba5c1e73fa878eb
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-12 20:25:46 +0100

    Add interface for TraktBoxOfficeMovie

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Movies/TraktBoxOfficeMovie_Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Movies
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Movies;
+    using TraktApiSharp.Objects.Get.Movies.Common;
+    using Xunit;
+
+    [Category("Objects.Get.Movies")]
+    public class TraktBoxOfficeMovie_Tests
+    {
+        [Fact]
+        public void Test_TraktBoxOfficeMovie_Implements_ITraktBoxOfficeMovie_Interface()
+        {
+            typeof(TraktBoxOfficeMovie).GetInterfaces().Should().Contain(typeof(ITraktBoxOfficeMovie));
+        }
+    }
+}
