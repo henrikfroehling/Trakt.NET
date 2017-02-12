aa9f11baba517852f63e4ab339509a289e687754
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-12 20:11:03 +0100

    Add interface for TraktTrendingMovie

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Movies/TraktTrendingMovie_Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Movies
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Movies;
+    using TraktApiSharp.Objects.Get.Movies.Common;
+    using Xunit;
+
+    [Category("Objects.Get.Movies")]
+    public class TraktTrendingMovie_Tests
+    {
+        [Fact]
+        public void Test_TraktTrendingMovie_Implements_ITraktTrendingMovie_Interface()
+        {
+            typeof(TraktTrendingMovie).GetInterfaces().Should().Contain(typeof(ITraktTrendingMovie));
+        }
+    }
+}
