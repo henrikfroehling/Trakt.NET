22935e5acc6f6fe2a3bced48eaee3552f20b4095
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-12 21:22:12 +0100

    Move TraktRecentlyUpdatedMovie into namespace Objects.Get.Movies

--- Source/Tests/TraktApiSharp.PreNextVersion.Tests/Objects/Get/Movies/Common/TraktMoviesRecentlyUpdatedTests.cs
+++ Source/Tests/TraktApiSharp.PreNextVersion.Tests/Objects/Get/Movies/Common/TraktMoviesRecentlyUpdatedTests.cs
@@ -6,7 +6,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using TraktApiSharp.Objects.Get.Movies.Common;
+    using TraktApiSharp.Objects.Get.Movies;
     using Utils;
 
     [TestClass]
