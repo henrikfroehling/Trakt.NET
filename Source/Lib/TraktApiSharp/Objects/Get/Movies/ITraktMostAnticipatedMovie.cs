e5aa19679637c4e9b0139e9d1bd016676ab5c6e5
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-12 20:40:00 +0100

    Add interface for TraktMostAnticipatedMovie

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Movies/ITraktMostAnticipatedMovie.cs
@@ -0,0 +1,9 @@
+﻿namespace TraktApiSharp.Objects.Get.Movies
+{
+    public interface ITraktMostAnticipatedMovie : ITraktMovie
+    {
+        int? ListCount { get; set; }
+
+        ITraktMovie Movie { get; set; }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Movies/ITraktMostAnticipatedMovie_Tests.cs
@@ -0,0 +1,44 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Movies
+{
+    using FluentAssertions;
+    using System.Linq;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Movies;
+    using Xunit;
+
+    [Category("Objects.Get.Movies")]
+    public class ITraktMostAnticipatedMovie_Tests
+    {
+        [Fact]
+        public void Test_ITraktMostAnticipatedMovie_Is_Interface()
+        {
+            typeof(ITraktMostAnticipatedMovie).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktMostAnticipatedMovie_Inherits_ITraktMovie_Interface()
+        {
+            typeof(ITraktMostAnticipatedMovie).GetInterfaces().Should().Contain(typeof(ITraktMovie));
+        }
+
+        [Fact]
+        public void Test_ITraktMostAnticipatedMovie_Has_ListCount_Property()
+        {
+            var propertyInfo = typeof(ITraktMostAnticipatedMovie).GetProperties().FirstOrDefault(p => p.Name == "ListCount");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(int?));
+        }
+
+        [Fact]
+        public void Test_ITraktMostAnticipatedMovie_Has_Movie_Property()
+        {
+            var propertyInfo = typeof(ITraktMostAnticipatedMovie).GetProperties().FirstOrDefault(p => p.Name == "Movie");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(ITraktMovie));
+        }
+    }
+}
