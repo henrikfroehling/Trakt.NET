499a4eb0e3237c708e01efaa77734ae9b3b6b415
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-13 20:33:44 +0100

    Move tests for TraktMovieTranslation into new test project

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Movies/TraktMovieTranslation_Tests.cs
@@ -0,0 +1,43 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Movies
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Movies;
+    using Xunit;
+
+    [Category("Objects.Get.Movies")]
+    public class TraktMovieTranslation_Tests
+    {
+        [Fact]
+        public void Test_TraktMovieTranslation_Default_Constructor()
+        {
+            var movieTranslation = new TraktMovieTranslation();
+
+            movieTranslation.Title.Should().BeNullOrEmpty();
+            movieTranslation.Overview.Should().BeNullOrEmpty();
+            movieTranslation.Tagline.Should().BeNullOrEmpty();
+            movieTranslation.LanguageCode.Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public void Test_TraktMovieTranslation_From_Json()
+        {
+            var movieTranslation = JsonConvert.DeserializeObject<TraktMovieTranslation>(JSON);
+
+            movieTranslation.Should().NotBeNull();
+            movieTranslation.Title.Should().Be("Star Wars: Episode VII - The Force Awakens");
+            movieTranslation.Overview.Should().Be("A continuation of the saga created by George Lucas, set thirty years after Star Wars: Episode VI – Return of the Jedi.");
+            movieTranslation.Tagline.Should().Be("The Force Lives On...");
+            movieTranslation.LanguageCode.Should().Be("en");
+        }
+
+        private const string JSON =
+            @"{
+                ""title"": ""Star Wars: Episode VII - The Force Awakens"",
+                ""overview"": ""A continuation of the saga created by George Lucas, set thirty years after Star Wars: Episode VI – Return of the Jedi."",
+                ""tagline"": ""The Force Lives On..."",
+                ""language"": ""en""
+              }";
+    }
+}
