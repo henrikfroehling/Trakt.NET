85b82e0727b40ff053bfc40b4cb064c91fd7bcea
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-14 12:18:38 +0100

    Add interface for TraktMostAnticipatedShow

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Shows/TraktMostAnticipatedShow_Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Shows
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Shows;
+    using TraktApiSharp.Objects.Get.Shows.Common;
+    using Xunit;
+
+    [Category("Objects.Get.Shows")]
+    public class TraktMostAnticipatedShow_Tests
+    {
+        [Fact]
+        public void Test_TraktMostAnticipatedShow_Implements_ITraktMostAnticipatedShow_Interface()
+        {
+            typeof(TraktMostAnticipatedShow).GetInterfaces().Should().Contain(typeof(ITraktMostAnticipatedShow));
+        }
+    }
+}
