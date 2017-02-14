4e19f2d566bbe311f384d8bdde5a3cbbee9f77f3
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-14 11:25:52 +0100

    Add interface for TraktShow

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Shows/TraktShow_Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Shows
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Shows;
+    using Xunit;
+
+    [Category("Objects.Get.Shows")]
+    public class TraktShow_Tests
+    {
+        [Fact]
+        public void Test_TraktShow_Implements_ITraktShow_Interface()
+        {
+            typeof(TraktShow).GetInterfaces().Should().Contain(typeof(ITraktShow));
+        }
+    }
+}
