aab439a2acee228271311ae61bfb3d297217993e
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-14 11:43:06 +0100

    Add interface for TraktTrendingShow

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Shows/TraktTrendingShow_Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Shows
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Shows;
+    using TraktApiSharp.Objects.Get.Shows.Common;
+    using Xunit;
+
+    [Category("Objects.Get.Shows")]
+    public class TraktTrendingShow_Tests
+    {
+        [Fact]
+        public void Test_TraktTrendingShow_Implements_ITraktTrendingShow_Interface()
+        {
+            typeof(TraktTrendingShow).GetInterfaces().Should().Contain(typeof(ITraktTrendingShow));
+        }
+    }
+}
