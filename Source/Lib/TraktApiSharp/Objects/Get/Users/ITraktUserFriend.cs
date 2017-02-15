f244a7467845630d3919f6aa7b9056a065d9f8d3
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-15 16:32:54 +0100

    Add interface for TraktUserFriend

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/ITraktUserFriend.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Objects.Get.Users
+{
+    using System;
+
+    public interface ITraktUserFriend : ITraktUser
+    {
+        DateTime? FriendsAt { get; set; }
+
+        ITraktUser User { get; set; }
+    }
+}
