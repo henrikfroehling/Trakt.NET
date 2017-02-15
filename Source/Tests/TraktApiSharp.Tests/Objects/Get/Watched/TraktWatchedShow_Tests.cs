ff53d367199b797619e4a0a121b2bd41524b91c1
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-15 15:49:53 +0100

    Add interface for TraktWatchedShow

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Watched/TraktWatchedShow_Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Watched
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Watched;
+    using Xunit;
+
+    [Category("Objects.Get.Watched")]
+    public class TraktWatchedShow_Tests
+    {
+        [Fact]
+        public void Test_TraktWatchedShow_Implements_ITraktWatchedShow_Interface()
+        {
+            typeof(TraktWatchedShow).GetInterfaces().Should().Contain(typeof(ITraktWatchedShow));
+        }
+    }
+}
