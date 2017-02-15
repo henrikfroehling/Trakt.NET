f21ba20cd46fb0831a6547053a7b7b7614885063
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-15 05:58:11 +0100

    Add interface for TraktCalendarShow

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Calendars/TraktCalendarShow_Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Calendars
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Calendars;
+    using Xunit;
+
+    [Category("Objects.Get.Calendars")]
+    public class TraktCalendarShow_Tests
+    {
+        [Fact]
+        public void Test_TraktCalendarShow_Implements_ITraktCalendarShow_Interface()
+        {
+            typeof(TraktCalendarShow).GetInterfaces().Should().Contain(typeof(ITraktCalendarShow));
+        }
+    }
+}
