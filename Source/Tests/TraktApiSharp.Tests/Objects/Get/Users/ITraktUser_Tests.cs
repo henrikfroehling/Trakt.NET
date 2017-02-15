d4f27870c8a6eaaebbf0c8e47eb4a76e89c306e0
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-15 16:07:40 +0100

    Add interface for TraktUser

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Users/ITraktUser_Tests.cs
@@ -0,0 +1,139 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Users
+{
+    using FluentAssertions;
+    using System;
+    using System.Linq;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Users;
+    using Xunit;
+
+    [Category("Objects.Get.Users")]
+    public class ITraktUser_Tests
+    {
+        [Fact]
+        public void Test_ITraktUser_Is_Interface()
+        {
+            typeof(ITraktUser).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_Username_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "Username");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_IsPrivate_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "IsPrivate");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(bool?));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_Ids_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "Ids");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(TraktUserIds));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_Name_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "Name");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_IsVIP_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "IsVIP");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(bool?));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_IsVIP_EP_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "IsVIP_EP");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(bool?));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_JoinedAt_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "JoinedAt");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(DateTime?));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_Location_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "Location");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_About_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "About");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_Gender_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "Gender");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_Age_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "Age");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(int?));
+        }
+
+        [Fact]
+        public void Test_ITraktUser_Has_Images_Property()
+        {
+            var propertyInfo = typeof(ITraktUser).GetProperties().FirstOrDefault(p => p.Name == "Images");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(TraktUserImages));
+        }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Users/TraktUser_Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Users
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Users;
+    using Xunit;
+
+    [Category("Objects.Get.Users")]
+    public class TraktUser_Tests
+    {
+        [Fact]
+        public void Test_TraktUser_Implements_ITraktUser_Interface()
+        {
+            typeof(TraktUser).GetInterfaces().Should().Contain(typeof(ITraktUser));
+        }
+    }
+}
