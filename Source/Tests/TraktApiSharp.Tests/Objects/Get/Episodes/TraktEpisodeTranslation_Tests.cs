0244ca080849bd31649fcee4e76c64cfddbc4480
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-16 17:44:14 +0100

    Move tests for TraktEpisodeTranslation into new test project

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Episodes/TraktEpisodeTranslation_Tests.cs
@@ -0,0 +1,50 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Episodes
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Episodes;
+    using Xunit;
+
+    [Category("Objects.Get.Episodes")]
+    public class TraktEpisodeTranslation_Tests
+    {
+        [Fact]
+        public void Test_TraktEpisodeTranslation_Default_Constructor()
+        {
+            var episodeTranslation = new TraktEpisodeTranslation();
+
+            episodeTranslation.Title.Should().BeNullOrEmpty();
+            episodeTranslation.Overview.Should().BeNullOrEmpty();
+            episodeTranslation.LanguageCode.Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public void Test_TraktEpisodeTranslation_From_Json()
+        {
+            var episodeTranslation = JsonConvert.DeserializeObject<TraktEpisodeTranslation>(JSON);
+
+            episodeTranslation.Should().NotBeNull();
+            episodeTranslation.Title.Should().Be("Winter Is Coming");
+            episodeTranslation.Overview.Should().Be("Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army.");
+            episodeTranslation.LanguageCode.Should().Be("en");
+        }
+
+        [Fact]
+        public void Test_TraktEpisodeTranslation_ToString()
+        {
+            var episodeTranslation = new TraktEpisodeTranslation();
+            episodeTranslation.ToString().Should().Be("no title set");
+
+            episodeTranslation.Title = "Winter Is Coming";
+            episodeTranslation.ToString().Should().Be("Winter Is Coming");
+        }
+
+        private const string JSON =
+            @"{
+                ""title"": ""Winter Is Coming"",
+                ""overview"": ""Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army."",
+                ""language"": ""en""
+              }";
+    }
+}
