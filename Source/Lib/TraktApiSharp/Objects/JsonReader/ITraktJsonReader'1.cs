1260aa76bf2571c8f1d378bb82b52a4143b9953a
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-16 16:44:32 +0100

    Add interface for json readers

---
+++ Source/Lib/TraktApiSharp/Objects/JsonReader/ITraktJsonReader'1.cs
@@ -0,0 +1,7 @@
+﻿namespace TraktApiSharp.Objects.JsonReader
+{
+    internal interface ITraktJsonReader<TReturnType>
+    {
+        TReturnType Read(string json);
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/JsonReader/ITraktJsonReader'1_Tests.cs
@@ -0,0 +1,33 @@
+﻿namespace TraktApiSharp.Tests.Objects.JsonReader
+{
+    using FluentAssertions;
+    using System.Linq;
+    using Traits;
+    using TraktApiSharp.Objects.JsonReader;
+    using Xunit;
+
+    [Category("Objects.JsonReader")]
+    public class ITraktJsonReader_1_Tests
+    {
+        [Fact]
+        public void Test_ITraktJsonReader_1_Is_Interface()
+        {
+            typeof(ITraktJsonReader<>).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktJsonReader_1_Has_Read_Method()
+        {
+            var methodInfo = typeof(ITraktJsonReader<int>).GetMethods().FirstOrDefault(m => m.Name == "Read");
+
+            methodInfo.ReturnType.Should().Be(typeof(int));
+            methodInfo.GetParameters().Should().NotBeEmpty().And.HaveCount(1);
+
+            var parameterInfo = methodInfo.GetParameters().First();
+
+            parameterInfo.Should().NotBeNull();
+            parameterInfo.ParameterType.Should().Be(typeof(string));
+            parameterInfo.Name.Should().Be("json");
+        }
+    }
+}
