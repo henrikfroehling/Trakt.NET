228ef460afce5e60b4804445df200bedd9c48257
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-16 18:04:07 +0100

    Add tests for TraktEpisodeWatchedProgress

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Episodes/TraktEpisodeWatchedProgress_Tests.cs
@@ -0,0 +1,41 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Episodes
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using System;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Episodes;
+    using Xunit;
+
+    [Category("Objects.Get.Episodes")]
+    public class TraktEpisodeWatchedProgress_Tests
+    {
+        [Fact]
+        public void Test_TraktEpisodeWatchedProgress_Default_Constructor()
+        {
+            var episodeCollectionProgress = new TraktEpisodeWatchedProgress();
+
+            episodeCollectionProgress.Number.Should().NotHaveValue();
+            episodeCollectionProgress.Completed.Should().NotHaveValue();
+            episodeCollectionProgress.LastWatchedAt.Should().NotHaveValue();
+        }
+
+        [Fact]
+        public void Test_TraktEpisodeWatchedProgress_From_Json()
+        {
+            var episodeCollectionProgress = JsonConvert.DeserializeObject<TraktEpisodeWatchedProgress>(JSON);
+
+            episodeCollectionProgress.Should().NotBeNull();
+            episodeCollectionProgress.Number.Should().Be(2);
+            episodeCollectionProgress.Completed.Should().BeFalse();
+            episodeCollectionProgress.LastWatchedAt.Should().Be(DateTime.Parse("2011-04-18T01:00:00.000Z").ToUniversalTime());
+        }
+
+        private const string JSON =
+            @"{
+                ""number"": 2,
+                ""completed"": false,
+                ""last_watched_at"": ""2011-04-18T01:00:00.000Z""
+              }";
+    }
+}
