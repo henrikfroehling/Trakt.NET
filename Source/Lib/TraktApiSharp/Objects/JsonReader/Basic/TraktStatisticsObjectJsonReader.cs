0564011bf1ad1f874fd6572575db0b539ca5a403
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-27 17:47:03 +0100

    Add json reader implementation for TraktStatistics

---
+++ Source/Lib/TraktApiSharp/Objects/JsonReader/Basic/TraktStatisticsObjectJsonReader.cs
@@ -0,0 +1,77 @@
+﻿namespace TraktApiSharp.Objects.JsonReader.Basic
+{
+    using Newtonsoft.Json;
+    using Objects.Basic;
+    using System.IO;
+
+    internal class TraktStatisticsObjectJsonReader : ITraktObjectJsonReader<TraktStatistics>
+    {
+        private const string PROPERTY_NAME_WATCHERS = "watchers";
+        private const string PROPERTY_NAME_PLAYS = "plays";
+        private const string PROPERTY_NAME_COLLECTORS = "collectors";
+        private const string PROPERTY_NAME_COLLECTED_EPISODES = "collected_episodes";
+        private const string PROPERTY_NAME_COMMENTS = "comments";
+        private const string PROPERTY_NAME_LISTS = "lists";
+        private const string PROPERTY_NAME_VOTES = "votes";
+
+        public TraktStatistics ReadObject(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return null;
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return ReadObject(jsonReader);
+            }
+        }
+
+        public TraktStatistics ReadObject(JsonTextReader jsonReader)
+        {
+            if (jsonReader == null)
+                return null;
+
+            if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartObject)
+            {
+                var traktStatistics = new TraktStatistics();
+
+                while (jsonReader.Read() && jsonReader.TokenType == JsonToken.PropertyName)
+                {
+                    var propertyName = jsonReader.Value.ToString();
+
+                    switch (propertyName)
+                    {
+                        case PROPERTY_NAME_WATCHERS:
+                            traktStatistics.Watchers = jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_PLAYS:
+                            traktStatistics.Plays = jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_COLLECTORS:
+                            traktStatistics.Collectors = jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_COLLECTED_EPISODES:
+                            traktStatistics.CollectedEpisodes = jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_COMMENTS:
+                            traktStatistics.Comments = jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_LISTS:
+                            traktStatistics.Lists = jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_VOTES:
+                            traktStatistics.Votes = jsonReader.ReadAsInt32();
+                            break;
+                        default:
+                            jsonReader.Read(); // read unmatched property value
+                            break;
+                    }
+                }
+
+                return traktStatistics;
+            }
+
+            return null;
+        }
+    }
+}
