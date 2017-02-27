6848442243828b4ae9e7f0015d459123a1eb64f2
Henrik Fröhling <henrik.froehling@gmail.com>
2017-02-27 17:09:58 +0100

    Add json reader implementation for TraktSharing

---
+++ Source/Lib/TraktApiSharp/Objects/JsonReader/Basic/TraktSharingObjectJsonReader.cs
@@ -0,0 +1,73 @@
+﻿namespace TraktApiSharp.Objects.JsonReader.Basic
+{
+    using Newtonsoft.Json;
+    using Objects.Basic;
+    using System.IO;
+
+    internal class TraktSharingObjectJsonReader : ITraktObjectJsonReader<TraktSharing>
+    {
+        private const string PROPERTY_NAME_FACEBOOK = "facebook";
+        private const string PROPERTY_NAME_TWITTER = "twitter";
+        private const string PROPERTY_NAME_GOOGLE = "google";
+        private const string PROPERTY_NAME_TUMBLR = "tumblr";
+        private const string PROPERTY_NAME_MEDIUM = "medium";
+        private const string PROPERTY_NAME_SLACK = "slack";
+
+        public TraktSharing ReadObject(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return null;
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return ReadObject(jsonReader);
+            }
+        }
+
+        public TraktSharing ReadObject(JsonTextReader jsonReader)
+        {
+            if (jsonReader == null)
+                return null;
+
+            if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartObject)
+            {
+                var traktSharing = new TraktSharing();
+
+                while (jsonReader.Read() && jsonReader.TokenType == JsonToken.PropertyName)
+                {
+                    var propertyName = jsonReader.Value.ToString();
+
+                    switch (propertyName)
+                    {
+                        case PROPERTY_NAME_FACEBOOK:
+                            traktSharing.Facebook = jsonReader.ReadAsBoolean();
+                            break;
+                        case PROPERTY_NAME_TWITTER:
+                            traktSharing.Twitter = jsonReader.ReadAsBoolean();
+                            break;
+                        case PROPERTY_NAME_GOOGLE:
+                            traktSharing.Google = jsonReader.ReadAsBoolean();
+                            break;
+                        case PROPERTY_NAME_TUMBLR:
+                            traktSharing.Tumblr = jsonReader.ReadAsBoolean();
+                            break;
+                        case PROPERTY_NAME_MEDIUM:
+                            traktSharing.Medium = jsonReader.ReadAsBoolean();
+                            break;
+                        case PROPERTY_NAME_SLACK:
+                            traktSharing.Slack = jsonReader.ReadAsBoolean();
+                            break;
+                        default:
+                            jsonReader.Read(); // read unmatched property value
+                            break;
+                    }
+                }
+
+                return traktSharing;
+            }
+
+            return null;
+        }
+    }
+}
