665fc2f02e34c11e51f3c552dac3979368dd6725
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-05 09:29:57 +0100

    Add json reader implementation for TraktSeasonIds

---
+++ Source/Lib/TraktApiSharp/Objects/JsonReader/Get/Seasons/TraktSeasonIdsObjectJsonReader.cs
@@ -0,0 +1,65 @@
+﻿namespace TraktApiSharp.Objects.JsonReader.Get.Seasons
+{
+    using Newtonsoft.Json;
+    using Objects.Get.Seasons;
+    using System.IO;
+
+    internal class TraktSeasonIdsObjectJsonReader : ITraktObjectJsonReader<TraktSeasonIds>
+    {
+        private const string PROPERTY_NAME_TRAKT = "trakt";
+        private const string PROPERTY_NAME_TVDB = "tvdb";
+        private const string PROPERTY_NAME_TMDB = "tmdb";
+        private const string PROPERTY_NAME_TVRAGE = "tvrage";
+
+        public TraktSeasonIds ReadObject(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return null;
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return ReadObject(jsonReader);
+            }
+        }
+
+        public TraktSeasonIds ReadObject(JsonTextReader jsonReader)
+        {
+            if (jsonReader == null)
+                return null;
+
+            if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartObject)
+            {
+                var traktSeasonIds = new TraktSeasonIds();
+
+                while (jsonReader.Read() && jsonReader.TokenType == JsonToken.PropertyName)
+                {
+                    var propertyName = jsonReader.Value.ToString();
+
+                    switch (propertyName)
+                    {
+                        case PROPERTY_NAME_TRAKT:
+                            traktSeasonIds.Trakt = (uint)jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_TVDB:
+                            traktSeasonIds.Tvdb = (uint)jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_TMDB:
+                            traktSeasonIds.Tmdb = (uint)jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_TVRAGE:
+                            traktSeasonIds.TvRage = (uint)jsonReader.ReadAsInt32();
+                            break;
+                        default:
+                            jsonReader.Read(); // read unmatched property value
+                            break;
+                    }
+                }
+
+                return traktSeasonIds;
+            }
+
+            return null;
+        }
+    }
+}
