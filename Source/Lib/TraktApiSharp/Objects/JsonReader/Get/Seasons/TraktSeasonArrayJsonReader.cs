1e098bc9f915a0b0ed2352716449fc8502615065
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-06 19:10:14 +0100

    Add json reader implementation for TraktSeason arrays

---
+++ Source/Lib/TraktApiSharp/Objects/JsonReader/Get/Seasons/TraktSeasonArrayJsonReader.cs
@@ -0,0 +1,46 @@
+﻿namespace TraktApiSharp.Objects.JsonReader.Get.Seasons
+{
+    using Newtonsoft.Json;
+    using Objects.Get.Seasons;
+    using System.Collections.Generic;
+    using System.IO;
+
+    internal class TraktSeasonArrayJsonReader : ITraktArrayJsonReader<TraktSeason>
+    {
+        public IEnumerable<TraktSeason> ReadArray(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return null;
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return ReadArray(jsonReader);
+            }
+        }
+
+        public IEnumerable<TraktSeason> ReadArray(JsonTextReader jsonReader)
+        {
+            if (jsonReader == null)
+                return null;
+
+            if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartArray)
+            {
+                var seasonReader = new TraktSeasonObjectJsonReader();
+                var traktSeasons = new List<TraktSeason>();
+
+                var traktSeason = seasonReader.ReadObject(jsonReader);
+
+                while (traktSeason != null)
+                {
+                    traktSeasons.Add(traktSeason);
+                    traktSeason = seasonReader.ReadObject(jsonReader);
+                }
+
+                return traktSeasons;
+            }
+
+            return null;
+        }
+    }
+}
