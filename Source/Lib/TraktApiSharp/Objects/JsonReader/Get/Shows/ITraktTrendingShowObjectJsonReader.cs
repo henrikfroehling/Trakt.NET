904bc7e80211dffddb30bc1b07724c4ed76c57b7
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-06 23:56:09 +0100

    Add json reader implementation for ITraktTrendingShow

---
+++ Source/Lib/TraktApiSharp/Objects/JsonReader/Get/Shows/ITraktTrendingShowObjectJsonReader.cs
@@ -0,0 +1,74 @@
+﻿namespace TraktApiSharp.Objects.JsonReader.Get.Shows
+{
+    using Newtonsoft.Json;
+    using Objects.Get.Shows;
+    using System.IO;
+
+    internal class ITraktTrendingShowObjectJsonReader : ITraktObjectJsonReader<ITraktTrendingShow>
+    {
+        private const string PROPERTY_NAME_WATCHERS = "watchers";
+        private const string PROPERTY_NAME_SHOW = "show";
+
+        public ITraktTrendingShow ReadObject(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return null;
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return ReadObject(jsonReader);
+            }
+        }
+
+        public ITraktTrendingShow ReadObject(JsonTextReader jsonReader)
+        {
+            if (jsonReader == null)
+                return null;
+
+            if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartObject)
+            {
+                var showObjectReader = new ITraktShowObjectJsonReader();
+                ITraktTrendingShow traktTrendingShow = new TraktTrendingShow();
+
+                while (jsonReader.Read() && jsonReader.TokenType == JsonToken.PropertyName)
+                {
+                    var propertyName = jsonReader.Value.ToString();
+
+                    switch (propertyName)
+                    {
+                        case PROPERTY_NAME_WATCHERS:
+                            traktTrendingShow.Watchers = jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_SHOW:
+                            traktTrendingShow.Show = showObjectReader.ReadObject(jsonReader);
+                            break;
+                        default:
+                            jsonReader.Read(); // read unmatched property value
+
+                            if (jsonReader.TokenType == JsonToken.StartArray)
+                            {
+                                // step over possible array values for unmatched property
+                                while (jsonReader.Read() && jsonReader.TokenType != JsonToken.EndArray)
+                                {
+                                }
+                            }
+                            else if (jsonReader.TokenType == JsonToken.StartObject)
+                            {
+                                // step over possible object values for unmatched property
+                                while (jsonReader.Read() && jsonReader.TokenType != JsonToken.EndObject)
+                                {
+                                }
+                            }
+
+                            break;
+                    }
+                }
+
+                return traktTrendingShow;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/JsonReader/Get/Shows/ITraktTrendingShowObjectJsonReader_Tests.cs
@@ -0,0 +1,372 @@
+﻿namespace TraktApiSharp.Tests.Objects.JsonReader.Get.Shows
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using System.IO;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Shows;
+    using TraktApiSharp.Objects.JsonReader;
+    using TraktApiSharp.Objects.JsonReader.Get.Shows;
+    using Xunit;
+
+    [Category("Objects.JsonReader.Get.Shows")]
+    public class ITraktTrendingShowObjectJsonReader_Tests
+    {
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_Implements_ITraktObjectJsonReader_Interface()
+        {
+            typeof(ITraktTrendingShowObjectJsonReader).GetInterfaces().Should().Contain(typeof(ITraktObjectJsonReader<ITraktTrendingShow>));
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_Json_String_Complete()
+        {
+            var jsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            var traktTrendingShow = jsonReader.ReadObject(JSON_COMPLETE);
+
+            traktTrendingShow.Should().NotBeNull();
+            traktTrendingShow.Watchers.Should().Be(35);
+            traktTrendingShow.Show.Should().NotBeNull();
+            traktTrendingShow.Show.Title.Should().Be("Game of Thrones");
+            traktTrendingShow.Show.Year.Should().Be(2011);
+            traktTrendingShow.Show.Ids.Should().NotBeNull();
+            traktTrendingShow.Show.Ids.Trakt.Should().Be(1390U);
+            traktTrendingShow.Show.Ids.Slug.Should().Be("game-of-thrones");
+            traktTrendingShow.Show.Ids.Tvdb.Should().Be(121361U);
+            traktTrendingShow.Show.Ids.Imdb.Should().Be("tt0944947");
+            traktTrendingShow.Show.Ids.Tmdb.Should().Be(1399U);
+            traktTrendingShow.Show.Ids.TvRage.Should().Be(24493U);
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_Json_String_Incomplete_1()
+        {
+            var jsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            var traktTrendingShow = jsonReader.ReadObject(JSON_INCOMPLETE_1);
+
+            traktTrendingShow.Should().NotBeNull();
+            traktTrendingShow.Watchers.Should().Be(35);
+            traktTrendingShow.Show.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_Json_String_Incomplete_2()
+        {
+            var jsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            var traktTrendingShow = jsonReader.ReadObject(JSON_INCOMPLETE_2);
+
+            traktTrendingShow.Should().NotBeNull();
+            traktTrendingShow.Watchers.Should().BeNull();
+            traktTrendingShow.Show.Should().NotBeNull();
+            traktTrendingShow.Show.Title.Should().Be("Game of Thrones");
+            traktTrendingShow.Show.Year.Should().Be(2011);
+            traktTrendingShow.Show.Ids.Should().NotBeNull();
+            traktTrendingShow.Show.Ids.Trakt.Should().Be(1390U);
+            traktTrendingShow.Show.Ids.Slug.Should().Be("game-of-thrones");
+            traktTrendingShow.Show.Ids.Tvdb.Should().Be(121361U);
+            traktTrendingShow.Show.Ids.Imdb.Should().Be("tt0944947");
+            traktTrendingShow.Show.Ids.Tmdb.Should().Be(1399U);
+            traktTrendingShow.Show.Ids.TvRage.Should().Be(24493U);
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_Json_String_Not_Valid_1()
+        {
+            var jsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            var traktTrendingShow = jsonReader.ReadObject(JSON_NOT_VALID_1);
+
+            traktTrendingShow.Should().NotBeNull();
+            traktTrendingShow.Watchers.Should().BeNull();
+            traktTrendingShow.Show.Should().NotBeNull();
+            traktTrendingShow.Show.Title.Should().Be("Game of Thrones");
+            traktTrendingShow.Show.Year.Should().Be(2011);
+            traktTrendingShow.Show.Ids.Should().NotBeNull();
+            traktTrendingShow.Show.Ids.Trakt.Should().Be(1390U);
+            traktTrendingShow.Show.Ids.Slug.Should().Be("game-of-thrones");
+            traktTrendingShow.Show.Ids.Tvdb.Should().Be(121361U);
+            traktTrendingShow.Show.Ids.Imdb.Should().Be("tt0944947");
+            traktTrendingShow.Show.Ids.Tmdb.Should().Be(1399U);
+            traktTrendingShow.Show.Ids.TvRage.Should().Be(24493U);
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_Json_String_Not_Valid_2()
+        {
+            var jsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            var traktTrendingShow = jsonReader.ReadObject(JSON_NOT_VALID_2);
+
+            traktTrendingShow.Should().NotBeNull();
+            traktTrendingShow.Watchers.Should().Be(35);
+            traktTrendingShow.Show.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_Json_String_Not_Valid_3()
+        {
+            var jsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            var traktTrendingShow = jsonReader.ReadObject(JSON_NOT_VALID_3);
+
+            traktTrendingShow.Should().NotBeNull();
+            traktTrendingShow.Watchers.Should().BeNull();
+            traktTrendingShow.Show.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_Json_String_Null()
+        {
+            var jsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            var traktTrendingShow = jsonReader.ReadObject(default(string));
+            traktTrendingShow.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_Json_String_Empty()
+        {
+            var jsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            var traktTrendingShow = jsonReader.ReadObject(string.Empty);
+            traktTrendingShow.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_JsonReader_Complete()
+        {
+            var traktJsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_COMPLETE))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktTrendingShow = traktJsonReader.ReadObject(jsonReader);
+
+                traktTrendingShow.Should().NotBeNull();
+                traktTrendingShow.Watchers.Should().Be(35);
+                traktTrendingShow.Show.Should().NotBeNull();
+                traktTrendingShow.Show.Title.Should().Be("Game of Thrones");
+                traktTrendingShow.Show.Year.Should().Be(2011);
+                traktTrendingShow.Show.Ids.Should().NotBeNull();
+                traktTrendingShow.Show.Ids.Trakt.Should().Be(1390U);
+                traktTrendingShow.Show.Ids.Slug.Should().Be("game-of-thrones");
+                traktTrendingShow.Show.Ids.Tvdb.Should().Be(121361U);
+                traktTrendingShow.Show.Ids.Imdb.Should().Be("tt0944947");
+                traktTrendingShow.Show.Ids.Tmdb.Should().Be(1399U);
+                traktTrendingShow.Show.Ids.TvRage.Should().Be(24493U);
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_JsonReader_Incomplete_1()
+        {
+            var traktJsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_INCOMPLETE_1))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktTrendingShow = traktJsonReader.ReadObject(jsonReader);
+
+                traktTrendingShow.Should().NotBeNull();
+                traktTrendingShow.Watchers.Should().Be(35);
+                traktTrendingShow.Show.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_JsonReader_Incomplete_2()
+        {
+            var traktJsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_INCOMPLETE_2))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktTrendingShow = traktJsonReader.ReadObject(jsonReader);
+
+                traktTrendingShow.Should().NotBeNull();
+                traktTrendingShow.Watchers.Should().BeNull();
+                traktTrendingShow.Show.Should().NotBeNull();
+                traktTrendingShow.Show.Title.Should().Be("Game of Thrones");
+                traktTrendingShow.Show.Year.Should().Be(2011);
+                traktTrendingShow.Show.Ids.Should().NotBeNull();
+                traktTrendingShow.Show.Ids.Trakt.Should().Be(1390U);
+                traktTrendingShow.Show.Ids.Slug.Should().Be("game-of-thrones");
+                traktTrendingShow.Show.Ids.Tvdb.Should().Be(121361U);
+                traktTrendingShow.Show.Ids.Imdb.Should().Be("tt0944947");
+                traktTrendingShow.Show.Ids.Tmdb.Should().Be(1399U);
+                traktTrendingShow.Show.Ids.TvRage.Should().Be(24493U);
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_JsonReader_Not_Valid_1()
+        {
+            var traktJsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_1))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktTrendingShow = traktJsonReader.ReadObject(jsonReader);
+
+                traktTrendingShow.Should().NotBeNull();
+                traktTrendingShow.Watchers.Should().BeNull();
+                traktTrendingShow.Show.Should().NotBeNull();
+                traktTrendingShow.Show.Title.Should().Be("Game of Thrones");
+                traktTrendingShow.Show.Year.Should().Be(2011);
+                traktTrendingShow.Show.Ids.Should().NotBeNull();
+                traktTrendingShow.Show.Ids.Trakt.Should().Be(1390U);
+                traktTrendingShow.Show.Ids.Slug.Should().Be("game-of-thrones");
+                traktTrendingShow.Show.Ids.Tvdb.Should().Be(121361U);
+                traktTrendingShow.Show.Ids.Imdb.Should().Be("tt0944947");
+                traktTrendingShow.Show.Ids.Tmdb.Should().Be(1399U);
+                traktTrendingShow.Show.Ids.TvRage.Should().Be(24493U);
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_JsonReader_Not_Valid_2()
+        {
+            var traktJsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_2))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktTrendingShow = traktJsonReader.ReadObject(jsonReader);
+
+                traktTrendingShow.Should().NotBeNull();
+                traktTrendingShow.Watchers.Should().Be(35);
+                traktTrendingShow.Show.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_JsonReader_Not_Valid_3()
+        {
+            var traktJsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_3))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktTrendingShow = traktJsonReader.ReadObject(jsonReader);
+
+                traktTrendingShow.Should().NotBeNull();
+                traktTrendingShow.Watchers.Should().BeNull();
+                traktTrendingShow.Show.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_JsonReader_Null()
+        {
+            var jsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            var traktTrendingShow = jsonReader.ReadObject(default(JsonTextReader));
+            traktTrendingShow.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktTrendingShowObjectJsonReader_ReadObject_From_JsonReader_Empty()
+        {
+            var traktJsonReader = new ITraktTrendingShowObjectJsonReader();
+
+            using (var reader = new StringReader(string.Empty))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktTrendingShow = traktJsonReader.ReadObject(jsonReader);
+                traktTrendingShow.Should().BeNull();
+            }
+        }
+
+        private const string JSON_COMPLETE =
+            @"{
+                ""watchers"": 35,
+                ""show"": {
+                  ""title"": ""Game of Thrones"",
+                  ""year"": 2011,
+                  ""ids"": {
+                    ""trakt"": 1390,
+                    ""slug"": ""game-of-thrones"",
+                    ""tvdb"": 121361,
+                    ""imdb"": ""tt0944947"",
+                    ""tmdb"": 1399,
+                    ""tvrage"": 24493
+                  }
+                }
+              }";
+
+        private const string JSON_INCOMPLETE_1 =
+            @"{
+                ""watchers"": 35
+              }";
+
+        private const string JSON_INCOMPLETE_2 =
+            @"{
+                ""show"": {
+                  ""title"": ""Game of Thrones"",
+                  ""year"": 2011,
+                  ""ids"": {
+                    ""trakt"": 1390,
+                    ""slug"": ""game-of-thrones"",
+                    ""tvdb"": 121361,
+                    ""imdb"": ""tt0944947"",
+                    ""tmdb"": 1399,
+                    ""tvrage"": 24493
+                  }
+                }
+              }";
+
+        private const string JSON_NOT_VALID_1 =
+            @"{
+                ""wa"": 35,
+                ""show"": {
+                  ""title"": ""Game of Thrones"",
+                  ""year"": 2011,
+                  ""ids"": {
+                    ""trakt"": 1390,
+                    ""slug"": ""game-of-thrones"",
+                    ""tvdb"": 121361,
+                    ""imdb"": ""tt0944947"",
+                    ""tmdb"": 1399,
+                    ""tvrage"": 24493
+                  }
+                }
+              }";
+
+        private const string JSON_NOT_VALID_2 =
+            @"{
+                ""watchers"": 35,
+                ""sh"": {
+                  ""title"": ""Game of Thrones"",
+                  ""year"": 2011,
+                  ""ids"": {
+                    ""trakt"": 1390,
+                    ""slug"": ""game-of-thrones"",
+                    ""tvdb"": 121361,
+                    ""imdb"": ""tt0944947"",
+                    ""tmdb"": 1399,
+                    ""tvrage"": 24493
+                  }
+                }
+              }";
+
+        private const string JSON_NOT_VALID_3 =
+            @"{
+                ""wa"": 35,
+                ""sh"": {
+                  ""title"": ""Game of Thrones"",
+                  ""year"": 2011,
+                  ""ids"": {
+                    ""trakt"": 1390,
+                    ""slug"": ""game-of-thrones"",
+                    ""tvdb"": 121361,
+                    ""imdb"": ""tt0944947"",
+                    ""tmdb"": 1399,
+                    ""tvrage"": 24493
+                  }
+                }
+              }";
+    }
+}
