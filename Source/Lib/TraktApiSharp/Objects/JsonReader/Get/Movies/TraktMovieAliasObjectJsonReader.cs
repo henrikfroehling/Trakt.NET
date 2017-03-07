f62dd0fb119e42f0165e60ebe4ca5731ee863caf
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-07 13:21:00 +0100

    Add json reader implementation for TraktMovieAlias

---
+++ Source/Lib/TraktApiSharp/Objects/JsonReader/Get/Movies/TraktMovieAliasObjectJsonReader.cs
@@ -0,0 +1,57 @@
+﻿namespace TraktApiSharp.Objects.JsonReader.Get.Movies
+{
+    using Newtonsoft.Json;
+    using Objects.Get.Movies;
+    using System.IO;
+
+    internal class TraktMovieAliasObjectJsonReader : ITraktObjectJsonReader<TraktMovieAlias>
+    {
+        private const string PROPERTY_NAME_TITLE = "title";
+        private const string PROPERTY_NAME_COUNTRY = "country";
+
+        public TraktMovieAlias ReadObject(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return null;
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return ReadObject(jsonReader);
+            }
+        }
+
+        public TraktMovieAlias ReadObject(JsonTextReader jsonReader)
+        {
+            if (jsonReader == null)
+                return null;
+
+            if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartObject)
+            {
+                var traktMovieAlias = new TraktMovieAlias();
+
+                while (jsonReader.Read() && jsonReader.TokenType == JsonToken.PropertyName)
+                {
+                    var propertyName = jsonReader.Value.ToString();
+
+                    switch (propertyName)
+                    {
+                        case PROPERTY_NAME_TITLE:
+                            traktMovieAlias.Title = jsonReader.ReadAsString();
+                            break;
+                        case PROPERTY_NAME_COUNTRY:
+                            traktMovieAlias.CountryCode = jsonReader.ReadAsString();
+                            break;
+                        default:
+                            JsonReaderHelper.OverreadInvalidContent(jsonReader);
+                            break;
+                    }
+                }
+
+                return traktMovieAlias;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/JsonReader/Get/Movies/TraktMovieAliasObjectJsonReader_Tests.cs
@@ -0,0 +1,263 @@
+﻿namespace TraktApiSharp.Tests.Objects.JsonReader.Get.Movies
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using System.IO;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Movies;
+    using TraktApiSharp.Objects.JsonReader;
+    using TraktApiSharp.Objects.JsonReader.Get.Movies;
+    using Xunit;
+
+    [Category("Objects.JsonReader.Get.Movies")]
+    public class TraktMovieAliasObjectJsonReader_Tests
+    {
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_Implements_ITraktObjectJsonReader_Interface()
+        {
+            typeof(TraktMovieAliasObjectJsonReader).GetInterfaces().Should().Contain(typeof(ITraktObjectJsonReader<TraktMovieAlias>));
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_Json_String_Complete()
+        {
+            var jsonReader = new TraktMovieAliasObjectJsonReader();
+
+            var traktMovieAlias = jsonReader.ReadObject(JSON_COMPLETE);
+
+            traktMovieAlias.Should().NotBeNull();
+            traktMovieAlias.Title.Should().Be("Star Wars: The Force Awakens");
+            traktMovieAlias.CountryCode.Should().Be("us");
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_Json_String_Incomplete_1()
+        {
+            var jsonReader = new TraktMovieAliasObjectJsonReader();
+
+            var traktMovieAlias = jsonReader.ReadObject(JSON_INCOMPLETE_1);
+
+            traktMovieAlias.Should().NotBeNull();
+            traktMovieAlias.Title.Should().Be("Star Wars: The Force Awakens");
+            traktMovieAlias.CountryCode.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_Json_String_Incomplete_2()
+        {
+            var jsonReader = new TraktMovieAliasObjectJsonReader();
+
+            var traktMovieAlias = jsonReader.ReadObject(JSON_INCOMPLETE_2);
+
+            traktMovieAlias.Should().NotBeNull();
+            traktMovieAlias.Title.Should().BeNull();
+            traktMovieAlias.CountryCode.Should().Be("us");
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_Json_String_Not_Valid_1()
+        {
+            var jsonReader = new TraktMovieAliasObjectJsonReader();
+
+            var traktMovieAlias = jsonReader.ReadObject(JSON_NOT_VALID_1);
+
+            traktMovieAlias.Should().NotBeNull();
+            traktMovieAlias.Title.Should().BeNull();
+            traktMovieAlias.CountryCode.Should().Be("us");
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_Json_String_Not_Valid_2()
+        {
+            var jsonReader = new TraktMovieAliasObjectJsonReader();
+
+            var traktMovieAlias = jsonReader.ReadObject(JSON_NOT_VALID_2);
+
+            traktMovieAlias.Should().NotBeNull();
+            traktMovieAlias.Title.Should().Be("Star Wars: The Force Awakens");
+            traktMovieAlias.CountryCode.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_Json_String_Not_Valid_3()
+        {
+            var jsonReader = new TraktMovieAliasObjectJsonReader();
+
+            var traktMovieAlias = jsonReader.ReadObject(JSON_NOT_VALID_3);
+
+            traktMovieAlias.Should().NotBeNull();
+            traktMovieAlias.Title.Should().BeNull();
+            traktMovieAlias.CountryCode.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_Json_String_Null()
+        {
+            var jsonReader = new TraktMovieAliasObjectJsonReader();
+
+            var traktMovieAlias = jsonReader.ReadObject(default(string));
+            traktMovieAlias.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_Json_String_Empty()
+        {
+            var jsonReader = new TraktMovieAliasObjectJsonReader();
+
+            var traktMovieAlias = jsonReader.ReadObject(string.Empty);
+            traktMovieAlias.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_JsonReader_Complete()
+        {
+            var traktJsonReader = new TraktMovieAliasObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_COMPLETE))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktMovieAlias = traktJsonReader.ReadObject(jsonReader);
+
+                traktMovieAlias.Should().NotBeNull();
+                traktMovieAlias.Title.Should().Be("Star Wars: The Force Awakens");
+                traktMovieAlias.CountryCode.Should().Be("us");
+            }
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_JsonReader_Incomplete_1()
+        {
+            var traktJsonReader = new TraktMovieAliasObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_INCOMPLETE_1))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktMovieAlias = traktJsonReader.ReadObject(jsonReader);
+
+                traktMovieAlias.Should().NotBeNull();
+                traktMovieAlias.Title.Should().Be("Star Wars: The Force Awakens");
+                traktMovieAlias.CountryCode.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_JsonReader_Incomplete_2()
+        {
+            var traktJsonReader = new TraktMovieAliasObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_INCOMPLETE_2))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktMovieAlias = traktJsonReader.ReadObject(jsonReader);
+
+                traktMovieAlias.Should().NotBeNull();
+                traktMovieAlias.Title.Should().BeNull();
+                traktMovieAlias.CountryCode.Should().Be("us");
+            }
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_JsonReader_Not_Valid_1()
+        {
+            var traktJsonReader = new TraktMovieAliasObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_1))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktMovieAlias = traktJsonReader.ReadObject(jsonReader);
+
+                traktMovieAlias.Should().NotBeNull();
+                traktMovieAlias.Title.Should().BeNull();
+                traktMovieAlias.CountryCode.Should().Be("us");
+            }
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_JsonReader_Not_Valid_2()
+        {
+            var traktJsonReader = new TraktMovieAliasObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_2))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktMovieAlias = traktJsonReader.ReadObject(jsonReader);
+
+                traktMovieAlias.Should().NotBeNull();
+                traktMovieAlias.Title.Should().Be("Star Wars: The Force Awakens");
+                traktMovieAlias.CountryCode.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_JsonReader_Not_Valid_3()
+        {
+            var traktJsonReader = new TraktMovieAliasObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_3))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktMovieAlias = traktJsonReader.ReadObject(jsonReader);
+
+                traktMovieAlias.Should().NotBeNull();
+                traktMovieAlias.Title.Should().BeNull();
+                traktMovieAlias.CountryCode.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_JsonReader_Null()
+        {
+            var jsonReader = new TraktMovieAliasObjectJsonReader();
+
+            var traktMovieAlias = jsonReader.ReadObject(default(JsonTextReader));
+            traktMovieAlias.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_TraktMovieAliasObjectJsonReader_ReadObject_From_JsonReader_Empty()
+        {
+            var traktJsonReader = new TraktMovieAliasObjectJsonReader();
+
+            using (var reader = new StringReader(string.Empty))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktMovieAlias = traktJsonReader.ReadObject(jsonReader);
+                traktMovieAlias.Should().BeNull();
+            }
+        }
+
+        private const string JSON_COMPLETE =
+            @"{
+                ""title"": ""Star Wars: The Force Awakens"",
+                ""country"": ""us""
+              }";
+
+        private const string JSON_INCOMPLETE_1 =
+            @"{
+                ""title"": ""Star Wars: The Force Awakens""
+              }";
+
+        private const string JSON_INCOMPLETE_2 =
+            @"{
+                ""country"": ""us""
+              }";
+
+        private const string JSON_NOT_VALID_1 =
+            @"{
+                ""ti"": ""Star Wars: The Force Awakens"",
+                ""country"": ""us""
+              }";
+
+        private const string JSON_NOT_VALID_2 =
+            @"{
+                ""title"": ""Star Wars: The Force Awakens"",
+                ""co"": ""us""
+              }";
+
+        private const string JSON_NOT_VALID_3 =
+            @"{
+                ""ti"": ""Star Wars: The Force Awakens"",
+                ""co"": ""us""
+              }";
+    }
+}
