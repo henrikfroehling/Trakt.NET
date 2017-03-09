312924fe87b2ecea04a3aec7e5bc023258bccdcd
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-09 22:53:16 +0100

    Add json reader implementation for ITraktBoxOfficeMovie

---
+++ Source/Lib/TraktApiSharp/Objects/JsonReader/Get/Movies/ITraktBoxOfficeMovieObjectJsonReader.cs
@@ -0,0 +1,58 @@
+﻿namespace TraktApiSharp.Objects.JsonReader.Get.Movies
+{
+    using Newtonsoft.Json;
+    using Objects.Get.Movies;
+    using System.IO;
+
+    internal class ITraktBoxOfficeMovieObjectJsonReader : ITraktObjectJsonReader<ITraktBoxOfficeMovie>
+    {
+        private const string PROPERTY_NAME_REVENUE = "revenue";
+        private const string PROPERTY_NAME_MOVIE = "movie";
+
+        public ITraktBoxOfficeMovie ReadObject(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return null;
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return ReadObject(jsonReader);
+            }
+        }
+
+        public ITraktBoxOfficeMovie ReadObject(JsonTextReader jsonReader)
+        {
+            if (jsonReader == null)
+                return null;
+
+            if (jsonReader.Read() && jsonReader.TokenType == JsonToken.StartObject)
+            {
+                var movieObjectReader = new ITraktMovieObjectJsonReader();
+                ITraktBoxOfficeMovie traktBoxOfficeMovie = new TraktBoxOfficeMovie();
+
+                while (jsonReader.Read() && jsonReader.TokenType == JsonToken.PropertyName)
+                {
+                    var propertyName = jsonReader.Value.ToString();
+
+                    switch (propertyName)
+                    {
+                        case PROPERTY_NAME_REVENUE:
+                            traktBoxOfficeMovie.Revenue = jsonReader.ReadAsInt32();
+                            break;
+                        case PROPERTY_NAME_MOVIE:
+                            traktBoxOfficeMovie.Movie = movieObjectReader.ReadObject(jsonReader);
+                            break;
+                        default:
+                            JsonReaderHelper.OverreadInvalidContent(jsonReader);
+                            break;
+                    }
+                }
+
+                return traktBoxOfficeMovie;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/JsonReader/Get/Movies/ITraktBoxOfficeMovieObjectJsonReader_Tests.cs
@@ -0,0 +1,350 @@
+﻿namespace TraktApiSharp.Tests.Objects.JsonReader.Get.Movies
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using System.IO;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Movies;
+    using TraktApiSharp.Objects.JsonReader;
+    using TraktApiSharp.Objects.JsonReader.Get.Movies;
+    using Xunit;
+
+    [Category("Objects.JsonReader.Get.Movies")]
+    public class ITraktBoxOfficeMovieObjectJsonReader_Tests
+    {
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_Implements_ITraktObjectJsonReader_Interface()
+        {
+            typeof(ITraktBoxOfficeMovieObjectJsonReader).GetInterfaces().Should().Contain(typeof(ITraktObjectJsonReader<ITraktBoxOfficeMovie>));
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_Json_String_Complete()
+        {
+            var jsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            var traktBoxOfficeMovie = jsonReader.ReadObject(JSON_COMPLETE);
+
+            traktBoxOfficeMovie.Should().NotBeNull();
+            traktBoxOfficeMovie.Revenue.Should().Be(166007347);
+            traktBoxOfficeMovie.Movie.Should().NotBeNull();
+            traktBoxOfficeMovie.Movie.Title.Should().Be("Star Wars: The Force Awakens");
+            traktBoxOfficeMovie.Movie.Year.Should().Be(2015);
+            traktBoxOfficeMovie.Movie.Ids.Should().NotBeNull();
+            traktBoxOfficeMovie.Movie.Ids.Trakt.Should().Be(94024U);
+            traktBoxOfficeMovie.Movie.Ids.Slug.Should().Be("star-wars-the-force-awakens-2015");
+            traktBoxOfficeMovie.Movie.Ids.Imdb.Should().Be("tt2488496");
+            traktBoxOfficeMovie.Movie.Ids.Tmdb.Should().Be(140607U);
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_Json_String_Incomplete_1()
+        {
+            var jsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            var traktBoxOfficeMovie = jsonReader.ReadObject(JSON_INCOMPLETE_1);
+
+            traktBoxOfficeMovie.Should().NotBeNull();
+            traktBoxOfficeMovie.Revenue.Should().Be(166007347);
+            traktBoxOfficeMovie.Movie.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_Json_String_Incomplete_2()
+        {
+            var jsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            var traktBoxOfficeMovie = jsonReader.ReadObject(JSON_INCOMPLETE_2);
+
+            traktBoxOfficeMovie.Should().NotBeNull();
+            traktBoxOfficeMovie.Revenue.Should().BeNull();
+            traktBoxOfficeMovie.Movie.Should().NotBeNull();
+            traktBoxOfficeMovie.Movie.Title.Should().Be("Star Wars: The Force Awakens");
+            traktBoxOfficeMovie.Movie.Year.Should().Be(2015);
+            traktBoxOfficeMovie.Movie.Ids.Should().NotBeNull();
+            traktBoxOfficeMovie.Movie.Ids.Trakt.Should().Be(94024U);
+            traktBoxOfficeMovie.Movie.Ids.Slug.Should().Be("star-wars-the-force-awakens-2015");
+            traktBoxOfficeMovie.Movie.Ids.Imdb.Should().Be("tt2488496");
+            traktBoxOfficeMovie.Movie.Ids.Tmdb.Should().Be(140607U);
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_Json_String_Not_Valid_1()
+        {
+            var jsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            var traktBoxOfficeMovie = jsonReader.ReadObject(JSON_NOT_VALID_1);
+
+            traktBoxOfficeMovie.Should().NotBeNull();
+            traktBoxOfficeMovie.Revenue.Should().BeNull();
+            traktBoxOfficeMovie.Movie.Should().NotBeNull();
+            traktBoxOfficeMovie.Movie.Title.Should().Be("Star Wars: The Force Awakens");
+            traktBoxOfficeMovie.Movie.Year.Should().Be(2015);
+            traktBoxOfficeMovie.Movie.Ids.Should().NotBeNull();
+            traktBoxOfficeMovie.Movie.Ids.Trakt.Should().Be(94024U);
+            traktBoxOfficeMovie.Movie.Ids.Slug.Should().Be("star-wars-the-force-awakens-2015");
+            traktBoxOfficeMovie.Movie.Ids.Imdb.Should().Be("tt2488496");
+            traktBoxOfficeMovie.Movie.Ids.Tmdb.Should().Be(140607U);
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_Json_String_Not_Valid_2()
+        {
+            var jsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            var traktBoxOfficeMovie = jsonReader.ReadObject(JSON_NOT_VALID_2);
+
+            traktBoxOfficeMovie.Should().NotBeNull();
+            traktBoxOfficeMovie.Revenue.Should().Be(166007347);
+            traktBoxOfficeMovie.Movie.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_Json_String_Not_Valid_3()
+        {
+            var jsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            var traktBoxOfficeMovie = jsonReader.ReadObject(JSON_NOT_VALID_3);
+
+            traktBoxOfficeMovie.Should().NotBeNull();
+            traktBoxOfficeMovie.Revenue.Should().BeNull();
+            traktBoxOfficeMovie.Movie.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_Json_String_Null()
+        {
+            var jsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            var traktBoxOfficeMovie = jsonReader.ReadObject(default(string));
+            traktBoxOfficeMovie.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_Json_String_Empty()
+        {
+            var jsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            var traktBoxOfficeMovie = jsonReader.ReadObject(string.Empty);
+            traktBoxOfficeMovie.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_JsonReader_Complete()
+        {
+            var traktJsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_COMPLETE))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktBoxOfficeMovie = traktJsonReader.ReadObject(jsonReader);
+
+                traktBoxOfficeMovie.Should().NotBeNull();
+                traktBoxOfficeMovie.Revenue.Should().Be(166007347);
+                traktBoxOfficeMovie.Movie.Should().NotBeNull();
+                traktBoxOfficeMovie.Movie.Title.Should().Be("Star Wars: The Force Awakens");
+                traktBoxOfficeMovie.Movie.Year.Should().Be(2015);
+                traktBoxOfficeMovie.Movie.Ids.Should().NotBeNull();
+                traktBoxOfficeMovie.Movie.Ids.Trakt.Should().Be(94024U);
+                traktBoxOfficeMovie.Movie.Ids.Slug.Should().Be("star-wars-the-force-awakens-2015");
+                traktBoxOfficeMovie.Movie.Ids.Imdb.Should().Be("tt2488496");
+                traktBoxOfficeMovie.Movie.Ids.Tmdb.Should().Be(140607U);
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_JsonReader_Incomplete_1()
+        {
+            var traktJsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_INCOMPLETE_1))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktBoxOfficeMovie = traktJsonReader.ReadObject(jsonReader);
+
+                traktBoxOfficeMovie.Should().NotBeNull();
+                traktBoxOfficeMovie.Revenue.Should().Be(166007347);
+                traktBoxOfficeMovie.Movie.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_JsonReader_Incomplete_2()
+        {
+            var traktJsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_INCOMPLETE_2))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktBoxOfficeMovie = traktJsonReader.ReadObject(jsonReader);
+
+                traktBoxOfficeMovie.Should().NotBeNull();
+                traktBoxOfficeMovie.Revenue.Should().BeNull();
+                traktBoxOfficeMovie.Movie.Should().NotBeNull();
+                traktBoxOfficeMovie.Movie.Title.Should().Be("Star Wars: The Force Awakens");
+                traktBoxOfficeMovie.Movie.Year.Should().Be(2015);
+                traktBoxOfficeMovie.Movie.Ids.Should().NotBeNull();
+                traktBoxOfficeMovie.Movie.Ids.Trakt.Should().Be(94024U);
+                traktBoxOfficeMovie.Movie.Ids.Slug.Should().Be("star-wars-the-force-awakens-2015");
+                traktBoxOfficeMovie.Movie.Ids.Imdb.Should().Be("tt2488496");
+                traktBoxOfficeMovie.Movie.Ids.Tmdb.Should().Be(140607U);
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_JsonReader_Not_Valid_1()
+        {
+            var traktJsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_1))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktBoxOfficeMovie = traktJsonReader.ReadObject(jsonReader);
+
+                traktBoxOfficeMovie.Should().NotBeNull();
+                traktBoxOfficeMovie.Revenue.Should().BeNull();
+                traktBoxOfficeMovie.Movie.Should().NotBeNull();
+                traktBoxOfficeMovie.Movie.Title.Should().Be("Star Wars: The Force Awakens");
+                traktBoxOfficeMovie.Movie.Year.Should().Be(2015);
+                traktBoxOfficeMovie.Movie.Ids.Should().NotBeNull();
+                traktBoxOfficeMovie.Movie.Ids.Trakt.Should().Be(94024U);
+                traktBoxOfficeMovie.Movie.Ids.Slug.Should().Be("star-wars-the-force-awakens-2015");
+                traktBoxOfficeMovie.Movie.Ids.Imdb.Should().Be("tt2488496");
+                traktBoxOfficeMovie.Movie.Ids.Tmdb.Should().Be(140607U);
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_JsonReader_Not_Valid_2()
+        {
+            var traktJsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_2))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktBoxOfficeMovie = traktJsonReader.ReadObject(jsonReader);
+
+                traktBoxOfficeMovie.Should().NotBeNull();
+                traktBoxOfficeMovie.Revenue.Should().Be(166007347);
+                traktBoxOfficeMovie.Movie.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_JsonReader_Not_Valid_3()
+        {
+            var traktJsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_3))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktBoxOfficeMovie = traktJsonReader.ReadObject(jsonReader);
+
+                traktBoxOfficeMovie.Should().NotBeNull();
+                traktBoxOfficeMovie.Revenue.Should().BeNull();
+                traktBoxOfficeMovie.Movie.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_JsonReader_Null()
+        {
+            var jsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            var traktBoxOfficeMovie = jsonReader.ReadObject(default(JsonTextReader));
+            traktBoxOfficeMovie.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_ITraktBoxOfficeMovieObjectJsonReader_ReadObject_From_JsonReader_Empty()
+        {
+            var traktJsonReader = new ITraktBoxOfficeMovieObjectJsonReader();
+
+            using (var reader = new StringReader(string.Empty))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var traktBoxOfficeMovie = traktJsonReader.ReadObject(jsonReader);
+                traktBoxOfficeMovie.Should().BeNull();
+            }
+        }
+
+        private const string JSON_COMPLETE =
+            @"{
+                ""revenue"": 166007347,
+                ""movie"": {
+                  ""title"": ""Star Wars: The Force Awakens"",
+                  ""year"": 2015,
+                  ""ids"": {
+                    ""trakt"": 94024,
+                    ""slug"": ""star-wars-the-force-awakens-2015"",
+                    ""imdb"": ""tt2488496"",
+                    ""tmdb"": 140607
+                  }
+                }
+              }";
+
+        private const string JSON_INCOMPLETE_1 =
+            @"{
+                ""revenue"": 166007347
+              }";
+
+        private const string JSON_INCOMPLETE_2 =
+            @"{
+                ""movie"": {
+                  ""title"": ""Star Wars: The Force Awakens"",
+                  ""year"": 2015,
+                  ""ids"": {
+                    ""trakt"": 94024,
+                    ""slug"": ""star-wars-the-force-awakens-2015"",
+                    ""imdb"": ""tt2488496"",
+                    ""tmdb"": 140607
+                  }
+                }
+              }";
+
+        private const string JSON_NOT_VALID_1 =
+            @"{
+                ""rev"": 166007347,
+                ""movie"": {
+                  ""title"": ""Star Wars: The Force Awakens"",
+                  ""year"": 2015,
+                  ""ids"": {
+                    ""trakt"": 94024,
+                    ""slug"": ""star-wars-the-force-awakens-2015"",
+                    ""imdb"": ""tt2488496"",
+                    ""tmdb"": 140607
+                  }
+                }
+              }";
+
+        private const string JSON_NOT_VALID_2 =
+            @"{
+                ""revenue"": 166007347,
+                ""mo"": {
+                  ""title"": ""Star Wars: The Force Awakens"",
+                  ""year"": 2015,
+                  ""ids"": {
+                    ""trakt"": 94024,
+                    ""slug"": ""star-wars-the-force-awakens-2015"",
+                    ""imdb"": ""tt2488496"",
+                    ""tmdb"": 140607
+                  }
+                }
+              }";
+
+        private const string JSON_NOT_VALID_3 =
+            @"{
+                ""rev"": 166007347,
+                ""mo"": {
+                  ""title"": ""Star Wars: The Force Awakens"",
+                  ""year"": 2015,
+                  ""ids"": {
+                    ""trakt"": 94024,
+                    ""slug"": ""star-wars-the-force-awakens-2015"",
+                    ""imdb"": ""tt2488496"",
+                    ""tmdb"": 140607
+                  }
+                }
+              }";
+    }
+}
