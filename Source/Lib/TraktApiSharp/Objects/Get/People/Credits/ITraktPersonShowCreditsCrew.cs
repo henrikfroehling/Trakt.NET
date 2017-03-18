723b13779d9abe39170eb4823305c3156d8ddd6c
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 15:38:17 +0100

    Add interface for TraktPersonShowCreditsCrew

---
+++ Source/Lib/TraktApiSharp/Objects/Get/People/Credits/ITraktPersonShowCreditsCrew.cs
@@ -0,0 +1,29 @@
+﻿namespace TraktApiSharp.Objects.Get.People.Credits
+{
+    using System.Collections.Generic;
+
+    public interface ITraktPersonShowCreditsCrew
+    {
+        IEnumerable<ITraktPersonShowCreditsCrewItem> Production { get; set; }
+
+        IEnumerable<ITraktPersonShowCreditsCrewItem> Art { get; set; }
+
+        IEnumerable<ITraktPersonShowCreditsCrewItem> Crew { get; set; }
+
+        IEnumerable<ITraktPersonShowCreditsCrewItem> CostumeAndMakeup { get; set; }
+
+        IEnumerable<ITraktPersonShowCreditsCrewItem> Directing { get; set; }
+
+        IEnumerable<ITraktPersonShowCreditsCrewItem> Writing { get; set; }
+
+        IEnumerable<ITraktPersonShowCreditsCrewItem> Sound { get; set; }
+
+        IEnumerable<ITraktPersonShowCreditsCrewItem> Camera { get; set; }
+
+        IEnumerable<ITraktPersonShowCreditsCrewItem> Lighting { get; set; }
+
+        IEnumerable<ITraktPersonShowCreditsCrewItem> VisualEffects { get; set; }
+
+        IEnumerable<ITraktPersonShowCreditsCrewItem> Editing { get; set; }
+    }
+}
