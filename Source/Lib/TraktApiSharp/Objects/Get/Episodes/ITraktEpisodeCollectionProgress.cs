3fa5453be6fe1c6518337340e1b34b5ae31eb105
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 14:34:37 +0100

    Add interface for TraktEpisodeCollectionProgress

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Episodes/ITraktEpisodeCollectionProgress.cs
@@ -0,0 +1,9 @@
+﻿namespace TraktApiSharp.Objects.Get.Episodes
+{
+    using System;
+
+    public interface ITraktEpisodeCollectionProgress : ITraktEpisodeProgress
+    {
+        DateTime? CollectedAt { get; set; }
+    }
+}
