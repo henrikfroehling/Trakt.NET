211596aec1ead4ff228257cb54b3588d7952b497
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 14:48:28 +0100

    Add interface for TraktEpisode

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Episodes/ITraktEpisode.cs
@@ -0,0 +1,34 @@
+﻿namespace TraktApiSharp.Objects.Get.Episodes
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface ITraktEpisode
+    {
+        int? SeasonNumber { get; set; }
+
+        int? Number { get; set; }
+
+        string Title { get; set; }
+
+        ITraktEpisodeIds Ids { get; set; }
+
+        int? NumberAbsolute { get; set; }
+
+        string Overview { get; set; }
+
+        int? Runtime { get; set; }
+
+        float? Rating { get; set; }
+
+        int? Votes { get; set; }
+
+        DateTime? FirstAired { get; set; }
+
+        DateTime? UpdatedAt { get; set; }
+
+        IEnumerable<string> AvailableTranslationLanguageCodes { get; set; }
+
+        IEnumerable<ITraktEpisodeTranslation> Translations { get; set; }
+    }
+}
