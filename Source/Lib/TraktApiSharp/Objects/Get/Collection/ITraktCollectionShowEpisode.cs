9033fe594518c5822c1490452004b400704ebec0
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 18:53:58 +0100

    Add interface for TraktCollectionShowEpisode

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Collection/ITraktCollectionShowEpisode.cs
@@ -0,0 +1,14 @@
+﻿namespace TraktApiSharp.Objects.Get.Collection
+{
+    using Basic;
+    using System;
+
+    public interface ITraktCollectionShowEpisode
+    {
+        int? Number { get; set; }
+
+        DateTime? CollectedAt { get; set; }
+
+        ITraktMetadata Metadata { get; set; }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Collection/Interfaces/ITraktCollectionShowEpisode_Tests.cs
@@ -0,0 +1,50 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Collection.Interfaces
+{
+    using FluentAssertions;
+    using System;
+    using System.Linq;
+    using Traits;
+    using TraktApiSharp.Objects.Basic;
+    using TraktApiSharp.Objects.Get.Collection;
+    using Xunit;
+
+    [Category("Objects.Get.Collection.Interfaces")]
+    public class ITraktCollectionShowEpisodeEpisode_Tests
+    {
+        [Fact]
+        public void Test_ITraktCollectionShowEpisode_Is_Interface()
+        {
+            typeof(ITraktCollectionShowEpisode).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktCollectionShowEpisode_Has_Number_Property()
+        {
+            var propertyInfo = typeof(ITraktCollectionShowEpisode).GetProperties().FirstOrDefault(p => p.Name == "Number");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(int?));
+        }
+
+        [Fact]
+        public void Test_ITraktCollectionShowEpisode_Has_CollectedAt_Property()
+        {
+            var propertyInfo = typeof(ITraktCollectionShowEpisode).GetProperties().FirstOrDefault(p => p.Name == "CollectedAt");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(DateTime?));
+        }
+
+        [Fact]
+        public void Test_ITraktCollectionShowEpisode_Has_Metadata_Property()
+        {
+            var propertyInfo = typeof(ITraktCollectionShowEpisode).GetProperties().FirstOrDefault(p => p.Name == "Metadata");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(ITraktMetadata));
+        }
+    }
+}
