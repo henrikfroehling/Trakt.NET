af95d7005899cb4079440af36a1066c2e1ee41a5
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 23:53:41 +0100

    Add interface for TraktSyncCommentsLastActivities

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Syncs/Activities/ITraktSyncCommentsLastActivities.cs
@@ -0,0 +1,9 @@
+﻿namespace TraktApiSharp.Objects.Get.Syncs.Activities
+{
+    using System;
+
+    public interface ITraktSyncCommentsLastActivities
+    {
+        DateTime? LikedAt { get; set; }
+    }
+}
