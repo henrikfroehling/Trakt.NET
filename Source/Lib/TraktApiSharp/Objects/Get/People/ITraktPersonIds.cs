d4f53b58c505667b7f1134b8343498cbf9382e9f
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 14:55:26 +0100

    Add interface for TraktPersonIds

---
+++ Source/Lib/TraktApiSharp/Objects/Get/People/ITraktPersonIds.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Objects.Get.People
+{
+    using Basic;
+
+    public interface ITraktPersonIds : IIds
+    {
+        uint Trakt { get; set; }
+
+        string Slug { get; set; }
+
+        string Imdb { get; set; }
+
+        uint? Tmdb { get; set; }
+
+        uint? TvRage { get; set; }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/People/Interfaces/ITraktPersonIds_Tests.cs
@@ -0,0 +1,75 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.People.Interfaces
+{
+    using FluentAssertions;
+    using System.Linq;
+    using Traits;
+    using TraktApiSharp.Objects.Basic;
+    using TraktApiSharp.Objects.Get.People;
+    using Xunit;
+
+    [Category("Objects.Get.People.Interfaces")]
+    public class ITraktPersonIds_Tests
+    {
+        [Fact]
+        public void Test_ITraktPersonIds_Is_Interface()
+        {
+            typeof(ITraktPersonIds).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktPersonIds_Inherits_IIds_Interface()
+        {
+            typeof(ITraktPersonIds).GetInterfaces().Should().Contain(typeof(IIds));
+        }
+
+        [Fact]
+        public void Test_ITraktPersonIds_Has_Trakt_Property()
+        {
+            var propertyInfo = typeof(ITraktPersonIds).GetProperties().FirstOrDefault(p => p.Name == "Trakt");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(uint));
+        }
+
+        [Fact]
+        public void Test_ITraktPersonIds_Has_Slug_Property()
+        {
+            var propertyInfo = typeof(ITraktPersonIds).GetProperties().FirstOrDefault(p => p.Name == "Slug");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void Test_ITraktPersonIds_Has_Imdb_Property()
+        {
+            var propertyInfo = typeof(ITraktPersonIds).GetProperties().FirstOrDefault(p => p.Name == "Imdb");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void Test_ITraktPersonIds_Has_Tmdb_Property()
+        {
+            var propertyInfo = typeof(ITraktPersonIds).GetProperties().FirstOrDefault(p => p.Name == "Tmdb");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(uint?));
+        }
+
+        [Fact]
+        public void Test_ITraktPersonIds_Has_TvRage_Property()
+        {
+            var propertyInfo = typeof(ITraktPersonIds).GetProperties().FirstOrDefault(p => p.Name == "TvRage");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(uint?));
+        }
+    }
+}
