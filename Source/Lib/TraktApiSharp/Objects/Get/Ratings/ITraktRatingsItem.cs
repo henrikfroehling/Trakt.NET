a8b2de3e1a777353a2696f133d97b2f4baf51325
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 20:14:51 +0100

    Add interface for TraktRatingsItem

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Ratings/ITraktRatingsItem.cs
@@ -0,0 +1,26 @@
+﻿namespace TraktApiSharp.Objects.Get.Ratings
+{
+    using Enums;
+    using Episodes;
+    using Movies;
+    using Seasons;
+    using Shows;
+    using System;
+
+    public interface ITraktRatingsItem
+    {
+        DateTime? RatedAt { get; set; }
+
+        int? Rating { get; set; }
+
+        TraktRatingsItemType Type { get; set; }
+
+        ITraktMovie Movie { get; set; }
+
+        ITraktShow Show { get; set; }
+
+        ITraktSeason Season { get; set; }
+
+        ITraktEpisode Episode { get; set; }
+    }
+}
