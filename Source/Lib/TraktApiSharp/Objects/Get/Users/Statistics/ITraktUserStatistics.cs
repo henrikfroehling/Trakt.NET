eff0c1f2b75edf60eea86f83d305f985e35dc2eb
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 21:15:28 +0100

    Add interface for TraktUserStatistics

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/Statistics/ITraktUserStatistics.cs
@@ -0,0 +1,17 @@
+﻿namespace TraktApiSharp.Objects.Get.Users.Statistics
+{
+    public interface ITraktUserStatistics
+    {
+        ITraktUserMoviesStatistics Movies { get; set; }
+
+        ITraktUserShowsStatistics Shows { get; set; }
+
+        ITraktUserSeasonsStatistics Seasons { get; set; }
+
+        ITraktUserEpisodesStatistics Episodes { get; set; }
+
+        ITraktUserNetworkStatistics Network { get; set; }
+
+        ITraktUserRatingsStatistics Ratings { get; set; }
+    }
+}
