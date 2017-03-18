e8c93dc01009c99ebedfa1c9c39b0b27304bcdba
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 16:21:49 +0100

    Add interface for TraktSeason

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Seasons/ITraktSeason.cs
@@ -0,0 +1,27 @@
+﻿namespace TraktApiSharp.Objects.Get.Seasons
+{
+    using Episodes;
+    using System;
+    using System.Collections.Generic;
+
+    public interface ITraktSeason
+    {
+        int? Number { get; set; }
+
+        ITraktSeasonIds Ids { get; set; }
+
+        float? Rating { get; set; }
+
+        int? Votes { get; set; }
+
+        int? TotalEpisodesCount { get; set; }
+
+        int? AiredEpisodesCount { get; set; }
+
+        string Overview { get; set; }
+
+        DateTime? FirstAired { get; set; }
+
+        IEnumerable<ITraktEpisode> Episodes { get; set; }
+    }
+}
