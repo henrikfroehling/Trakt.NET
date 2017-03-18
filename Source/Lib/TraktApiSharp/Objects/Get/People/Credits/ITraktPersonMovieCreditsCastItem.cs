50a40a084f92c1bdbd2c3f98de4ca89ea18fdcae
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 15:07:21 +0100

    Add interface for TraktPersonMovieCreditsCastItem

---
+++ Source/Lib/TraktApiSharp/Objects/Get/People/Credits/ITraktPersonMovieCreditsCastItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Objects.Get.People.Credits
+{
+    using Movies;
+
+    public interface ITraktPersonMovieCreditsCastItem
+    {
+        string Character { get; set; }
+
+        ITraktMovie Movie { get; set; }
+    }
+}
