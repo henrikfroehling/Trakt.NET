0e603f1e92de00e93508e65072c0a591e4a2db16
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 23:46:54 +0100

    Add interface for TraktSyncPlaybackProgressItem

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Syncs/Playback/ITraktSyncPlaybackProgressItem.cs
@@ -0,0 +1,25 @@
+﻿namespace TraktApiSharp.Objects.Get.Syncs.Playback
+{
+    using Enums;
+    using Episodes;
+    using Movies;
+    using Shows;
+    using System;
+
+    public interface ITraktSyncPlaybackProgressItem
+    {
+        uint Id { get; set; }
+
+        float? Progress { get; set; }
+
+        DateTime? PausedAt { get; set; }
+
+        TraktSyncType Type { get; set; }
+
+        ITraktMovie Movie { get; set; }
+
+        ITraktEpisode Episode { get; set; }
+
+        ITraktShow Show { get; set; }
+    }
+}
