c4ddd1b442940e2f3ec24150a479872d1a4c8f53
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 16:51:36 +0100

    Add interface for TraktMovieRelease

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Movies/ITraktMovieRelease.cs
@@ -0,0 +1,18 @@
+﻿namespace TraktApiSharp.Objects.Get.Movies
+{
+    using Enums;
+    using System;
+
+    public interface ITraktMovieRelease
+    {
+        string CountryCode { get; set; }
+
+        string Certification { get; set; }
+
+        DateTime? ReleaseDate { get; set; }
+
+        TraktReleaseType ReleaseType { get; set; }
+
+        string Note { get; set; }
+    }
+}
