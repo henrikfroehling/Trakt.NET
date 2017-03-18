bb6d7d73cf5801ee9fa17e864d093610e149768d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 20:09:15 +0100

    Add interface for TraktHistoryItem

---
+++ Source/Lib/TraktApiSharp/Objects/Get/History/ITraktHistoryItem.cs
@@ -0,0 +1,28 @@
+﻿namespace TraktApiSharp.Objects.Get.History
+{
+    using Enums;
+    using Episodes;
+    using Movies;
+    using Seasons;
+    using Shows;
+    using System;
+
+    public interface ITraktHistoryItem
+    {
+        ulong Id { get; set; }
+
+        DateTime? WatchedAt { get; set; }
+
+        TraktHistoryActionType Action { get; set; }
+
+        TraktSyncItemType Type { get; set; }
+
+        ITraktMovie Movie { get; set; }
+
+        ITraktShow Show { get; set; }
+
+        ITraktSeason Season { get; set; }
+
+        ITraktEpisode Episode { get; set; }
+    }
+}
