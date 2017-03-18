1c8e9922bdb2b6389b0da25056aa536cee777c2d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 21:09:41 +0100

    Add interface for TraktUserShowsStatistics

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/Statistics/ITraktUserShowsStatistics.cs
@@ -0,0 +1,13 @@
+﻿namespace TraktApiSharp.Objects.Get.Users.Statistics
+{
+    public interface ITraktUserShowsStatistics
+    {
+        int? Watched { get; set; }
+
+        int? Collected { get; set; }
+
+        int? Ratings { get; set; }
+
+        int? Comments { get; set; }
+    }
+}
