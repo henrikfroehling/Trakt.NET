069270c9e8294d6e1f53c9a53bdc815a2d4a9c6d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-19 00:48:36 +0100

    Add interface for TraktAccountSettings

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Users/Interfaces/ITraktAccountSettings_Tests.cs
@@ -0,0 +1,48 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Users.Interfaces
+{
+    using FluentAssertions;
+    using System.Linq;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Users;
+    using Xunit;
+
+    [Category("Objects.Get.Users.Interfaces")]
+    public class ITraktAccountSettings_Tests
+    {
+        [Fact]
+        public void Test_ITraktAccountSettings_Is_Interface()
+        {
+            typeof(ITraktAccountSettings).IsInterface.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_ITraktAccountSettings_Has_TimeZoneId_Property()
+        {
+            var propertyInfo = typeof(ITraktAccountSettings).GetProperties().FirstOrDefault(p => p.Name == "TimeZoneId");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+
+        [Fact]
+        public void Test_ITraktAccountSettings_Has_Time24Hr_Property()
+        {
+            var propertyInfo = typeof(ITraktAccountSettings).GetProperties().FirstOrDefault(p => p.Name == "Time24Hr");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(bool?));
+        }
+
+        [Fact]
+        public void Test_ITraktAccountSettings_Has_CoverImage_Property()
+        {
+            var propertyInfo = typeof(ITraktAccountSettings).GetProperties().FirstOrDefault(p => p.Name == "CoverImage");
+
+            propertyInfo.CanRead.Should().BeTrue();
+            propertyInfo.CanWrite.Should().BeTrue();
+            propertyInfo.PropertyType.Should().Be(typeof(string));
+        }
+    }
+}
