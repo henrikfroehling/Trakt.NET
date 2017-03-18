0c8dcdca829be8f5e26508a4bb00f6f2d3acd35e
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-18 19:38:07 +0100

    Add interface for TraktCrew

---
+++ Source/Lib/TraktApiSharp/Objects/Basic/ITraktCrew.cs
@@ -0,0 +1,29 @@
+﻿namespace TraktApiSharp.Objects.Basic
+{
+    using System.Collections.Generic;
+
+    public interface ITraktCrew
+    {
+        IEnumerable<ITraktCrewMember> Production { get; set; }
+
+        IEnumerable<ITraktCrewMember> Art { get; set; }
+
+        IEnumerable<ITraktCrewMember> Crew { get; set; }
+
+        IEnumerable<ITraktCrewMember> CostumeAndMakeup { get; set; }
+
+        IEnumerable<ITraktCrewMember> Directing { get; set; }
+
+        IEnumerable<ITraktCrewMember> Writing { get; set; }
+
+        IEnumerable<ITraktCrewMember> Sound { get; set; }
+
+        IEnumerable<ITraktCrewMember> Camera { get; set; }
+
+        IEnumerable<ITraktCrewMember> Lighting { get; set; }
+
+        IEnumerable<ITraktCrewMember> VisualEffects { get; set; }
+
+        IEnumerable<ITraktCrewMember> Editing { get; set; }
+    }
+}
