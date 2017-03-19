3e2342a98ebe2338ab94f005227e607424c32cbb
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-19 01:37:59 +0100

    Add interface for TraktUserWatchingItem

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/ITraktUserWatchingItem.cs
@@ -0,0 +1,25 @@
+﻿namespace TraktApiSharp.Objects.Get.Users
+{
+    using Enums;
+    using Episodes;
+    using Movies;
+    using Shows;
+    using System;
+
+    public interface ITraktUserWatchingItem
+    {
+        DateTime? StartedAt { get; set; }
+
+        DateTime? ExpiresAt { get; set; }
+
+        TraktHistoryActionType Action { get; set; }
+
+        TraktSyncType Type { get; set; }
+
+        ITraktMovie Movie { get; set; }
+
+        ITraktShow Show { get; set; }
+
+        ITraktEpisode Episode { get; set; }
+    }
+}
