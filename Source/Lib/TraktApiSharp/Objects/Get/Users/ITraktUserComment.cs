0d72384c78756787e43fa3e7e27143c28602984b
Henrik Fröhling <henrik.froehling@gmail.com>
2017-03-19 01:01:29 +0100

    Add interface for TraktUserComment

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/ITraktUserComment.cs
@@ -0,0 +1,27 @@
+﻿namespace TraktApiSharp.Objects.Get.Users
+{
+    using Basic;
+    using Enums;
+    using Episodes;
+    using Lists;
+    using Movies;
+    using Seasons;
+    using Shows;
+
+    public interface ITraktUserComment
+    {
+        TraktObjectType Type { get; set; }
+
+        ITraktComment Comment { get; set; }
+
+        ITraktMovie Movie { get; set; }
+
+        ITraktShow Show { get; set; }
+
+        ITraktSeason Season { get; set; }
+
+        ITraktEpisode Episode { get; set; }
+
+        ITraktList List { get; set; }
+    }
+}
