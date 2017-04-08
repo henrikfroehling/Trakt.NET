efb1a040f055828f68af35ab832c471b10ab5309
Henrik Fröhling <henrik.froehling@gmail.com>
2017-04-08 10:58:52 +0200

    Reorganize tests for season json readers

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Seasons/JsonReader/ITraktSeasonWatchedProgressArrayJsonReader_Tests/ITraktSeasonWatchedProgressArrayJsonReader_Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Seasons.JsonReader
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Seasons;
+    using TraktApiSharp.Objects.Get.Seasons.JsonReader;
+    using TraktApiSharp.Objects.JsonReader;
+    using Xunit;
+
+    [Category("Objects.Get.Seasons.JsonReader")]
+    public partial class ITraktSeasonWatchedProgressArrayJsonReader_Tests
+    {
+        [Fact]
+        public void Test_ITraktSeasonWatchedProgressArrayJsonReader_Implements_ITraktArrayJsonReader_Interface()
+        {
+            typeof(ITraktSeasonWatchedProgressArrayJsonReader).GetInterfaces().Should().Contain(typeof(ITraktArrayJsonReader<ITraktSeasonWatchedProgress>));
+        }
+    }
+}
