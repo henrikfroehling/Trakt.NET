9eda30d4ab30ef8f5f7922a438439f695f01331d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-04-08 09:56:33 +0200

    Reorganize tests for people json readers

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/People/JsonReader/ITraktPersonIdsObjectJsonReader_Tests/ITraktPersonIdsObjectJsonReader_Stream_Tests.cs
@@ -0,0 +1,10 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.People.JsonReader
+{
+    using Traits;
+
+    [Category("Objects.Get.People.JsonReader")]
+    public partial class ITraktPersonIdsObjectJsonReader_Tests
+    {
+
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/People/JsonReader/ITraktPersonIdsObjectJsonReader_Tests/ITraktPersonIdsObjectJsonReader_Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.People.JsonReader
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.People;
+    using TraktApiSharp.Objects.Get.People.JsonReader;
+    using TraktApiSharp.Objects.JsonReader;
+    using Xunit;
+
+    [Category("Objects.Get.People.JsonReader")]
+    public partial class ITraktPersonIdsObjectJsonReader_Tests
+    {
+        [Fact]
+        public void Test_ITraktPersonIdsObjectJsonReader_Implements_ITraktObjectJsonReader_Interface()
+        {
+            typeof(ITraktPersonIdsObjectJsonReader).GetInterfaces().Should().Contain(typeof(ITraktObjectJsonReader<ITraktPersonIds>));
+        }
+    }
+}
