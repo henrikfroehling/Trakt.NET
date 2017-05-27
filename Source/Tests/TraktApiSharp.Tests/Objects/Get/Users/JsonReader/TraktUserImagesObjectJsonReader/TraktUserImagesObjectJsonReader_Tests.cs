40e95c541a015a65ef006f46e0b1e1653e8b24ef
Henrik Fröhling <henrik.froehling@gmail.com>
2017-05-27 03:12:53 +0200

    Add json reader implementation for ITraktUserImages

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Users/JsonReader/TraktUserImagesObjectJsonReader/TraktUserImagesObjectJsonReader_Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Users.JsonReader
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Users;
+    using TraktApiSharp.Objects.Get.Users.JsonReader;
+    using TraktApiSharp.Objects.JsonReader;
+    using Xunit;
+
+    [Category("Objects.Get.Users.JsonReader")]
+    public partial class TraktUserImagesObjectJsonReader_Tests
+    {
+        [Fact]
+        public void Test_TraktUserImagesObjectJsonReader_Implements_ITraktObjectJsonReader_Interface()
+        {
+            typeof(TraktUserImagesObjectJsonReader).GetInterfaces().Should().Contain(typeof(ITraktObjectJsonReader<ITraktUserImages>));
+        }
+    }
+}
