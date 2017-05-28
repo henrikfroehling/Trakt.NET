bca343d85c4f81b23896a13da63a47c346e38a75
Henrik Fröhling <henrik.froehling@gmail.com>
2017-05-28 07:28:50 +0200

    Add json reader implementation for ITraktListIds

---
+++ Source/Tests/TraktApiSharp.Tests/Objects/Get/Users/Lists/JsonReader/TraktListIdsObjectJsonReader/TraktListIdsObjectJsonReader_Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace TraktApiSharp.Tests.Objects.Get.Users.Lists.JsonReader
+{
+    using FluentAssertions;
+    using Traits;
+    using TraktApiSharp.Objects.Get.Users.Lists;
+    using TraktApiSharp.Objects.Get.Users.Lists.JsonReader;
+    using TraktApiSharp.Objects.JsonReader;
+    using Xunit;
+
+    [Category("Objects.Get.Users.Lists.JsonReader")]
+    public partial class TraktListIdsObjectJsonReader_Tests
+    {
+        [Fact]
+        public void Test_TraktListIdsObjectJsonReader_Implements_ITraktObjectJsonReader_Interface()
+        {
+            typeof(TraktListIdsObjectJsonReader).GetInterfaces().Should().Contain(typeof(ITraktObjectJsonReader<ITraktListIds>));
+        }
+    }
+}
