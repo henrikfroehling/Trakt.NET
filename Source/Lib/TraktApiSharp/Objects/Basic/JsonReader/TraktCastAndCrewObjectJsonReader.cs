6cfdf2f52bdcf52c4b029eee87d8c1c2cc0b8e9d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-10 16:50:44 +0200

    Add json reader implementation for ITraktCastAndCrew

---
+++ Source/Lib/TraktApiSharp/Objects/Basic/JsonReader/TraktCastAndCrewObjectJsonReader.cs
@@ -0,0 +1,74 @@
+﻿namespace TraktApiSharp.Objects.Basic.JsonReader
+{
+    using Implementations;
+    using Newtonsoft.Json;
+    using Objects.JsonReader;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class TraktCastAndCrewObjectJsonReader : ITraktObjectJsonReader<ITraktCastAndCrew>
+    {
+        private const string PROPERTY_NAME_CAST = "cast";
+        private const string PROPERTY_NAME_CREW = "crew";
+
+        public Task<ITraktCastAndCrew> ReadObjectAsync(string json, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (string.IsNullOrEmpty(json))
+                return Task.FromResult(default(ITraktCastAndCrew));
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return ReadObjectAsync(jsonReader, cancellationToken);
+            }
+        }
+
+        public Task<ITraktCastAndCrew> ReadObjectAsync(Stream stream, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (stream == null)
+                return Task.FromResult(default(ITraktCastAndCrew));
+
+            using (var streamReader = new StreamReader(stream))
+            using (var jsonReader = new JsonTextReader(streamReader))
+            {
+                return ReadObjectAsync(jsonReader, cancellationToken);
+            }
+        }
+
+        public async Task<ITraktCastAndCrew> ReadObjectAsync(JsonTextReader jsonReader, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (jsonReader == null)
+                return await Task.FromResult(default(ITraktCastAndCrew));
+
+            if (await jsonReader.ReadAsync(cancellationToken) && jsonReader.TokenType == JsonToken.StartObject)
+            {
+                var castReader = new TraktCastMemberArrayJsonReader();
+                var crewReader = new TraktCrewObjectJsonReader();
+                ITraktCastAndCrew traktCastAndCrew = new TraktCastAndCrew();
+
+                while (await jsonReader.ReadAsync(cancellationToken) && jsonReader.TokenType == JsonToken.PropertyName)
+                {
+                    var propertyName = jsonReader.Value.ToString();
+
+                    switch (propertyName)
+                    {
+                        case PROPERTY_NAME_CAST:
+                            traktCastAndCrew.Cast = await castReader.ReadArrayAsync(jsonReader, cancellationToken);
+                            break;
+                        case PROPERTY_NAME_CREW:
+                            traktCastAndCrew.Crew = await crewReader.ReadObjectAsync(jsonReader, cancellationToken);
+                            break;
+                        default:
+                            await JsonReaderHelper.ReadAndIgnoreInvalidContentAsync(jsonReader, cancellationToken);
+                            break;
+                    }
+                }
+
+                return traktCastAndCrew;
+            }
+
+            return await Task.FromResult(default(ITraktCastAndCrew));
+        }
+    }
+}
