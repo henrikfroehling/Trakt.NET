37938a9659e7e5b9b91b720dd5ea3e93c2bba203
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-11 17:16:19 +0200

    Add json reader implementation for ITraktUserComment

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/JsonReader/TraktUserCommentObjectJsonReader.cs
@@ -0,0 +1,105 @@
+﻿namespace TraktApiSharp.Objects.Get.Users.JsonReader
+{
+    using Basic.JsonReader;
+    using Enums;
+    using Episodes.JsonReader;
+    using Implementations;
+    using Lists.JsonReader;
+    using Movies.JsonReader;
+    using Newtonsoft.Json;
+    using Objects.JsonReader;
+    using Seasons.JsonReader;
+    using Shows.JsonReader;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class TraktUserCommentObjectJsonReader : ITraktObjectJsonReader<ITraktUserComment>
+    {
+        private const string PROPERTY_NAME_TYPE = "type";
+        private const string PROPERTY_NAME_COMMENT = "comment";
+        private const string PROPERTY_NAME_MOVIE = "movie";
+        private const string PROPERTY_NAME_SHOW = "show";
+        private const string PROPERTY_NAME_SEASON = "season";
+        private const string PROPERTY_NAME_EPISODE = "episode";
+        private const string PROPERTY_NAME_LIST = "list";
+
+        public Task<ITraktUserComment> ReadObjectAsync(string json, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (string.IsNullOrEmpty(json))
+                return Task.FromResult(default(ITraktUserComment));
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return ReadObjectAsync(jsonReader, cancellationToken);
+            }
+        }
+
+        public Task<ITraktUserComment> ReadObjectAsync(Stream stream, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (stream == null)
+                return Task.FromResult(default(ITraktUserComment));
+
+            using (var streamReader = new StreamReader(stream))
+            using (var jsonReader = new JsonTextReader(streamReader))
+            {
+                return ReadObjectAsync(jsonReader, cancellationToken);
+            }
+        }
+
+        public async Task<ITraktUserComment> ReadObjectAsync(JsonTextReader jsonReader, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (jsonReader == null)
+                return await Task.FromResult(default(ITraktUserComment));
+
+            if (await jsonReader.ReadAsync(cancellationToken) && jsonReader.TokenType == JsonToken.StartObject)
+            {
+                var commentReader = new TraktCommentObjectJsonReader();
+                var movieReader = new TraktMovieObjectJsonReader();
+                var showReader = new TraktShowObjectJsonReader();
+                var seasonReader = new TraktSeasonObjectJsonReader();
+                var episodeReader = new TraktEpisodeObjectJsonReader();
+                var listReader = new TraktListObjectJsonReader();
+                ITraktUserComment traktUserComment = new TraktUserComment();
+
+                while (await jsonReader.ReadAsync(cancellationToken) && jsonReader.TokenType == JsonToken.PropertyName)
+                {
+                    var propertyName = jsonReader.Value.ToString();
+
+                    switch (propertyName)
+                    {
+                        case PROPERTY_NAME_TYPE:
+                            traktUserComment.Type = await JsonReaderHelper.ReadEnumerationValueAsync<TraktObjectType>(jsonReader, cancellationToken);
+                            break;
+                        case PROPERTY_NAME_COMMENT:
+                            traktUserComment.Comment = await commentReader.ReadObjectAsync(jsonReader, cancellationToken);
+                            break;
+                        case PROPERTY_NAME_MOVIE:
+                            traktUserComment.Movie = await movieReader.ReadObjectAsync(jsonReader, cancellationToken);
+                            break;
+                        case PROPERTY_NAME_SHOW:
+                            traktUserComment.Show = await showReader.ReadObjectAsync(jsonReader, cancellationToken);
+                            break;
+                        case PROPERTY_NAME_SEASON:
+                            traktUserComment.Season = await seasonReader.ReadObjectAsync(jsonReader, cancellationToken);
+                            break;
+                        case PROPERTY_NAME_EPISODE:
+                            traktUserComment.Episode = await episodeReader.ReadObjectAsync(jsonReader, cancellationToken);
+                            break;
+                        case PROPERTY_NAME_LIST:
+                            traktUserComment.List = await listReader.ReadObjectAsync(jsonReader, cancellationToken);
+                            break;
+                        default:
+                            await JsonReaderHelper.ReadAndIgnoreInvalidContentAsync(jsonReader, cancellationToken);
+                            break;
+                    }
+                }
+
+                return traktUserComment;
+            }
+
+            return await Task.FromResult(default(ITraktUserComment));
+        }
+    }
+}
