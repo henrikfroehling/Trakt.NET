b778b76333307324345256abee4a239375af6ea2
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-17 16:41:22 +0200

    Add json reader factories for history objects

---
+++ Source/Lib/TraktApiSharp/Objects/Get/History/JsonReader/Factories/TraktHistoryItemJsonReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Objects.Get.History.JsonReader.Factories
+{
+    using Objects.JsonReader;
+    using System;
+
+    internal class TraktHistoryItemJsonReaderFactory : ITraktJsonReaderFactory<ITraktHistoryItem>
+    {
+        public ITraktObjectJsonReader<ITraktHistoryItem> CreateObjectReader() => new TraktHistoryItemObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktHistoryItem> CreateArrayReader()
+        {
+            throw new NotSupportedException($"A array json reader for {nameof(ITraktHistoryItem)} is not supported.");
+        }
+    }
+}
