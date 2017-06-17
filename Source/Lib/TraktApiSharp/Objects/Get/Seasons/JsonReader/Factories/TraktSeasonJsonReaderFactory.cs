4dde5de34e1aab276dacc7b7e2fe902952907683
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-17 19:20:32 +0200

    Add json reader factories for season objects

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Seasons/JsonReader/Factories/TraktSeasonJsonReaderFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Objects.Get.Seasons.JsonReader.Factories
+{
+    using Objects.JsonReader;
+
+    internal class TraktSeasonJsonReaderFactory : ITraktJsonReaderFactory<ITraktSeason>
+    {
+        public ITraktObjectJsonReader<ITraktSeason> CreateObjectReader() => new TraktSeasonObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktSeason> CreateArrayReader() => new TraktSeasonArrayJsonReader();
+    }
+}
