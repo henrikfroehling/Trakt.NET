bc8699686ee5cddf8be7010710f13e0cf5783f11
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-17 16:54:01 +0200

    Add json reader factories for watchlist objects

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Watchlist/JsonReader/Factories/TraktWatchlistItemJsonReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Objects.Get.Watchlist.JsonReader.Factories
+{
+    using Objects.JsonReader;
+    using System;
+
+    internal class TraktWatchlistItemJsonReaderFactory : ITraktJsonReaderFactory<ITraktWatchlistItem>
+    {
+        public ITraktObjectJsonReader<ITraktWatchlistItem> CreateObjectReader() => new TraktWatchlistItemObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktWatchlistItem> CreateArrayReader()
+        {
+            throw new NotSupportedException($"A array json reader for {nameof(ITraktWatchlistItem)} is not supported.");
+        }
+    }
+}
