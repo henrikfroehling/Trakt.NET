f01c00e66e7abe0b14c7d9bc6f974174ec954f60
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-17 16:50:55 +0200

    Add json reader factories for watched objects

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Watched/JsonReader/Factories/TraktWatchedShowJsonReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Objects.Get.Watched.JsonReader.Factories
+{
+    using Objects.JsonReader;
+    using System;
+
+    internal class TraktWatchedShowJsonReaderFactory : ITraktJsonReaderFactory<ITraktWatchedShow>
+    {
+        public ITraktObjectJsonReader<ITraktWatchedShow> CreateObjectReader() => new TraktWatchedShowObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktWatchedShow> CreateArrayReader()
+        {
+            throw new NotSupportedException($"A array json reader for {nameof(ITraktWatchedShow)} is not supported.");
+        }
+    }
+}
---
+++ Source/Lib/TraktApiSharp/Objects/Get/Watched/JsonReader/Factories/TraktWatchedShowSeasonJsonReaderFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Objects.Get.Watched.JsonReader.Factories
+{
+    using Objects.JsonReader;
+
+    internal class TraktWatchedShowSeasonJsonReaderFactory : ITraktJsonReaderFactory<ITraktWatchedShowSeason>
+    {
+        public ITraktObjectJsonReader<ITraktWatchedShowSeason> CreateObjectReader() => new TraktWatchedShowSeasonObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktWatchedShowSeason> CreateArrayReader() => new TraktWatchedShowSeasonArrayJsonReader();
+    }
+}
