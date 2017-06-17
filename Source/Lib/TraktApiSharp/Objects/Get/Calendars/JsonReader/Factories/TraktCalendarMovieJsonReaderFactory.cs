54bac628d4031895e20160b229b539842e12a82d
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-17 16:31:22 +0200

    Add json reader factories for calendar objects

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Calendars/JsonReader/Factories/TraktCalendarMovieJsonReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Objects.Get.Calendars.JsonReader.Factories
+{
+    using Objects.JsonReader;
+    using System;
+
+    internal class TraktCalendarMovieJsonReaderFactory : ITraktJsonReaderFactory<ITraktCalendarMovie>
+    {
+        public ITraktObjectJsonReader<ITraktCalendarMovie> CreateObjectReader() => new TraktCalendarMovieObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktCalendarMovie> CreateArrayReader()
+        {
+            throw new NotSupportedException($"A array json reader for {nameof(ITraktCalendarMovie)} is not supported.");
+        }
+    }
+}
