fe928977169c3486a1023ffb662c8071724bf52f
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-17 17:40:16 +0200

    Add json reader factories for movie objects

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Movies/JsonReader/Factories/TraktBoxOfficeMovieJsonReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Objects.Get.Movies.JsonReader.Factories
+{
+    using Objects.JsonReader;
+    using System;
+
+    internal class TraktBoxOfficeMovieJsonReaderFactory : ITraktJsonReaderFactory<ITraktBoxOfficeMovie>
+    {
+        public ITraktObjectJsonReader<ITraktBoxOfficeMovie> CreateObjectReader() => new TraktBoxOfficeMovieObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktBoxOfficeMovie> CreateArrayReader()
+        {
+            throw new NotSupportedException($"A array json reader for {nameof(ITraktBoxOfficeMovie)} is not supported.");
+        }
+    }
+}
---
+++ Source/Lib/TraktApiSharp/Objects/Get/Movies/JsonReader/Factories/TraktMovieIdsJsonReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Objects.Get.Movies.JsonReader.Factories
+{
+    using Objects.JsonReader;
+    using System;
+
+    internal class TraktMovieIdsJsonReaderFactory : ITraktJsonReaderFactory<ITraktMovieIds>
+    {
+        public ITraktObjectJsonReader<ITraktMovieIds> CreateObjectReader() => new TraktMovieIdsObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktMovieIds> CreateArrayReader()
+        {
+            throw new NotSupportedException($"A array json reader for {nameof(ITraktMovieIds)} is not supported.");
+        }
+    }
+}
