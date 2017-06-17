bc0fbe66161582731a4537b67575e412bad75e11
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-17 18:49:40 +0200

    Add json reader factories for people objects

---
+++ Source/Lib/TraktApiSharp/Objects/Get/People/JsonReader/Factories/TraktPersonIdsJsonReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Objects.Get.People.JsonReader.Factories
+{
+    using Objects.JsonReader;
+    using System;
+
+    internal class TraktPersonIdsJsonReaderFactory : ITraktJsonReaderFactory<ITraktPersonIds>
+    {
+        public ITraktObjectJsonReader<ITraktPersonIds> CreateObjectReader() => new TraktPersonIdsObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktPersonIds> CreateArrayReader()
+        {
+            throw new NotSupportedException($"A array json reader for {nameof(ITraktPersonIds)} is not supported.");
+        }
+    }
+}
