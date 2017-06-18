8a91d506c268f2d2b81add606a7e5132ec522cdb
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-18 08:03:20 +0200

    Add json reader factories for sync playback objects

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Syncs/Playback/JsonReader/Factories/TraktSyncPlaybackProgressItemJsonReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Objects.Get.Syncs.Playback.JsonReader.Factories
+{
+    using Objects.JsonReader;
+    using System;
+
+    internal class TraktSyncPlaybackProgressItemJsonReaderFactory : ITraktJsonReaderFactory<ITraktSyncPlaybackProgressItem>
+    {
+        public ITraktObjectJsonReader<ITraktSyncPlaybackProgressItem> CreateObjectReader() => new TraktSyncPlaybackProgressItemObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktSyncPlaybackProgressItem> CreateArrayReader()
+        {
+            throw new NotSupportedException($"A array json reader for {nameof(ITraktSyncPlaybackProgressItem)} is not supported.");
+        }
+    }
+}
