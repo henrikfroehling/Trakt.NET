cda2e437e6a1fc6c21ef26359432102ed02a9d3a
Henrik Fröhling <henrik.froehling@gmail.com>
2017-06-18 08:20:13 +0200

    Add json reader factories for user list objects

---
+++ Source/Lib/TraktApiSharp/Objects/Get/Users/Lists/JsonReader/Factories/TraktListJsonReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace TraktApiSharp.Objects.Get.Users.Lists.JsonReader.Factories
+{
+    using Objects.JsonReader;
+    using System;
+
+    internal class TraktListJsonReaderFactory : ITraktJsonReaderFactory<ITraktList>
+    {
+        public ITraktObjectJsonReader<ITraktList> CreateObjectReader() => new TraktListObjectJsonReader();
+
+        public ITraktArrayJsonReader<ITraktList> CreateArrayReader()
+        {
+            throw new NotSupportedException($"A array json reader for {nameof(ITraktList)} is not supported.");
+        }
+    }
+}
