23067c82d2e81717e3bdfec21b0d8a2a9720fcf8
Henrik Fröhling <henrik.froehling@gmail.com>
2017-07-22 13:09:35 +0200

    Add array json reader for sync objects (missing tests)

--- Source/Lib/TraktApiSharp/Objects/Get/Syncs/Playback/JsonReader/Factories/TraktSyncPlaybackProgressItemJsonReaderFactory.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/Syncs/Playback/JsonReader/Factories/TraktSyncPlaybackProgressItemJsonReaderFactory.cs
@@ -1,15 +1,11 @@
 ﻿namespace TraktApiSharp.Objects.Get.Syncs.Playback.JsonReader.Factories
 {
     using Objects.JsonReader;
-    using System;
 
     internal class TraktSyncPlaybackProgressItemJsonReaderFactory : ITraktJsonReaderFactory<ITraktSyncPlaybackProgressItem>
     {
         public ITraktObjectJsonReader<ITraktSyncPlaybackProgressItem> CreateObjectReader() => new TraktSyncPlaybackProgressItemObjectJsonReader();
 
-        public ITraktArrayJsonReader<ITraktSyncPlaybackProgressItem> CreateArrayReader()
-        {
-            throw new NotSupportedException($"A array json reader for {nameof(ITraktSyncPlaybackProgressItem)} is not supported.");
-        }
+        public ITraktArrayJsonReader<ITraktSyncPlaybackProgressItem> CreateArrayReader() => new TraktSyncPlaybackProgressItemArrayJsonReader();
     }
 }
