8d582ca1b4f73d58cc98db96935e4316fff5f5e4
Henrik Fröhling <henrik.froehling@gmail.com>
2017-07-22 13:03:27 +0200

    Add array json reader for ratings objects (missing tests)

--- Source/Lib/TraktApiSharp/Objects/Get/Ratings/JsonReader/Factories/TraktRatingsItemJsonReaderFactory.cs
+++ Source/Lib/TraktApiSharp/Objects/Get/Ratings/JsonReader/Factories/TraktRatingsItemJsonReaderFactory.cs
@@ -1,15 +1,11 @@
 ﻿namespace TraktApiSharp.Objects.Get.Ratings.JsonReader.Factories
 {
     using Objects.JsonReader;
-    using System;
 
     internal class TraktRatingsItemJsonReaderFactory : ITraktJsonReaderFactory<ITraktRatingsItem>
     {
         public ITraktObjectJsonReader<ITraktRatingsItem> CreateObjectReader() => new TraktRatingsItemObjectJsonReader();
 
-        public ITraktArrayJsonReader<ITraktRatingsItem> CreateArrayReader()
-        {
-            throw new NotSupportedException($"A array json reader for {nameof(ITraktRatingsItem)} is not supported.");
-        }
+        public ITraktArrayJsonReader<ITraktRatingsItem> CreateArrayReader() => new TraktRatingsItemArrayJsonReader();
     }
 }
---
+++ Source/Lib/TraktApiSharp/Objects/Get/Ratings/JsonReader/TraktRatingsItemArrayJsonReader.cs
@@ -0,0 +1,65 @@
+﻿namespace TraktApiSharp.Objects.Get.Ratings.JsonReader
+{
+    using Newtonsoft.Json;
+    using Objects.JsonReader;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class TraktRatingsItemArrayJsonReader : ITraktArrayJsonReader<ITraktRatingsItem>
+    {
+        public Task<IEnumerable<ITraktRatingsItem>> ReadArrayAsync(string json, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (string.IsNullOrEmpty(json))
+                return Task.FromResult(default(IEnumerable<ITraktRatingsItem>));
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return ReadArrayAsync(jsonReader, cancellationToken);
+            }
+        }
+
+        public Task<IEnumerable<ITraktRatingsItem>> ReadArrayAsync(Stream stream, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (stream == null)
+                return Task.FromResult(default(IEnumerable<ITraktRatingsItem>));
+
+            using (var streamReader = new StreamReader(stream))
+            using (var jsonReader = new JsonTextReader(streamReader))
+            {
+                return ReadArrayAsync(jsonReader, cancellationToken);
+            }
+        }
+
+        public async Task<IEnumerable<ITraktRatingsItem>> ReadArrayAsync(JsonTextReader jsonReader, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (jsonReader == null)
+                return await Task.FromResult(default(IEnumerable<ITraktRatingsItem>));
+
+            if (await jsonReader.ReadAsync(cancellationToken) && jsonReader.TokenType == JsonToken.StartArray)
+            {
+                var ratingsItemReader = new TraktRatingsItemObjectJsonReader();
+                //var ratingsItemReadingTasks = new List<Task<ITraktRatingsItem>>();
+                var ratingsItems = new List<ITraktRatingsItem>();
+
+                //ratingsItemReadingTasks.Add(ratingsItemReader.ReadObjectAsync(jsonReader, cancellationToken));
+                ITraktRatingsItem ratingsItem = await ratingsItemReader.ReadObjectAsync(jsonReader, cancellationToken);
+
+                while (ratingsItem != null)
+                {
+                    ratingsItems.Add(ratingsItem);
+                    //ratingsItemReadingTasks.Add(ratingsItemReader.ReadObjectAsync(jsonReader, cancellationToken));
+                    ratingsItem = await ratingsItemReader.ReadObjectAsync(jsonReader, cancellationToken);
+                }
+
+                //var readRatingsItems = await Task.WhenAll(ratingsItemReadingTasks);
+                //return (IEnumerable<ITraktRatingsItem>)readRatingsItems.GetEnumerator();
+                return ratingsItems;
+            }
+
+            return await Task.FromResult(default(IEnumerable<ITraktRatingsItem>));
+        }
+    }
+}
