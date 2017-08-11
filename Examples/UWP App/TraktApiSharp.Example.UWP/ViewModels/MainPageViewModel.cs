53859fd92084bea2a251a6b968d11beac047f771
Henrik Fröhling <henrik.froehling@gmail.com>
2017-08-11 20:13:41 +0200

    Remove example projects

--- Examples/UWP App/TraktApiSharp.Example.UWP/ViewModels/MainPageViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TraktApiSharp.Example.UWP.ViewModels
-{
-    using Services.TraktService;
-
-    public class MainPageViewModel : BaseViewModel
-    {
-        public bool IsTraktClientIdAvailable => TraktServiceProvider.Instance.Client.IsValidForUseWithoutAuthorization;
-    }
-}
