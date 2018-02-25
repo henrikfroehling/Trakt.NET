8d89c043615ed7f0a7b2e70cede938e01426884d
Henrik Fröhling <henrik.froehling@gmail.com>
2018-02-25 01:48:54 +0100

    GH-75: Add json writer for sync post response objects

--- Source/Lib/TraktApiSharp/Objects/Post/Syncs/Responses/Json/Factories/SyncPostResponseGroupJsonIOFactory.cs
+++ Source/Lib/TraktApiSharp/Objects/Post/Syncs/Responses/Json/Factories/SyncPostResponseGroupJsonIOFactory.cs
@@ -1,26 +1,17 @@
 ﻿namespace TraktApiSharp.Objects.Post.Syncs.Responses.Json.Factories
 {
     using Objects.Json;
-    using Post.Syncs.Responses.Json.Reader;
+    using Reader;
     using System;
+    using Writer;
 
     internal class SyncPostResponseGroupJsonIOFactory : IJsonIOFactory<ITraktSyncPostResponseGroup>
     {
         public IObjectJsonReader<ITraktSyncPostResponseGroup> CreateObjectReader() => new SyncPostResponseGroupObjectJsonReader();
 
         public IArrayJsonReader<ITraktSyncPostResponseGroup> CreateArrayReader()
-        {
-            throw new NotSupportedException($"A array json reader for {nameof(ITraktSyncPostResponseGroup)} is not supported.");
-        }
+            => throw new NotSupportedException($"A array json reader for {nameof(ITraktSyncPostResponseGroup)} is not supported.");
 
-        public IObjectJsonWriter<ITraktSyncPostResponseGroup> CreateObjectWriter()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IArrayJsonWriter<ITraktSyncPostResponseGroup> CreateArrayWriter()
-        {
-            throw new NotImplementedException();
-        }
+        public IObjectJsonWriter<ITraktSyncPostResponseGroup> CreateObjectWriter() => new SyncPostResponseGroupObjectJsonWriter();
     }
 }
