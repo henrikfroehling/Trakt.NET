c0043e6176977e2ea027259b00d13decb933589a
Henrik Fröhling <henrik.froehling@gmail.com>
2018-03-31 21:57:23 +0200

    GH-87: Add http client provider

---
+++ Source/Lib/TraktApiSharp/Requests/Handler/IHttpClientProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktApiSharp.Requests.Handler
+{
+    using System.Net.Http;
+
+    public interface IHttpClientProvider
+    {
+        HttpClient GetHttpClient();
+
+        HttpClient GetAuthorizationHttpClient();
+    }
+}
