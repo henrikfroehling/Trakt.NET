5e5e1e9694d72f98351dd2609ee511b99b2663f4
Henrik Fröhling <henrik.froehling@gmail.com>
2018-03-31 21:48:56 +0200

    GH-87: Move genres module tests

---
+++ Source/Tests/TraktApiSharp.Tests/Modules/TraktGenresModule/TraktGenresModule_GetShowGenres_Tests.cs
@@ -0,0 +1,105 @@
+﻿namespace TraktApiSharp.Tests.Modules.TraktGenresModule
+{
+    using FluentAssertions;
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+    using TestUtils;
+    using Traits;
+    using TraktApiSharp.Enums;
+    using TraktApiSharp.Exceptions;
+    using TraktApiSharp.Objects.Basic;
+    using TraktApiSharp.Responses;
+    using Xunit;
+
+    [Category("Modules.Genres")]
+    public partial class TraktGenresModule_Tests
+    {
+        [Fact]
+        public void Test_TraktGenresModule_GetShowGenres()
+        {
+            TestUtility.SetupMockResponseWithoutOAuth("genres/shows", SHOW_GENRES_JSON);
+
+            var response = TestUtility.MOCK_TEST_CLIENT.Genres.GetShowGenresAsync().Result;
+
+            response.Should().NotBeNull();
+            response.IsSuccess.Should().BeTrue();
+            response.HasValue.Should().BeTrue();
+            response.Value.Should().NotBeNull().And.HaveCount(28);
+            response.Value.All(g => g.Type == TraktGenreType.Shows).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Test_TraktGenresModule_GetShowGenresExceptions()
+        {
+            const string uri = "genres/shows";
+
+            TestUtility.SetupMockResponseWithoutOAuth(uri, HttpStatusCode.NotFound);
+
+            Func<Task<TraktListResponse<ITraktGenre>>> act =
+                async () => await TestUtility.MOCK_TEST_CLIENT.Genres.GetShowGenresAsync();
+            act.Should().Throw<TraktNotFoundException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, HttpStatusCode.Unauthorized);
+            act.Should().Throw<TraktAuthorizationException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, HttpStatusCode.BadRequest);
+            act.Should().Throw<TraktBadRequestException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, HttpStatusCode.Forbidden);
+            act.Should().Throw<TraktForbiddenException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, HttpStatusCode.MethodNotAllowed);
+            act.Should().Throw<TraktMethodNotFoundException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, HttpStatusCode.Conflict);
+            act.Should().Throw<TraktConflictException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, HttpStatusCode.InternalServerError);
+            act.Should().Throw<TraktServerException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, HttpStatusCode.BadGateway);
+            act.Should().Throw<TraktBadGatewayException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, (HttpStatusCode)412);
+            act.Should().Throw<TraktPreconditionFailedException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, (HttpStatusCode)422);
+            act.Should().Throw<TraktValidationException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, (HttpStatusCode)429);
+            act.Should().Throw<TraktRateLimitException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, (HttpStatusCode)503);
+            act.Should().Throw<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, (HttpStatusCode)504);
+            act.Should().Throw<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, (HttpStatusCode)520);
+            act.Should().Throw<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, (HttpStatusCode)521);
+            act.Should().Throw<TraktServerUnavailableException>();
+
+            TestUtility.ClearMockHttpClient();
+            TestUtility.SetupMockResponseWithoutOAuth(uri, (HttpStatusCode)522);
+            act.Should().Throw<TraktServerUnavailableException>();
+        }
+    }
+}
---
+++ Source/Tests/TraktApiSharp.Tests/Modules/TraktGenresModule/TraktGenresModule_Tests_Json_Data.cs
@@ -0,0 +1,253 @@
+﻿namespace TraktApiSharp.Tests.Modules.TraktGenresModule
+{
+    public partial class TraktGenresModule_Tests
+    {
+        private const string MOVIE_GENRES_JSON =
+            @"[
+                {
+                  ""name"": ""Action"",
+                  ""slug"": ""action""
+                },
+                {
+                  ""name"": ""Adventure"",
+                  ""slug"": ""adventure""
+                },
+                {
+                  ""name"": ""Animation"",
+                  ""slug"": ""animation""
+                },
+                {
+                  ""name"": ""Comedy"",
+                  ""slug"": ""comedy""
+                },
+                {
+                  ""name"": ""Crime"",
+                  ""slug"": ""crime""
+                },
+                {
+                  ""name"": ""Disaster"",
+                  ""slug"": ""disaster""
+                },
+                {
+                  ""name"": ""Documentary"",
+                  ""slug"": ""documentary""
+                },
+                {
+                  ""name"": ""Drama"",
+                  ""slug"": ""drama""
+                },
+                {
+                  ""name"": ""Eastern"",
+                  ""slug"": ""eastern""
+                },
+                {
+                  ""name"": ""Family"",
+                  ""slug"": ""family""
+                },
+                {
+                  ""name"": ""Fan Film"",
+                  ""slug"": ""fan-film""
+                },
+                {
+                  ""name"": ""Fantasy"",
+                  ""slug"": ""fantasy""
+                },
+                {
+                  ""name"": ""Film Noir"",
+                  ""slug"": ""film-noir""
+                },
+                {
+                  ""name"": ""History"",
+                  ""slug"": ""history""
+                },
+                {
+                  ""name"": ""Holiday"",
+                  ""slug"": ""holiday""
+                },
+                {
+                  ""name"": ""Horror"",
+                  ""slug"": ""horror""
+                },
+                {
+                  ""name"": ""Indie"",
+                  ""slug"": ""indie""
+                },
+                {
+                  ""name"": ""Music"",
+                  ""slug"": ""music""
+                },
+                {
+                  ""name"": ""Musical"",
+                  ""slug"": ""musical""
+                },
+                {
+                  ""name"": ""Mystery"",
+                  ""slug"": ""mystery""
+                },
+                {
+                  ""name"": ""None"",
+                  ""slug"": ""none""
+                },
+                {
+                  ""name"": ""Road"",
+                  ""slug"": ""road""
+                },
+                {
+                  ""name"": ""Romance"",
+                  ""slug"": ""romance""
+                },
+                {
+                  ""name"": ""Science Fiction"",
+                  ""slug"": ""science-fiction""
+                },
+                {
+                  ""name"": ""Short"",
+                  ""slug"": ""short""
+                },
+                {
+                  ""name"": ""Sports"",
+                  ""slug"": ""sports""
+                },
+                {
+                  ""name"": ""Sporting Event"",
+                  ""slug"": ""sporting-event""
+                },
+                {
+                  ""name"": ""Suspense"",
+                  ""slug"": ""suspense""
+                },
+                {
+                  ""name"": ""Thriller"",
+                  ""slug"": ""thriller""
+                },
+                {
+                  ""name"": ""Tv Movie"",
+                  ""slug"": ""tv-movie""
+                },
+                {
+                  ""name"": ""War"",
+                  ""slug"": ""war""
+                },
+                {
+                  ""name"": ""Western"",
+                  ""slug"": ""western""
+                }
+              ]";
+
+        private const string SHOW_GENRES_JSON =
+            @"[
+                {
+                  ""name"": ""Action"",
+                  ""slug"": ""action""
+                },
+                {
+                  ""name"": ""Adventure"",
+                  ""slug"": ""adventure""
+                },
+                {
+                  ""name"": ""Animation"",
+                  ""slug"": ""animation""
+                },
+                {
+                  ""name"": ""Biography"",
+                  ""slug"": ""biography""
+                },
+                {
+                  ""name"": ""Children"",
+                  ""slug"": ""children""
+                },
+                {
+                  ""name"": ""Comedy"",
+                  ""slug"": ""comedy""
+                },
+                {
+                  ""name"": ""Disaster"",
+                  ""slug"": ""disaster""
+                },
+                {
+                  ""name"": ""Documentary"",
+                  ""slug"": ""documentary""
+                },
+                {
+                  ""name"": ""Drama"",
+                  ""slug"": ""drama""
+                },
+                {
+                  ""name"": ""Eastern"",
+                  ""slug"": ""eastern""
+                },
+                {
+                  ""name"": ""Fantasy"",
+                  ""slug"": ""fantasy""
+                },
+                {
+                  ""name"": ""Game Show"",
+                  ""slug"": ""game-show""
+                },
+                {
+                  ""name"": ""History"",
+                  ""slug"": ""history""
+                },
+                {
+                  ""name"": ""Home And Garden"",
+                  ""slug"": ""home-and-garden""
+                },
+                {
+                  ""name"": ""Holiday"",
+                  ""slug"": ""holiday""
+                },
+                {
+                  ""name"": ""Horror"",
+                  ""slug"": ""horror""
+                },
+                {
+                  ""name"": ""Mini Series"",
+                  ""slug"": ""mini-series""
+                },
+                {
+                  ""name"": ""News"",
+                  ""slug"": ""news""
+                },
+                {
+                  ""name"": ""None"",
+                  ""slug"": ""none""
+                },
+                {
+                  ""name"": ""Reality"",
+                  ""slug"": ""reality""
+                },
+                {
+                  ""name"": ""Science Fiction"",
+                  ""slug"": ""science-fiction""
+                },
+                {
+                  ""name"": ""Soap"",
+                  ""slug"": ""soap""
+                },
+                {
+                  ""name"": ""Special Interest"",
+                  ""slug"": ""special-interest""
+                },
+                {
+                  ""name"": ""Sports"",
+                  ""slug"": ""sports""
+                },
+                {
+                  ""name"": ""Sporting Event"",
+                  ""slug"": ""sporting-event""
+                },
+                {
+                  ""name"": ""Talk Show"",
+                  ""slug"": ""talk-show""
+                },
+                {
+                  ""name"": ""Thriller"",
+                  ""slug"": ""thriller""
+                },
+                {
+                  ""name"": ""Western"",
+                  ""slug"": ""western""
+                }
+              ]";
+    }
+}
