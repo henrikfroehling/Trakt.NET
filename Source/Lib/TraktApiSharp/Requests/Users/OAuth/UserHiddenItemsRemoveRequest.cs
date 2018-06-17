b4a0e23666d6362c0c564b60675791f9b7e50a52
Henrik Fröhling <henrik.froehling@gmail.com>
2018-06-17 22:54:44 +0200

    GH-72: Add request implementation for hidden items remove post

---
+++ Source/Lib/TraktApiSharp/Requests/Users/OAuth/UserHiddenItemsRemoveRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TraktApiSharp.Requests.Users.OAuth
+{
+    using Objects.Post.Users.HiddenItems.Responses;
+
+    internal sealed class UserHiddenItemsRemoveRequest : AUserHiddenItemsRequest<ITraktUserHiddenItemsRemovePostResponse>
+    {
+        public override string UriTemplate => "users/hidden/{section}/remove";
+    }
+}
