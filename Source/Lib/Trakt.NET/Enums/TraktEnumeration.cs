444417592eaabdf0167d7727e65f37d2937df0c5
Henrik Fröhling <henrik.froehling@gmail.com>
2018-06-27 22:03:38 +0200

    Move enumeration converter into own file

--- Source/Lib/Trakt.NET/Enums/TraktEnumeration.cs
+++ Source/Lib/Trakt.NET/Enums/TraktEnumeration.cs
@@ -1,6 +1,5 @@
 ﻿namespace TraktNet.Enums
 {
-    using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -129,30 +128,6 @@
         {
             TraktEnumeration matchingItem = GetAll<T>().FirstOrDefault(predicate);
             return matchingItem != null ? matchingItem as T : null;
-        }
-    }
-
-    public class TraktEnumerationConverter<T> : JsonConverter where T : TraktEnumeration, new()
-    {
-        public override bool CanConvert(Type objectType) => objectType == typeof(string);
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.Value == null)
-                return null;
-
-            var enumString = reader.Value as string;
-
-            if (string.IsNullOrEmpty(enumString))
-                return Activator.CreateInstance(typeof(T));
-
-            return TraktEnumeration.FromObjectName<T>(enumString);
-        }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            var enumeration = (T)value;
-            writer.WriteValue(enumeration.ObjectName);
         }
     }
 }
---
+++ Source/Lib/Trakt.NET/Enums/TraktEnumerationConverter'1.cs
@@ -0,0 +1,29 @@
+﻿namespace TraktNet.Enums
+{
+    using Newtonsoft.Json;
+    using System;
+
+    public class TraktEnumerationConverter<T> : JsonConverter where T : TraktEnumeration, new()
+    {
+        public override bool CanConvert(Type objectType) => objectType == typeof(string);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null)
+                return null;
+
+            var enumString = reader.Value as string;
+
+            if (string.IsNullOrEmpty(enumString))
+                return Activator.CreateInstance(typeof(T));
+
+            return TraktEnumeration.FromObjectName<T>(enumString);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var enumeration = (T)value;
+            writer.WriteValue(enumeration.ObjectName);
+        }
+    }
+}
