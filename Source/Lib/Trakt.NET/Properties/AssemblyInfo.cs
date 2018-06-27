f97d118ec468641942a0d1c68c4e409b759d34f4
Henrik Fröhling <henrik.froehling@gmail.com>
2018-06-27 21:58:02 +0200

    Fix assembly info

--- Source/Lib/Trakt.NET/Properties/AssemblyInfo.cs
+++ Source/Lib/Trakt.NET/Properties/AssemblyInfo.cs
@@ -4,8 +4,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Trakt.NET.Tests")]
 
-[assembly: AssemblyDescription(".NET wrapper library for the Trakt.tv API.")]
-[assembly: AssemblyCopyright("Copyright © 2016 - 2018 Henrik Fröhling et al.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
