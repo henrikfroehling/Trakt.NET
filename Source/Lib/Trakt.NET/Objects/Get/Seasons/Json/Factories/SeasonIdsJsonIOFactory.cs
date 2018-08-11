bcb0bbf9343371062049638ad26b442c469b0238
Henrik Fröhling <henrik.froehling@gmail.com>
2018-08-11 22:26:01 +0200

    GH-33: Implement missing json readers for season objects

--- Source/Lib/Trakt.NET/Objects/Get/Seasons/Json/Factories/SeasonIdsJsonIOFactory.cs
+++ Source/Lib/Trakt.NET/Objects/Get/Seasons/Json/Factories/SeasonIdsJsonIOFactory.cs
@@ -3,14 +3,12 @@
     using Get.Seasons.Json.Reader;
     using Get.Seasons.Json.Writer;
     using Objects.Json;
-    using System;
 
     internal class SeasonIdsJsonIOFactory : IJsonIOFactory<ITraktSeasonIds>
     {
         public IObjectJsonReader<ITraktSeasonIds> CreateObjectReader() => new SeasonIdsObjectJsonReader();
 
-        public IArrayJsonReader<ITraktSeasonIds> CreateArrayReader()
-            => throw new NotSupportedException($"A array json reader for {nameof(ITraktSeasonIds)} is not supported.");
+        public IArrayJsonReader<ITraktSeasonIds> CreateArrayReader() => new SeasonIdsArrayJsonReader();
 
         public IObjectJsonWriter<ITraktSeasonIds> CreateObjectWriter() => new SeasonIdsObjectJsonWriter();
     }
---
+++ Source/Lib/Trakt.NET/Objects/Get/Seasons/Json/Reader/SeasonIdsArrayJsonReader.cs
@@ -0,0 +1,34 @@
+﻿namespace TraktNet.Objects.Get.Seasons.Json.Reader
+{
+    using Newtonsoft.Json;
+    using Objects.Json;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class SeasonIdsArrayJsonReader : AArrayJsonReader<ITraktSeasonIds>
+    {
+        public override async Task<IEnumerable<ITraktSeasonIds>> ReadArrayAsync(JsonTextReader jsonReader, CancellationToken cancellationToken = default)
+        {
+            if (jsonReader == null)
+                return await Task.FromResult(default(IEnumerable<ITraktSeasonIds>));
+
+            if (await jsonReader.ReadAsync(cancellationToken) && jsonReader.TokenType == JsonToken.StartArray)
+            {
+                var seasonIdsReader = new SeasonIdsObjectJsonReader();
+                var seasonIdss = new List<ITraktSeasonIds>();
+                ITraktSeasonIds seasonIds = await seasonIdsReader.ReadObjectAsync(jsonReader, cancellationToken);
+
+                while (seasonIds != null)
+                {
+                    seasonIdss.Add(seasonIds);
+                    seasonIds = await seasonIdsReader.ReadObjectAsync(jsonReader, cancellationToken);
+                }
+
+                return seasonIdss;
+            }
+
+            return await Task.FromResult(default(IEnumerable<ITraktSeasonIds>));
+        }
+    }
+}
