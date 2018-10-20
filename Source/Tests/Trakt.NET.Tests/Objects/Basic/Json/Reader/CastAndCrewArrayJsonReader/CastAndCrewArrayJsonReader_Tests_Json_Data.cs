65ec779034f6b05c5251c4c8b14f33c47031b29b
Henrik Fröhling <henrik.froehling@gmail.com>
2018-10-20 16:02:40 +0200

    GH-1: Add json deserialization tests for basic objects (WIP)

---
+++ Source/Tests/Trakt.NET.Tests/Objects/Basic/Json/Reader/CastAndCrewArrayJsonReader/CastAndCrewArrayJsonReader_Tests_Json_Data.cs
@@ -0,0 +1,215 @@
+﻿namespace TraktNet.Tests.Objects.Basic.Json.Reader.CastAndCrewArrayJsonReader
+{
+    public partial class CastAndCrewArrayJsonReader_Tests
+    {
+        private const string JSON_EMPTY_ARRAY = "[]";
+
+        private const string JSON_COMPLETE =
+            @"[
+                {
+                  ""cast"": [
+                    {
+                      ""character"": ""Joe Brody"",
+                      ""person"": {
+                        ""name"": ""Bryan Cranston"",
+                        ""ids"": {
+                          ""trakt"": 297737,
+                          ""slug"": ""bryan-cranston"",
+                          ""imdb"": ""nm0186505"",
+                          ""tmdb"": 17419,
+                          ""tvrage"": 1797
+                        }
+                      }
+                    }
+                  ]
+                },
+                {
+                  ""cast"": [
+                    {
+                      ""character"": ""Joe Brody"",
+                      ""person"": {
+                        ""name"": ""Bryan Cranston"",
+                        ""ids"": {
+                          ""trakt"": 297737,
+                          ""slug"": ""bryan-cranston"",
+                          ""imdb"": ""nm0186505"",
+                          ""tmdb"": 17419,
+                          ""tvrage"": 1797
+                        }
+                      }
+                    }
+                  ]
+                }
+              ]";
+
+        private const string JSON_INCOMPLETE_1 =
+            @"[
+                {
+                  ""cast"": [
+                    {
+                      ""character"": ""Joe Brody"",
+                      ""person"": {
+                        ""name"": ""Bryan Cranston"",
+                        ""ids"": {
+                          ""trakt"": 297737,
+                          ""slug"": ""bryan-cranston"",
+                          ""imdb"": ""nm0186505"",
+                          ""tmdb"": 17419,
+                          ""tvrage"": 1797
+                        }
+                      }
+                    }
+                  ]
+                },
+                {
+                  ""cast"": [
+                    {
+                      ""character"": ""Joe Brody""
+                    }
+                  ]
+                }
+              ]";
+
+        private const string JSON_INCOMPLETE_2 =
+            @"[
+                {
+                  ""cast"": [
+                    {
+                      ""character"": ""Joe Brody""
+                    }
+                  ]
+                },
+                {
+                  ""cast"": [
+                    {
+                      ""character"": ""Joe Brody"",
+                      ""person"": {
+                        ""name"": ""Bryan Cranston"",
+                        ""ids"": {
+                          ""trakt"": 297737,
+                          ""slug"": ""bryan-cranston"",
+                          ""imdb"": ""nm0186505"",
+                          ""tmdb"": 17419,
+                          ""tvrage"": 1797
+                        }
+                      }
+                    }
+                  ]
+                }
+              ]";
+
+        private const string JSON_NOT_VALID_1 =
+            @"[
+                {
+                  ""ca"": [
+                    {
+                      ""character"": ""Joe Brody"",
+                      ""person"": {
+                        ""name"": ""Bryan Cranston"",
+                        ""ids"": {
+                          ""trakt"": 297737,
+                          ""slug"": ""bryan-cranston"",
+                          ""imdb"": ""nm0186505"",
+                          ""tmdb"": 17419,
+                          ""tvrage"": 1797
+                        }
+                      }
+                    }
+                  ]
+                },
+                {
+                  ""cast"": [
+                    {
+                      ""character"": ""Joe Brody"",
+                      ""person"": {
+                        ""name"": ""Bryan Cranston"",
+                        ""ids"": {
+                          ""trakt"": 297737,
+                          ""slug"": ""bryan-cranston"",
+                          ""imdb"": ""nm0186505"",
+                          ""tmdb"": 17419,
+                          ""tvrage"": 1797
+                        }
+                      }
+                    }
+                  ]
+                }
+              ]";
+
+        private const string JSON_NOT_VALID_2 =
+            @"[
+                {
+                  ""cast"": [
+                    {
+                      ""character"": ""Joe Brody"",
+                      ""person"": {
+                        ""name"": ""Bryan Cranston"",
+                        ""ids"": {
+                          ""trakt"": 297737,
+                          ""slug"": ""bryan-cranston"",
+                          ""imdb"": ""nm0186505"",
+                          ""tmdb"": 17419,
+                          ""tvrage"": 1797
+                        }
+                      }
+                    }
+                  ]
+                },
+                {
+                  ""ca"": [
+                    {
+                      ""character"": ""Joe Brody"",
+                      ""person"": {
+                        ""name"": ""Bryan Cranston"",
+                        ""ids"": {
+                          ""trakt"": 297737,
+                          ""slug"": ""bryan-cranston"",
+                          ""imdb"": ""nm0186505"",
+                          ""tmdb"": 17419,
+                          ""tvrage"": 1797
+                        }
+                      }
+                    }
+                  ]
+                }
+              ]";
+
+        private const string JSON_NOT_VALID_3 =
+            @"[
+                {
+                  ""ca"": [
+                    {
+                      ""character"": ""Joe Brody"",
+                      ""person"": {
+                        ""name"": ""Bryan Cranston"",
+                        ""ids"": {
+                          ""trakt"": 297737,
+                          ""slug"": ""bryan-cranston"",
+                          ""imdb"": ""nm0186505"",
+                          ""tmdb"": 17419,
+                          ""tvrage"": 1797
+                        }
+                      }
+                    }
+                  ]
+                },
+                {
+                  ""ca"": [
+                    {
+                      ""character"": ""Joe Brody"",
+                      ""person"": {
+                        ""name"": ""Bryan Cranston"",
+                        ""ids"": {
+                          ""trakt"": 297737,
+                          ""slug"": ""bryan-cranston"",
+                          ""imdb"": ""nm0186505"",
+                          ""tmdb"": 17419,
+                          ""tvrage"": 1797
+                        }
+                      }
+                    }
+                  ]
+                }
+              ]";
+    }
+}
