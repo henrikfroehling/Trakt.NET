6d6383f577150eecb49443e44125455a8a617539
Henrik Fröhling <henrik.froehling@gmail.com>
2018-10-21 14:39:36 +0200

    GH-1: Add json deserialization tests for basic objects (WIP)

---
+++ Source/Tests/Trakt.NET.Tests/Objects/Basic/Json/Reader/MetadataArrayJsonReader/MetadataArrayJsonReader_Tests_Json_Data.cs
@@ -0,0 +1,113 @@
+﻿namespace TraktNet.Tests.Objects.Basic.Json.Reader.MetadataArrayJsonReader
+{
+    public partial class MetadataArrayJsonReader_Tests
+    {
+        private const string JSON_EMPTY_ARRAY = "[]";
+
+        private const string JSON_COMPLETE =
+            @"[
+                {
+                  ""media_type"": ""digital"",
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                },
+                {
+                  ""media_type"": ""digital"",
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                }
+              ]";
+
+        private const string JSON_INCOMPLETE_1 =
+            @"[
+                {
+                  ""media_type"": ""digital"",
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                },
+                {
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                }
+              ]";
+
+        private const string JSON_INCOMPLETE_2 =
+            @"[
+                {
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                },
+                {
+                  ""media_type"": ""digital"",
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                }
+              ]";
+
+        private const string JSON_NOT_VALID_1 =
+            @"[
+                {
+                  ""mt"": ""digital"",
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                },
+                {
+                  ""media_type"": ""digital"",
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                }
+              ]";
+
+        private const string JSON_NOT_VALID_2 =
+            @"[
+                {
+                  ""media_type"": ""digital"",
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                },
+                {
+                  ""mediaaa_type"": ""digital"",
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                }
+              ]";
+
+        private const string JSON_NOT_VALID_3 =
+            @"[
+                {
+                  ""mt"": ""digital"",
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                },
+                {
+                  ""mediaaa_type"": ""digital"",
+                  ""resolution"": ""hd_720p"",
+                  ""audio"": ""aac"",
+                  ""audio_channels"": ""5.1"",
+                  ""3d"": true
+                }
+              ]";
+    }
+}
