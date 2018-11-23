9d15248316e3fe15badd59b13a06affdf66b2807
Henrik Fröhling <henrik.froehling@gmail.com>
2018-11-23 19:29:31 +0100

    GH-50: Implement missing json readers for search objects

---
+++ Source/Tests/Trakt.NET.Tests/Objects/Basic/Json/Reader/SearchResultArrayJsonReader/SearchResultArrayJsonReader_Movie_Stream_Tests.cs
@@ -0,0 +1,9 @@
+﻿namespace TraktNet.Tests.Objects.Basic.Json.Reader
+{
+    using Traits;
+
+    [Category("Objects.Basic.JsonReader")]
+    public partial class SearchResultArrayJsonReader_Tests
+    {
+    }
+}
---
+++ Source/Tests/Trakt.NET.Tests/Objects/Basic/Json/Reader/SearchResultArrayJsonReader/SearchResultArrayJsonReader_Reader_Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace TraktNet.Tests.Objects.Basic.Json.Reader
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Traits;
+    using TraktNet.Objects.Basic;
+    using TraktNet.Objects.Basic.Json.Reader;
+    using Xunit;
+
+    [Category("Objects.Basic.JsonReader")]
+    public partial class SearchResultArrayJsonReader_Tests
+    {
+        [Fact]
+        public async Task Test_SearchResultArrayJsonReader_ReadArray_From_JsonReader_Null()
+        {
+            var traktJsonReader = new SearchResultArrayJsonReader();
+            IEnumerable<ITraktSearchResult> traktSearchResults = await traktJsonReader.ReadArrayAsync(default(JsonTextReader));
+            traktSearchResults.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Test_SearchResultArrayJsonReader_ReadArray_From_JsonReader_Empty()
+        {
+            var traktJsonReader = new SearchResultArrayJsonReader();
+
+            using (var reader = new StringReader(string.Empty))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                IEnumerable<ITraktSearchResult> traktSearchResults = await traktJsonReader.ReadArrayAsync(jsonReader);
+                traktSearchResults.Should().BeNull();
+            }
+        }
+    }
+}
