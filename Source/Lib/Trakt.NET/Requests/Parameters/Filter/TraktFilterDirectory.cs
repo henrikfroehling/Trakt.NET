3598ee0644bd069e3aa24afd07e809fd72b4631b
Henrik Fröhling <henrik.froehling@gmail.com>
2019-03-08 17:12:52 +0100

    GH-74: Add filter directory class

---
+++ Source/Lib/Trakt.NET/Requests/Parameters/Filter/TraktFilterDirectory.cs
@@ -0,0 +1,13 @@
+﻿namespace TraktNet.Requests.Parameters.Filter
+{
+    public static class TraktFilterDirectory
+    {
+        public static TraktCalendarFilter CalendarFilter => new TraktCalendarFilter();
+
+        public static TraktMovieFilter MovieFilter => new TraktMovieFilter();
+
+        public static TraktShowFilter ShowFilter => new TraktShowFilter();
+
+        public static TraktSearchFilter SearchFilter => new TraktSearchFilter();
+    }
+}
