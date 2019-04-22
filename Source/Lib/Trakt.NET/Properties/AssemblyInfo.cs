1132e9223502e26917217736754069262aba09ca
Henrik Fröhling <henrik.froehling@gmail.com>
2019-04-22 23:26:47 +0200

    GH-107: Remove old test project

--- Source/Lib/Trakt.NET/Properties/AssemblyInfo.cs
+++ Source/Lib/Trakt.NET/Properties/AssemblyInfo.cs
@@ -2,7 +2,6 @@
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Trakt.NET.Tests")]
 [assembly: InternalsVisibleTo("Trakt.NET.Core.Tests")]
 [assembly: InternalsVisibleTo("Trakt.NET.Modules.Tests")]
 [assembly: InternalsVisibleTo("Trakt.NET.Objects.Tests")]
