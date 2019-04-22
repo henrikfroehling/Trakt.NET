3fdd261d841518d9a9f7ae297e2341ccd7d59f0a
Henrik Fröhling <henrik.froehling@gmail.com>
2019-04-22 22:59:26 +0200

    GH-107: Setup test utility project

--- Source/Tests/Trakt.NET.Tests.Utility/Properties/AssemblyInfo.cs
+++ Source/Tests/Trakt.NET.Tests.Utility/Properties/AssemblyInfo.cs
@@ -1,13 +1,11 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Trakt.NET.Tests")]
 [assembly: InternalsVisibleTo("Trakt.NET.Core.Tests")]
 [assembly: InternalsVisibleTo("Trakt.NET.Modules.Tests")]
 [assembly: InternalsVisibleTo("Trakt.NET.Objects.Tests")]
 [assembly: InternalsVisibleTo("Trakt.NET.Requests.Tests")]
-[assembly: InternalsVisibleTo("Trakt.NET.Tests.Utility")]
 
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
