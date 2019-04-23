43c14719e0c56382b4e7ad263d1c88ad4c32983e
Henrik Fröhling <henrik.froehling@gmail.com>
2019-04-23 23:51:41 +0200

    GH-77: Add interface and implementation for trakt language

---
+++ Source/Lib/Trakt.NET/Objects/Basic/Implementations/TraktLanguage.cs
@@ -0,0 +1,12 @@
+﻿namespace TraktNet.Objects.Basic.Implementations
+{
+    /// <summary>A Trakt language.</summary>
+    public class TraktLanguage : ITraktLanguage
+    {
+        /// <summary>Gets or sets the name of the language.</summary>
+        public string Name { get; set; }
+
+        /// <summary>Gets or sets the code of the language.</summary>
+        public string Code { get; set; }
+    }
+}
