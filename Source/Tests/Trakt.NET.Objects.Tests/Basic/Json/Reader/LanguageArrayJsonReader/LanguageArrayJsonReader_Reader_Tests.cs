b37d5811e0f10164bcbf3126047e48f12a00c446
Henrik Fröhling <henrik.froehling@gmail.com>
2019-04-24 01:46:46 +0200

    GH-77: Add tests for json io of ITraktLanguage

---
+++ Source/Tests/Trakt.NET.Objects.Tests/Basic/Json/Reader/LanguageArrayJsonReader/LanguageArrayJsonReader_Reader_Tests.cs
@@ -0,0 +1,189 @@
+﻿namespace TraktNet.Objects.Tests.Basic.Json.Reader
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Trakt.NET.Tests.Utility.Traits;
+    using TraktNet.Objects.Basic;
+    using TraktNet.Objects.Basic.Json.Reader;
+    using Xunit;
+
+    [Category("Objects.Basic.JsonReader")]
+    public partial class LanguageArrayJsonReader_Tests
+    {
+        [Fact]
+        public async Task Test_LanguageArrayJsonReader_ReadArray_From_JsonReader_Empty_Array()
+        {
+            var traktJsonReader = new LanguageArrayJsonReader();
+
+            using (var reader = new StringReader(JSON_EMPTY_ARRAY))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                IEnumerable<ITraktLanguage> traktLanguages = await traktJsonReader.ReadArrayAsync(jsonReader);
+                traktLanguages.Should().NotBeNull().And.BeEmpty();
+            }
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonReader_ReadArray_From_JsonReader_Complete()
+        {
+            var traktJsonReader = new LanguageArrayJsonReader();
+
+            using (var reader = new StringReader(JSON_COMPLETE))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                IEnumerable<ITraktLanguage> traktLanguages = await traktJsonReader.ReadArrayAsync(jsonReader);
+
+                traktLanguages.Should().NotBeNull();
+                ITraktLanguage[] languages = traktLanguages.ToArray();
+
+                languages[0].Should().NotBeNull();
+                languages[0].Name.Should().Be("English");
+                languages[0].Code.Should().Be("en");
+
+                languages[1].Should().NotBeNull();
+                languages[1].Name.Should().Be("English");
+                languages[1].Code.Should().Be("en");
+            }
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonReader_ReadArray_From_JsonReader_Incomplete_1()
+        {
+            var traktJsonReader = new LanguageArrayJsonReader();
+
+            using (var reader = new StringReader(JSON_INCOMPLETE_1))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                IEnumerable<ITraktLanguage> traktLanguages = await traktJsonReader.ReadArrayAsync(jsonReader);
+
+                traktLanguages.Should().NotBeNull();
+                ITraktLanguage[] languages = traktLanguages.ToArray();
+
+                languages[0].Should().NotBeNull();
+                languages[0].Name.Should().Be("English");
+                languages[0].Code.Should().Be("en");
+
+                languages[1].Should().NotBeNull();
+                languages[1].Name.Should().BeNull();
+                languages[1].Code.Should().Be("en");
+            }
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonReader_ReadArray_From_JsonReader_Incomplete_2()
+        {
+            var traktJsonReader = new LanguageArrayJsonReader();
+
+            using (var reader = new StringReader(JSON_INCOMPLETE_2))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                IEnumerable<ITraktLanguage> traktLanguages = await traktJsonReader.ReadArrayAsync(jsonReader);
+
+                traktLanguages.Should().NotBeNull();
+                ITraktLanguage[] languages = traktLanguages.ToArray();
+
+                languages[0].Should().NotBeNull();
+                languages[0].Name.Should().BeNull();
+                languages[0].Code.Should().Be("en");
+
+                languages[1].Should().NotBeNull();
+                languages[1].Name.Should().Be("English");
+                languages[1].Code.Should().Be("en");
+            }
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonReader_ReadArray_From_JsonReader_Not_Valid_1()
+        {
+            var traktJsonReader = new LanguageArrayJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_1))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                IEnumerable<ITraktLanguage> traktLanguages = await traktJsonReader.ReadArrayAsync(jsonReader);
+
+                traktLanguages.Should().NotBeNull();
+                ITraktLanguage[] languages = traktLanguages.ToArray();
+
+                languages[0].Should().NotBeNull();
+                languages[0].Name.Should().Be("English");
+                languages[0].Code.Should().Be("en");
+
+                languages[1].Should().NotBeNull();
+                languages[1].Name.Should().BeNull();
+                languages[1].Code.Should().Be("en");
+            }
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonReader_ReadArray_From_JsonReader_Not_Valid_2()
+        {
+            var traktJsonReader = new LanguageArrayJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_2))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                IEnumerable<ITraktLanguage> traktLanguages = await traktJsonReader.ReadArrayAsync(jsonReader);
+
+                traktLanguages.Should().NotBeNull();
+                ITraktLanguage[] languages = traktLanguages.ToArray();
+
+                languages[0].Should().NotBeNull();
+                languages[0].Name.Should().Be("English");
+                languages[0].Code.Should().BeNull();
+
+                languages[1].Should().NotBeNull();
+                languages[1].Name.Should().Be("English");
+                languages[1].Code.Should().Be("en");
+            }
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonReader_ReadArray_From_JsonReader_Not_Valid_3()
+        {
+            var traktJsonReader = new LanguageArrayJsonReader();
+
+            using (var reader = new StringReader(JSON_NOT_VALID_3))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                IEnumerable<ITraktLanguage> traktLanguages = await traktJsonReader.ReadArrayAsync(jsonReader);
+
+                traktLanguages.Should().NotBeNull();
+                ITraktLanguage[] languages = traktLanguages.ToArray();
+
+                languages[0].Should().NotBeNull();
+                languages[0].Name.Should().BeNull();
+                languages[0].Code.Should().Be("en");
+
+                languages[1].Should().NotBeNull();
+                languages[1].Name.Should().Be("English");
+                languages[1].Code.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonReader_ReadArray_From_JsonReader_Null()
+        {
+            var traktJsonReader = new LanguageArrayJsonReader();
+            IEnumerable<ITraktLanguage> traktLanguages = await traktJsonReader.ReadArrayAsync(default(JsonTextReader));
+            traktLanguages.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonReader_ReadArray_From_JsonReader_Empty()
+        {
+            var traktJsonReader = new LanguageArrayJsonReader();
+
+            using (var reader = new StringReader(string.Empty))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                IEnumerable<ITraktLanguage> traktLanguages = await traktJsonReader.ReadArrayAsync(jsonReader);
+                traktLanguages.Should().BeNull();
+            }
+        }
+    }
+}
---
+++ Source/Tests/Trakt.NET.Objects.Tests/Basic/Json/Writer/LanguageArrayJsonWriter/LanguageArrayJsonWriter_Array_Tests.cs
@@ -0,0 +1,78 @@
+﻿namespace TraktNet.Objects.Tests.Basic.Json.Writer
+{
+    using FluentAssertions;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Trakt.NET.Tests.Utility.Traits;
+    using TraktNet.Objects.Basic;
+    using TraktNet.Objects.Json;
+    using Xunit;
+
+    [Category("Objects.Basic.JsonWriter")]
+    public partial class LanguageArrayJsonWriter_Tests
+    {
+        [Fact]
+        public void Test_LanguageArrayJsonWriter_WriteArray_Array_Exceptions()
+        {
+            var traktJsonWriter = new ArrayJsonWriter<ITraktLanguage>();
+            Func<Task<string>> action = () => traktJsonWriter.WriteArrayAsync(default);
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonWriter_WriteArray_Array_Empty()
+        {
+            IEnumerable<ITraktLanguage> traktLanguages = new List<TraktLanguage>();
+            var traktJsonWriter = new ArrayJsonWriter<ITraktLanguage>();
+            string json = await traktJsonWriter.WriteArrayAsync(traktLanguages);
+            json.Should().Be("[]");
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonWriter_WriteArray_Array_SingleObject()
+        {
+            IEnumerable<ITraktLanguage> traktLanguages = new List<ITraktLanguage>
+            {
+                new TraktLanguage
+                {
+                    Name = "English",
+                    Code = "en"
+                }
+            };
+
+            var traktJsonWriter = new ArrayJsonWriter<ITraktLanguage>();
+            string json = await traktJsonWriter.WriteArrayAsync(traktLanguages);
+            json.Should().Be(@"[{""name"":""English"",""code"":""en""}]");
+        }
+
+        [Fact]
+        public async Task Test_LanguageArrayJsonWriter_WriteArray_Array_Complete()
+        {
+            IEnumerable<ITraktLanguage> traktLanguages = new List<ITraktLanguage>
+            {
+                new TraktLanguage
+                {
+                    Name = "English",
+                    Code = "en"
+                },
+                new TraktLanguage
+                {
+                    Name = "Italian",
+                    Code = "it"
+                },
+                new TraktLanguage
+                {
+                    Name = "Polish",
+                    Code = "pl"
+                }
+            };
+
+            var traktJsonWriter = new ArrayJsonWriter<ITraktLanguage>();
+            string json = await traktJsonWriter.WriteArrayAsync(traktLanguages);
+            json.Should().Be(@"[{""name"":""English"",""code"":""en""}," +
+                             @"{""name"":""Italian"",""code"":""it""}," +
+                             @"{""name"":""Polish"",""code"":""pl""}]");
+        }
+    }
+}
