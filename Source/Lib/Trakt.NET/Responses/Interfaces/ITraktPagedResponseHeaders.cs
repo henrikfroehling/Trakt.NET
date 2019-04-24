dc080ffc2d4665df621d2ee664d21bd6be1dee52
Henrik Fröhling <henrik.froehling@gmail.com>
2019-04-25 00:49:50 +0200

    GH-28: Add missing documentation

--- Source/Lib/Trakt.NET/Responses/Interfaces/ITraktPagedResponseHeaders.cs
+++ Source/Lib/Trakt.NET/Responses/Interfaces/ITraktPagedResponseHeaders.cs
@@ -2,8 +2,10 @@
 {
     public interface ITraktPagedResponseHeaders : ITraktResponseHeaders
     {
+        /// <summary>Gets the page count for this response.</summary>
         int? PageCount { get; set; }
 
+        /// <summary>Gets the item count per page for this response.</summary>
         int? ItemCount { get; set; }
     }
 }
--- Source/Lib/Trakt.NET/Services/TraktSerializationService.cs
+++ Source/Lib/Trakt.NET/Services/TraktSerializationService.cs
@@ -12,6 +12,13 @@
     /// <summary>Provides helper methods for serializing and deserializing Trakt objects.</summary>
     public static class TraktSerializationService
     {
+        /// <summary>Serializes a <see cref="ITraktAuthorization" /> instance to a JSON string.</summary>
+        /// <param name="authorization">The <see cref="ITraktAuthorization" /> instance, which will be serialized.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the serialization should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <returns>Returns a JSON string of the given <see cref="ITraktAuthorization" /> instance.</returns>
         public static Task<string> SerializeAsync(ITraktAuthorization authorization, CancellationToken cancellationToken = default)
         {
             if (authorization == null)
@@ -23,6 +30,13 @@
             return authorizationObjectJsonWriter.WriteObjectAsync(authorization, cancellationToken);
         }
 
+        /// <summary>Deserializes a JSON string to a <see cref="ITraktAuthorization" /> instance.</summary>
+        /// <param name="authorizationJson">The JSON string, which will be deserialized to a <see cref="ITraktAuthorization" /> instance.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the deserialization should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <returns>A deserialized <see cref="ITraktAuthorization" /> instance from a JSON string.</returns>
         public static Task<ITraktAuthorization> DeserializeAsync(string authorizationJson, CancellationToken cancellationToken = default)
         {
             if (authorizationJson == null)
@@ -37,6 +51,14 @@
             return authorizationObjectJsonReader.ReadObjectAsync(authorizationJson, cancellationToken);
         }
 
+        /// <summary>Serializes a <typeparamref name="TObjectType"/> instance to a JSON string.</summary>
+        /// <typeparam name="TObjectType">The type of the object, which will be serialized to a JSON string.</typeparam>
+        /// <param name="obj">The object instance, which will be serialized.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the serialization should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <returns>Returns a JSON string of the given object instance.</returns>
         public static Task<string> SerializeAsync<TObjectType>(TObjectType obj, CancellationToken cancellationToken = default)
         {
             if (EqualityComparer<TObjectType>.Default.Equals(obj, default))
@@ -46,6 +68,14 @@
             return objectJsonWriter.WriteObjectAsync(obj, cancellationToken);
         }
 
+        /// <summary>Deserializes a JSON string to a <typeparamref name="TObjectType"/> instance.</summary>
+        /// <typeparam name="TObjectType">The type of the object, which will be deserialized from a JSON string.</typeparam>
+        /// <param name="json">The JSON string, which will be deserialized to a object instance.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the deserialization should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <returns>A deserialized object instance from a JSON string.</returns>
         public static Task<TObjectType> DeserializeAsync<TObjectType>(string json, CancellationToken cancellationToken = default)
         {
             if (json == null)
@@ -58,6 +88,14 @@
             return objectJsonReader.ReadObjectAsync(json, cancellationToken);
         }
 
+        /// <summary>Serializes a collection of <typeparamref name="TObjectType"/> instances to a JSON string.</summary>
+        /// <typeparam name="TObjectType">The type of the objects, which will be serialized to a JSON string.</typeparam>
+        /// <param name="objects">The objects, which will be serialized.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the serialization should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <returns>Returns a JSON string of the given objects collection.</returns>
         public static Task<string> SerializeCollectionAsync<TObjectType>(IEnumerable<TObjectType> objects, CancellationToken cancellationToken = default)
         {
             if (objects == null)
@@ -67,6 +105,14 @@
             return arrayJsonWriter.WriteArrayAsync(objects, cancellationToken);
         }
 
+        /// <summary>Deserializes a JSON string to a collection of <typeparamref name="TObjectType"/> instances.</summary>
+        /// <typeparam name="TObjectType">The type of the objects, which will be deserialized from a JSON string.</typeparam>
+        /// <param name="json">The JSON string, which will be deserialized to a object collection.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the deserialization should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <returns>A deserialized object collection from a JSON string.</returns>
         public static Task<IEnumerable<TObjectType>> DeserializeCollectionAsync<TObjectType>(string json, CancellationToken cancellationToken = default)
         {
             if (json == null)
