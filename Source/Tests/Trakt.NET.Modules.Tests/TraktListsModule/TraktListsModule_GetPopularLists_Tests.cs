84aad5cd5325236c94d3472ff8bfd19da5e6cc73
Henrik Fröhling <henrik.froehling@gmail.com>
2019-09-15 17:48:20 +0200

    Use theory tests to reduce exception test methods in lists module

--- Source/Tests/Trakt.NET.Modules.Tests/TraktListsModule/TraktListsModule_GetPopularLists_Tests.cs
+++ Source/Tests/Trakt.NET.Modules.Tests/TraktListsModule/TraktListsModule_GetPopularLists_Tests.cs
@@ -91,132 +91,36 @@
             response.PageCount.Should().HaveValue().And.Be(1);
         }
 
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_NotFoundException()
+        [Theory]
+        [InlineData(HttpStatusCode.NotFound, typeof(TraktNotFoundException))]
+        [InlineData(HttpStatusCode.Unauthorized, typeof(TraktAuthorizationException))]
+        [InlineData(HttpStatusCode.BadRequest, typeof(TraktBadRequestException))]
+        [InlineData(HttpStatusCode.Forbidden, typeof(TraktForbiddenException))]
+        [InlineData(HttpStatusCode.MethodNotAllowed, typeof(TraktMethodNotFoundException))]
+        [InlineData(HttpStatusCode.Conflict, typeof(TraktConflictException))]
+        [InlineData(HttpStatusCode.InternalServerError, typeof(TraktServerException))]
+        [InlineData(HttpStatusCode.BadGateway, typeof(TraktBadGatewayException))]
+        [InlineData(HttpStatusCode.PreconditionFailed, typeof(TraktPreconditionFailedException))]
+        [InlineData(HttpStatusCode.UnprocessableEntity, typeof(TraktValidationException))]
+        [InlineData(HttpStatusCode.TooManyRequests, typeof(TraktRateLimitException))]
+        [InlineData(HttpStatusCode.ServiceUnavailable, typeof(TraktServerUnavailableException))]
+        [InlineData(HttpStatusCode.GatewayTimeout, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)520, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)521, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)522, typeof(TraktServerUnavailableException))]
+        public async Task Test_TraktListsModule_GetPopularLists_Throws_API_Exception(HttpStatusCode statusCode, Type exceptionType)
         {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, HttpStatusCode.NotFound);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktNotFoundException>();
-        }
+            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, statusCode);
 
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_AuthorizationException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, HttpStatusCode.Unauthorized);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktAuthorizationException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_BadRequestException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, HttpStatusCode.BadRequest);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktBadRequestException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_ForbiddenException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, HttpStatusCode.Forbidden);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktForbiddenException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_MethodNotFoundException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, HttpStatusCode.MethodNotAllowed);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktMethodNotFoundException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_ConflictException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, HttpStatusCode.Conflict);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktConflictException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_ServerException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, HttpStatusCode.InternalServerError);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktServerException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_BadGatewayException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, HttpStatusCode.BadGateway);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktBadGatewayException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_PreconditionFailedException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, (HttpStatusCode)412);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktPreconditionFailedException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_ValidationException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, (HttpStatusCode)422);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktValidationException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_RateLimitException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, (HttpStatusCode)429);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktRateLimitException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_ServerUnavailableException_503()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, (HttpStatusCode)503);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_ServerUnavailableException_504()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, (HttpStatusCode)504);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_ServerUnavailableException_520()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, (HttpStatusCode)520);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_ServerUnavailableException_521()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, (HttpStatusCode)521);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktListsModule_GetPopularLists_Throws_ServerUnavailableException_522()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_POPULAR_LISTS_URI, (HttpStatusCode)522);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.Lists.GetPopularListsAsync();
-            act.Should().Throw<TraktServerUnavailableException>();
+            try
+            {
+                await client.Lists.GetPopularListsAsync();
+                Assert.False(true);
+            }
+            catch (Exception exception)
+            {
+                (exception.GetType() == exceptionType).Should().BeTrue();
+            }
         }
     }
 }
