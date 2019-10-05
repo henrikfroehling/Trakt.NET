c992f0a4009f53ddf53f1a400539637c309e1a6f
Henrik Fröhling <henrik.froehling@gmail.com>
2019-10-05 21:38:04 +0200

    Use theory tests to reduce exception test methods in movies module

--- Source/Tests/Trakt.NET.Modules.Tests/TraktMoviesModule/TraktMoviesModule_GetMovieStatistics_Tests.cs
+++ Source/Tests/Trakt.NET.Modules.Tests/TraktMoviesModule/TraktMoviesModule_GetMovieStatistics_Tests.cs
@@ -37,132 +37,36 @@
             responseValue.Votes.Should().Be(10359);
         }
 
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_NotFoundException()
+        [Theory]
+        [InlineData(HttpStatusCode.NotFound, typeof(TraktMovieNotFoundException))]
+        [InlineData(HttpStatusCode.Unauthorized, typeof(TraktAuthorizationException))]
+        [InlineData(HttpStatusCode.BadRequest, typeof(TraktBadRequestException))]
+        [InlineData(HttpStatusCode.Forbidden, typeof(TraktForbiddenException))]
+        [InlineData(HttpStatusCode.MethodNotAllowed, typeof(TraktMethodNotFoundException))]
+        [InlineData(HttpStatusCode.Conflict, typeof(TraktConflictException))]
+        [InlineData(HttpStatusCode.InternalServerError, typeof(TraktServerException))]
+        [InlineData(HttpStatusCode.BadGateway, typeof(TraktBadGatewayException))]
+        [InlineData(HttpStatusCode.PreconditionFailed, typeof(TraktPreconditionFailedException))]
+        [InlineData(HttpStatusCode.UnprocessableEntity, typeof(TraktValidationException))]
+        [InlineData(HttpStatusCode.TooManyRequests, typeof(TraktRateLimitException))]
+        [InlineData(HttpStatusCode.ServiceUnavailable, typeof(TraktServerUnavailableException))]
+        [InlineData(HttpStatusCode.GatewayTimeout, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)520, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)521, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)522, typeof(TraktServerUnavailableException))]
+        public async Task Test_TraktMoviesModule_GetMovieStatistics_Throws_API_Exception(HttpStatusCode statusCode, Type exceptionType)
         {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, HttpStatusCode.NotFound);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktMovieNotFoundException>();
-        }
+            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, statusCode);
 
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_AuthorizationException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, HttpStatusCode.Unauthorized);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktAuthorizationException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_BadRequestException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, HttpStatusCode.BadRequest);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktBadRequestException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_ForbiddenException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, HttpStatusCode.Forbidden);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktForbiddenException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_MethodNotFoundException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, HttpStatusCode.MethodNotAllowed);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktMethodNotFoundException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_ConflictException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, HttpStatusCode.Conflict);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktConflictException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_ServerException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, HttpStatusCode.InternalServerError);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktServerException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_BadGatewayException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, HttpStatusCode.BadGateway);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktBadGatewayException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_PreconditionFailedException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, (HttpStatusCode)412);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktPreconditionFailedException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_ValidationException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, (HttpStatusCode)422);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktValidationException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_RateLimitException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, (HttpStatusCode)429);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktRateLimitException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_ServerUnavailableException_503()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, (HttpStatusCode)503);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_ServerUnavailableException_504()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, (HttpStatusCode)504);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_ServerUnavailableException_520()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, (HttpStatusCode)520);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_ServerUnavailableException_521()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, (HttpStatusCode)521);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktMoviesModule_GetMovieStatistics_Throws_ServerUnavailableException_522()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_MOVIE_STATISTICS_URI, (HttpStatusCode)522);
-            Func<Task<TraktResponse<ITraktStatistics>>> act = () => client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
+            try
+            {
+                await client.Movies.GetMovieStatisticsAsync(MOVIE_ID);
+                Assert.False(true);
+            }
+            catch (Exception exception)
+            {
+                (exception.GetType() == exceptionType).Should().BeTrue();
+            }
         }
 
         [Fact]
