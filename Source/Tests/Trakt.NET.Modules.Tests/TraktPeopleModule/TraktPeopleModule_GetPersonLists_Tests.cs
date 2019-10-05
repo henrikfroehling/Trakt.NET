94db9e882120c1f79b3178d6d805644befd34018
Henrik Fröhling <henrik.froehling@gmail.com>
2019-10-05 21:49:29 +0200

    Use theory tests to reduce exception test methods in people module

--- Source/Tests/Trakt.NET.Modules.Tests/TraktPeopleModule/TraktPeopleModule_GetPersonLists_Tests.cs
+++ Source/Tests/Trakt.NET.Modules.Tests/TraktPeopleModule/TraktPeopleModule_GetPersonLists_Tests.cs
@@ -260,132 +260,36 @@
             response.PageCount.Should().HaveValue().And.Be(1);
         }
 
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_NotFoundException()
+        [Theory]
+        [InlineData(HttpStatusCode.NotFound, typeof(TraktPersonNotFoundException))]
+        [InlineData(HttpStatusCode.Unauthorized, typeof(TraktAuthorizationException))]
+        [InlineData(HttpStatusCode.BadRequest, typeof(TraktBadRequestException))]
+        [InlineData(HttpStatusCode.Forbidden, typeof(TraktForbiddenException))]
+        [InlineData(HttpStatusCode.MethodNotAllowed, typeof(TraktMethodNotFoundException))]
+        [InlineData(HttpStatusCode.Conflict, typeof(TraktConflictException))]
+        [InlineData(HttpStatusCode.InternalServerError, typeof(TraktServerException))]
+        [InlineData(HttpStatusCode.BadGateway, typeof(TraktBadGatewayException))]
+        [InlineData(HttpStatusCode.PreconditionFailed, typeof(TraktPreconditionFailedException))]
+        [InlineData(HttpStatusCode.UnprocessableEntity, typeof(TraktValidationException))]
+        [InlineData(HttpStatusCode.TooManyRequests, typeof(TraktRateLimitException))]
+        [InlineData(HttpStatusCode.ServiceUnavailable, typeof(TraktServerUnavailableException))]
+        [InlineData(HttpStatusCode.GatewayTimeout, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)520, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)521, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)522, typeof(TraktServerUnavailableException))]
+        public async Task Test_TraktPeopleModule_GetPersonLists_Throws_API_Exception(HttpStatusCode statusCode, Type exceptionType)
         {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, HttpStatusCode.NotFound);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktPersonNotFoundException>();
-        }
+            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, statusCode);
 
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_AuthorizationException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, HttpStatusCode.Unauthorized);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktAuthorizationException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_BadRequestException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, HttpStatusCode.BadRequest);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktBadRequestException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_ForbiddenException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, HttpStatusCode.Forbidden);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktForbiddenException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_MethodNotFoundException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, HttpStatusCode.MethodNotAllowed);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktMethodNotFoundException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_ConflictException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, HttpStatusCode.Conflict);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktConflictException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_ServerException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, HttpStatusCode.InternalServerError);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_BadGatewayException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, HttpStatusCode.BadGateway);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktBadGatewayException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_PreconditionFailedException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, (HttpStatusCode)412);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktPreconditionFailedException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_ValidationException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, (HttpStatusCode)422);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktValidationException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_RateLimitException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, (HttpStatusCode)429);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktRateLimitException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_ServerUnavailableException_503()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, (HttpStatusCode)503);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_ServerUnavailableException_504()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, (HttpStatusCode)504);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_ServerUnavailableException_520()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, (HttpStatusCode)520);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_ServerUnavailableException_521()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, (HttpStatusCode)521);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonLists_Throws_ServerUnavailableException_522()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_LISTS_URI, (HttpStatusCode)522);
-            Func<Task<TraktPagedResponse<ITraktList>>> act = () => client.People.GetPersonListsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
+            try
+            {
+                await client.People.GetPersonListsAsync(PERSON_ID);
+                Assert.False(true);
+            }
+            catch (Exception exception)
+            {
+                (exception.GetType() == exceptionType).Should().BeTrue();
+            }
         }
 
         [Fact]
--- Source/Tests/Trakt.NET.Modules.Tests/TraktPeopleModule/TraktPeopleModule_GetPersonMovieCredits_Tests.cs
+++ Source/Tests/Trakt.NET.Modules.Tests/TraktPeopleModule/TraktPeopleModule_GetPersonMovieCredits_Tests.cs
@@ -193,132 +193,36 @@
             writing[0].Movie.Ids.Tmdb.Should().Be(124905U);
         }
 
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_NotFoundException()
+        [Theory]
+        [InlineData(HttpStatusCode.NotFound, typeof(TraktPersonNotFoundException))]
+        [InlineData(HttpStatusCode.Unauthorized, typeof(TraktAuthorizationException))]
+        [InlineData(HttpStatusCode.BadRequest, typeof(TraktBadRequestException))]
+        [InlineData(HttpStatusCode.Forbidden, typeof(TraktForbiddenException))]
+        [InlineData(HttpStatusCode.MethodNotAllowed, typeof(TraktMethodNotFoundException))]
+        [InlineData(HttpStatusCode.Conflict, typeof(TraktConflictException))]
+        [InlineData(HttpStatusCode.InternalServerError, typeof(TraktServerException))]
+        [InlineData(HttpStatusCode.BadGateway, typeof(TraktBadGatewayException))]
+        [InlineData(HttpStatusCode.PreconditionFailed, typeof(TraktPreconditionFailedException))]
+        [InlineData(HttpStatusCode.UnprocessableEntity, typeof(TraktValidationException))]
+        [InlineData(HttpStatusCode.TooManyRequests, typeof(TraktRateLimitException))]
+        [InlineData(HttpStatusCode.ServiceUnavailable, typeof(TraktServerUnavailableException))]
+        [InlineData(HttpStatusCode.GatewayTimeout, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)520, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)521, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)522, typeof(TraktServerUnavailableException))]
+        public async Task Test_TraktPeopleModule_GetPersonMovieCredits_Throws_API_Exception(HttpStatusCode statusCode, Type exceptionType)
         {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, HttpStatusCode.NotFound);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktPersonNotFoundException>();
-        }
+            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, statusCode);
 
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_AuthorizationException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, HttpStatusCode.Unauthorized);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktAuthorizationException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_BadRequestException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, HttpStatusCode.BadRequest);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktBadRequestException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_ForbiddenException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, HttpStatusCode.Forbidden);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktForbiddenException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_MethodNotFoundException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, HttpStatusCode.MethodNotAllowed);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktMethodNotFoundException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_ConflictException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, HttpStatusCode.Conflict);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktConflictException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_ServerException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, HttpStatusCode.InternalServerError);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_BadGatewayException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, HttpStatusCode.BadGateway);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktBadGatewayException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_PreconditionFailedException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, (HttpStatusCode)412);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktPreconditionFailedException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_ValidationException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, (HttpStatusCode)422);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktValidationException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_RateLimitException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, (HttpStatusCode)429);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktRateLimitException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_ServerUnavailableException_503()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, (HttpStatusCode)503);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_ServerUnavailableException_504()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, (HttpStatusCode)504);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_ServerUnavailableException_520()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, (HttpStatusCode)520);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_ServerUnavailableException_521()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, (HttpStatusCode)521);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktPeopleModule_GetPersonMovieCredits_Throws_ServerUnavailableException_522()
-        {
-            TraktClient client = TestUtility.GetMockClient(GET_PERSON_MOVIE_CREDITS_URI, (HttpStatusCode)522);
-            Func<Task<TraktResponse<ITraktPersonMovieCredits>>> act = () => client.People.GetPersonMovieCreditsAsync(PERSON_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
+            try
+            {
+                await client.People.GetPersonMovieCreditsAsync(PERSON_ID);
+                Assert.False(true);
+            }
+            catch (Exception exception)
+            {
+                (exception.GetType() == exceptionType).Should().BeTrue();
+            }
         }
 
         [Fact]
