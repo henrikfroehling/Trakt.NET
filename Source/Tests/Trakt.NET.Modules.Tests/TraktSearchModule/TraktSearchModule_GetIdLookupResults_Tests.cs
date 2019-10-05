3ebcddd7033714b7bf530049412ce43595bba0fd
Henrik Fröhling <henrik.froehling@gmail.com>
2019-10-05 22:36:37 +0200

    Use theory tests to reduce exception test methods in search module

--- Source/Tests/Trakt.NET.Modules.Tests/TraktSearchModule/TraktSearchModule_GetIdLookupResults_Tests.cs
+++ Source/Tests/Trakt.NET.Modules.Tests/TraktSearchModule/TraktSearchModule_GetIdLookupResults_Tests.cs
@@ -365,132 +365,36 @@
             response.PageCount.Should().HaveValue().And.Be(1);
         }
 
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_NotFoundException()
+        [Theory]
+        [InlineData(HttpStatusCode.NotFound, typeof(TraktNotFoundException))]
+        [InlineData(HttpStatusCode.Unauthorized, typeof(TraktAuthorizationException))]
+        [InlineData(HttpStatusCode.BadRequest, typeof(TraktBadRequestException))]
+        [InlineData(HttpStatusCode.Forbidden, typeof(TraktForbiddenException))]
+        [InlineData(HttpStatusCode.MethodNotAllowed, typeof(TraktMethodNotFoundException))]
+        [InlineData(HttpStatusCode.Conflict, typeof(TraktConflictException))]
+        [InlineData(HttpStatusCode.InternalServerError, typeof(TraktServerException))]
+        [InlineData(HttpStatusCode.BadGateway, typeof(TraktBadGatewayException))]
+        [InlineData(HttpStatusCode.PreconditionFailed, typeof(TraktPreconditionFailedException))]
+        [InlineData(HttpStatusCode.UnprocessableEntity, typeof(TraktValidationException))]
+        [InlineData(HttpStatusCode.TooManyRequests, typeof(TraktRateLimitException))]
+        [InlineData(HttpStatusCode.ServiceUnavailable, typeof(TraktServerUnavailableException))]
+        [InlineData(HttpStatusCode.GatewayTimeout, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)520, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)521, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)522, typeof(TraktServerUnavailableException))]
+        public async Task Test_TraktSearchModule_GetIdLookupResult_Throws_API_Exception(HttpStatusCode statusCode, Type exceptionType)
         {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, HttpStatusCode.NotFound);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktNotFoundException>();
-        }
+            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, statusCode);
 
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_AuthorizationException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, HttpStatusCode.Unauthorized);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktAuthorizationException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_BadRequestException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, HttpStatusCode.BadRequest);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktBadRequestException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_ForbiddenException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, HttpStatusCode.Forbidden);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktForbiddenException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_MethodNotFoundException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, HttpStatusCode.MethodNotAllowed);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktMethodNotFoundException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_ConflictException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, HttpStatusCode.Conflict);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktConflictException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_ServerException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, HttpStatusCode.InternalServerError);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktServerException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_BadGatewayException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, HttpStatusCode.BadGateway);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktBadGatewayException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_PreconditionFailedException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, (HttpStatusCode)412);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktPreconditionFailedException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_ValidationException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, (HttpStatusCode)422);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktValidationException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_RateLimitException()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, (HttpStatusCode)429);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktRateLimitException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_ServerUnavailableException_503()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, (HttpStatusCode)503);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_ServerUnavailableException_504()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, (HttpStatusCode)504);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_ServerUnavailableException_520()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, (HttpStatusCode)520);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_ServerUnavailableException_521()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, (HttpStatusCode)521);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktSearchModule_GetIdLookupResults_Throws_ServerUnavailableException_522()
-        {
-            TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, (HttpStatusCode)522);
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
-            act.Should().Throw<TraktServerUnavailableException>();
+            try
+            {
+                await client.Search.GetIdLookupResultsAsync(ID_LOOKUP_TYPE, LOOKUP_ID);
+                Assert.False(true);
+            }
+            catch (Exception exception)
+            {
+                (exception.GetType() == exceptionType).Should().BeTrue();
+            }
         }
 
         [Fact]
@@ -499,7 +403,7 @@
             TraktClient client = TestUtility.GetMockClient(GetIdLookupUri, SEARCH_ID_LOOKUP_RESULTS_JSON,
                                                            1, 10, 1, ID_LOOKUP_ITEM_COUNT);
 
-            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(default(TraktSearchIdType), LOOKUP_ID);
+            Func<Task<TraktPagedResponse<ITraktSearchResult>>> act = () => client.Search.GetIdLookupResultsAsync(default, LOOKUP_ID);
             act.Should().Throw<ArgumentNullException>();
 
             act = () => client.Search.GetIdLookupResultsAsync(TraktSearchIdType.Unspecified, LOOKUP_ID);
