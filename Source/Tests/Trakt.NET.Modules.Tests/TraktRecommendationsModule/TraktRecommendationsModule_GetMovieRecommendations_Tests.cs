9b9f16d5d62435cbaafa25300e1f72ba71426fff
Henrik Fröhling <henrik.froehling@gmail.com>
2019-10-05 22:05:10 +0200

    Use theory tests to reduce exception test methods in recommendations module

--- Source/Tests/Trakt.NET.Modules.Tests/TraktRecommendationsModule/TraktRecommendationsModule_GetMovieRecommendations_Tests.cs
+++ Source/Tests/Trakt.NET.Modules.Tests/TraktRecommendationsModule/TraktRecommendationsModule_GetMovieRecommendations_Tests.cs
@@ -80,132 +80,36 @@
             response.Limit.Should().Be(LIMIT);
         }
 
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_NotFoundException()
+        [Theory]
+        [InlineData(HttpStatusCode.NotFound, typeof(TraktNotFoundException))]
+        [InlineData(HttpStatusCode.Unauthorized, typeof(TraktAuthorizationException))]
+        [InlineData(HttpStatusCode.BadRequest, typeof(TraktBadRequestException))]
+        [InlineData(HttpStatusCode.Forbidden, typeof(TraktForbiddenException))]
+        [InlineData(HttpStatusCode.MethodNotAllowed, typeof(TraktMethodNotFoundException))]
+        [InlineData(HttpStatusCode.Conflict, typeof(TraktConflictException))]
+        [InlineData(HttpStatusCode.InternalServerError, typeof(TraktServerException))]
+        [InlineData(HttpStatusCode.BadGateway, typeof(TraktBadGatewayException))]
+        [InlineData(HttpStatusCode.PreconditionFailed, typeof(TraktPreconditionFailedException))]
+        [InlineData(HttpStatusCode.UnprocessableEntity, typeof(TraktValidationException))]
+        [InlineData(HttpStatusCode.TooManyRequests, typeof(TraktRateLimitException))]
+        [InlineData(HttpStatusCode.ServiceUnavailable, typeof(TraktServerUnavailableException))]
+        [InlineData(HttpStatusCode.GatewayTimeout, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)520, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)521, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)522, typeof(TraktServerUnavailableException))]
+        public async Task Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_API_Exception(HttpStatusCode statusCode, Type exceptionType)
         {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, HttpStatusCode.NotFound);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktNotFoundException>();
-        }
+            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, statusCode);
 
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_AuthorizationException()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, HttpStatusCode.Unauthorized);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktAuthorizationException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_BadRequestException()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, HttpStatusCode.BadRequest);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktBadRequestException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_ForbiddenException()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, HttpStatusCode.Forbidden);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktForbiddenException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_MethodNotFoundException()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, HttpStatusCode.MethodNotAllowed);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktMethodNotFoundException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_ConflictException()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, HttpStatusCode.Conflict);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktConflictException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_ServerException()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, HttpStatusCode.InternalServerError);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktServerException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_BadGatewayException()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, HttpStatusCode.BadGateway);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktBadGatewayException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_PreconditionFailedException()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, (HttpStatusCode)412);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktPreconditionFailedException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_ValidationException()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, (HttpStatusCode)422);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktValidationException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_RateLimitException()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, (HttpStatusCode)429);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktRateLimitException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_ServerUnavailableException_503()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, (HttpStatusCode)503);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_ServerUnavailableException_504()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, (HttpStatusCode)504);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_ServerUnavailableException_520()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, (HttpStatusCode)520);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_ServerUnavailableException_521()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, (HttpStatusCode)521);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktServerUnavailableException>();
-        }
-
-        [Fact]
-        public void Test_TraktRecommendationsModule_GetMovieRecommendations_Throws_ServerUnavailableException_522()
-        {
-            TraktClient client = TestUtility.GetOAuthMockClient(GET_MOVIE_RECOMMENDATIONS_URI, (HttpStatusCode)522);
-            Func<Task<TraktPagedResponse<ITraktMovie>>> act = () => client.Recommendations.GetMovieRecommendationsAsync();
-            act.Should().Throw<TraktServerUnavailableException>();
+            try
+            {
+                await client.Recommendations.GetMovieRecommendationsAsync();
+                Assert.False(true);
+            }
+            catch (Exception exception)
+            {
+                (exception.GetType() == exceptionType).Should().BeTrue();
+            }
         }
     }
 }
