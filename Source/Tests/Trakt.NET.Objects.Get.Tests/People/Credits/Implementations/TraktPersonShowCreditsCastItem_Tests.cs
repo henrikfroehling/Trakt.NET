d0708164fb1471ee979254fd5e145b531cd594ba
Henrik Fröhling <henrik.froehling@gmail.com>
2020-03-05 22:14:27 +0100

    GH-125: Change tests for person show credits objects

--- Source/Tests/Trakt.NET.Objects.Get.Tests/People/Credits/Implementations/TraktPersonShowCreditsCastItem_Tests.cs
+++ Source/Tests/Trakt.NET.Objects.Get.Tests/People/Credits/Implementations/TraktPersonShowCreditsCastItem_Tests.cs
@@ -1,10 +1,8 @@
 ﻿namespace TraktNet.Objects.Get.Tests.People.Credits.Implementations
 {
     using FluentAssertions;
-    using System;
     using System.Threading.Tasks;
     using Trakt.NET.Tests.Utility.Traits;
-    using TraktNet.Enums;
     using TraktNet.Objects.Get.People.Credits;
     using TraktNet.Objects.Get.People.Credits.Json.Reader;
     using Xunit;
@@ -18,6 +16,7 @@
             var creditsCastItem = new TraktPersonShowCreditsCastItem();
 
             creditsCastItem.Character.Should().BeNullOrEmpty();
+            creditsCastItem.Characters.Should().BeNullOrEmpty();
             creditsCastItem.Show.Should().BeNull();
         }
 
@@ -29,6 +28,7 @@
 
             creditsCastItem.Should().NotBeNull();
             creditsCastItem.Character.Should().Be("Joe Brody");
+            creditsCastItem.Characters.Should().NotBeNull().And.HaveCount(1).And.Contain("Joe Brody");
             creditsCastItem.Show.Should().NotBeNull();
             creditsCastItem.Show.Title.Should().Be("Game of Thrones");
             creditsCastItem.Show.Year.Should().Be(2011);
@@ -59,50 +59,12 @@
             creditsCastItem.Show.AiredEpisodes.Should().NotHaveValue();
         }
 
-        [Fact]
-        public async Task Test_TraktPersonShowCreditsCastItem_From_Full_Json()
-        {
-            var jsonReader = new PersonShowCreditsCastItemObjectJsonReader();
-            var creditsCastItem = await jsonReader.ReadObjectAsync(FULL_JSON) as TraktPersonShowCreditsCastItem;
-
-            creditsCastItem.Should().NotBeNull();
-            creditsCastItem.Character.Should().Be("Joe Brody");
-            creditsCastItem.Show.Should().NotBeNull();
-            creditsCastItem.Show.Title.Should().Be("Game of Thrones");
-            creditsCastItem.Show.Year.Should().Be(2011);
-            creditsCastItem.Show.Airs.Should().NotBeNull();
-            creditsCastItem.Show.Airs.Day.Should().Be("Sunday");
-            creditsCastItem.Show.Airs.Time.Should().Be("21:00");
-            creditsCastItem.Show.Airs.TimeZoneId.Should().Be("America/New_York");
-            creditsCastItem.Show.AvailableTranslationLanguageCodes.Should().NotBeNull().And.HaveCount(4).And.Contain("en", "fr", "it", "de");
-            creditsCastItem.Show.Ids.Should().NotBeNull();
-            creditsCastItem.Show.Ids.Trakt.Should().Be(1390U);
-            creditsCastItem.Show.Ids.Slug.Should().Be("game-of-thrones");
-            creditsCastItem.Show.Ids.Tvdb.Should().Be(121361U);
-            creditsCastItem.Show.Ids.Imdb.Should().Be("tt0944947");
-            creditsCastItem.Show.Ids.Tmdb.Should().Be(1399U);
-            creditsCastItem.Show.Ids.TvRage.Should().Be(24493U);
-            creditsCastItem.Show.Genres.Should().NotBeNull().And.HaveCount(5).And.Contain("drama", "fantasy", "science-fiction", "action", "adventure");
-            creditsCastItem.Show.Seasons.Should().BeNull();
-            creditsCastItem.Show.Overview.Should().Be("Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and the icy horrors beyond.");
-            creditsCastItem.Show.FirstAired.Should().Be(DateTime.Parse("2011-04-17T07:00:00Z").ToUniversalTime());
-            creditsCastItem.Show.Runtime.Should().Be(60);
-            creditsCastItem.Show.Certification.Should().Be("TV-MA");
-            creditsCastItem.Show.Network.Should().Be("HBO");
-            creditsCastItem.Show.CountryCode.Should().Be("us");
-            creditsCastItem.Show.UpdatedAt.Should().Be(DateTime.Parse("2016-04-06T10:39:11Z").ToUniversalTime());
-            creditsCastItem.Show.Trailer.Should().Be("http://youtube.com/watch?v=F9Bo89m2f6g");
-            creditsCastItem.Show.Homepage.Should().Be("http://www.hbo.com/game-of-thrones");
-            creditsCastItem.Show.Status.Should().Be(TraktShowStatus.ReturningSeries);
-            creditsCastItem.Show.Rating.Should().Be(9.38327f);
-            creditsCastItem.Show.Votes.Should().Be(44773);
-            creditsCastItem.Show.LanguageCode.Should().Be("en");
-            creditsCastItem.Show.AiredEpisodes.Should().Be(50);
-        }
-
         private const string MINIMAL_JSON =
             @"{
                 ""character"": ""Joe Brody"",
+                ""characters"": [
+                  ""Joe Brody""
+                ],
                 ""show"": {
                   ""title"": ""Game of Thrones"",
                   ""year"": 2011,
@@ -114,55 +76,6 @@
                     ""tmdb"": 1399,
                     ""tvrage"": 24493
                   }
-                }
-              }";
-
-        private const string FULL_JSON =
-            @"{
-                ""character"": ""Joe Brody"",
-                ""show"": {
-                  ""title"": ""Game of Thrones"",
-                  ""year"": 2011,
-                  ""ids"": {
-                    ""trakt"": 1390,
-                    ""slug"": ""game-of-thrones"",
-                    ""tvdb"": 121361,
-                    ""imdb"": ""tt0944947"",
-                    ""tmdb"": 1399,
-                    ""tvrage"": 24493
-                  },
-                  ""overview"": ""Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and the icy horrors beyond."",
-                  ""first_aired"": ""2011-04-17T07:00:00Z"",
-                  ""airs"": {
-                    ""day"": ""Sunday"",
-                    ""time"": ""21:00"",
-                    ""timezone"": ""America/New_York""
-                  },
-                  ""runtime"": 60,
-                  ""certification"": ""TV-MA"",
-                  ""network"": ""HBO"",
-                  ""country"": ""us"",
-                  ""trailer"": ""http://youtube.com/watch?v=F9Bo89m2f6g"",
-                  ""homepage"": ""http://www.hbo.com/game-of-thrones"",
-                  ""status"": ""returning series"",
-                  ""rating"": 9.38327,
-                  ""votes"": 44773,
-                  ""updated_at"": ""2016-04-06T10:39:11Z"",
-                  ""language"": ""en"",
-                  ""available_translations"": [
-                    ""en"",
-                    ""fr"",
-                    ""it"",
-                    ""de""
-                  ],
-                  ""genres"": [
-                    ""drama"",
-                    ""fantasy"",
-                    ""science-fiction"",
-                    ""action"",
-                    ""adventure""
-                  ],
-                  ""aired_episodes"": 50
                 }
               }";
     }
