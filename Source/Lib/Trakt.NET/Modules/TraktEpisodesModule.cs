80765f28f175347cd7ba0363971cc2939454426d
Henrik Fröhling <henrik.froehling@gmail.com>
2020-03-19 21:41:53 +0100

    GH-125: Add request implementation for episode people

--- Source/Lib/Trakt.NET/Modules/TraktEpisodesModule.cs
+++ Source/Lib/Trakt.NET/Modules/TraktEpisodesModule.cs
@@ -209,6 +209,43 @@
         }
 
         /// <summary>
+        /// Gets all people for a <see cref="ITraktEpisode" /> in a show with the given Trakt-Id or -Slug.
+        /// <para>OAuth authorization not required.</para>
+        /// <para>
+        /// See <a href="https://trakt.docs.apiary.io/#reference/episodes/people/get-all-people-for-an-episode">"Trakt API Doc - Episodes: People"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="showIdOrSlug">The show's Trakt-Id or -Slug. See also <seealso cref="ITraktShowIds" />.</param>
+        /// <param name="seasonNumber">The number of the season, for which the people should be queried.</param>
+        /// <param name="episodeNumber">The number of the episode, for which the people should be queried.</param>
+        /// <param name="extendedInfo">
+        /// The extended info, which determines how much data about the people should be queried.
+        /// See also <seealso cref="TraktExtendedInfo" />.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the request should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <returns>An <see cref="ITraktCastAndCrew" /> instance, containing the cast and crew for a episode with the given showIdOrSlug and the given season number.</returns>
+        /// <exception cref="TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="ArgumentException">Thrown, if the given showIdOrSlug is null, empty or contains spaces.</exception>
+        public Task<TraktResponse<ITraktCastAndCrew>> GetEpisodePeopleAsync(string showIdOrSlug, uint seasonNumber, uint episodeNumber,
+                                                                            TraktExtendedInfo extendedInfo = null,
+                                                                            CancellationToken cancellationToken = default)
+        {
+            var requestHandler = new RequestHandler(Client);
+
+            return requestHandler.ExecuteSingleItemRequestAsync(new EpisodePeopleRequest
+            {
+                Id = showIdOrSlug,
+                SeasonNumber = seasonNumber,
+                EpisodeNumber = episodeNumber,
+                ExtendedInfo = extendedInfo
+            },
+            cancellationToken);
+        }
+
+        /// <summary>
         /// Gets the ratings for a <see cref="ITraktEpisode" /> in a show with the given Trakt-Show-Id or -Slug.
         /// <para>OAuth authorization not required.</para>
         /// <para>
