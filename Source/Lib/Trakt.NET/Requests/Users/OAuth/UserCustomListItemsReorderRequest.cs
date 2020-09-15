32ed683503e9d5ca5dc4a716ed794800d6214c05
Henrik Fröhling <henrik.froehling@gmail.com>
2020-09-15 23:53:12 +0200

    GH-66: Add request implementation

---
+++ Source/Lib/Trakt.NET/Requests/Users/OAuth/UserCustomListItemsReorderRequest.cs
@@ -0,0 +1,44 @@
+﻿namespace TraktNet.Requests.Users.OAuth
+{
+    using Base;
+    using Extensions;
+    using Objects.Post.Users;
+    using Objects.Post.Users.Responses;
+    using System;
+    using System.Collections.Generic;
+
+    internal sealed class UserCustomListItemsReorderRequest : APostRequest<ITraktUserCustomListsReorderPostResponse, ITraktUserCustomListsReorderPost>
+    {
+        internal string Username { get; set; }
+
+        public string Id { get; set; }
+
+        public override ITraktUserCustomListsReorderPost RequestBody { get; set; }
+
+        public override string UriTemplate => "users/{username}/lists/{id}/items/reorder";
+
+        public override IDictionary<string, object> GetUriPathParameters()
+            => new Dictionary<string, object>
+            {
+                ["username"] = Username,
+                ["id"] = Id
+            };
+
+        public override void Validate()
+        {
+            base.Validate();
+
+            if (Username == null)
+                throw new ArgumentNullException($"{nameof(Username)} must not be null", default(Exception));
+
+            if (Username == string.Empty || Username.ContainsSpace())
+                throw new ArgumentException($"{nameof(Username)} is not valid");
+
+            if (Id == null)
+                throw new ArgumentNullException(nameof(Id));
+
+            if (Id == string.Empty || Id.ContainsSpace())
+                throw new ArgumentException("id not valid", nameof(Id));
+        }
+    }
+}
