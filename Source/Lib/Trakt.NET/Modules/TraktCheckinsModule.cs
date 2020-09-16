65e387afac2677e9e59fc431eebc66332304945f
Henrik Fröhling <henrik.froehling@gmail.com>
2020-09-16 21:57:46 +0200

    GH-81: Add support for absolute episode numbers in "/checkin" POST request

--- Source/Lib/Trakt.NET/Modules/TraktCheckinsModule.cs
+++ Source/Lib/Trakt.NET/Modules/TraktCheckinsModule.cs
@@ -190,6 +190,64 @@
         }
 
         /// <summary>
+        /// Checks into the given <see cref="ITraktEpisode" />. Use this method, if the given episode has no valid ids.
+        /// <para>OAuth authorization required.</para>
+        /// <para>
+        /// See <a href="http://docs.trakt.apiary.io/#reference/checkin/check-into-an-item">"Trakt API Doc - Checkin: Checkin"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="absoluteEpisodeNumber">The absolute number of the episode, which will be checked in.</param>
+        /// <param name="show">The <see cref="ITraktShow" />, which will be used to check into the given episode.</param>
+        /// <param name="appVersion">Optional application version for the checkin.</param>
+        /// <param name="appBuildDate">Optional application build date for the checkin. Will be converted to the Trakt date-format.</param>
+        /// <param name="message">The message, which will be used for sharing. If none is given, the user's default message will be used.</param>
+        /// <param name="sharing">Optional sharing settings, which will override the user's default sharing settings.</param>
+        /// <param name="foursquareVenueID">Optional Foursquare venue id for the checkin.</param>
+        /// <param name="foursquareVenueName">Optional Foursquare venue name for the checkin.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the request should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <returns>An <see cref="ITraktEpisodeCheckinPostResponse" /> instance, containing the successfully checked in episode's data.</returns>
+        /// <exception cref="TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="ArgumentException">Thrown, if the given show's title is null or empty.</exception>
+        /// <exception cref="ArgumentNullException">Thrown, if the given episode is null. Thrown, if the given show is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown, if the given episode's season number or the given episode's number is below zero.</exception>
+        public Task<TraktResponse<ITraktEpisodeCheckinPostResponse>> CheckIntoEpisodeWithShowAsync(int absoluteEpisodeNumber, ITraktShow show,
+                                                                                                   string appVersion = null, DateTime? appBuildDate = null,
+                                                                                                   string message = null, ITraktSharing sharing = null,
+                                                                                                   string foursquareVenueID = null, string foursquareVenueName = null,
+                                                                                                   CancellationToken cancellationToken = default)
+        {
+            var requestBody = new TraktEpisodeCheckinPost
+            {
+                Episode = new TraktEpisode
+                {
+                    NumberAbsolute = absoluteEpisodeNumber
+                },
+                Show = show,
+                Message = message,
+                Sharing = sharing,
+                FoursquareVenueId = foursquareVenueID,
+                FoursquareVenueName = foursquareVenueName
+            };
+
+            if (!string.IsNullOrEmpty(appVersion))
+                requestBody.AppVersion = appVersion;
+
+            if (appBuildDate.HasValue)
+                requestBody.AppDate = appBuildDate.Value.ToTraktDateString();
+
+            var requestHandler = new RequestHandler(Client);
+
+            return requestHandler.ExecuteSingleItemRequestAsync(new CheckinRequest<ITraktEpisodeCheckinPostResponse, ITraktEpisodeCheckinPost>
+            {
+                RequestBody = requestBody
+            },
+            cancellationToken);
+        }
+
+        /// <summary>
         /// Deletes any active checkins.
         /// <para>OAuth authorization required.</para>
         /// <para>
