c9d49f6e4641e7f2a53609599efa26e3f34d8df8
Henrik Fröhling <henrik.froehling@gmail.com>
2020-10-06 21:19:11 +0200

    GH-5: Add new capabilities for shows

---
+++ Source/Lib/Trakt.NET/Objects/Post/Builder/Interfaces/Capabilities/Shows/ITraktPostBuilderAddShowWithSeasonsCollection'3.cs
@@ -0,0 +1,11 @@
+﻿namespace TraktNet.Objects.Post.Builder.Interfaces.Capabilities
+{
+    using Get.Shows;
+
+    public interface ITraktPostBuilderAddShowWithSeasonsCollection<TPostBuilder, TPostObject, TSeasonCollection>
+        : ITraktPostBuilder<TPostObject>,
+          ITraktPostBuilderWithShow<ITraktPostBuilderAddShowWithSeasonsCollection<TPostBuilder, TPostObject, TSeasonCollection>, TPostObject> where TPostBuilder : ITraktPostBuilder<TPostObject>
+    {
+        ITraktPostBuilderShowAddedSeasonsCollection<ITraktPostBuilderAddShowWithSeasonsCollection<TPostBuilder, TPostObject, TSeasonCollection>, TPostObject, TSeasonCollection> AddShow(ITraktShow show);
+    }
+}
---
+++ Source/Lib/Trakt.NET/Objects/Post/Builder/Interfaces/Capabilities/Shows/ITraktPostBuilderShowAddedSeasonsCollection'3.cs
@@ -0,0 +1,8 @@
+﻿namespace TraktNet.Objects.Post.Builder.Interfaces.Capabilities
+{
+    public interface ITraktPostBuilderShowAddedSeasonsCollection<TPostBuilderAddShow, out TPostObject, TSeasonCollection>
+        : ITraktPostBuilderShowAddedSeasons<TPostBuilderAddShow, TPostObject>
+    {
+        TPostBuilderAddShow WithSeasons(TSeasonCollection seasons);
+    }
+}
