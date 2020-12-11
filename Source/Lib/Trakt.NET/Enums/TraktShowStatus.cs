e839fdaf8e2114a04c8824c4d877dc547f0f6ee5
Henrik Fröhling <henrik.froehling@gmail.com>
2020-12-12 00:36:15 +0100

    GH-222: Add value "upcoming" in TraktShowStatus

--- Source/Lib/Trakt.NET/Enums/TraktShowStatus.cs
+++ Source/Lib/Trakt.NET/Enums/TraktShowStatus.cs
@@ -18,6 +18,9 @@
         /// <summary>The status for a show, which has ended.</summary>
         public static TraktShowStatus Ended { get; } = new TraktShowStatus(8, "ended", "ended", "Ended");
 
+        /// <summary>The status for a show, which is upcoming.</summary>
+        public static TraktShowStatus Upcoming { get; } = new TraktShowStatus(16, "upcoming", "upcoming", "Upcoming");
+
         /// <summary>
         /// Initializes a new instance of the <see cref="TraktShowStatus" /> class.<para />
         /// The initialized <see cref="TraktShowStatus" /> is invalid.
