774a0b7a4df4a876a9f6a8eb3641b078aa287607
Henrik Fröhling <henrik.froehling@gmail.com>
2022-01-20 23:19:35 +0100

    GH-205: Add tests

---
+++ Source/Tests/Trakt.NET.Requests.Tests/Syncs/OAuth/SyncRecommendationsAddRequest_Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace TraktNet.Requests.Tests.Syncs.OAuth
+{
+    using FluentAssertions;
+    using Trakt.NET.Tests.Utility.Traits;
+    using TraktNet.Requests.Syncs.OAuth;
+    using Xunit;
+
+    [Category("Requests.Syncs.OAuth")]
+    public class SyncRecommendationsAddRequest_Tests
+    {
+        [Fact]
+        public void Test_SyncRecommendationsAddRequest_Has_Valid_UriTemplate()
+        {
+            var request = new SyncRecommendationsAddRequest();
+            request.UriTemplate.Should().Be("sync/recommendations");
+        }
+    }
+}
