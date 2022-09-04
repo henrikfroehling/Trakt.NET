6e3cd3be9ace8691ed6dafd1a3d8c31b05aaa51e
Henrik Fröhling <henrik.froehling@gmail.com>
2022-09-04 02:51:45 +0200

    GH-277: Add support for "lists/{id}" GET request

---
+++ Source/Lib/Trakt.NET/Requests/Lists/AListRequest'1.cs
@@ -0,0 +1,26 @@
+﻿namespace TraktNet.Requests.Lists
+{
+    using Base;
+    using Interfaces;
+    using System;
+    using System.Collections.Generic;
+
+    internal abstract class AListRequest<TResponseContentType> : AGetRequest<TResponseContentType>, IHasId<int>
+    {
+        public int Id { get; set; }
+
+        public RequestObjectType RequestObjectType => RequestObjectType.Lists;
+
+        public override IDictionary<string, object> GetUriPathParameters()
+            => new Dictionary<string, object>
+            {
+                ["id"] = Id.ToString()
+            };
+
+        public override void Validate()
+        {
+            if (Id <= 0)
+                throw new ArgumentException("list id not valid", nameof(Id));
+        }
+    }
+}
---
+++ Source/Tests/Trakt.NET.Requests.Tests/Lists/AListRequest_Tests.cs
@@ -0,0 +1,53 @@
+﻿namespace TraktNet.Requests.Tests.Lists
+{
+    using FluentAssertions;
+    using System;
+    using System.Collections.Generic;
+    using Trakt.NET.Tests.Utility.Traits;
+    using TraktNet.Requests.Base;
+    using TraktNet.Requests.Lists;
+    using Xunit;
+
+    [Category("Requests.Lists")]
+    public class AListRequest_Tests
+    {
+        internal class ListRequestMock : AListRequest<int>
+        {
+            public override string UriTemplate { get { throw new NotImplementedException(); } }
+        }
+
+        [Fact]
+        public void Test_AListRequest_Has_AuthorizationRequirement_NotRequired()
+        {
+            var requestMock = new ListRequestMock();
+            requestMock.AuthorizationRequirement.Should().Be(AuthorizationRequirement.NotRequired);
+        }
+
+        [Fact]
+        public void Test_AistRequest_Returns_Valid_UriPathParameters()
+        {
+            var request = new ListRequestMock { Id = 1 };
+
+            request.GetUriPathParameters().Should().NotBeNull()
+                                                   .And.HaveCount(1)
+                                                   .And.Contain(new Dictionary<string, object>
+                                                   {
+                                                       ["id"] = "1"
+                                                   });
+        }
+
+        [Fact]
+        public void Test_AListRequest_Validate_Throws_Exceptions()
+        {
+            var requestMock = new ListRequestMock { Id = -1 };
+
+            Action act = () => requestMock.Validate();
+            act.Should().Throw<ArgumentException>();
+
+            requestMock = new ListRequestMock { Id = 0 };
+
+            act = () => requestMock.Validate();
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
