03894e945386bffbdbb2cecf2f5db1c8479cf3b2
Henrik Fröhling <henrik.froehling@gmail.com>
2022-09-10 01:27:03 +0200

    GH-282: Add support for "users/saved_filters" GET request

---
+++ Source/Lib/Trakt.NET/Objects/Get/Users/Json/Factories/UserSavedFilterJsonIOFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace TraktNet.Objects.Get.Users.Json.Factories
+{
+    using Get.Users.Json.Reader;
+    using Get.Users.Json.Writer;
+    using Objects.Json;
+
+    internal class UserSavedFilterJsonIOFactory : IJsonIOFactory<ITraktUserSavedFilter>
+    {
+        public IObjectJsonReader<ITraktUserSavedFilter> CreateObjectReader() => new UserSavedFilterObjectJsonReader();
+
+        public IObjectJsonWriter<ITraktUserSavedFilter> CreateObjectWriter() => new UserSavedFilterObjectJsonWriter();
+    }
+}
---
+++ Source/Tests/Trakt.NET.Core.Tests/Enums/TraktFilterSection_Tests.cs
@@ -0,0 +1,23 @@
+﻿namespace TraktNet.Core.Tests.Enums
+{
+    using FluentAssertions;
+    using System.Collections.Generic;
+    using Trakt.NET.Tests.Utility.Traits;
+    using TraktNet.Enums;
+    using Xunit;
+
+    [Category("Enums")]
+    public class TraktFilterSection_Tests
+    {
+        [Fact]
+        public void Test_TraktFilterSection_GetAll()
+        {
+            var allValues = TraktEnumeration.GetAll<TraktFilterSection>();
+
+            allValues.Should().NotBeNull().And.HaveCount(5);
+            allValues.Should().Contain(new List<TraktFilterSection>() { TraktFilterSection.Unspecified, TraktFilterSection.Movies,
+                                                                        TraktFilterSection.Shows, TraktFilterSection.Calendars,
+                                                                        TraktFilterSection.Search });
+        }
+    }
+}
