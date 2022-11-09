25b43c17a5395c3de28ec9c5a1d634175a00b7a4
Henrik Fröhling <henrik.froehling@gmail.com>
2022-11-09 20:41:19 +0100

    Update example projects

--- Source/Examples/Authentication/TraktOAuthAuthenticationExample/OAuthAuthentication.cs
+++ Source/Examples/Authentication/TraktOAuthAuthenticationExample/OAuthAuthentication.cs
@@ -1,198 +1,128 @@
-﻿namespace TraktOAuthAuthenticationExample
+﻿using Trakt.Net.Examples.Utility;
+using TraktNet;
+using TraktNet.Exceptions;
+using TraktNet.Objects.Authentication;
+using TraktNet.Responses;
+
+var client = new TraktClient("ENTER_CLIENT_ID_HERE", "ENTER_CLIENT_SECRET_HERE");
+
+try
 {
-    using System;
-    using System.Threading.Tasks;
-    using TraktNet;
-    using TraktNet.Exceptions;
-    using TraktNet.Objects.Authentication;
-    using TraktNet.Responses;
+    if (!client.IsValidForAuthenticationProcess)
+        throw new InvalidOperationException("Trakt Client not valid for authentication");
 
-    internal static class OAuthAuthentication
+    await TryToOAuthAuthenticateAsync().ConfigureAwait(false);
+
+    ITraktAuthorization authorization = client.Authorization;
+
+    if (authorization?.IsValid != true)
+        throw new InvalidOperationException("Trakt Client not authenticated for requests, that require OAuth");
+
+    Console.Write("\nDo you want to refresh the current authorization? [y/n]: ");
+    string yesNo = Console.ReadLine();
+
+    if (yesNo.Equals("y"))
+        await TryToRefreshAuthorizationAsync().ConfigureAwait(false);
+
+    Console.Write("\nDo you want to revoke your authorization? [y/n]: ");
+    yesNo = Console.ReadLine();
+
+    if (yesNo.Equals("y"))
+        await TryToRevokeAuthorizationAsync().ConfigureAwait(false);
+}
+catch (TraktException ex)
+{
+    ExamplesUtility.PrintTraktException(ex);
+}
+catch (Exception ex)
+{
+    ExamplesUtility.PrintException(ex);
+}
+
+Console.ReadLine();
+
+async Task TryToOAuthAuthenticateAsync()
+{
+    string authorizationUrl = client.Authentication.CreateAuthorizationUrl();
+
+    if (!string.IsNullOrEmpty(authorizationUrl))
     {
-        private const string CLIENT_ID = "ENTER_CLIENT_ID_HERE";
-        private const string CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE";
+        Console.WriteLine("You have to authenticate this application.");
+        Console.WriteLine("Please visit the following webpage:");
+        Console.WriteLine($"{authorizationUrl}\n");
+        Console.Write("Enter the PIN code from Trakt.tv: ");
 
-        private static TraktClient _client = null;
+        string code = Console.ReadLine();
 
-        private static async Task Main()
+        if (!string.IsNullOrEmpty(code))
         {
-            try
+            TraktResponse<ITraktAuthorization> authorizationResponse = await client.Authentication.GetAuthorizationAsync(code).ConfigureAwait(false);
+
+            if (authorizationResponse)
             {
-                SetupClient();
-                await TryToOAuthAuthenticate().ConfigureAwait(false);
+                ITraktAuthorization authorization = authorizationResponse.Value;
 
-                ITraktAuthorization authorization = _client.Authorization;
-
-                if (authorization?.IsValid != true)
-                    throw new InvalidOperationException("Trakt Client not authenticated for requests, that require OAuth");
-
-                Console.Write("\nDo you want to refresh the current authorization? [y/n]: ");
-                string yesNo = Console.ReadLine();
-
-                if (yesNo.Equals("y"))
-                    await TryToRefreshAuthorization().ConfigureAwait(false);
-
-                Console.Write("\nDo you want to revoke your authorization? [y/n]: ");
-                yesNo = Console.ReadLine();
-
-                if (yesNo.Equals("y"))
-                    await TryToRevokeAuthorization().ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                PrintException(ex);
-            }
-
-            Console.ReadLine();
-        }
-
-        private static void SetupClient()
-        {
-            if (_client == null)
-            {
-                _client = new TraktClient(CLIENT_ID, CLIENT_SECRET);
-
-                if (!_client.IsValidForAuthenticationProcess)
-                    throw new InvalidOperationException("Trakt Client not valid for authentication");
-            }
-        }
-
-        private static async Task TryToOAuthAuthenticate()
-        {
-            try
-            {
-                string authorizationUrl = _client.Authentication.CreateAuthorizationUrl();
-
-                if (!string.IsNullOrEmpty(authorizationUrl))
+                if (authorization?.IsValid == true)
                 {
-                    Console.WriteLine("You have to authenticate this application.");
-                    Console.WriteLine("Please visit the following webpage:");
-                    Console.WriteLine($"{authorizationUrl}\n");
-                    Console.Write("Enter the PIN code from Trakt.tv: ");
-
-                    string code = Console.ReadLine();
-
-                    if (!string.IsNullOrEmpty(code))
-                    {
-                        TraktResponse<ITraktAuthorization> authorizationResponse = await _client.Authentication.GetAuthorizationAsync(code).ConfigureAwait(false);
-
-                        if (authorizationResponse)
-                        {
-                            ITraktAuthorization authorization = authorizationResponse.Value;
-
-                            if (authorization?.IsValid == true)
-                            {
-                                Console.WriteLine("-------------- Authentication successful --------------");
-                                WriteAuthorizationInformation(authorization);
-                                Console.WriteLine("-------------------------------------------------------");
-                            }
-                        }
-                    }
+                    Console.WriteLine("-------------- Authentication successful --------------");
+                    WriteAuthorizationInformation(authorization);
+                    Console.WriteLine("-------------------------------------------------------");
                 }
             }
-            catch (TraktException ex)
-            {
-                PrintTraktException(ex);
-            }
-            catch (Exception ex)
-            {
-                PrintException(ex);
-            }
-        }
-
-        private static async Task TryToRefreshAuthorization()
-        {
-            try
-            {
-                TraktResponse<ITraktAuthorization> newAuthorizationResponse = await _client.Authentication.RefreshAuthorizationAsync().ConfigureAwait(false);
-
-                if (newAuthorizationResponse)
-                {
-                    ITraktAuthorization newAuthorization = newAuthorizationResponse.Value;
-
-                    if (newAuthorization?.IsValid == true)
-                    {
-                        Console.WriteLine("-------------- Authorization refreshed successfully --------------");
-                        WriteAuthorizationInformation(newAuthorization);
-                        Console.WriteLine("-------------------------------------------------------");
-                    }
-                    else
-                        Console.WriteLine("-------------- Authentication failed --------------");
-                }
-            }
-            catch (TraktException ex)
-            {
-                PrintTraktException(ex);
-            }
-            catch (Exception ex)
-            {
-                PrintException(ex);
-            }
-        }
-
-        private static async Task TryToRevokeAuthorization()
-        {
-            try
-            {
-                TraktNoContentResponse response = await _client.Authentication.RevokeAuthorizationAsync().ConfigureAwait(false);
-
-                if (response)
-                {
-                    // If no exception was thrown, revoking was successfull
-                    Console.WriteLine("-----------------------------------");
-                    Console.WriteLine("Authorization revoked successfully");
-                    Console.WriteLine("-----------------------------------");
-                }
-            }
-            catch (TraktException ex)
-            {
-                PrintTraktException(ex);
-            }
-            catch (Exception ex)
-            {
-                PrintException(ex);
-            }
-        }
-
-        private static void WriteAuthorizationInformation(ITraktAuthorization authorization)
-        {
-            Console.WriteLine($"Created (UTC): {authorization.CreatedAt}");
-            Console.WriteLine($"Access Scope: {authorization.Scope.DisplayName}");
-            Console.WriteLine($"Refresh Possible: {authorization.IsRefreshPossible}");
-            Console.WriteLine($"Valid: {authorization.IsValid}");
-            Console.WriteLine($"Token Type: {authorization.TokenType.DisplayName}");
-            Console.WriteLine($"Access Token: {authorization.AccessToken}");
-            Console.WriteLine($"Refresh Token: {authorization.RefreshToken}");
-            Console.WriteLine($"Token Expired: {authorization.IsExpired}");
-
-            var created = authorization.CreatedAt;
-            var expirationDate = created.AddSeconds(authorization.ExpiresInSeconds);
-            var difference = expirationDate - DateTime.UtcNow;
-
-            var days = difference.Days > 0 ? difference.Days : 0;
-            var hours = difference.Hours > 0 ? difference.Hours : 0;
-            var minutes = difference.Minutes > 0 ? difference.Minutes : 0;
-
-            Console.WriteLine($"Expires in {days} Days, {hours} Hours, {minutes} Minutes");
-        }
-
-        private static void PrintTraktException(TraktException ex)
-        {
-            Console.WriteLine("-------------- Trakt Exception --------------");
-            Console.WriteLine($"Exception message: {ex.Message}");
-            Console.WriteLine($"Status code: {ex.StatusCode}");
-            Console.WriteLine($"Request URL: {ex.RequestUrl}");
-            Console.WriteLine($"Request message: {ex.RequestBody}");
-            Console.WriteLine($"Request response: {ex.Response}");
-            Console.WriteLine($"Server Reason Phrase: {ex.ServerReasonPhrase}");
-            Console.WriteLine("---------------------------------------------");
-        }
-
-        private static void PrintException(Exception ex)
-        {
-            Console.WriteLine("-------------- Exception --------------");
-            Console.WriteLine($"Exception message: {ex.Message}");
-            Console.WriteLine("---------------------------------------");
         }
     }
+}
+
+async Task TryToRefreshAuthorizationAsync()
+{
+    TraktResponse<ITraktAuthorization> newAuthorizationResponse = await client.Authentication.RefreshAuthorizationAsync().ConfigureAwait(false);
+
+    if (newAuthorizationResponse)
+    {
+        ITraktAuthorization newAuthorization = newAuthorizationResponse.Value;
+
+        if (newAuthorization?.IsValid == true)
+        {
+            Console.WriteLine("-------------- Authorization refreshed successfully --------------");
+            WriteAuthorizationInformation(newAuthorization);
+            Console.WriteLine("-------------------------------------------------------");
+        }
+        else
+            Console.WriteLine("-------------- Authentication failed --------------");
+    }
+}
+
+async Task TryToRevokeAuthorizationAsync()
+{
+    TraktNoContentResponse response = await client.Authentication.RevokeAuthorizationAsync().ConfigureAwait(false);
+
+    if (response)
+    {
+        // If no exception was thrown, revoking was successfull
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("Authorization revoked successfully");
+        Console.WriteLine("-----------------------------------");
+    }
+}
+
+void WriteAuthorizationInformation(ITraktAuthorization authorization)
+{
+    Console.WriteLine($"Created (UTC): {authorization.CreatedAt}");
+    Console.WriteLine($"Access Scope: {authorization.Scope.DisplayName}");
+    Console.WriteLine($"Refresh Possible: {authorization.IsRefreshPossible}");
+    Console.WriteLine($"Valid: {authorization.IsValid}");
+    Console.WriteLine($"Token Type: {authorization.TokenType.DisplayName}");
+    Console.WriteLine($"Access Token: {authorization.AccessToken}");
+    Console.WriteLine($"Refresh Token: {authorization.RefreshToken}");
+    Console.WriteLine($"Token Expired: {authorization.IsExpired}");
+
+    var created = authorization.CreatedAt;
+    var expirationDate = created.AddSeconds(authorization.ExpiresInSeconds);
+    var difference = expirationDate - DateTime.UtcNow;
+
+    var days = difference.Days > 0 ? difference.Days : 0;
+    var hours = difference.Hours > 0 ? difference.Hours : 0;
+    var minutes = difference.Minutes > 0 ? difference.Minutes : 0;
+
+    Console.WriteLine($"Expires in {days} Days, {hours} Hours, {minutes} Minutes");
 }
--- Source/Examples/Modules/ShowsModule/GetSingleShowExample/GetSingleShow.cs
+++ Source/Examples/Modules/ShowsModule/GetSingleShowExample/GetSingleShow.cs
@@ -1,172 +1,149 @@
-﻿namespace GetSingleShowExample
+﻿using Trakt.Net.Examples.Utility;
+using TraktNet;
+using TraktNet.Exceptions;
+using TraktNet.Objects.Get.Shows;
+using TraktNet.Parameters;
+using TraktNet.Responses;
+
+const string SHOW_ID = "game-of-thrones";
+
+var client = new TraktClient("ENTER_CLIENT_ID_HERE");
+
+try
 {
-    using System;
-    using System.Threading.Tasks;
-    using TraktNet;
-    using TraktNet.Exceptions;
-    using TraktNet.Objects.Get.Shows;
-    using TraktNet.Parameters;
-    using TraktNet.Responses;
+    if (!client.IsValidForUseWithoutAuthorization)
+        throw new InvalidOperationException("Trakt Client not valid for requests, which do not require OAuth authorization");
 
-    internal static class GetSingleShow
+    Console.Write("Enter the Trakt-Id or -Slug of the Show: ");
+    string showIdOrSlug = Console.ReadLine();
+
+    if (!string.IsNullOrEmpty(showIdOrSlug))
+        await GetShowAsync(showIdOrSlug).ConfigureAwait(false);
+    else
+        await GetShowAsync(SHOW_ID).ConfigureAwait(false);
+}
+catch (Exception ex)
+{
+    ExamplesUtility.PrintException(ex);
+}
+
+Console.ReadLine();
+
+async Task GetShowAsync(string showIdOrSlug)
+{
+    try
     {
-        private const string CLIENT_ID = "ENTER_CLIENT_ID_HERE";
-        private const string DEFAULT_SHOW_SLUG = "game-of-thrones";
+        await GetShowMinimalAsync(showIdOrSlug).ConfigureAwait(false);
+        await GetShowFullAsync(showIdOrSlug).ConfigureAwait(false);
+    }
+    catch (TraktException ex)
+    {
+        Console.WriteLine("-------------- Trakt Exception --------------");
+        Console.WriteLine($"Exception message: {ex.Message}");
+        Console.WriteLine($"Status code: {ex.StatusCode}");
+        Console.WriteLine($"Request URL: {ex.RequestUrl}");
+        Console.WriteLine($"Request message: {ex.RequestBody}");
+        Console.WriteLine($"Request response: {ex.Response}");
+        Console.WriteLine($"Server Reason Phrase: {ex.ServerReasonPhrase}");
 
-        private static TraktClient _client = null;
-
-        private static async Task Main()
+        if (ex is TraktShowNotFoundException showEx)
         {
-            try
-            {
-                SetupClient();
-
-                Console.Write("Enter the Trakt-Id or -Slug of the Show: ");
-                string showIdOrSlug = Console.ReadLine();
-
-                if (!string.IsNullOrEmpty(showIdOrSlug))
-                    await GetShow(showIdOrSlug).ConfigureAwait(false);
-                else
-                    await GetShow(DEFAULT_SHOW_SLUG).ConfigureAwait(false);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("-------------- Exception --------------");
-                Console.WriteLine($"Exception message: {ex.Message}");
-                Console.WriteLine("---------------------------------------");
-            }
-
-            Console.ReadLine();
+            Console.WriteLine($"Show-Id or -Slug: {showEx.ObjectId}");
         }
 
-        private static void SetupClient()
-        {
-            if (_client == null)
-            {
-                _client = new TraktClient(CLIENT_ID);
+        Console.WriteLine("---------------------------------------------");
+    }
+}
 
-                if (!_client.IsValidForUseWithoutAuthorization)
-                    throw new InvalidOperationException("Trakt Client not valid for requests, which do not require OAuth authorization");
-            }
+async Task GetShowMinimalAsync(string showIdOrSlug)
+{
+    Console.WriteLine("------------------------- Show Minimal -------------------------");
+    TraktResponse<ITraktShow> showResponse = await client.Shows.GetShowAsync(showIdOrSlug).ConfigureAwait(false);
+
+    if (showResponse)
+        WriteShowMinimal(showResponse.Value);
+
+    Console.WriteLine("----------------------------------------------------------------");
+}
+
+async Task GetShowFullAsync(string showIdOrSlug)
+{
+    var extendedInfo = new TraktExtendedInfo().SetFull();
+
+    Console.WriteLine("------------------------- Show Full -------------------------");
+    TraktResponse<ITraktShow> showResponse = await client.Shows.GetShowAsync(showIdOrSlug, extendedInfo).ConfigureAwait(false);
+
+    if (showResponse)
+        WriteShowFull(showResponse.Value);
+
+    Console.WriteLine("-------------------------------------------------------------");
+}
+
+void WriteShowMinimal(ITraktShow show)
+{
+    if (show != null)
+    {
+        Console.WriteLine($"Title: {show.Title}");
+        Console.WriteLine($"Year: {show.Year ?? 0}");
+
+        ITraktShowIds ids = show.Ids;
+
+        if (ids != null)
+        {
+            Console.WriteLine($"Trakt-Id: {ids.Trakt}");
+            Console.WriteLine($"Slug: {ids.Slug}");
+            Console.WriteLine($"ImDB-Id: {ids.Imdb}");
+            Console.WriteLine($"TmDB-Id: {ids.Tmdb ?? 0}");
+            Console.WriteLine($"TVDB-Id: {ids.Tvdb ?? 0}");
+            Console.WriteLine($"TVRage-Id: {ids.TvRage ?? 0}");
+        }
+    }
+}
+
+void WriteShowFull(ITraktShow show)
+{
+    WriteShowMinimal(show);
+
+    if (show != null)
+    {
+        Console.WriteLine($"Overview: {show.Overview}");
+
+        if (show.FirstAired.HasValue)
+            Console.WriteLine($"First Aired (UTC): {show.FirstAired.Value}");
+
+        ITraktShowAirs airs = show.Airs;
+
+        if (airs != null)
+        {
+            Console.WriteLine($"Airs on: {airs.Day}");
+            Console.WriteLine($"Airs at: {airs.Time}");
+            Console.WriteLine($"Airs in: {airs.TimeZoneId}");
         }
 
-        private static async Task GetShow(string showIdOrSlug)
-        {
-            try
-            {
-                await GetShowMinimal(showIdOrSlug).ConfigureAwait(false);
-                await GetShowFull(showIdOrSlug).ConfigureAwait(false);
-            }
-            catch (TraktException ex)
-            {
-                Console.WriteLine("-------------- Trakt Exception --------------");
-                Console.WriteLine($"Exception message: {ex.Message}");
-                Console.WriteLine($"Status code: {ex.StatusCode}");
-                Console.WriteLine($"Request URL: {ex.RequestUrl}");
-                Console.WriteLine($"Request message: {ex.RequestBody}");
-                Console.WriteLine($"Request response: {ex.Response}");
-                Console.WriteLine($"Server Reason Phrase: {ex.ServerReasonPhrase}");
+        Console.WriteLine($"Runtime: {show.Runtime ?? 0} Minutes");
 
-                if (ex is TraktShowNotFoundException && ex is TraktShowNotFoundException showEx)
-                {
-                    Console.WriteLine($"Show-Id or -Slug: {showEx.ObjectId}");
-                }
+        if (show.Genres != null)
+            Console.WriteLine($"Genres: {string.Join(", ", show.Genres)}");
 
-                Console.WriteLine("---------------------------------------------");
-            }
-        }
+        Console.WriteLine($"Certification: {show.Certification}");
+        Console.WriteLine($"Network: {show.Network}");
+        Console.WriteLine($"Aired Episodes: {show.AiredEpisodes ?? 0}");
 
-        private static async Task GetShowMinimal(string showIdOrSlug)
-        {
-            Console.WriteLine("------------------------- Show Minimal -------------------------");
-            TraktResponse<ITraktShow> showResponse = await _client.Shows.GetShowAsync(showIdOrSlug).ConfigureAwait(false);
+        if (show.Status != null)
+            Console.WriteLine($"Status: {show.Status.DisplayName}");
 
-            if (showResponse)
-                WriteShowMinimal(showResponse.Value);
+        Console.WriteLine($"Rating: {show.Rating ?? 0.0f}");
+        Console.WriteLine($"Votes: {show.Votes ?? 0}");
+        Console.WriteLine($"Country Code: {show.CountryCode}");
+        Console.WriteLine($"Language Code: {show.LanguageCode}");
 
-            Console.WriteLine("----------------------------------------------------------------");
-        }
+        if (show.UpdatedAt.HasValue)
+            Console.WriteLine($"Updated At (UTC): {show.UpdatedAt.Value}");
 
-        private static async Task GetShowFull(string showIdOrSlug)
-        {
-            var extendedInfo = new TraktExtendedInfo().SetFull();
+        if (show.AvailableTranslationLanguageCodes != null)
+            Console.WriteLine($"Available Translation Languages: {string.Join(", ", show.AvailableTranslationLanguageCodes)}");
 
-            Console.WriteLine("------------------------- Show Full -------------------------");
-            TraktResponse<ITraktShow> showResponse = await _client.Shows.GetShowAsync(showIdOrSlug, extendedInfo).ConfigureAwait(false);
-
-            if (showResponse)
-                WriteShowFull(showResponse.Value);
-
-            Console.WriteLine("-------------------------------------------------------------");
-        }
-
-        private static void WriteShowMinimal(ITraktShow show)
-        {
-            if (show != null)
-            {
-                Console.WriteLine($"Title: {show.Title}");
-                Console.WriteLine($"Year: {show.Year ?? 0}");
-
-                ITraktShowIds ids = show.Ids;
-
-                if (ids != null)
-                {
-                    Console.WriteLine($"Trakt-Id: {ids.Trakt}");
-                    Console.WriteLine($"Slug: {ids.Slug}");
-                    Console.WriteLine($"ImDB-Id: {ids.Imdb}");
-                    Console.WriteLine($"TmDB-Id: {ids.Tmdb ?? 0}");
-                    Console.WriteLine($"TVDB-Id: {ids.Tvdb ?? 0}");
-                    Console.WriteLine($"TVRage-Id: {ids.TvRage ?? 0}");
-                }
-            }
-        }
-
-        private static void WriteShowFull(ITraktShow show)
-        {
-            WriteShowMinimal(show);
-
-            if (show != null)
-            {
-                Console.WriteLine($"Overview: {show.Overview}");
-
-                if (show.FirstAired.HasValue)
-                    Console.WriteLine($"First Aired (UTC): {show.FirstAired.Value}");
-
-                ITraktShowAirs airs = show.Airs;
-
-                if (airs != null)
-                {
-                    Console.WriteLine($"Airs on: {airs.Day}");
-                    Console.WriteLine($"Airs at: {airs.Time}");
-                    Console.WriteLine($"Airs in: {airs.TimeZoneId}");
-                }
-
-                Console.WriteLine($"Runtime: {show.Runtime ?? 0} Minutes");
-
-                if (show.Genres != null)
-                    Console.WriteLine($"Genres: {string.Join(", ", show.Genres)}");
-
-                Console.WriteLine($"Certification: {show.Certification}");
-                Console.WriteLine($"Network: {show.Network}");
-                Console.WriteLine($"Aired Episodes: {show.AiredEpisodes ?? 0}");
-
-                if (show.Status != null)
-                    Console.WriteLine($"Status: {show.Status.DisplayName}");
-
-                Console.WriteLine($"Rating: {show.Rating ?? 0.0f}");
-                Console.WriteLine($"Votes: {show.Votes ?? 0}");
-                Console.WriteLine($"Country Code: {show.CountryCode}");
-                Console.WriteLine($"Language Code: {show.LanguageCode}");
-
-                if (show.UpdatedAt.HasValue)
-                    Console.WriteLine($"Updated At (UTC): {show.UpdatedAt.Value}");
-
-                if (show.AvailableTranslationLanguageCodes != null)
-                    Console.WriteLine($"Available Translation Languages: {string.Join(", ", show.AvailableTranslationLanguageCodes)}");
-
-                Console.WriteLine($"Trailer: {show.Trailer}");
-                Console.WriteLine($"Homepage: {show.Homepage}");
-            }
-        }
+        Console.WriteLine($"Trailer: {show.Trailer}");
+        Console.WriteLine($"Homepage: {show.Homepage}");
     }
 }
