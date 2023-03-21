8b609075ee97c38138bb7243c4764858b3b23941
John Peters <jrpetersjr@gmail.com>
2023-03-21 19:41:46 -0400

    Added GetShowsStreamAsync function to TraktShowsModule Added unit tests

--- Source/Lib/Trakt.NET/Modules/TraktShowsModule.cs
+++ Source/Lib/Trakt.NET/Modules/TraktShowsModule.cs
@@ -16,8 +16,10 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Runtime.CompilerServices;
     using System.Threading;
     using System.Threading.Tasks;
+    using TraktNet.Extensions;
 
     /// <summary>
     /// Provides access to data retrieving methods specific to shows.
@@ -80,6 +82,7 @@
         /// <returns>A list of <see cref="ITraktShow" /> instances with the data of each queried show.</returns>
         /// <exception cref="TraktException">Thrown, if one request fails.</exception>
         /// <exception cref="TraktRequestValidationException">Thrown, if validation of request data fails.</exception>
+        [Obsolete("GetMultipleShowsAsync is deprecated, please use GetShowsStreamAsync instead.")]
         public async Task<IEnumerable<TraktResponse<ITraktShow>>> GetMultipleShowsAsync(TraktMultipleObjectsQueryParams showsQueryParams,
                                                                                         CancellationToken cancellationToken = default)
         {
@@ -96,6 +99,42 @@
 
             TraktResponse<ITraktShow>[] shows = await Task.WhenAll(tasks).ConfigureAwait(false);
             return shows.ToList();
+        }
+
+        /// <summary>
+        /// Gets multiple different <see cref="ITraktShow" />s at once with the given Trakt-Ids or -Slugs.
+        /// <para>OAuth authorization not required.</para>
+        /// <para>
+        /// See <a href="http://docs.trakt.apiary.io/#reference/shows/summary/get-a-single-show">"Trakt API Doc - Shows: Summary"</a> for more information.
+        /// </para>
+        /// <para>See also <seealso cref="GetShowAsync(string, TraktExtendedInfo, CancellationToken)" />.</para>
+        /// </summary>
+        /// <param name="showsQueryParams">A list of show ids and optional extended infos. See also <seealso cref="TraktMultipleObjectsQueryParams" />.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the request should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <returns>An <a href="https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.iasyncenumerable-1?view=net-7.0">async stream</a> of <see cref="ITraktShow" /> instances with the data of each queried show.</returns>
+        /// <exception cref="TraktException">Thrown, if one request fails.</exception>
+        /// <exception cref="TraktRequestValidationException">Thrown, if validation of request data fails.</exception>
+        public async IAsyncEnumerable<TraktResponse<ITraktShow>> GetShowsStreamAsync(TraktMultipleObjectsQueryParams showsQueryParams,
+                                                                                        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            if (showsQueryParams == null || showsQueryParams.Count == 0)
+                yield break;
+
+            var tasks = new List<Task<TraktResponse<ITraktShow>>>();
+
+            foreach (TraktObjectsQueryParams queryParam in showsQueryParams)
+            {
+                Task<TraktResponse<ITraktShow>> task = GetShowAsync(queryParam.Id, queryParam.ExtendedInfo, cancellationToken);
+                tasks.Add(task);
+            }
+
+            await foreach(TraktResponse<ITraktShow> result in tasks.StreamFinishedTaskResultsAsync().ConfigureAwait(false))
+            {
+                yield return result;
+            }
         }
 
         /// <summary>
---
+++ Source/Tests/Trakt.NET.Modules.Tests/TraktShowsModule/TraktShowsModule_GetShowStream_Tests.cs
@@ -0,0 +1,171 @@
+﻿namespace TraktNet.Modules.Tests.TraktShowsModule
+{
+    using FluentAssertions;
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Trakt.NET.Tests.Utility;
+    using Trakt.NET.Tests.Utility.Traits;
+    using TraktNet.Enums;
+    using TraktNet.Exceptions;
+    using TraktNet.Objects.Get.Shows;
+    using TraktNet.Responses;
+    using TraktNet.Extensions;
+    using Xunit;
+
+    [TestCategory("Modules.Shows")]
+    public partial class TraktShowsModule_Tests
+    {
+        private readonly string GET_SHOWS_STREAM_URI = $"shows/{SHOW_ID}";
+
+        [Fact]
+        public async Task Test_TraktShowsModule_GetShowsStreamAsync()
+        {
+            TraktMultipleObjectsQueryParams parameters = new TraktMultipleObjectsQueryParams
+            {
+                { SHOW_ID },
+                { SHOW_ID }
+            };
+            int totalShows = parameters.Count;
+            TraktClient client = TestUtility.GetMockClient(GET_SHOWS_STREAM_URI, SHOW_JSON);
+            IAsyncEnumerable<TraktResponse<ITraktShow>> responses = client.Shows.GetShowsStreamAsync(parameters);
+
+            int returnedShows = 0;
+            await foreach (var response in responses)
+            {
+                response.Should().NotBeNull();
+                response.IsSuccess.Should().BeTrue();
+                response.HasValue.Should().BeTrue();
+                response.Value.Should().NotBeNull();
+
+                ITraktShow responseValue = response.Value;
+
+                responseValue.Title.Should().Be("Game of Thrones");
+                responseValue.Year.Should().Be(2011);
+                responseValue.Ids.Should().NotBeNull();
+                responseValue.Ids.Trakt.Should().Be(1390U);
+                responseValue.Ids.Slug.Should().Be("game-of-thrones");
+                responseValue.Ids.Tvdb.Should().Be(121361U);
+                responseValue.Ids.Imdb.Should().Be("tt0944947");
+                responseValue.Ids.Tmdb.Should().Be(1399U);
+                responseValue.Ids.TvRage.Should().Be(24493U);
+                returnedShows++;
+            }
+            returnedShows.Should().Be(totalShows);
+        }
+
+        [Fact]
+        public async Task Test_TraktShowsModule_GetShowStreamAsync_With_ExtendedInfo()
+        {
+            TraktMultipleObjectsQueryParams parameters = new TraktMultipleObjectsQueryParams
+            {
+                { SHOW_ID, EXTENDED_INFO },
+                { SHOW_ID, EXTENDED_INFO }
+            };
+            int totalShows = parameters.Count;
+            TraktClient client = TestUtility.GetMockClient($"{GET_SHOWS_STREAM_URI}?extended={EXTENDED_INFO}", SHOW_JSON);
+            IAsyncEnumerable<TraktResponse<ITraktShow>> responses = client.Shows.GetShowsStreamAsync(parameters);
+
+            int returnedShows = 0;
+            await foreach (var response in responses.ConfigureAwait(false))
+            {
+                response.Should().NotBeNull();
+                response.IsSuccess.Should().BeTrue();
+                response.HasValue.Should().BeTrue();
+                response.Value.Should().NotBeNull();
+
+                ITraktShow responseValue = response.Value;
+
+                responseValue.Title.Should().Be("Game of Thrones");
+                responseValue.Year.Should().Be(2011);
+                responseValue.Airs.Should().NotBeNull();
+                responseValue.Airs.Day.Should().Be("Sunday");
+                responseValue.Airs.Time.Should().Be("21:00");
+                responseValue.Airs.TimeZoneId.Should().Be("America/New_York");
+                responseValue.AvailableTranslationLanguageCodes.Should().NotBeNull().And.HaveCount(4).And.Contain("en", "fr", "it", "de");
+                responseValue.Ids.Should().NotBeNull();
+                responseValue.Ids.Trakt.Should().Be(1390U);
+                responseValue.Ids.Slug.Should().Be("game-of-thrones");
+                responseValue.Ids.Tvdb.Should().Be(121361U);
+                responseValue.Ids.Imdb.Should().Be("tt0944947");
+                responseValue.Ids.Tmdb.Should().Be(1399U);
+                responseValue.Ids.TvRage.Should().Be(24493U);
+                responseValue.Genres.Should().NotBeNull().And.HaveCount(5).And.Contain("drama", "fantasy", "science-fiction", "action", "adventure");
+                responseValue.Seasons.Should().BeNull();
+                responseValue.Overview.Should().Be("Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and the icy horrors beyond.");
+                responseValue.FirstAired.Should().Be(DateTime.Parse("2011-04-17T07:00:00Z").ToUniversalTime());
+                responseValue.Runtime.Should().Be(60);
+                responseValue.Certification.Should().Be("TV-MA");
+                responseValue.Network.Should().Be("HBO");
+                responseValue.CountryCode.Should().Be("us");
+                responseValue.UpdatedAt.Should().Be(DateTime.Parse("2016-04-06T10:39:11Z").ToUniversalTime());
+                responseValue.Trailer.Should().Be("http://youtube.com/watch?v=F9Bo89m2f6g");
+                responseValue.Homepage.Should().Be("http://www.hbo.com/game-of-thrones");
+                responseValue.Status.Should().Be(TraktShowStatus.ReturningSeries);
+                responseValue.Rating.Should().Be(9.38327f);
+                responseValue.Votes.Should().Be(44773);
+                responseValue.LanguageCode.Should().Be("en");
+                responseValue.AiredEpisodes.Should().Be(50);
+                returnedShows++;
+            }
+            returnedShows.Should().Be(totalShows);
+        }
+
+        [Fact]
+        public async Task Test_TraktShowsModule_GetShowStreamAsync_WithNullParameters()
+        {
+            TraktClient client = TestUtility.GetMockClient($"{GET_SHOWS_STREAM_URI}?extended={EXTENDED_INFO}", SHOW_JSON);
+            IAsyncEnumerable<TraktResponse<ITraktShow>> responses = client.Shows.GetShowsStreamAsync(null);
+            (await responses.ToListAsync().ConfigureAwait(false)).Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Test_TraktShowsModule_GetShowStreamAsync_WithEmptyParameters()
+        {
+            TraktMultipleObjectsQueryParams parameters = new TraktMultipleObjectsQueryParams();
+            int totalShows = parameters.Count;
+            TraktClient client = TestUtility.GetMockClient($"{GET_SHOWS_STREAM_URI}?extended={EXTENDED_INFO}", SHOW_JSON);
+            IAsyncEnumerable<TraktResponse<ITraktShow>> responses = client.Shows.GetShowsStreamAsync(parameters);
+            (await responses.ToListAsync().ConfigureAwait(false)).Should().BeEmpty();
+        }
+
+        [Theory]
+        [InlineData(HttpStatusCode.NotFound, typeof(TraktShowNotFoundException))]
+        [InlineData(HttpStatusCode.Unauthorized, typeof(TraktAuthorizationException))]
+        [InlineData(HttpStatusCode.BadRequest, typeof(TraktBadRequestException))]
+        [InlineData(HttpStatusCode.Forbidden, typeof(TraktForbiddenException))]
+        [InlineData(HttpStatusCode.MethodNotAllowed, typeof(TraktMethodNotFoundException))]
+        [InlineData(HttpStatusCode.Conflict, typeof(TraktConflictException))]
+        [InlineData(HttpStatusCode.InternalServerError, typeof(TraktServerException))]
+        [InlineData(HttpStatusCode.BadGateway, typeof(TraktBadGatewayException))]
+        [InlineData(HttpStatusCode.PreconditionFailed, typeof(TraktPreconditionFailedException))]
+        [InlineData(HttpStatusCode.UnprocessableEntity, typeof(TraktValidationException))]
+        [InlineData(HttpStatusCode.TooManyRequests, typeof(TraktRateLimitException))]
+        [InlineData(HttpStatusCode.ServiceUnavailable, typeof(TraktServerUnavailableException))]
+        [InlineData(HttpStatusCode.GatewayTimeout, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)520, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)521, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)522, typeof(TraktServerUnavailableException))]
+        public async Task Test_TraktShowsModule_GetShowsStreamAsync_Throws_API_Exception(HttpStatusCode statusCode, Type exceptionType)
+        {
+            TraktMultipleObjectsQueryParams parameters = new TraktMultipleObjectsQueryParams
+            {
+                { SHOW_ID },
+                { SHOW_ID }
+            };
+            TraktClient client = TestUtility.GetMockClient(GET_SHOWS_STREAM_URI, statusCode);
+
+            try
+            {
+                var responses = client.Shows.GetShowsStreamAsync(parameters);
+                (await responses.ToListAsync().ConfigureAwait(false)).Should().NotBeNullOrEmpty();
+                Assert.False(true);
+            }
+            catch (Exception exception)
+            {
+                (exception.GetType() == exceptionType).Should().BeTrue();
+            }
+        }
+    }
+}
