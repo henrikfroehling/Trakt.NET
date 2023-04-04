4f7f59b4875227b5204a009af1b4dbe860d65960
Henrik Fröhling <henrik.froehling@gmail.com>
2023-04-04 20:40:15 +0000

    GH-439: Reduce code duplication

--- Source/Lib/Trakt.NET/Objects/Get/Recommendations/ITraktRecommendedShow.cs
+++ Source/Lib/Trakt.NET/Objects/Get/Recommendations/ITraktRecommendedShow.cs
@@ -1,9 +1,12 @@
 namespace TraktNet.Objects.Get.Recommendations
 {
     using Shows;
+    using System.Collections.Generic;
 
     /// <summary>A Trakt recommended show.</summary>
-    public interface ITraktRecommendedShow : ITraktRecommendedObject, ITraktShow
+    public interface ITraktRecommendedShow : ITraktShow
     {
+        /// <summary>Gets or sets the list of users who recommended this show. See also <seealso cref="ITraktRecommendedBy" />.<para>Nullable</para></summary>
+        IEnumerable<ITraktRecommendedBy> RecommendedBy { get; set; }
     }
 }
