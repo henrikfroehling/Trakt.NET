a311ddbcadc89353dc6e6e6dd16044993eb53d2c
Henrik Fröhling <henrik.froehling@gmail.com>
2023-04-08 16:40:40 +0000

    GH-371: Improve docs

--- Source/Lib/Trakt.NET/Enums/TraktSortBy.cs
+++ Source/Lib/Trakt.NET/Enums/TraktSortBy.cs
@@ -1,45 +1,45 @@
 ﻿namespace TraktNet.Enums
 {
-    /// <summary>Determines the sort-by type.</summary>
+    /// <summary>Determines how items are ordered.</summary>
     public sealed class TraktSortBy : TraktEnumeration
     {
         /// <summary>An invalid sort-by type.</summary>
         public static TraktSortBy Unspecified { get; } = new TraktSortBy();
 
-        /// <summary>Determines the sort-by type ordered by rank.</summary>
+        /// <summary>Items are ordered by rank.</summary>
         public static TraktSortBy Rank { get; } = new TraktSortBy(1, "rank", "rank", "Rank");
 
-        /// <summary>Determines the sort-by type ordered by added timestamp.</summary>
+        /// <summary>Items are ordered by added timestamp.</summary>
         public static TraktSortBy Added { get; } = new TraktSortBy(2, "added", "added", "Added");
 
-        /// <summary>Determines the sort-by type ordered by title.</summary>
+        /// <summary>Items are ordered by title.</summary>
         public static TraktSortBy Title { get; } = new TraktSortBy(4, "title", "title", "Title");
 
-        /// <summary>Determines the sort-by type ordered by released timestamp.</summary>
+        /// <summary>Items are ordered by released timestamp.</summary>
         public static TraktSortBy Released { get; } = new TraktSortBy(8, "released", "released", "Released");
 
-        /// <summary>Determines the sort-by type ordered by runtime.</summary>
+        /// <summary>Items are ordered by runtime.</summary>
         public static TraktSortBy Runtime { get; } = new TraktSortBy(16, "runtime", "runtime", "Runtime");
 
-        /// <summary>Determines the sort-by type ordered by popularity.</summary>
+        /// <summary>Items are ordered by popularity.</summary>
         public static TraktSortBy Popularity { get; } = new TraktSortBy(32, "popularity", "popularity", "Popularity");
 
-        /// <summary>Determines the sort-by type ordered by percentage.</summary>
+        /// <summary>Items are ordered by percentage.</summary>
         public static TraktSortBy Percentage { get; } = new TraktSortBy(64, "percentage", "percentage", "Percentage");
 
-        /// <summary>Determines the sort-by type ordered by votes.</summary>
+        /// <summary>Items are ordered by votes.</summary>
         public static TraktSortBy Votes { get; } = new TraktSortBy(128, "votes", "votes", "Votes");
 
-        /// <summary>Determines the sort-by type ordered by own user rating.</summary>
+        /// <summary>Items are ordered by own user rating.</summary>
         public static TraktSortBy MyRating { get; } = new TraktSortBy(512, "my_rating", "my_rating", "My Rating");
 
-        /// <summary>Determines the sort-by type ordered by random.</summary>
+        /// <summary>Items are ordered by random.</summary>
         public static TraktSortBy Random { get; } = new TraktSortBy(1024, "random", "random", "Random");
 
-        /// <summary>Determines the sort-by type ordered by watched timestamp.</summary>
+        /// <summary>Items are ordered by watched timestamp.</summary>
         public static TraktSortBy Watched { get; } = new TraktSortBy(2048, "watched", "watched", "Watched");
 
-        /// <summary>Determines the sort-by type ordered by collected timestamp.</summary>
+        /// <summary>Items are ordered by collected timestamp.</summary>
         public static TraktSortBy Collected { get; } = new TraktSortBy(4096, "collected", "movies", "Collected");
 
         /// <summary>
--- Source/Lib/Trakt.NET/Enums/TraktSortHow.cs
+++ Source/Lib/Trakt.NET/Enums/TraktSortHow.cs
@@ -1,15 +1,15 @@
 ﻿namespace TraktNet.Enums
 {
-    /// <summary>Determines the sort-how type.</summary>
+    /// <summary>Determines how items are ordered.</summary>
     public sealed class TraktSortHow : TraktEnumeration
     {
         /// <summary>An invalid sort-how type.</summary>
         public static TraktSortHow Unspecified { get; } = new TraktSortHow();
 
-        /// <summary>Determines the sort-how type ordered in ascending order.</summary>
+        /// <summary>Items are ordered in ascending order.</summary>
         public static TraktSortHow Ascending { get; } = new TraktSortHow(1, "asc", "asc", "Ascending");
 
-        /// <summary>Determines the sort-how type ordered in descending order.</summary>
+        /// <summary>Items are ordered in descending order.</summary>
         public static TraktSortHow Descending { get; } = new TraktSortHow(2, "desc", "desc", "Descending");
 
         /// <summary>
