988c68f7a68e7614427154bd1a0fd3efb7d9853c
Henrik Fröhling <henrik.froehling@gmail.com>
2023-04-08 23:42:44 +0000

    GH-416: Fix some tests by using simple `foreach` loop

--- Source/Lib/Trakt.NET/Extensions/IEnumerableExtensions.cs
+++ Source/Lib/Trakt.NET/Extensions/IEnumerableExtensions.cs
@@ -1,7 +1,6 @@
 ﻿namespace TraktNet.Extensions
 {
     using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
 
     /// <summary>Provides helper methods for IAsyncEnumerables.</summary>
@@ -15,23 +14,9 @@
             if(source is null || source.Count == 0)
                 yield break;
 
-            while (source.Any())
+            foreach (Task<TSource> task in source)
             {
-                Task<TSource> finishedTask = await Task.WhenAny(source).ConfigureAwait(false);
-                source.Remove(finishedTask);
-                yield return await finishedTask;
-
-            }
-        }
-
-        /// <summary>Returns results from an IEnumerable of tasks as they finish.</summary>
-        /// <param name="source">The collection of tasks to stream results from as they finish.</param>
-        /// <returns>An IAsyncEnumerable of the results from the tasks.</returns>
-        public static async IAsyncEnumerable<TSource> StreamFinishedTaskResultsAsync<TSource>(this IEnumerable<Task<TSource>> source)
-        {
-            await foreach(TSource value in StreamFinishedTaskResultsAsync(source.ToList()).ConfigureAwait(false))
-            {
-                yield return value;
+                yield return await task;
             }
         }
     }
