644fcb01bc309c1559d5dbb863185ed2bf75589c
Henrik Fröhling <henrik.froehling@gmail.com>
2023-06-01 19:47:35 +0000

    GH-480: Add property for "id" in `ITraktRecommendation`

--- Source/Lib/Trakt.NET/Objects/Get/Users/Json/Writer/RecommendationObjectJsonWriter.cs
+++ Source/Lib/Trakt.NET/Objects/Get/Users/Json/Writer/RecommendationObjectJsonWriter.cs
@@ -16,6 +16,12 @@
             CheckJsonTextWriter(jsonWriter);
             await jsonWriter.WriteStartObjectAsync(cancellationToken).ConfigureAwait(false);
 
+            if (obj.Id.HasValue)
+            {
+                await jsonWriter.WritePropertyNameAsync(JsonProperties.PROPERTY_NAME_ID, cancellationToken).ConfigureAwait(false);
+                await jsonWriter.WriteValueAsync(obj.Id.Value, cancellationToken).ConfigureAwait(false);
+            }
+
             if (obj.Rank.HasValue)
             {
                 await jsonWriter.WritePropertyNameAsync(JsonProperties.PROPERTY_NAME_RANK, cancellationToken).ConfigureAwait(false);
