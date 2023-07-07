f1e97d6421555ea9db17f2db992be8b41be586ca
Henrik Fröhling <henrik.froehling@gmail.com>
2023-07-08 00:03:46 +0200

    GH-502 GH-501: Add parameter `TraktExtendedInfo` in `TraktEpisodesModule.GetEpisodeListsAsync()`

--- Source/Lib/Trakt.NET/Modules/TraktEpisodesModule.cs
+++ Source/Lib/Trakt.NET/Modules/TraktEpisodesModule.cs
@@ -192,6 +192,10 @@
         /// <param name="episodeNumber">The number of the episode, for which the lists should be queried.</param>
         /// <param name="listType">The type of lists, that should be queried. Defaults to personal lists.</param>
         /// <param name="listSortOrder">The list sort order. See also <seealso cref="TraktListSortOrder" />. Defaults to sorted by popularity.</param>
+        /// <param name="extendedInfo">
+        /// The extended info, which determines how much data about the list items should be queried.
+        /// See also <seealso cref="TraktExtendedInfo" />.
+        /// </param>
         /// <param name="pagedParameters">Specifies pagination parameters. <see cref="TraktPagedParameters" />.</param>
         /// <param name="cancellationToken">
         /// Propagates notification that the request should be canceled.<para/>
@@ -208,6 +212,7 @@
         /// <exception cref="TraktRequestValidationException">Thrown, if validation of request data fails.</exception>
         public Task<TraktPagedResponse<ITraktList>> GetEpisodeListsAsync(string showIdOrSlug, uint seasonNumber, uint episodeNumber,
                                                                          TraktListType listType = null, TraktListSortOrder listSortOrder = null,
+                                                                         TraktExtendedInfo extendedInfo = null,
                                                                          TraktPagedParameters pagedParameters = null,
                                                                          CancellationToken cancellationToken = default)
         {
@@ -218,6 +223,7 @@
                 EpisodeNumber = episodeNumber,
                 Type = listType,
                 SortOrder = listSortOrder,
+                ExtendedInfo = extendedInfo,
                 Page = pagedParameters?.Page,
                 Limit = pagedParameters?.Limit
             };
