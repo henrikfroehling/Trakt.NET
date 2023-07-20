b1cbc8f9d8a1fb6d172adfd91cf8a32da85bfe06
Henrik Fröhling <henrik.froehling@gmail.com>
2023-07-20 22:50:24 +0200

    GH-484: - add new tests
    - remove unnecessary tests

---
+++ Source/Tests/Trakt.NET.Objects.Post.Tests/Basic/Json/Reader/ListItemUpdatePostObjectJsonReader/ListItemUpdatePostObjectJsonReader_Tests.cs
@@ -0,0 +1,62 @@
+﻿namespace TraktNet.Objects.Post.Tests.Basic.Json.Reader
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Trakt.NET.Tests.Utility.Traits;
+    using TraktNet.Objects.Post.Basic;
+    using TraktNet.Objects.Post.Basic.Json.Reader;
+    using Xunit;
+
+    [TestCategory("Objects.Post.Basic.JsonReader")]
+    public partial class ListItemUpdatePostObjectJsonReader_Tests
+    {
+        [Fact]
+        public async Task Test_ListItemUpdatePostObjectJsonReader_ReadObject_From_JsonReader_Complete()
+        {
+            var traktJsonReader = new ListItemUpdatePostObjectJsonReader();
+
+            using var reader = new StringReader(JSON_COMPLETE);
+            using var jsonReader = new JsonTextReader(reader);
+            var listItemUpdatePost = await traktJsonReader.ReadObjectAsync(jsonReader);
+
+            listItemUpdatePost.Should().NotBeNull();
+            listItemUpdatePost.Notes.Should().Be("This is a great movie!");
+        }
+
+        [Fact]
+        public async Task Test_ListItemUpdatePostObjectJsonReader_ReadObject_From_JsonReader_Not_Valid()
+        {
+            var traktJsonReader = new ListItemUpdatePostObjectJsonReader();
+
+            using var reader = new StringReader(JSON_NOT_VALID);
+            using var jsonReader = new JsonTextReader(reader);
+            var listItemUpdatePost = await traktJsonReader.ReadObjectAsync(jsonReader);
+
+            listItemUpdatePost.Should().NotBeNull();
+            listItemUpdatePost.Notes.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Test_ListItemUpdatePostObjectJsonReader_ReadObject_From_JsonReader_Null()
+        {
+            var traktJsonReader = new ListItemUpdatePostObjectJsonReader();
+            Func<Task<ITraktListItemUpdatePost>> listItemUpdatePost = () => traktJsonReader.ReadObjectAsync(default(JsonTextReader));
+            await listItemUpdatePost.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task Test_ListItemUpdatePostObjectJsonReader_ReadObject_From_JsonReader_Empty()
+        {
+            var traktJsonReader = new ListItemUpdatePostObjectJsonReader();
+
+            using var reader = new StringReader(string.Empty);
+            using var jsonReader = new JsonTextReader(reader);
+
+            var listItemUpdatePost = await traktJsonReader.ReadObjectAsync(jsonReader);
+            listItemUpdatePost.Should().BeNull();
+        }
+    }
+}
---
+++ Source/Tests/Trakt.NET.Objects.Post.Tests/Basic/Json/Writer/ListItemUpdatePostObjectJsonWriter_Tests.cs
@@ -0,0 +1,61 @@
+﻿namespace TraktNet.Objects.Post.Tests.Basic.Json.Writer
+{
+    using FluentAssertions;
+    using System;
+    using System.Threading.Tasks;
+    using Trakt.NET.Tests.Utility.Traits;
+    using TraktNet.Objects.Post.Basic;
+    using TraktNet.Objects.Post.Basic.Json.Writer;
+    using Xunit;
+
+    [TestCategory("Objects.Post.Basic.JsonWriter")]
+    public class ListItemUpdatePostObjectJsonWriter_Tests
+    {
+        [Fact]
+        public async Task Test_ListItemUpdatePostObjectJsonWriter_WriteObject_Object_Exceptions()
+        {
+            var traktJsonWriter = new ListItemUpdatePostObjectJsonWriter();
+            Func<Task<string>> action = () => traktJsonWriter.WriteObjectAsync(default);
+            await action.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task Test_ListItemUpdatePostObjectJsonWriter_WriteObject_Object_Complete()
+        {
+            ITraktListItemUpdatePost listItemUpdatePost = new TraktListItemUpdatePost
+            {
+                Notes = "This is a great movie!"
+            };
+
+            var traktJsonWriter = new ListItemUpdatePostObjectJsonWriter();
+            string json = await traktJsonWriter.WriteObjectAsync(listItemUpdatePost);
+            json.Should().Be(@"{""notes"":""This is a great movie!""}");
+        }
+
+        [Fact]
+        public async Task Test_ListItemUpdatePostObjectJsonWriter_WriteObject_Object_Complete_Notes_Empty()
+        {
+            ITraktListItemUpdatePost listItemUpdatePost = new TraktListItemUpdatePost
+            {
+                Notes = ""
+            };
+
+            var traktJsonWriter = new ListItemUpdatePostObjectJsonWriter();
+            string json = await traktJsonWriter.WriteObjectAsync(listItemUpdatePost);
+            json.Should().Be(@"{""notes"":""""}");
+        }
+
+        [Fact]
+        public async Task Test_ListItemUpdatePostObjectJsonWriter_WriteObject_Object_Complete_Notes_Null()
+        {
+            ITraktListItemUpdatePost listItemUpdatePost = new TraktListItemUpdatePost
+            {
+                Notes = null
+            };
+
+            var traktJsonWriter = new ListItemUpdatePostObjectJsonWriter();
+            string json = await traktJsonWriter.WriteObjectAsync(listItemUpdatePost);
+            json.Should().Be(@"{""notes"":null}");
+        }
+    }
+}
