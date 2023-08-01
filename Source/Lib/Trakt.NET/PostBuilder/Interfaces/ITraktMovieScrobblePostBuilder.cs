85fcc1ee9adbacbe021933da98b4cf395f661322
Henrik Fröhling <henrik.froehling@gmail.com>
2023-08-02 00:34:30 +0200

    GH-421: Add method overloads in scrobble post builder

--- Source/Lib/Trakt.NET/PostBuilder/Interfaces/ITraktMovieScrobblePostBuilder.cs
+++ Source/Lib/Trakt.NET/PostBuilder/Interfaces/ITraktMovieScrobblePostBuilder.cs
@@ -1,8 +1,8 @@
 ﻿namespace TraktNet.PostBuilder
 {
+    using Objects.Get.Movies;
+    using Objects.Post.Scrobbles;
     using System;
-    using TraktNet.Objects.Get.Movies;
-    using TraktNet.Objects.Post.Scrobbles;
 
     public interface ITraktMovieScrobblePostBuilder : ITraktScrobblePostBuilder<ITraktMovieScrobblePostBuilder, ITraktMovieScrobblePost>
     {
@@ -12,6 +12,15 @@
         /// <exception cref="ArgumentNullException">Thrown, if the given <paramref name="movie"/> is null.</exception>
         /// <exception cref="ArgumentNullException">Thrown, if the given <paramref name="movie"/>s ids are null.</exception>
         /// <exception cref="ArgumentException">Thrown, if the given <paramref name="movie"/>s ids are not valid.</exception>
+        /// <remarks>Overrides values already set by <see cref="WithMovie(ITraktMovieIds)"/>.</remarks>
         ITraktMovieScrobblePostBuilder WithMovie(ITraktMovie movie);
+
+        /// <summary>Adds the given <paramref name="movieIds"/> to the builder.</summary>
+        /// <param name="movieIds">The <see cref="ITraktMovieIds"/> which will be added.</param>
+        /// <returns>Returns a reference to itself. See also <seealso cref="ITraktMovieCheckinPostBuilder"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown, if the given <paramref name="movieIds"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown, if the given <paramref name="movieIds"/>s are not valid.</exception>
+        /// <remarks>Overrides values already set by <see cref="WithMovie(ITraktMovie)"/>.</remarks>
+        ITraktMovieScrobblePostBuilder WithMovie(ITraktMovieIds movieIds);
     }
 }
