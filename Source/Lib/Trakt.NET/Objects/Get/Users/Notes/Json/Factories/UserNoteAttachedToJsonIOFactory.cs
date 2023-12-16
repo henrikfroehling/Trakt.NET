6182866b14b0873f5bf94e7e6bb2780de7826511
Henrik Fröhling <henrik.froehling@gmail.com>
2023-12-16 23:38:00 +0100

    GH-597: Add user note json objects

---
+++ Source/Lib/Trakt.NET/Objects/Get/Users/Notes/Json/Factories/UserNoteAttachedToJsonIOFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace TraktNet.Objects.Get.Users.Notes.Json.Factories
+{
+    using Get.Users.Notes.Json.Reader;
+    using Get.Users.Notes.Json.Writer;
+    using Objects.Json;
+
+    internal class UserNoteAttachedToJsonIOFactory : IJsonIOFactory<ITraktUserNoteAttachedTo>
+    {
+        public IObjectJsonReader<ITraktUserNoteAttachedTo> CreateObjectReader() => new UserNoteAttachedToObjectJsonReader();
+
+        public IObjectJsonWriter<ITraktUserNoteAttachedTo> CreateObjectWriter() => new UserNoteAttachedToObjectJsonWriter();
+    }
+}
