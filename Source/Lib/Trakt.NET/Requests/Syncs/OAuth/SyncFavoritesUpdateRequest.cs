92358a48133fe51729505c2a0bc3279afaedf448
Henrik Fröhling <henrik.froehling@gmail.com>
2024-01-07 19:19:23 +0100

    GH-600: Add update favorites list put request

---
+++ Source/Lib/Trakt.NET/Requests/Syncs/OAuth/SyncFavoritesUpdateRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktNet.Requests.Syncs.OAuth
+{
+    using Base;
+    using Objects.Get.Lists;
+    using Objects.Post.Syncs.Lists;
+    using System.Collections.Generic;
+
+    internal sealed class SyncFavoritesUpdateRequest : APutRequest<ITraktList, ITraktUpdateListPost>
+    {
+        public override string UriTemplate => "sync/favorites";
+
+        public override ITraktUpdateListPost RequestBody { get; set; }
+
+        public override IDictionary<string, object> GetUriPathParameters() => new Dictionary<string, object>();
+    }
+}
