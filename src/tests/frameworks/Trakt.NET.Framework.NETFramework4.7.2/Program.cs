29ede6eb0399a2b3d26e465f23eb4e3877e45dac
Henrik Fröhling <henrik.froehling@gmail.com>
2024-03-20 21:54:46 +0100

    [v2] Add framework tests

---
+++ src/tests/frameworks/Trakt.NET.Framework.NETFramework4.7.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TraktNET;
+
+namespace Trakt.NET.Framework.NETFramework472
+{
+    internal sealed class Program
+    {
+        static void Main(string[] _)
+        {
+            var client = new TraktClient("clientID", "clientSecret");
+
+            Console.WriteLine("Welcome to Trakt.NET in .NET Framework 4.7.2.");
+        }
+    }
+}
---
+++ src/tests/frameworks/Trakt.NET.Framework.NETFramework4.8.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TraktNET;
+
+namespace Trakt.NET.Framework.NETFramework481
+{
+    internal sealed class Program
+    {
+        static void Main(string[] _)
+        {
+            var client = new TraktClient("clientID", "clientSecret");
+
+            Console.WriteLine("Welcome to Trakt.NET in .NET Framework 4.8.1.");
+        }
+    }
+}
