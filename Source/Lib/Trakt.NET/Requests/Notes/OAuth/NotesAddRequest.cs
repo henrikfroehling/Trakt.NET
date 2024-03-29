e6f1d42df7469f32e367d42198ba1ca3bb5fd037
Henrik Fröhling <henrik.froehling@gmail.com>
2024-03-29 17:34:42 +0100

    #632: Add notes add request

---
+++ Source/Lib/Trakt.NET/Requests/Notes/OAuth/NotesAddRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace TraktNet.Requests.Notes.OAuth
+{
+    using System.Collections.Generic;
+    using TraktNet.Objects.Get.Notes;
+    using TraktNet.Objects.Post.Notes;
+    using TraktNet.Requests.Base;
+
+    internal sealed class NotesAddRequest : APostRequest<ITraktNote, ITraktNotePost>
+    {
+        public override ITraktNotePost RequestBody { get; set; }
+
+        public override string UriTemplate => "notes";
+
+        public override IDictionary<string, object> GetUriPathParameters() => new Dictionary<string, object>();
+    }
+}
