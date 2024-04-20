6840592d20036634faac7af013ecd73ca472ec37
Henrik Fröhling <henrik.froehling@gmail.com>
2024-04-20 22:49:40 +0200

    #630: Add support for "movies/{id}/refresh" POST request

---
+++ Source/Lib/Trakt.NET/Modules/Movies/TraktMoviesModule_RefreshMovie.cs
@@ -0,0 +1,112 @@
+﻿namespace TraktNet.Modules
+{
+    using Exceptions;
+    using Objects.Get.Movies;
+    using Requests.Handler;
+    using Responses;
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using TraktNet.Requests.Movies.OAuth;
+
+    public partial class TraktMoviesModule : ATraktModule
+    {
+        /// <summary>
+        /// Queue a <see cref="ITraktMovie" /> for a full metadata and image refresh.
+        /// <para>OAuth authorization required.</para>
+        /// <para>
+        /// See <a href="https://trakt.docs.apiary.io/#reference/movies/refresh/refresh-movie-metadata">"Trakt API Doc - Refresh: Refresh movie metadata"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="movieIdOrSlug">The movie's Trakt-Id or -Slug. See also <seealso cref="ITraktMovieIds" />.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the request should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <exception cref="TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="TraktRequestValidationException">Thrown, if validation of request data fails.</exception>
+        public Task<TraktNoContentResponse> RefreshMovieAsync(string movieIdOrSlug, CancellationToken cancellationToken = default)
+        {
+            var requestHandler = new RequestHandler(Client);
+
+            return requestHandler.ExecuteNoContentRequestAsync(new MovieRefreshRequest
+            {
+                Id = movieIdOrSlug
+            },
+            cancellationToken);
+        }
+
+        /// <summary>
+        /// Queue a <see cref="ITraktMovie" /> for a full metadata and image refresh.
+        /// <para>OAuth authorization required.</para>
+        /// <para>
+        /// See <a href="https://trakt.docs.apiary.io/#reference/movies/refresh/refresh-movie-metadata">"Trakt API Doc - Refresh: Refresh movie metadata"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="traktMovieId">The movie's Trakt-Id. See also <seealso cref="ITraktMovieIds" />.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the request should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <exception cref="TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="TraktRequestValidationException">Thrown, if validation of request data fails.</exception>
+        /// <exception cref="ArgumentException">Thrown, if the given <paramref name="traktMovieId"/> is 0.</exception>
+        public Task<TraktNoContentResponse> RefreshMovieAsync(uint traktMovieId, CancellationToken cancellationToken = default)
+        {
+            if (traktMovieId == 0)
+                throw new ArgumentException("movie id must not be 0", nameof(traktMovieId));
+
+            return RefreshMovieAsync(traktMovieId.ToString(), cancellationToken);
+        }
+
+        /// <summary>
+        /// Queue a <see cref="ITraktMovie" /> for a full metadata and image refresh.
+        /// <para>OAuth authorization required.</para>
+        /// <para>
+        /// See <a href="https://trakt.docs.apiary.io/#reference/movies/refresh/refresh-movie-metadata">"Trakt API Doc - Refresh: Refresh movie metadata"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="movieIds">The movie's ids. See also <seealso cref="ITraktMovieIds" />.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the request should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <exception cref="TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="TraktRequestValidationException">Thrown, if validation of request data fails.</exception>
+        /// <exception cref="ArgumentNullException">Thrown, if the given <paramref name="movieIds"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown, if the given <paramref name="movieIds"/> has not any ids set.</exception>
+        public Task<TraktNoContentResponse> RefreshMovieAsync(ITraktMovieIds movieIds, CancellationToken cancellationToken = default)
+        {
+            if (movieIds == null)
+                throw new ArgumentNullException(nameof(movieIds));
+
+            if (!movieIds.HasAnyId)
+                throw new ArgumentException($"{nameof(movieIds)} has not any ids set", nameof(movieIds));
+
+            return RefreshMovieAsync(movieIds.GetBestId(), cancellationToken);
+        }
+
+        /// <summary>
+        /// Queue a <see cref="ITraktMovie" /> for a full metadata and image refresh.
+        /// <para>OAuth authorization required.</para>
+        /// <para>
+        /// See <a href="https://trakt.docs.apiary.io/#reference/movies/refresh/refresh-movie-metadata">"Trakt API Doc - Refresh: Refresh movie metadata"</a> for more information.
+        /// </para>
+        /// </summary>
+        /// <param name="movie">The movie. See also <seealso cref="ITraktMovie" />.</param>
+        /// <param name="cancellationToken">
+        /// Propagates notification that the request should be canceled.<para/>
+        /// If provided, the exception <see cref="OperationCanceledException" /> should be catched.
+        /// </param>
+        /// <exception cref="TraktException">Thrown, if the request fails.</exception>
+        /// <exception cref="TraktRequestValidationException">Thrown, if validation of request data fails.</exception>
+        /// <exception cref="ArgumentNullException">Thrown, if the given <paramref name="movie"/> is null.</exception>
+        public Task<TraktNoContentResponse> RefreshMovieAsync(ITraktMovie movie, CancellationToken cancellationToken = default)
+        {
+            if (movie == null)
+                throw new ArgumentNullException(nameof(movie));
+
+            return RefreshMovieAsync(movie.Ids, cancellationToken);
+        }
+    }
+}
---
+++ Source/Tests/Trakt.NET.Modules.Tests/TraktMoviesModule/TraktMoviesModule_RefreshMovie_Tests.cs
@@ -0,0 +1,155 @@
+﻿namespace TraktNet.Modules.Tests.TraktMoviesModule
+{
+    using FluentAssertions;
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Trakt.NET.Tests.Utility;
+    using Trakt.NET.Tests.Utility.Traits;
+    using TraktNet.Exceptions;
+    using TraktNet.Objects.Get.Movies;
+    using TraktNet.Responses;
+    using Xunit;
+
+    [TestCategory("Modules.Movies")]
+    public partial class TraktMoviesModule_Tests
+    {
+        private readonly string REFRESH_MOVIE_URI = $"movies/{MOVIE_ID}/refresh";
+
+        [Fact]
+        public async Task Test_TraktMoviesModule_RefreshMovie()
+        {
+            TraktClient client = TestUtility.GetOAuthMockClient(REFRESH_MOVIE_URI, HttpStatusCode.Created);
+            TraktNoContentResponse response = await client.Movies.RefreshMovieAsync(MOVIE_ID);
+
+            response.Should().NotBeNull();
+            response.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Test_TraktMoviesModule_RefreshMovie_With_TraktID()
+        {
+            TraktClient client = TestUtility.GetOAuthMockClient($"movies/{TRAKT_MOVIE_ID}/refresh", HttpStatusCode.Created);
+            TraktNoContentResponse response = await client.Movies.RefreshMovieAsync(TRAKT_MOVIE_ID);
+
+            response.Should().NotBeNull();
+            response.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Test_TraktMoviesModule_RefreshMovie_With_MovieIds_TraktID()
+        {
+            var movieIds = new TraktMovieIds
+            {
+                Trakt = TRAKT_MOVIE_ID
+            };
+
+            TraktClient client = TestUtility.GetOAuthMockClient($"movies/{TRAKT_MOVIE_ID}/refresh", HttpStatusCode.Created);
+            TraktNoContentResponse response = await client.Movies.RefreshMovieAsync(movieIds);
+
+            response.Should().NotBeNull();
+            response.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Test_TraktMoviesModule_RefreshMovie_With_MovieIds_Slug()
+        {
+            var movieIds = new TraktMovieIds
+            {
+                Slug = MOVIE_SLUG
+            };
+
+            TraktClient client = TestUtility.GetOAuthMockClient($"movies/{MOVIE_SLUG}/refresh", HttpStatusCode.Created);
+            TraktNoContentResponse response = await client.Movies.RefreshMovieAsync(movieIds);
+
+            response.Should().NotBeNull();
+            response.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Test_TraktMoviesModule_RefreshMovie_With_MovieIds()
+        {
+            var movieIds = new TraktMovieIds
+            {
+                Trakt = TRAKT_MOVIE_ID,
+                Slug = MOVIE_SLUG
+            };
+
+            TraktClient client = TestUtility.GetOAuthMockClient($"movies/{TRAKT_MOVIE_ID}/refresh", HttpStatusCode.Created);
+            TraktNoContentResponse response = await client.Movies.RefreshMovieAsync(movieIds);
+
+            response.Should().NotBeNull();
+            response.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Test_TraktMoviesModule_RefreshMovie_With_Movie()
+        {
+            var movie = new TraktMovie
+            {
+                Ids = new TraktMovieIds
+                {
+                    Trakt = TRAKT_MOVIE_ID,
+                    Slug = MOVIE_SLUG
+                }
+            };
+
+            TraktClient client = TestUtility.GetOAuthMockClient($"movies/{TRAKT_MOVIE_ID}/refresh", HttpStatusCode.Created);
+            TraktNoContentResponse response = await client.Movies.RefreshMovieAsync(movie);
+
+            response.Should().NotBeNull();
+            response.IsSuccess.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(HttpStatusCode.NotFound, typeof(TraktMovieNotFoundException))]
+        [InlineData(HttpStatusCode.Unauthorized, typeof(TraktAuthorizationException))]
+        [InlineData(HttpStatusCode.BadRequest, typeof(TraktBadRequestException))]
+        [InlineData(HttpStatusCode.Forbidden, typeof(TraktForbiddenException))]
+        [InlineData(HttpStatusCode.MethodNotAllowed, typeof(TraktMethodNotFoundException))]
+        [InlineData(HttpStatusCode.Conflict, typeof(TraktConflictException))]
+        [InlineData(HttpStatusCode.InternalServerError, typeof(TraktServerException))]
+        [InlineData(HttpStatusCode.BadGateway, typeof(TraktBadGatewayException))]
+        [InlineData(HttpStatusCode.PreconditionFailed, typeof(TraktPreconditionFailedException))]
+        [InlineData(HttpStatusCode.UnprocessableEntity, typeof(TraktValidationException))]
+        [InlineData((HttpStatusCode)426, typeof(TraktFailedVIPValidationException))]
+        [InlineData(HttpStatusCode.TooManyRequests, typeof(TraktRateLimitException))]
+        [InlineData(HttpStatusCode.ServiceUnavailable, typeof(TraktServerUnavailableException))]
+        [InlineData(HttpStatusCode.GatewayTimeout, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)520, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)521, typeof(TraktServerUnavailableException))]
+        [InlineData((HttpStatusCode)522, typeof(TraktServerUnavailableException))]
+        public async Task Test_TraktMoviesModule_RefreshMovie_Throws_API_Exception(HttpStatusCode statusCode, Type exceptionType)
+        {
+            TraktClient client = TestUtility.GetOAuthMockClient(REFRESH_MOVIE_URI, statusCode);
+
+            try
+            {
+                await client.Movies.RefreshMovieAsync(MOVIE_ID);
+                Assert.False(true);
+            }
+            catch (Exception exception)
+            {
+                (exception.GetType() == exceptionType).Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public async Task Test_TraktMoviesModule_RefreshMovie_Throws_ArgumentExceptions()
+        {
+            TraktClient client = TestUtility.GetOAuthMockClient(REFRESH_MOVIE_URI, HttpStatusCode.Created);
+
+            Func<Task<TraktNoContentResponse>> act = () => client.Movies.RefreshMovieAsync(default(ITraktMovieIds));
+            await act.Should().ThrowAsync<ArgumentNullException>();
+
+            act = () => client.Movies.RefreshMovieAsync(default(ITraktMovie));
+            await act.Should().ThrowAsync<ArgumentNullException>();
+
+            act = () => client.Movies.RefreshMovieAsync(new TraktMovieIds());
+            await act.Should().ThrowAsync<ArgumentException>();
+
+            act = () => client.Movies.RefreshMovieAsync(0);
+            await act.Should().ThrowAsync<ArgumentException>();
+        }
+    }
+}
