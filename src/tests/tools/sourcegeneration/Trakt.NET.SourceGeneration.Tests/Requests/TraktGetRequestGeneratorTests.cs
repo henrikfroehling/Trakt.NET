a1d434a304293d9123fc67e8108230ddcc492f42
Henrik Fröhling <git@henrikfroehling.com>
2024-06-29 01:28:10 +0200

    [v2] Setup test handler for request source generator and add first test case

---
+++ src/tests/tools/sourcegeneration/Trakt.NET.SourceGeneration.Tests/Requests/TraktGetRequestGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿namespace TraktNET.SourceGeneration.Requests
+{
+    public sealed class TraktGetRequestGeneratorTests
+    {
+        [Fact]
+        public Task TestGenerateGetRequest()
+        {
+            string source = """
+                using TraktNET;
+                
+                namespace SourceGeneraterTestNamespace
+                {
+                    [TraktGetRequest("notes")]
+                    public sealed partial class TestGetRequest
+                    {
+                    }
+                }
+                """;
+
+            return TestHelper.Verify<TraktGetRequestSourceGenerator>("Requests", "SourceGeneration.GetRequest", source, RequestTestType.GetRequest);
+        }
+    }
+}
--- src/tests/tools/sourcegeneration/Trakt.NET.SourceGeneration.Tests/TestHelper.cs
+++ src/tests/tools/sourcegeneration/Trakt.NET.SourceGeneration.Tests/TestHelper.cs
@@ -5,9 +5,26 @@ using System.Reflection;
 
 namespace TraktNET.SourceGeneration
 {
+    internal enum RequestTestType
+    {
+        None,
+        DeleteRequest,
+        GetRequest,
+        PostRequest,
+        PutRequest
+    }
+
     internal sealed class TestHelper
     {
-        internal static Task Verify<T>(string subDirectory, string compilationName, string source) where T : IIncrementalGenerator, new()
+        private const string SolutionRootDirectory = "../../../../../../..";
+        private const string TraktNETLibraryRootDirectory = $"{SolutionRootDirectory}/libs/Trakt.NET";
+        private const string TraktRequestAttributesDirectory = $"{TraktNETLibraryRootDirectory}/Internal/Attributes/Requests";
+        private const string TraktDeleteRequestSourceFile = $"{TraktRequestAttributesDirectory}/TraktDeleteRequestAttribute.cs";
+        private const string TraktGetRequestSourceFile = $"{TraktRequestAttributesDirectory}/TraktGetRequestAttribute.cs";
+        private const string TraktPostRequestSourceFile = $"{TraktRequestAttributesDirectory}/TraktPostRequestAttribute.cs";
+        private const string TraktPutRequestSourceFile = $"{TraktRequestAttributesDirectory}/TraktPutRequestAttribute.cs";
+
+        internal static Task Verify<T>(string subDirectory, string compilationName, string source, RequestTestType requestTestType = RequestTestType.None) where T : IIncrementalGenerator, new()
         {
             IEnumerable<PortableExecutableReference> references = new[]
             {
@@ -18,7 +35,41 @@ namespace TraktNET.SourceGeneration
                 MetadataReference.CreateFromFile(typeof(TraktClient).Assembly.Location)
             };
 
+            var syntaxTrees = new List<SyntaxTree>();
+
             SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(source);
+            syntaxTrees.Add(syntaxTree);
+
+            if (requestTestType != RequestTestType.None)
+            {
+                // The request source generator looks up "DeclaringSyntaxReferences" for the attribute classes,
+                // which is empty if the assembly is only added as MetadataReference, because the locations
+                // of the attribute symbols are empty and their syntax trees are null.
+                // To fix this, it is necessary to add the attributes syntax trees explicitly to the compilation.
+                
+                // NOTE: It's odd that adding all syntax trees at once still results in empty "DeclaringSyntaxReferences".
+                //       Therefore only add the actual needed syntax tree for the specific request test case.
+
+                switch (requestTestType)
+                {
+                    case RequestTestType.DeleteRequest:
+                        SyntaxTree deleteRequestSyntaxTree = CSharpSyntaxTree.ParseText(File.ReadAllText(TraktDeleteRequestSourceFile));
+                        syntaxTrees.Add(deleteRequestSyntaxTree);
+                        break;
+                    case RequestTestType.GetRequest:
+                        SyntaxTree getRequestSyntaxTree = CSharpSyntaxTree.ParseText(File.ReadAllText(TraktGetRequestSourceFile));
+                        syntaxTrees.Add(getRequestSyntaxTree);
+                        break;
+                    case RequestTestType.PostRequest:
+                        SyntaxTree postRequestSyntaxTree = CSharpSyntaxTree.ParseText(File.ReadAllText(TraktPostRequestSourceFile));
+                        syntaxTrees.Add(postRequestSyntaxTree);
+                        break;
+                    case RequestTestType.PutRequest:
+                        SyntaxTree putRequestSyntaxTree = CSharpSyntaxTree.ParseText(File.ReadAllText(TraktPutRequestSourceFile));
+                        syntaxTrees.Add(putRequestSyntaxTree);
+                        break;
+                }
+            }
 
             // Trakt attributes are only accessible internally.
             // Set compilation options to import all symbols.
@@ -32,7 +83,9 @@ namespace TraktNET.SourceGeneration
 
             topLevelBinderFlagsProperty?.SetValue(options, (uint)1 << 22);
 
-            var compilation = CSharpCompilation.Create(assemblyName: compilationName, syntaxTrees: new[] { syntaxTree },
+            var compilation = CSharpCompilation.Create(
+                assemblyName: compilationName,
+                syntaxTrees: syntaxTrees,
                 references: references, options: options);
 
             // Reference for the .NET 8 library
