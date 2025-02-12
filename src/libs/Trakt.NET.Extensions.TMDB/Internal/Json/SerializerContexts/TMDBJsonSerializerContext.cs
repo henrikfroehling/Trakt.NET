b23289b7ceafbfa2f2dd39dadef082f0de78288b
Henrik Fröhling <git@henrikfroehling.com>
2025-02-12 19:04:08 +0100

    [v2] Add Json serializer context for TMDB Json objects (#153 #158)

---
+++ src/libs/Trakt.NET.Extensions.TMDB/Internal/Json/SerializerContexts/TMDBJsonSerializerContext.cs
@@ -0,0 +1,32 @@
+#if NET6_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace TraktNET
+{
+    [ExcludeFromCodeCoverage]
+    [JsonSerializable(typeof(TMDBConfiguration))]
+    [JsonSerializable(typeof(IReadOnlyList<TMDBConfiguration>))]
+    [JsonSerializable(typeof(TMDBConfigurationImages))]
+    [JsonSerializable(typeof(IReadOnlyList<TMDBConfigurationImages>))]
+    [JsonSerializable(typeof(TMDBEpisodeImages))]
+    [JsonSerializable(typeof(IReadOnlyList<TMDBEpisodeImages>))]
+    [JsonSerializable(typeof(TMDBImage))]
+    [JsonSerializable(typeof(IReadOnlyList<TMDBImage>))]
+    [JsonSerializable(typeof(TMDBMovieImages))]
+    [JsonSerializable(typeof(IReadOnlyList<TMDBMovieImages>))]
+    [JsonSerializable(typeof(TMDBPersonImages))]
+    [JsonSerializable(typeof(IReadOnlyList<TMDBPersonImages>))]
+    [JsonSerializable(typeof(TMDBSeasonImages))]
+    [JsonSerializable(typeof(IReadOnlyList<TMDBSeasonImages>))]
+    [JsonSerializable(typeof(TMDBShowImages))]
+    [JsonSerializable(typeof(IReadOnlyList<TMDBShowImages>))]
+    [JsonSerializable(typeof(TMDBVideo))]
+    [JsonSerializable(typeof(IReadOnlyList<TMDBVideo>))]
+    [JsonSerializable(typeof(TMDBVideos))]
+    [JsonSerializable(typeof(IReadOnlyList<TMDBVideos>))]
+    public sealed partial class TMDBJsonSerializerContext : JsonSerializerContext
+    {
+    }
+}
+#endif
---
+++ src/libs/Trakt.NET.Extensions.TMDB/Internal/Json/SerializerContexts/TMDBJsonSerializerContextFactory.cs
@@ -0,0 +1,14 @@
+#if NET6_0_OR_GREATER
+using System.Text.Json.Serialization;
+using TraktNET.Utilities.Json;
+
+namespace TraktNET
+{
+    internal sealed class TMDBJsonSerializerContextFactory : IJsonSerializerContextFactory
+    {
+        private static readonly TMDBJsonSerializerContext s_jsonSerializerContext = new();
+
+        public JsonSerializerContext GetContext<TJsonObjectType>() => s_jsonSerializerContext;
+    }
+}
+#endif
